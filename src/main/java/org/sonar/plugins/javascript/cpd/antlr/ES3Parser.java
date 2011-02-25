f7e01698dd9071d5fec9c25982dfdb48f94b2171
Eriks Nukis <eriks.nukis@gmail.com>
2011-02-25 08:06:54 +0000

    [SONARPLUGINS-1006] license header updated, tabs replaced with two spaces in pom.xml

--- src/main/java/org/sonar/plugins/javascript/cpd/antlr/ES3Parser.java
+++ src/main/java/org/sonar/plugins/javascript/cpd/antlr/ES3Parser.java
@@ -1,21 +1,20 @@
-/**
+/*
  * Sonar JavaScript Plugin
- * Extension for Sonar, open source software quality management tool.
  * Copyright (C) 2011 Eriks Nukis
- * mailto: eriks.nukis@gmail.com
+ * dev@sonar.codehaus.org
  *
- * Sonar JavaScript Plugin is free software; you can redistribute it and/or
+ * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 3 of the License, or (at your option) any later version.
  *
- * Sonar JavaScript Plugin is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Sonar JavaScript Plugin; if not, write to the Free Software
+ * License along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
  */
 
