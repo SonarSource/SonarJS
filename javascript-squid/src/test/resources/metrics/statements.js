80563b7f7a1cfa47c593bef85a79ac6691ac51f8
Evgeny Mandrikov <mandrikov@gmail.com>
2012-09-17 18:20:53 +0600

    Add more tests

--- javascript-squid/src/test/resources/metrics/statements.js
+++ javascript-squid/src/test/resources/metrics/statements.js
@@ -7,12 +7,41 @@ function sayHello() {
 
   if (false) // +1 if statement
   { // +0 compound statement
+    throw new Exception(); // +1 throw statement
   }
 
   label: // +0 labelled statement
   for (i = 0; i < 10; i++) // +1 for statement
   { // +0 compound statement
     break; // +1 break statement
+  }
+
+  while (false) // +1 while statement
+  { // +0 compound statement
+    continue; // +1 continue statement
+  }
+
+  do // +1 do-while statement
+  { // +0 compound statement
+  } while (false);
+
+  for (a in b) // +1 for-in statement
+  { // +0 compound statement
+  }
+
+  with (a) // +1 with statement
+  { // +0 compound statement
+  }
+
+  switch (param) { // +1 switch statement
+    case 0:
+    case 1:
+    default:
+  }
+
+  try { // +1 try statement
+  } catch (e) {
+  } finally {
   }
 
   debugger; // +1 debugger statement
---
+++ sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/JavaScriptProfileTest.java
@@ -0,0 +1,63 @@
+/*
+ * Sonar JavaScript Plugin
+ * Copyright (C) 2011 Eriks Nukis and SonarSource
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.plugins.javascript;
+
+import org.junit.Test;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import org.sonar.api.profiles.AnnotationProfileParser;
+import org.sonar.api.profiles.RulesProfile;
+import org.sonar.api.rules.Rule;
+import org.sonar.api.rules.RuleFinder;
+import org.sonar.api.utils.ValidationMessages;
+import org.sonar.javascript.checks.CheckList;
+import org.sonar.plugins.javascript.core.JavaScript;
+
+import static org.fest.assertions.Assertions.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class JavaScriptProfileTest {
+
+  @Test
+  public void should_create_sonar_way_profile() {
+    ValidationMessages validation = ValidationMessages.create();
+
+    RuleFinder ruleFinder = ruleFinder();
+    JavaScriptProfile definition = new JavaScriptProfile(new AnnotationProfileParser(ruleFinder));
+    RulesProfile profile = definition.createProfile(validation);
+
+    assertThat(profile.getLanguage()).isEqualTo(JavaScript.KEY);
+    assertThat(profile.getName()).isEqualTo(RulesProfile.SONAR_WAY_NAME);
+    assertThat(profile.getActiveRulesByRepository(CheckList.REPOSITORY_KEY)).hasSize(22);
+    assertThat(validation.hasErrors()).isFalse();
+  }
+
+  static RuleFinder ruleFinder() {
+    return when(mock(RuleFinder.class).findByKey(anyString(), anyString())).thenAnswer(new Answer<Rule>() {
+      public Rule answer(InvocationOnMock invocation) {
+        Object[] arguments = invocation.getArguments();
+        return Rule.create((String) arguments[0], (String) arguments[1], (String) arguments[1]);
+      }
+    }).getMock();
+  }
+
+}
