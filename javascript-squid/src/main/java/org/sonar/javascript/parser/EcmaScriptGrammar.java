4b92e795e58ad3d16c43b584c6b9f7c48f2225d7
Evgeny Mandrikov <mandrikov@gmail.com>
2013-03-06 16:20:48 +0100

    SONARPLUGINS-2590 Fix grammar to avoid NPE in Toolkit

--- javascript-squid/src/main/java/org/sonar/javascript/parser/EcmaScriptGrammar.java
+++ javascript-squid/src/main/java/org/sonar/javascript/parser/EcmaScriptGrammar.java
@@ -296,15 +296,15 @@ public enum EcmaScriptGrammar implements GrammarRuleKey {
 
     b.rule(EOS).is(b.firstOf(
         b.sequence(SPACING, SEMI),
-        b.sequence(SPACING_NO_LB, b.token(EcmaScriptTokenType.EMPTY, LINE_TERMINATOR_SEQUENCE)),
-        b.sequence(SPACING_NO_LB, b.token(EcmaScriptTokenType.EMPTY, b.next("}"))),
+        b.sequence(b.token(EcmaScriptTokenType.EMPTY, SPACING_NO_LB), LINE_TERMINATOR_SEQUENCE),
+        b.sequence(b.token(EcmaScriptTokenType.EMPTY, SPACING_NO_LB), b.next("}")),
         b.sequence(SPACING, b.token(EcmaScriptTokenType.EMPTY, b.endOfInput()))));
 
     b.rule(EOS_NO_LB).is(b.firstOf(
         b.sequence(SPACING_NO_LB, NEXT_NOT_LB, SEMI),
-        b.sequence(SPACING_NO_LB, b.token(EcmaScriptTokenType.EMPTY, LINE_TERMINATOR_SEQUENCE)),
-        b.sequence(SPACING_NO_LB, b.token(EcmaScriptTokenType.EMPTY, b.next("}"))),
-        b.sequence(SPACING_NO_LB, b.token(EcmaScriptTokenType.EMPTY, b.endOfInput()))));
+        b.sequence(b.token(EcmaScriptTokenType.EMPTY, SPACING_NO_LB), LINE_TERMINATOR_SEQUENCE),
+        b.sequence(b.token(EcmaScriptTokenType.EMPTY, SPACING_NO_LB), b.next("}")),
+        b.sequence(b.token(EcmaScriptTokenType.EMPTY, SPACING_NO_LB), b.endOfInput())));
 
     b.rule(EOF).is(b.token(GenericTokenType.EOF, b.endOfInput())).skip();
     b.rule(IDENTIFIER).is(
