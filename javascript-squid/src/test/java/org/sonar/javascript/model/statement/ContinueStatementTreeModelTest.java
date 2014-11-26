6c45403de253ff377f46fb9dbaf2201137cf2a51
Linda Martin <linda.martin@sonarsource.com>
2014-11-26 16:51:57 +0100

    Strongly Typed AST: Add test for CONTINUE_STATEMENT

---
+++ javascript-squid/src/test/java/org/sonar/javascript/model/statement/ContinueStatementTreeModelTest.java
@@ -0,0 +1,49 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011 SonarSource and Eriks Nukis
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.javascript.model.statement;
+
+import org.junit.Test;
+import org.sonar.javascript.api.EcmaScriptKeyword;
+import org.sonar.javascript.model.JavaScriptTreeModelTest;
+import org.sonar.javascript.model.interfaces.Tree.Kind;
+import org.sonar.javascript.model.interfaces.statement.ContinueStatementTree;
+
+import static org.fest.assertions.Assertions.assertThat;
+
+public class ContinueStatementTreeModelTest extends JavaScriptTreeModelTest {
+
+  @Test
+  public void with_label() throws Exception {
+    ContinueStatementTree tree = parse("continue label ;", Kind.CONTINUE_STATEMENT);
+
+    assertThat(tree.is(Kind.CONTINUE_STATEMENT)).isTrue();
+    assertThat(tree.continueKeyword().text()).isEqualTo(EcmaScriptKeyword.CONTINUE.getValue());
+    assertThat(tree.label().name()).isEqualTo("label");
+  }
+
+  @Test
+  public void without_label() throws Exception {
+    ContinueStatementTree tree = parse("continue ;", Kind.CONTINUE_STATEMENT);
+
+    assertThat(tree.is(Kind.CONTINUE_STATEMENT)).isTrue();
+    assertThat(tree.continueKeyword().text()).isEqualTo(EcmaScriptKeyword.CONTINUE.getValue());
+    assertThat(tree.label()).isNull();
+  }
+}
