03ebb9e1b2166b48d0750209992cd2885b247f48
Linda Martin <linda.martin@sonarsource.com>
2014-12-17 09:43:03 +0100

    Revert mitstake done in 2475de0d7adce4f21bd0e53c49e192a7e5cbafe1

--- javascript-squid/src/main/java/org/sonar/javascript/model/interfaces/expression/ExpressionTree.java
+++ javascript-squid/src/main/java/org/sonar/javascript/model/interfaces/expression/ExpressionTree.java
@@ -22,7 +22,7 @@ package org.sonar.javascript.model.interfaces.expression;
 import org.sonar.javascript.model.interfaces.Tree;
 
 /**
- * Common interface for all types of <a href="http://www.ecma-international.org/ecma-262/5.1/#sec-A.5">Functions</a>.
+ * Common interface for all types of <a href="http://www.ecma-international.org/ecma-262/5.1/#sec-11">expressions</a>.
  *
  * <p>This interface is not intended to be implemented by clients.</p>
  */
