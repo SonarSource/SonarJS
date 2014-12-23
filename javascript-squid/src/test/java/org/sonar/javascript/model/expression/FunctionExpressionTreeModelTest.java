af2870b27236ea663d247fdea6d8f609291a59f8
Linda Martin <linda.martin@sonarsource.com>
2014-12-23 15:57:50 +0100

    Add test for function expression

---
+++ javascript-squid/src/test/java/org/sonar/javascript/model/expression/FunctionExpressionTreeModelTest.java
@@ -0,0 +1,59 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011 SonarSource and Eriks Nukis
+ * dev@sonar.codehaus.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.javascript.model.expression;
+
+import static org.fest.assertions.Assertions.assertThat;
+import org.junit.Test;
+import org.sonar.javascript.api.EcmaScriptKeyword;
+import org.sonar.javascript.api.EcmaScriptPunctuator;
+import org.sonar.javascript.model.JavaScriptTreeModelTest;
+import org.sonar.javascript.model.interfaces.Tree.Kind;
+import org.sonar.javascript.model.interfaces.expression.FunctionTree;
+import org.sonar.javascript.model.interfaces.expression.RestElementTree;
+
+public class FunctionExpressionTreeModelTest extends JavaScriptTreeModelTest {
+
+  @Test
+  public void named() throws Exception {
+    FunctionTree tree = parse("a = function f() {}", Kind.FUNCTION_EXPRESSION);
+
+    assertThat(tree.is(Kind.FUNCTION_EXPRESSION)).isTrue();
+    assertThat(tree.functionKeyword().text()).isEqualTo(EcmaScriptKeyword.FUNCTION.getValue());
+    assertThat(tree.name().name()).isEqualTo("f");
+    assertThat(tree.openParenthesis().text()).isEqualTo(EcmaScriptPunctuator.LPARENTHESIS.getValue());
+    assertThat(tree.closeParenthesis().text()).isEqualTo(EcmaScriptPunctuator.RPARENTHESIS.getValue());
+    assertThat(tree.openCurlyBrace().text()).isEqualTo(EcmaScriptPunctuator.LCURLYBRACE.getValue());
+    assertThat(tree.closeCurlyBrace().text()).isEqualTo(EcmaScriptPunctuator.RCURLYBRACE.getValue());
+  }
+
+  @Test
+  public void not_named() throws Exception {
+    FunctionTree tree = parse("a = function () {}", Kind.FUNCTION_EXPRESSION);
+
+    assertThat(tree.is(Kind.FUNCTION_EXPRESSION)).isTrue();
+    assertThat(tree.functionKeyword().text()).isEqualTo(EcmaScriptKeyword.FUNCTION.getValue());
+    assertThat(tree.name()).isNull();
+    assertThat(tree.openParenthesis().text()).isEqualTo(EcmaScriptPunctuator.LPARENTHESIS.getValue());
+    assertThat(tree.closeParenthesis().text()).isEqualTo(EcmaScriptPunctuator.RPARENTHESIS.getValue());
+    assertThat(tree.openCurlyBrace().text()).isEqualTo(EcmaScriptPunctuator.LCURLYBRACE.getValue());
+    assertThat(tree.closeCurlyBrace().text()).isEqualTo(EcmaScriptPunctuator.RCURLYBRACE.getValue());
+  }
+
+}
