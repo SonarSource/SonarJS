b4946df0d63451d5e78e6779afa1ad69ef0c8b81
René Wolfert <ne-tje@gmx.net>
2015-05-28 20:36:30 +0200

    Revert "SONARJS-368 - S1125: Raise an issue when a boolean literal is used as an operand of the ternary operator"
    This reverts commit 37fc81d66ec3c07dad05cc384b8632e148d885e2.

--- javascript-checks/src/main/java/org/sonar/javascript/checks/BooleanEqualityComparisonCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/BooleanEqualityComparisonCheck.java
@@ -25,10 +25,8 @@ import org.sonar.check.Rule;
 import org.sonar.plugins.javascript.api.visitors.BaseTreeVisitor;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
 import org.sonar.plugins.javascript.api.tree.expression.BinaryExpressionTree;
-import org.sonar.plugins.javascript.api.tree.expression.ConditionalExpressionTree;
 import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.javascript.api.tree.expression.LiteralTree;
-import org.sonar.plugins.javascript.api.tree.expression.ParenthesisedExpressionTree;
 import org.sonar.plugins.javascript.api.tree.expression.UnaryExpressionTree;
 import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
@@ -51,40 +49,9 @@ public class BooleanEqualityComparisonCheck extends BaseTreeVisitor {
       Kind.NOT_EQUAL_TO
   };
 
-  private boolean isBooleanExpression(ExpressionTree expressionTree) {
-    final boolean isBoolean;
-    if (expressionTree.is(Kind.PARENTHESISED_EXPRESSION)) {
-      ParenthesisedExpressionTree parenthesisedExpressionTree = (ParenthesisedExpressionTree)expressionTree;
-      isBoolean = isBooleanExpression(parenthesisedExpressionTree.expression());
-    } else if (expressionTree.is(Kind.CONDITIONAL_EXPRESSION)) {
-      ConditionalExpressionTree conditionalExpressionTree = (ConditionalExpressionTree)expressionTree;
-      isBoolean = isBooleanExpression(conditionalExpressionTree.trueExpression())
-              && isBooleanExpression(conditionalExpressionTree.falseExpression());
-    } else {
-      isBoolean = expressionTree.is(Kind.BOOLEAN_LITERAL)
-          || expressionTree.is(Kind.LOGICAL_COMPLEMENT)
-          || expressionTree.is(BINARY_OPERATORS);
-    }
-    return isBoolean;
-  }
-
-  @Override
-  public void visitConditionalExpression(ConditionalExpressionTree tree) {
-    if (isBooleanExpression(tree.condition())) {
-      visitExpression(tree.condition());
-    }
-    if (isBooleanExpression(tree.trueExpression()) && isBooleanExpression(tree.falseExpression())) {
-      visitExpression(tree.trueExpression());
-      visitExpression(tree.falseExpression());
-    }
-    super.visitConditionalExpression(tree);
-  }
-
-
-  
   @Override
   public void visitUnaryExpression(UnaryExpressionTree tree) {
-    if (isBooleanExpression(tree)) {
+    if (tree.is(Kind.LOGICAL_COMPLEMENT)) {
       visitExpression(tree.expression());
     }
     super.visitUnaryExpression(tree);
@@ -92,7 +59,7 @@ public class BooleanEqualityComparisonCheck extends BaseTreeVisitor {
 
   @Override
   public void visitBinaryExpression(BinaryExpressionTree tree) {
-    if (isBooleanExpression(tree)) {
+    if (tree.is(BINARY_OPERATORS)) {
       visitExpression(tree.leftOperand());
       visitExpression(tree.rightOperand());
     }
@@ -100,12 +67,10 @@ public class BooleanEqualityComparisonCheck extends BaseTreeVisitor {
   }
 
   private void visitExpression(ExpressionTree expression) {
-    if (expression.is(Kind.PARENTHESISED_EXPRESSION)) {
-      ParenthesisedExpressionTree parenthesisExpressionTree = (ParenthesisedExpressionTree)expression;
-      visitExpression(parenthesisExpressionTree.expression());
-    } else if (expression.is(Kind.BOOLEAN_LITERAL)){
+    if (expression.is(Kind.BOOLEAN_LITERAL)){
       String message = String.format("Remove the literal \"%s\" boolean value.", ((LiteralTree) expression).value());
       getContext().addIssue(this, expression, message);
     }
   }
+
 }
