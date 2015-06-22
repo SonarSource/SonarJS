39f07ae651985b80511110574fd0a83c4cb8518a
Eric Hartmann <hartmann.eric@gmail.com>
2015-06-22 15:18:04 +0200

    Update licence for Javascript

--- javascript-squid/src/main/java/org/sonar/plugins/javascript/api/tree/expression/ComputedPropertyNameTree.java
+++ javascript-squid/src/main/java/org/sonar/plugins/javascript/api/tree/expression/ComputedPropertyNameTree.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube JavaScript Plugin
  * Copyright (C) 2011 SonarSource and Eriks Nukis
- * dev@sonar.codehaus.org
+ * sonarqube@googlegroups.com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
