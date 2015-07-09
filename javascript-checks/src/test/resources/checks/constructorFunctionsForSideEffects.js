42941db877dbbe126ec2c867719f2f2b8d315669
Linda Martin <linda.martin@sonarsource.com>
2015-07-09 17:12:26 +0200

    SONARJS-460 Add test case.

--- javascript-checks/src/test/resources/checks/constructorFunctionsForSideEffects.js
+++ javascript-checks/src/test/resources/checks/constructorFunctionsForSideEffects.js
@@ -1,4 +1,5 @@
 var something = new MyConstructor(); // OK
-something = new MyConstructor(); // OK
-new MyConstructor(); // NOK
-callMethod(new MyConstructor()); // OK
+something = new MyConstructor();     // OK
+new MyConstructor();                 // NOK
+callMethod(new MyConstructor());     // OK
+new MyConstructor().doSomething();   // OK
