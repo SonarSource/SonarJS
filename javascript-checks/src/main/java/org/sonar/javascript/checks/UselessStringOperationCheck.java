d9b7fd695c9e9db639fa7c2a328e58852409ff86
pynicolas <pierre-yves.nicolas@sonarsource.com>
2015-08-21 16:32:14 +0200

    SONARJS-515 Rule: Results of operations on strings should not be ignored

---
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/UselessStringOperationCheck.java
@@ -0,0 +1,71 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011 SonarSource and Eriks Nukis
+ * sonarqube@googlegroups.com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this program; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02
+ */
+package org.sonar.javascript.checks;
+
+import com.google.common.collect.ImmutableList;
+import org.sonar.api.server.rule.RulesDefinition;
+import org.sonar.check.Priority;
+import org.sonar.check.Rule;
+import org.sonar.javascript.checks.utils.CheckUtils;
+import org.sonar.plugins.javascript.api.symbols.Type;
+import org.sonar.plugins.javascript.api.tree.Tree;
+import org.sonar.plugins.javascript.api.tree.Tree.Kind;
+import org.sonar.plugins.javascript.api.tree.expression.CallExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.MemberExpressionTree;
+import org.sonar.plugins.javascript.api.tree.statement.ExpressionStatementTree;
+import org.sonar.plugins.javascript.api.visitors.SubscriptionBaseTreeVisitor;
+import org.sonar.squidbridge.annotations.ActivatedByDefault;
+import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
+import org.sonar.squidbridge.annotations.SqaleSubCharacteristic;
+
+import java.util.List;
+
+@Rule(
+  key = "S1154",
+  name = "Results of operations on strings should not be ignored",
+  priority = Priority.BLOCKER,
+  tags = {Tags.BUG})
+@ActivatedByDefault
+@SqaleSubCharacteristic(RulesDefinition.SubCharacteristics.INSTRUCTION_RELIABILITY)
+@SqaleConstantRemediation("20min")
+public class UselessStringOperationCheck extends SubscriptionBaseTreeVisitor {
+
+  @Override
+  public List<Kind> nodesToVisit() {
+    return ImmutableList.of(Kind.EXPRESSION_STATEMENT);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    Tree expression = ((ExpressionStatementTree) tree).expression();
+    if (expression.is(Kind.CALL_EXPRESSION)) {
+      ExpressionTree callee = ((CallExpressionTree) expression).callee();
+      if (callee.is(Kind.DOT_MEMBER_EXPRESSION)) {
+        MemberExpressionTree memberExpression = (MemberExpressionTree) callee;
+        if (memberExpression.object().types().containsOnly(Type.Kind.STRING)) {
+          String variableName = CheckUtils.asString(memberExpression.object());
+          addIssue(tree, variableName + " is an immutable object; you must either store or return the result of the operation.");
+        }
+      }
+    }
+  }
+
+}
