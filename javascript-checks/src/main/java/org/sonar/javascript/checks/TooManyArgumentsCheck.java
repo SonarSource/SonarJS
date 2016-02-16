renewolfert/sonar-javascript

SONARJS-632 Create type for arrow function

javascript-checks/src/main/java/org/sonar/javascript/checks/TooManyArgumentsCheck.java
@@ -19,13 +19,13 @@
  */
 package org.sonar.javascript.checks;
 
+import java.util.List;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.utils.CheckUtils;
-import org.sonar.javascript.tree.impl.SeparatedList;
 import org.sonar.javascript.tree.symbols.Scope;
 import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.plugins.javascript.api.symbols.Symbol;
@@ -57,13 +57,13 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
 
     if (functionTree != null) {
 
-      int parametersNumber = functionTree.parameters().parameters().size();
+      int parametersNumber = functionTree.parametersList().size();
       int argumentsNumber = tree.arguments().parameters().size();
 
       if (!hasRestParameter(functionTree) && !builtInArgumentsUsed(functionTree) && argumentsNumber > parametersNumber) {
         String message = getMessage(tree, parametersNumber, argumentsNumber);
         newIssue(tree.arguments(), message)
-          .secondary(functionTree.parameters(), "Formal parameters");
+          .secondary(functionTree.parameterClause(), "Formal parameters");
       }
 
     }
@@ -85,7 +85,7 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
    * @return true if function's last parameter has "... p" format and stands for all rest parameters
    */
   private static boolean hasRestParameter(FunctionTree functionTree) {
-    SeparatedList<Tree> parameters = functionTree.parameters().parameters();
+    List<Tree> parameters = functionTree.parametersList();
     return !parameters.isEmpty() && (parameters.get(parameters.size() - 1).is(Tree.Kind.REST_ELEMENT));
   }
 

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/AccessorMethodDeclarationTreeImpl.java
@@ -22,6 +22,7 @@ package org.sonar.javascript.tree.impl.declaration;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
@@ -75,13 +76,18 @@ public class AccessorMethodDeclarationTreeImpl extends JavaScriptTree implements
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
   @Override
   public BlockTreeImpl body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parametersList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/FunctionDeclarationTreeImpl.java
@@ -21,6 +21,7 @@ package org.sonar.javascript.tree.impl.declaration;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.expression.IdentifierTreeImpl;
@@ -88,13 +89,18 @@ public class FunctionDeclarationTreeImpl extends JavaScriptTree implements Funct
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
   @Override
   public BlockTree body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parametersList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/GeneratorMethodDeclarationTreeImpl.java
@@ -22,6 +22,7 @@ package org.sonar.javascript.tree.impl.declaration;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
@@ -75,13 +76,18 @@ public class GeneratorMethodDeclarationTreeImpl extends JavaScriptTree implement
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
   @Override
   public BlockTreeImpl body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parametersList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/MethodDeclarationTreeImpl.java
@@ -21,6 +21,7 @@ package org.sonar.javascript.tree.impl.declaration;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
@@ -67,7 +68,7 @@ public class MethodDeclarationTreeImpl extends JavaScriptTree implements MethodD
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
@@ -89,5 +90,10 @@ public class MethodDeclarationTreeImpl extends JavaScriptTree implements MethodD
   @Override
   public void accept(DoubleDispatchVisitor visitor) {
     visitor.visitMethodDeclaration(this);
+  }
+
+  @Override
+  public List<Tree> parametersList() {
+    return parameters.parameters();
   }
 }

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/expression/ArrowFunctionTreeImpl.java
@@ -26,10 +26,12 @@ import java.util.List;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.declaration.ParameterListTreeImpl;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
+import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.javascript.tree.symbols.type.TypableTree;
 import org.sonar.plugins.javascript.api.symbols.Type;
 import org.sonar.plugins.javascript.api.symbols.TypeSet;
 import org.sonar.plugins.javascript.api.tree.Tree;
+import org.sonar.plugins.javascript.api.tree.declaration.ParameterListTree;
 import org.sonar.plugins.javascript.api.tree.expression.ArrowFunctionTree;
 import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
 import org.sonar.plugins.javascript.api.tree.lexical.SyntaxToken;
@@ -40,15 +42,18 @@ public class ArrowFunctionTreeImpl extends JavaScriptTree implements ArrowFuncti
   private final Tree parameters;
   private final SyntaxToken doubleArrow;
   private Tree body;
+  private Type functionType;
 
   public ArrowFunctionTreeImpl(Tree parameters, InternalSyntaxToken doubleArrow, Tree body) {
     this.parameters = parameters;
     this.doubleArrow = doubleArrow;
     this.body = body;
+
+    this.functionType = FunctionType.create(this);
   }
 
   @Override
-  public Tree parameters() {
+  public Tree parameterClause() {
     return parameters;
   }
 
@@ -58,8 +63,18 @@ public class ArrowFunctionTreeImpl extends JavaScriptTree implements ArrowFuncti
   }
 
   @Override
-  public Tree conciseBody() {
+  public Tree body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parametersList() {
+    if (this.parameters.is(Tree.Kind.FORMAL_PARAMETER_LIST)) {
+      return ((ParameterListTree) this.parameters).parameters();
+
+    } else {
+      return ImmutableList.of(this.parameterClause());
+    }
   }
 
   @Override
@@ -87,7 +102,9 @@ public class ArrowFunctionTreeImpl extends JavaScriptTree implements ArrowFuncti
 
   @Override
   public TypeSet types() {
-    return TypeSet.emptyTypeSet();
+    TypeSet set = TypeSet.emptyTypeSet();
+    set.add(functionType);
+    return set;
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/expression/FunctionExpressionTreeImpl.java
@@ -21,6 +21,7 @@ package org.sonar.javascript.tree.impl.expression;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.declaration.ParameterListTreeImpl;
@@ -144,7 +145,7 @@ public class FunctionExpressionTreeImpl extends JavaScriptTree implements Functi
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
@@ -156,6 +157,11 @@ public class FunctionExpressionTreeImpl extends JavaScriptTree implements Functi
   @Override
   public BlockTreeImpl body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parametersList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/plugins/javascript/api/tree/declaration/FunctionTree.java
@@ -20,11 +20,13 @@
 package org.sonar.plugins.javascript.api.tree.declaration;
 
 
+import java.util.List;
 import org.sonar.plugins.javascript.api.tree.Tree;
-import org.sonar.plugins.javascript.api.tree.statement.BlockTree;
 
 public interface FunctionTree extends Tree {
-  ParameterListTree parameters();
+  Tree parameterClause();
 
-  BlockTree body();
+  Tree body();
+
+  List<Tree> parametersList();
 }

SonarSource/SonarJS

javascript-checks/src/main/java/org/sonar/javascript/checks/TooManyArgumentsCheck.java
@@ -19,13 +19,13 @@
  */
 package org.sonar.javascript.checks;
 
+import java.util.List;
 import java.util.Set;
 import javax.annotation.Nullable;
 import org.sonar.api.server.rule.RulesDefinition;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.utils.CheckUtils;
-import org.sonar.javascript.tree.impl.SeparatedList;
 import org.sonar.javascript.tree.symbols.Scope;
 import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.plugins.javascript.api.symbols.Symbol;
@@ -57,13 +57,13 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
 
     if (functionTree != null) {
 
-      int parametersNumber = functionTree.parameters().parameters().size();
+      int parametersNumber = functionTree.parameterList().size();
       int argumentsNumber = tree.arguments().parameters().size();
 
       if (!hasRestParameter(functionTree) && !builtInArgumentsUsed(functionTree) && argumentsNumber > parametersNumber) {
         String message = getMessage(tree, parametersNumber, argumentsNumber);
         newIssue(tree.arguments(), message)
-          .secondary(functionTree.parameters(), "Formal parameters");
+          .secondary(functionTree.parameterClause(), "Formal parameters");
       }
 
     }
@@ -85,7 +85,7 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
    * @return true if function's last parameter has "... p" format and stands for all rest parameters
    */
   private static boolean hasRestParameter(FunctionTree functionTree) {
-    SeparatedList<Tree> parameters = functionTree.parameters().parameters();
+    List<Tree> parameters = functionTree.parameterList();
     return !parameters.isEmpty() && (parameters.get(parameters.size() - 1).is(Tree.Kind.REST_ELEMENT));
   }
 

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/AccessorMethodDeclarationTreeImpl.java
@@ -22,6 +22,7 @@ package org.sonar.javascript.tree.impl.declaration;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
@@ -75,13 +76,18 @@ public class AccessorMethodDeclarationTreeImpl extends JavaScriptTree implements
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
   @Override
   public BlockTreeImpl body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parameterList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/FunctionDeclarationTreeImpl.java
@@ -21,6 +21,7 @@ package org.sonar.javascript.tree.impl.declaration;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.expression.IdentifierTreeImpl;
@@ -88,13 +89,18 @@ public class FunctionDeclarationTreeImpl extends JavaScriptTree implements Funct
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
   @Override
   public BlockTree body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parameterList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/GeneratorMethodDeclarationTreeImpl.java
@@ -22,6 +22,7 @@ package org.sonar.javascript.tree.impl.declaration;
 import com.google.common.base.Preconditions;
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
@@ -75,13 +76,18 @@ public class GeneratorMethodDeclarationTreeImpl extends JavaScriptTree implement
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
   @Override
   public BlockTreeImpl body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parameterList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/declaration/MethodDeclarationTreeImpl.java
@@ -21,6 +21,7 @@ package org.sonar.javascript.tree.impl.declaration;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
@@ -67,7 +68,7 @@ public class MethodDeclarationTreeImpl extends JavaScriptTree implements MethodD
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
@@ -89,5 +90,10 @@ public class MethodDeclarationTreeImpl extends JavaScriptTree implements MethodD
   @Override
   public void accept(DoubleDispatchVisitor visitor) {
     visitor.visitMethodDeclaration(this);
+  }
+
+  @Override
+  public List<Tree> parameterList() {
+    return parameters.parameters();
   }
 }

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/expression/ArrowFunctionTreeImpl.java
@@ -26,10 +26,12 @@ import java.util.List;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.declaration.ParameterListTreeImpl;
 import org.sonar.javascript.tree.impl.lexical.InternalSyntaxToken;
+import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.javascript.tree.symbols.type.TypableTree;
 import org.sonar.plugins.javascript.api.symbols.Type;
 import org.sonar.plugins.javascript.api.symbols.TypeSet;
 import org.sonar.plugins.javascript.api.tree.Tree;
+import org.sonar.plugins.javascript.api.tree.declaration.ParameterListTree;
 import org.sonar.plugins.javascript.api.tree.expression.ArrowFunctionTree;
 import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
 import org.sonar.plugins.javascript.api.tree.lexical.SyntaxToken;
@@ -40,15 +42,18 @@ public class ArrowFunctionTreeImpl extends JavaScriptTree implements ArrowFuncti
   private final Tree parameters;
   private final SyntaxToken doubleArrow;
   private Tree body;
+  private Type functionType;
 
   public ArrowFunctionTreeImpl(Tree parameters, InternalSyntaxToken doubleArrow, Tree body) {
     this.parameters = parameters;
     this.doubleArrow = doubleArrow;
     this.body = body;
+
+    this.functionType = FunctionType.create(this);
   }
 
   @Override
-  public Tree parameters() {
+  public Tree parameterClause() {
     return parameters;
   }
 
@@ -58,8 +63,18 @@ public class ArrowFunctionTreeImpl extends JavaScriptTree implements ArrowFuncti
   }
 
   @Override
-  public Tree conciseBody() {
+  public Tree body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parameterList() {
+    if (this.parameters.is(Tree.Kind.FORMAL_PARAMETER_LIST)) {
+      return ((ParameterListTree) this.parameters).parameters();
+
+    } else {
+      return ImmutableList.of(this.parameterClause());
+    }
   }
 
   @Override
@@ -87,7 +102,9 @@ public class ArrowFunctionTreeImpl extends JavaScriptTree implements ArrowFuncti
 
   @Override
   public TypeSet types() {
-    return TypeSet.emptyTypeSet();
+    TypeSet set = TypeSet.emptyTypeSet();
+    set.add(functionType);
+    return set;
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/javascript/tree/impl/expression/FunctionExpressionTreeImpl.java
@@ -21,6 +21,7 @@ package org.sonar.javascript.tree.impl.expression;
 
 import com.google.common.collect.Iterators;
 import java.util.Iterator;
+import java.util.List;
 import javax.annotation.Nullable;
 import org.sonar.javascript.tree.impl.JavaScriptTree;
 import org.sonar.javascript.tree.impl.declaration.ParameterListTreeImpl;
@@ -144,7 +145,7 @@ public class FunctionExpressionTreeImpl extends JavaScriptTree implements Functi
   }
 
   @Override
-  public ParameterListTree parameters() {
+  public ParameterListTree parameterClause() {
     return parameters;
   }
 
@@ -156,6 +157,11 @@ public class FunctionExpressionTreeImpl extends JavaScriptTree implements Functi
   @Override
   public BlockTreeImpl body() {
     return body;
+  }
+
+  @Override
+  public List<Tree> parameterList() {
+    return parameters.parameters();
   }
 
   @Override

javascript-frontend/src/main/java/org/sonar/plugins/javascript/api/tree/declaration/FunctionTree.java
@@ -20,11 +20,13 @@
 package org.sonar.plugins.javascript.api.tree.declaration;
 
 
+import java.util.List;
 import org.sonar.plugins.javascript.api.tree.Tree;
-import org.sonar.plugins.javascript.api.tree.statement.BlockTree;
 
 public interface FunctionTree extends Tree {
-  ParameterListTree parameters();
+  Tree parameterClause();
 
-  BlockTree body();
+  Tree body();
+
+  List<Tree> parameterList();
 }