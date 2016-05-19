a99455228680ac900760ea649502c6da9c48be8c
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-05-19 15:07:52 +0200

    SONARJS-692 FP BooleanEqualityComparisonCheck: not condition expression

--- javascript-checks/src/test/resources/checks/booleanEqualityComparison.js
+++ javascript-checks/src/test/resources/checks/booleanEqualityComparison.js
@@ -12,31 +12,22 @@ false == a;       // Noncompliant
 true == a;        // Noncompliant
 false != a;       // Noncompliant
 true != a;        // Noncompliant
-false && foo();   // Noncompliant {{Remove the literal "false" boolean value.}}
-foo() || true;    // Noncompliant {{Remove the literal "true" boolean value.}}
+false && foo();   // Ok covered by always-true-false-condition rule
 
 a == foo(true);   // OK
 true < 0;         // OK
 ~true;            // OK
 ++ true;          // OK
 !foo;             // OK
-foo() && bar();   // OK
 
 a == true == b    // Noncompliant
 a == b == false    // Noncompliant
-a == (true && b) == b //Noncompliant
+a == (true == b) == b //Noncompliant
 
 
 
 !(true) // Noncompliant
 a == (false) // Noncompliant
-foo() || (true) // Noncompliant
 
-true ? a : b    // Noncompliant
-false ? a : b    // Noncompliant
-var x = true ? a : b  // Noncompliant
-x = a ? a :
-  true? b : a   // Noncompliant
-x = (true) ? a : b // Noncompliant
-
+true ? a : b     // Ok covered by always-true-false-condition rule
 cond ? "true" : true // OK
