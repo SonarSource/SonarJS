58aa54664a35e1798b7ee4387f23ab33fb2cec95
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-05-26 09:49:20 +0200

    SONARJS-677 Migrate ElementUsedWithClassSelectorCheck

--- javascript-checks/src/test/resources/checks/ElementUsedWithClassSelector.js
+++ javascript-checks/src/test/resources/checks/ElementUsedWithClassSelector.js
@@ -1,14 +1,15 @@
-$("div.className");  // NOK
+$("div.className");  // Noncompliant {{Remove "div" in this selector.}}
 
 $(".className");  // OK
 
-var x = $("div#id") // OK
+var x = $("div#id"); // OK
 
 $("div.className div.className");  // OK
 
 $(":contains('text').className"); // OK
 
-var $s = jQuery(" p.class-name ")  // NOK
+var $s = jQuery(" p.class-name ");  // Noncompliant {{Remove "p" in this selector.}}
+//       ^^^^^^^^^^^^^^^^^^^^^^^^
 
 foo("div.className"); // OK
 
