546d17b700064ac6f442745f3f97433211f6d1a4
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-05-27 11:57:53 +0200

    SONARJS-676 Migrate ConstructorFunctionsForSideEffectsCheck

--- javascript-checks/src/test/resources/checks/constructorFunctionsForSideEffects.js
+++ javascript-checks/src/test/resources/checks/constructorFunctionsForSideEffects.js
@@ -1,6 +1,7 @@
 export default new MyConstructor();  // OK
 var something = new MyConstructor(); // OK
 something = new MyConstructor();     // OK
-new MyConstructor();                 // Noncompliant {{Either remove this useless object instantiation of "MyConstructor" or use it}}
+  new MyConstructor();                 // Noncompliant {{Either remove this useless object instantiation of "MyConstructor" or use it}}
+//^^^^^^^^^^^^^^^^^^^
 callMethod(new MyConstructor());     // OK
 new MyConstructor().doSomething();   // OK
