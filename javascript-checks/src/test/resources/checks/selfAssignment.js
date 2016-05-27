76432bed404a06c8cd2cee362677260f403e96a2
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-05-27 11:56:11 +0200

    SONARJS-676 Migrate SelfAssignmentCheck

--- javascript-checks/src/test/resources/checks/selfAssignment.js
+++ javascript-checks/src/test/resources/checks/selfAssignment.js
@@ -1,7 +1,8 @@
 x = 1;
 x = y;
-x = x; // NOK
+x = x; // Noncompliant {{Remove or correct this useless self-assignment.}}
 x = this.x;
-this.x = this.x; // NOK
+  this.x = this.x; // Noncompliant
+//^^^^^^^^^^^^^^^
 x += x;
 x = x + x;
