fdcb930b828af16266f125c33c0ccbb4c2b364bf
Yves Dubois-Pelerin <yves.duboispelerin@sonarsource.com>
2016-06-09 11:20:55 +0200

    SONARJS-701 update unit test file

--- javascript-checks/src/test/resources/checks/commaOperatorInSwitchCase.js
+++ javascript-checks/src/test/resources/checks/commaOperatorInSwitchCase.js
@@ -1,20 +1,22 @@
 function foo() {
   switch (a) {
+    case 0:         // OK
     case 1:         // OK
-      foo();
-      break;
-    case 1:         // OK
-      foo();
+      foo1();
       break;
     case 2,3:       // Noncompliant {{Explicitly specify 2 separate cases that fall through; currently this case clause only works for "3".}}
 //       ^^^
-      foo();
+      foo2();
       break;
     case "a","b","c","d": // Noncompliant {{Explicitly specify 4 separate cases that fall through; currently this case clause only works for ""d"".}}
 //       ^^^^^^^^^^^^^^^
+      foo3();
+      break;
+    case bar(), baz() : // Noncompliant {{Explicitly specify 2 separate cases that fall through; currently this case clause only works for "baz()".}}
+//       ^^^^^^^^^^^^
       foo();
       break;
     default:
-      foo();
+      foo4();
   }
 }
