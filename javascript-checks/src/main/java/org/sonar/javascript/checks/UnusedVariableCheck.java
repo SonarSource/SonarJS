b68cd53f7b6cf82bddda160233b189705ddc6c37
ivandalbosco <ivandalbosco@users.noreply.github.com>
2016-06-16 15:52:58 +0200

    SONARJS-664 Improve UnusedVariable: consider unused local functions (#280)
    * SONARJS-664 Improve UnusedVariable: consider unused local functions
    
    * SONARJS-664 Improve UnusedVariable: consider unused local functions - update the plugin description
    
    * SONARJS-664 Add missing word to the rule description

--- javascript-checks/src/main/java/org/sonar/javascript/checks/UnusedVariableCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/UnusedVariableCheck.java
@@ -22,8 +22,10 @@ package org.sonar.javascript.checks;
 import java.util.Collection;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
+import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.plugins.javascript.api.symbols.Symbol;
 import org.sonar.plugins.javascript.api.symbols.SymbolModel;
+import org.sonar.plugins.javascript.api.symbols.Type;
 import org.sonar.plugins.javascript.api.symbols.Usage;
 import org.sonar.plugins.javascript.api.tree.ScriptTree;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
@@ -33,25 +35,32 @@ import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 
 @Rule(
   key = "UnusedVariable",
-  name = "Unused local variables should be removed",
+  name = "Unused local variables and functions should be removed",
   priority = Priority.MAJOR,
   tags = {Tags.UNUSED})
 @ActivatedByDefault
 @SqaleConstantRemediation("5min")
 public class UnusedVariableCheck extends DoubleDispatchVisitorCheck {
 
-  private static final String MESSAGE = "Remove the declaration of the unused '%s' variable.";
+  private static final String MESSAGE_FOR_VARIABLE = "Remove the declaration of the unused '%s' variable.";
+  
+  private static final String MESSAGE_FOR_FUNCTION = "Remove unused function '%s'.";
 
   @Override
   public void visitScript(ScriptTree tree) {
     SymbolModel symbolModel = getContext().getSymbolModel();
 
     for (Symbol symbol : symbolModel.getSymbols()) {
-
-      if (symbol.isVariable()) {
-        Collection<Usage> usages = symbol.usages();
-        if (noUsages(usages) && !isGlobalOrCatchVariable(symbol) && !symbol.builtIn()) {
-          raiseIssuesOnDeclarations(symbol, String.format(MESSAGE, symbol.name()));
+      FunctionType functionType = (FunctionType) symbol.types().getUniqueType(Type.Kind.FUNCTION);
+      if (functionType != null && symbol.is(Symbol.Kind.FUNCTION) && functionType.functionTree().is(Kind.FUNCTION_EXPRESSION, Kind.GENERATOR_FUNCTION_EXPRESSION)) {
+        continue;
+      }
+      Collection<Usage> usages = symbol.usages();
+      if (noUsages(usages) && !isGlobalOrCatchVariable(symbol) && !symbol.builtIn()) {
+        if (symbol.isVariable()) {
+          raiseIssuesOnDeclarations(symbol, String.format(MESSAGE_FOR_VARIABLE, symbol.name()));
+        } else if (symbol.is(Symbol.Kind.FUNCTION)) {
+          raiseIssuesOnDeclarations(symbol, String.format(MESSAGE_FOR_FUNCTION, symbol.name()));
         }
       }
     }
