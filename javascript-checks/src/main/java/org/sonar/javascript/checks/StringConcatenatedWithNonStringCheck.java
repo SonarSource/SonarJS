e351d4632cf4d85ae3ebdf3e813596ec23613118
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-07-29 14:35:43 +0200

    SONARJS-749 Improve StringConcatenatedWithNonStringCheck with SE

--- javascript-checks/src/main/java/org/sonar/javascript/checks/StringConcatenatedWithNonStringCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/StringConcatenatedWithNonStringCheck.java
@@ -19,15 +19,21 @@
  */
 package org.sonar.javascript.checks;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import javax.annotation.CheckForNull;
 import org.sonar.check.Priority;
 import org.sonar.check.Rule;
-import org.sonar.javascript.checks.utils.CheckUtils;
-import org.sonar.plugins.javascript.api.symbols.Type;
+import org.sonar.javascript.se.Constraint;
+import org.sonar.javascript.se.ProgramState;
+import org.sonar.javascript.se.SeCheck;
+import org.sonar.javascript.se.Type;
+import org.sonar.javascript.tree.symbols.Scope;
+import org.sonar.plugins.javascript.api.tree.Tree;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
 import org.sonar.plugins.javascript.api.tree.expression.BinaryExpressionTree;
 import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
 import org.sonar.squidbridge.annotations.ActivatedByDefault;
 import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
 
@@ -38,34 +44,63 @@ import org.sonar.squidbridge.annotations.SqaleConstantRemediation;
   tags = Tags.SUSPICIOUS)
 @ActivatedByDefault
 @SqaleConstantRemediation("15min")
-public class StringConcatenatedWithNonStringCheck extends DoubleDispatchVisitorCheck {
+public class StringConcatenatedWithNonStringCheck extends SeCheck {
 
-  private static final String MESSAGE = "Either make this concatenation explicit or cast \"%s\" operand to a number.";
+  private static final String MESSAGE = "Either make this concatenation explicit or cast one operand to a number.";
+
+  // For each "+" binary expression tree this map contains true if types of operands are string and non-string in all execution paths,
+  // true if types are good in at least one execution path
+  private Map<BinaryExpressionTree, Boolean> appropriateTypes = new HashMap<>();
 
   @Override
-  public void visitBinaryExpression(BinaryExpressionTree tree) {
-    if (tree.is(Kind.PLUS)) {
-      ExpressionTree stringOperand = getOnlyStringOperand(tree.leftOperand(), tree.rightOperand());
+  public void beforeBlockElement(ProgramState currentState, Tree element) {
+    if (element.is(Kind.PLUS)) {
 
-      if (stringOperand != null && stringOperand.is(Kind.IDENTIFIER_REFERENCE)) {
-        raiseIssue(tree, stringOperand);
+      BinaryExpressionTree expression = (BinaryExpressionTree) element;
+
+      ExpressionTree onlyStringOperand = getOnlyStringOperand(expression.leftOperand(), expression.rightOperand(), currentState);
+
+      if (onlyStringOperand == null || !onlyStringOperand.is(Kind.IDENTIFIER_REFERENCE)) {
+        appropriateTypes.put(expression, false);
+
+      } else if (!appropriateTypes.containsKey(expression)) {
+        appropriateTypes.put(expression, true);
       }
     }
+  }
 
-    super.visitBinaryExpression(tree);
+  @Override
+  public void endOfExecution(Scope functionScope) {
+    for (Entry<BinaryExpressionTree, Boolean> entry : appropriateTypes.entrySet()) {
+      if (entry.getValue()) {
+        BinaryExpressionTree tree = entry.getKey();
+        addIssue(tree.operator(), MESSAGE)
+          .secondary(tree.leftOperand())
+          .secondary(tree.rightOperand());
+      }
+    }
+  }
+
+  @Override
+  public void startOfExecution(Scope functionScope) {
+    appropriateTypes.clear();
   }
 
   @CheckForNull
-  private static ExpressionTree getOnlyStringOperand(ExpressionTree leftOperand, ExpressionTree rightOperand) {
-    Type leftOperandType = leftOperand.types().getUniqueKnownType();
-    Type rightOperandType = rightOperand.types().getUniqueKnownType();
+  private static ExpressionTree getOnlyStringOperand(ExpressionTree leftOperand, ExpressionTree rightOperand, ProgramState currentState) {
+    Constraint rightConstraint = currentState.getConstraint(currentState.peekStack(0));
+    Constraint leftConstraint = currentState.getConstraint(currentState.peekStack(1));
 
-    if (leftOperandType != null && rightOperandType != null) {
+    Type rightType = rightConstraint.type();
+    Type leftType = leftConstraint.type();
 
-      if (isString(leftOperandType) && !isString(rightOperandType)) {
+
+    if (leftType != null && rightType != null) {
+
+      if (leftType == Type.STRING && rightType != Type.STRING) {
         return leftOperand;
 
-      } else if (!isString(leftOperandType) && isString(rightOperandType)) {
+      } else if (leftType != Type.STRING && rightType == Type.STRING) {
         return rightOperand;
       }
     }
@@ -73,15 +108,4 @@ public class StringConcatenatedWithNonStringCheck extends DoubleDispatchVisitorC
     return null;
   }
 
-  private void raiseIssue(BinaryExpressionTree tree, ExpressionTree stringOperand) {
-    String message = String.format(MESSAGE, CheckUtils.asString(stringOperand));
-    addIssue(tree.operator(), message)
-      .secondary(tree.leftOperand())
-      .secondary(tree.rightOperand());
-
-  }
-
-  private static boolean isString(Type type) {
-    return type.kind() == Type.Kind.STRING;
-  }
 }
