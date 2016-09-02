75c5a707429b8cc6dc6421c473e616847df142ff
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-09-02 15:15:29 +0200

    SONARJS-430 Extend FunctionNameCheck (S100)
    Cover function expression and object property

--- javascript-checks/src/test/resources/checks/FunctionName.js
+++ javascript-checks/src/test/resources/checks/FunctionName.js
@@ -32,11 +32,34 @@ class C {
     }
 }
 
-var MyObj = function Object () {            // OK - not handled by the check
+var MyObj1 = function Object () {            // Noncompliant
+//  ^^^^^^
+    this.a = 1;
+};
+
+let MyObj2 = function Object () {            // Noncompliant
     this.a = 1;
 };
 
 var myObj = {
+
+  Method1() { // Noncompliant
+
+  },
+
+  Method2: function() {                    // Noncompliant
+//^^^^^^^
+  },
+
+  Method3: function * () {                    // Noncompliant
+//^^^^^^^
+  },
+
+  Method4: (a, b) => { foo(a + b) },       // Noncompliant
+//^^^^^^^
+
+  ["Method5"] : function() {              // OK - not handled by the check
+  },
 
   set my_field (val) {                     // OK - not handled by the check
       this.my_field = val;
