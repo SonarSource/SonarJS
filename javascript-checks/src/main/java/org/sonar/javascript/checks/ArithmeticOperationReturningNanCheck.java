e90631aa7ccb761f71f9a25429a5136ddcc82408
pynicolas <pierre-yves.nicolas@sonarsource.com>
2016-11-01 14:49:09 +0100

    SONARJS-802 Rule: Arithmetic operations should not always result in NaN (#368)

---
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/ArithmeticOperationReturningNanCheck.java
@@ -0,0 +1,166 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2016 SonarSource SA
+ * mailto:contact AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import org.sonar.check.Rule;
+import org.sonar.javascript.se.Constraint;
+import org.sonar.javascript.se.ProgramState;
+import org.sonar.javascript.se.SeCheck;
+import org.sonar.javascript.tree.symbols.Scope;
+import org.sonar.plugins.javascript.api.symbols.Symbol;
+import org.sonar.plugins.javascript.api.tree.Tree;
+import org.sonar.plugins.javascript.api.tree.Tree.Kind;
+import org.sonar.plugins.javascript.api.tree.expression.AssignmentExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.BinaryExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
+import org.sonar.plugins.javascript.api.tree.expression.UnaryExpressionTree;
+import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
+
+@Rule(key = "S3757")
+public class ArithmeticOperationReturningNanCheck extends SeCheck {
+
+  private static final String MESSAGE = "Change the expression which uses this operand so that it can't evaluate to \"NaN\" (Not a Number).";
+
+  private static final Constraint NUMBER_OR_BOOLEAN = Constraint.NUMBER.or(Constraint.BOOLEAN);
+  private static final Constraint NUMBER_OR_BOOLEAN_OR_UNDEFINED = NUMBER_OR_BOOLEAN.or(Constraint.UNDEFINED);
+  private static final Constraint NON_DATE_OBJECT = Constraint.OBJECT.and(Constraint.DATE.not());
+  private static final Constraint UNDEFINED_OR_NON_DATE_OBJECT = Constraint.UNDEFINED.or(NON_DATE_OBJECT);
+
+  private final BinaryOperationChecker plusChecker = new PlusChecker();
+  private final BinaryOperationChecker otherBinaryOperationChecker = new OtherBinaryOperationChecker();
+
+  private final Set<Symbol> symbolsWithIssues = new HashSet<>();
+
+  @Override
+  public void startOfExecution(Scope functionScope) {
+    symbolsWithIssues.clear();
+    super.startOfExecution(functionScope);
+  }
+
+  @Override
+  public void beforeBlockElement(ProgramState currentState, Tree element) {
+    if (element.is(Kind.PLUS, Kind.PLUS_ASSIGNMENT)) {
+
+      checkBinaryOperation(currentState, element, plusChecker);
+
+    } else if (element.is(
+      Kind.MINUS,
+      Kind.MULTIPLY,
+      Kind.DIVIDE,
+      Kind.REMAINDER,
+      Kind.MINUS_ASSIGNMENT,
+      Kind.MULTIPLY_ASSIGNMENT,
+      Kind.DIVIDE_ASSIGNMENT,
+      Kind.REMAINDER_ASSIGNMENT)) {
+
+      checkBinaryOperation(currentState, element, otherBinaryOperationChecker);
+
+    } else if (element.is(
+      Kind.UNARY_PLUS,
+      Kind.UNARY_MINUS,
+      Kind.POSTFIX_INCREMENT,
+      Kind.POSTFIX_DECREMENT,
+      Kind.PREFIX_INCREMENT,
+      Kind.PREFIX_DECREMENT)) {
+
+      Constraint constraint = currentState.getConstraint(currentState.peekStack(0));
+      if (constraint.isStricterOrEqualTo(UNDEFINED_OR_NON_DATE_OBJECT)) {
+        UnaryExpressionTree unaryExpression = (UnaryExpressionTree) element;
+        raiseIssue(unaryExpression.expression(), unaryExpression.operator());
+      }
+    }
+  }
+
+  private void raiseIssue(Tree operand, Tree... secondaryLocations) {
+    Symbol operandSymbol = null;
+    if (operand.is(Kind.IDENTIFIER_REFERENCE)) {
+      IdentifierTree identifier = (IdentifierTree) operand;
+      operandSymbol = identifier.symbol();
+    }
+    if (!symbolsWithIssues.contains(operandSymbol)) {
+      PreciseIssue issue = addIssue(operand, MESSAGE);
+      Arrays.asList(secondaryLocations).forEach(issue::secondary);
+      if (operandSymbol != null) {
+        symbolsWithIssues.add(operandSymbol);
+      }
+    }
+  }
+
+  private static void checkBinaryOperation(ProgramState currentState, Tree element, BinaryOperationChecker checker) {
+    Constraint rightConstraint = currentState.getConstraint(currentState.peekStack(0));
+    Constraint leftConstraint = currentState.getConstraint(currentState.peekStack(1));
+    ExpressionTree leftOperand;
+    ExpressionTree rightOperand;
+    Tree operator;
+    if (element instanceof AssignmentExpressionTree) {
+      AssignmentExpressionTree assignment = (AssignmentExpressionTree) element;
+      leftOperand = assignment.variable();
+      rightOperand = assignment.expression();
+      operator = assignment.operator();
+    } else {
+      BinaryExpressionTree binaryExpression = (BinaryExpressionTree) element;
+      leftOperand = binaryExpression.leftOperand();
+      rightOperand = binaryExpression.rightOperand();
+      operator = binaryExpression.operator();
+    }
+    checker.check(leftConstraint, rightConstraint, leftOperand, rightOperand, operator);
+  }
+
+  @FunctionalInterface
+  private interface BinaryOperationChecker {
+
+    void check(Constraint leftConstraint, Constraint rightConstraint, Tree leftOperand, Tree rightOperand, Tree operator);
+
+  }
+
+  private class PlusChecker implements BinaryOperationChecker {
+
+    @Override
+    public void check(Constraint leftConstraint, Constraint rightConstraint, Tree leftOperand, Tree rightOperand, Tree operator) {
+      if (isAdditionWithUndefined(rightConstraint, leftConstraint)) {
+        raiseIssue(leftOperand, operator, rightOperand);
+      } else if (isAdditionWithUndefined(leftConstraint, rightConstraint)) {
+        raiseIssue(rightOperand, operator, leftOperand);
+      }
+    }
+
+    private boolean isAdditionWithUndefined(Constraint constraint1, Constraint constraint2) {
+      return constraint1.isStricterOrEqualTo(NUMBER_OR_BOOLEAN_OR_UNDEFINED)
+        && constraint2.isStricterOrEqualTo(Constraint.UNDEFINED);
+    }
+
+  }
+
+  private class OtherBinaryOperationChecker implements BinaryOperationChecker {
+    @Override
+    public void check(Constraint leftConstraint, Constraint rightConstraint, Tree leftOperand, Tree rightOperand, Tree operator) {
+      if (leftConstraint.isStricterOrEqualTo(UNDEFINED_OR_NON_DATE_OBJECT)) {
+        raiseIssue(leftOperand, operator, rightOperand);
+      } else if (rightConstraint.isStricterOrEqualTo(UNDEFINED_OR_NON_DATE_OBJECT)) {
+        raiseIssue(rightOperand, operator, leftOperand);
+      }
+    }
+  }
+
+}
---
+++ javascript-checks/src/test/resources/checks/ArithmeticOperationReturningNan.js
@@ -0,0 +1,84 @@
+function f1() {
+  42 - 7;
+  42 - [1,2]; // Noncompliant {{Change the expression which uses this operand so that it can't evaluate to "NaN" (Not a Number).}}
+//     ^^^^^
+
+  [1,2] - 42; // Noncompliant
+  [1,2] -  // Noncompliant [[id=1]]
+//S     ^ 1
+          42;
+//S       ^^ 1
+
+  [1,2] / 42; // Noncompliant
+  [1,2] * 42; // Noncompliant
+  [1,2] % 42; // Noncompliant
+  
+  42 / [1,2]; // Noncompliant
+  42 / unknown; //ok
+
+  var array1 = [1,2];
+  array1++; // Noncompliant
+//^^^^^^
+  var array2 = [1,2];
+  array2--; // Noncompliant
+  var array3 = [1,2];
+  ++array3; // Noncompliant
+  var array4 = [1,2];
+  --array4; // Noncompliant
+  
+  var array5 = [1,2];
+  array5 -= 42; // Noncompliant
+  var array6 = [1,2];
+  array6 *= 42; // Noncompliant
+  var array7 = [1,2];
+  array7 /= 42; // Noncompliant
+  var array8 = [1,2];
+  array8 %= 42; // Noncompliant
+  
+  foo(+[1,2]); // Noncompliant
+//     ^^^^^
+  foo(-[1,2]); // Noncompliant
+  foo(+42);
+  foo(-42);
+  
+  undefined + 42; // Noncompliant
+  42 + undefined; // Noncompliant
+  undefined + true; // Noncompliant
+  var x = undefined;
+  x += 42; // Noncompliant
+  
+  undefined + ""; // ok
+  undefined + {}; // ok
+  undefined + undefined; // Noncompliant
+
+  null + 42; // ok
+  true + 42; // ok
+  
+  var obj1 = {}
+  obj1 - 42; // Noncompliant
+  var obj2 = {}
+  obj2 + 42; // ok
+  var obj3 = {};
+  obj3 += 42; // ok
+}
+
+function dates() {
+  var date1 = new Date();
+  var date2 = new Date();
+  +date1; // ok
+  date1 - date2; // ok
+  date1 / date2; // ok
+  new Date() / 42; // ok
+  42 / new Date(); // ok
+}
+
+function single_issue_per_expression() {
+  var x, y;
+  x / y; // Noncompliant we should have only 1 issue here
+}
+
+function single_issue_per_symbol() {
+  var x = [1, 2];
+  x / 42; // Noncompliant
+  x - 42; // ok, an issue was already raised on the value of x
+}
