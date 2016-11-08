8b96a2a45c8a9b1b1ce8789023574566867f07ed
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-11-08 14:31:39 +0100

    Remove type-inference IT

--- its/type-inference/projects/documentcloud/application.js
+++
@@ -1,9 +0,0 @@
-// Provide top-level namespaces for our javascript.
-(function() {
-  window.dc = {};
-  dc.translations = {};
-  dc.controllers = {};
-  dc.model = {};
-  dc.app = {};
-  dc.ui = {};
-})();
