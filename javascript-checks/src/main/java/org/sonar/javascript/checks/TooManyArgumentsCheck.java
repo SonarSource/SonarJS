hetzijzo/sonar-javascript

SONARJS-831 Improve TooManyArgumentsCheck: consider built-in functions

javascript-checks/src/main/java/org/sonar/javascript/checks/TooManyArgumentsCheck.java
@@ -19,54 +19,149 @@
  */
 package org.sonar.javascript.checks;
 
+import com.google.common.collect.Lists;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Objects;
 import java.util.Set;
+import java.util.function.IntFunction;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.utils.CheckUtils;
+import org.sonar.javascript.se.Constraint;
+import org.sonar.javascript.se.ProgramState;
+import org.sonar.javascript.se.SeCheck;
+import org.sonar.javascript.se.sv.BuiltInFunctionSymbolicValue;
+import org.sonar.javascript.se.sv.SymbolicValue;
+import org.sonar.javascript.tree.impl.SeparatedList;
 import org.sonar.javascript.tree.symbols.Scope;
 import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.plugins.javascript.api.symbols.Symbol;
+import org.sonar.plugins.javascript.api.symbols.SymbolModel;
 import org.sonar.plugins.javascript.api.symbols.Type;
+import org.sonar.plugins.javascript.api.tree.ScriptTree;
 import org.sonar.plugins.javascript.api.tree.Tree;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
 import org.sonar.plugins.javascript.api.tree.declaration.FunctionTree;
 import org.sonar.plugins.javascript.api.tree.expression.CallExpressionTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
+import org.sonar.plugins.javascript.api.tree.expression.DotMemberExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+import org.sonar.plugins.javascript.api.visitors.SubscriptionVisitor;
+import org.sonar.plugins.javascript.api.visitors.TreeVisitorContext;
 
 @Rule(key = "S930")
-public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
+public class TooManyArgumentsCheck extends SeCheck {
 
   private static final String MESSAGE = "%s expects %s argument%s, but %s %s provided.";
+  
+  private static final List<String> ignoredBuiltInFunctions = Lists.newArrayList("toString", "toLocaleString");
+
+  /**
+   * Expressions for which an issue has already be raised. Used to avoid multiple identical issues due to different branches.
+   */
+  private Set<CallExpressionTree> withIssue = new HashSet<>();
 
   @Override
-  public void visitCallExpression(CallExpressionTree tree) {
+  public void startOfFile(ScriptTree scriptTree) {
+    issues.reset();
+    withIssue.clear();
+  }
+
+  /**
+   * Gets the issues for built-in functions.
+   */
+  @Override
+  public void beforeBlockElement(ProgramState currentState, Tree element) {
+    if (element.is(Kind.CALL_EXPRESSION)) {
+      CallExpressionTree callExpression = (CallExpressionTree) element;
+      SeparatedList<Tree> actualArguments = callExpression.arguments().parameters();
+      int nbActualArguments = actualArguments.size();
+
+      SymbolicValue calleeValue = currentState.peekStack(actualArguments.size());
+      if (nbActualArguments > 0 && calleeValue instanceof BuiltInFunctionSymbolicValue && mustProcess(callExpression)) {
+        IntFunction<Constraint> signature = ((BuiltInFunctionSymbolicValue) calleeValue).signature();
+        // the following ugly algorithm is specifically tuned for functions with rest arguments (that is, with "..." in the argument list)
+        int index = nbActualArguments - 1;
+        while (index >= 0) {
+          // for a non-existing argument, method "apply" returns null
+          if (signature.apply(index) != null) {
+            break;
+          }
+          index--;
+        }
+        int nbAllowedArguments = index + 1;
+
+        if (nbActualArguments > nbAllowedArguments) {
+          String message = getMessage(callExpression, nbAllowedArguments, nbActualArguments);
+          addIssue(callExpression.arguments(), message);
+          withIssue.add(callExpression);
+        }
+      }
+    }
+  }
+
+  @Override
+  public List<Issue> scanFile(TreeVisitorContext context) {
+    // issues from built-in functions (obtained by symbolic execution)
+    List<Issue> allIssues = Lists.newArrayList(issues.getList());
+
+    // issues from user-defined functions (obtained by the visitor)
+    CallExpressionGatherer visitor = new CallExpressionGatherer();
+    visitor.scanTree(context.getTopTree());
+    visitor.callExpressions
+      .stream()
+      .map(callExpression -> checkCallExpression(callExpression, context.getSymbolModel()))
+      .filter(Objects::nonNull)
+      .forEach(allIssues::add);
+
+    return allIssues;
+  }
+
+  private Issue checkCallExpression(CallExpressionTree tree, SymbolModel symbolModel) {
     FunctionTree functionTree = getFunction(tree);
 
     if (functionTree != null) {
-
       int parametersNumber = functionTree.parameterList().size();
       int argumentsNumber = tree.arguments().parameters().size();
 
-      if (!hasRestParameter(functionTree) && !builtInArgumentsUsed(functionTree) && argumentsNumber > parametersNumber) {
+      if (!hasRestParameter(functionTree) && !builtInArgumentsUsed(functionTree, symbolModel) && argumentsNumber > parametersNumber) {
         String message = getMessage(tree, parametersNumber, argumentsNumber);
-        addIssue(tree.arguments(), message)
+        return addIssue(tree.arguments(), message)
           .secondary(functionTree.parameterClause(), "Formal parameters");
       }
-
     }
-
-    super.visitCallExpression(tree);
+    return null;
   }
 
   private static String getMessage(CallExpressionTree tree, int parametersNumber, int argumentsNumber) {
-    String callee;
-    if (CheckUtils.removeParenthesis(tree.callee()).is(Kind.FUNCTION_EXPRESSION)) {
-      callee = "This function";
+    ExpressionTree callee = getCallee(tree); 
+    String calleeName;
+    if (callee.is(Kind.FUNCTION_EXPRESSION)) {
+      calleeName = "This function";
+    } else if (callee.is(Kind.DOT_MEMBER_EXPRESSION)) {
+      calleeName = "\"" + ((DotMemberExpressionTree)callee).property().name() + "\"";
     } else {
-      callee = "\"" + CheckUtils.asString(tree.callee()) + "\"";
+      calleeName = "\"" + CheckUtils.asString(callee) + "\"";
     }
-    return String.format(MESSAGE, callee, parametersNumber, parametersNumber == 1 ? "" : "s", argumentsNumber, argumentsNumber > 1 ? "were" : "was");
+    return String.format(MESSAGE, calleeName, parametersNumber, parametersNumber == 1 ? "" : "s", argumentsNumber, argumentsNumber > 1 ? "were" : "was");
+  }
+
+  private static ExpressionTree getCallee(CallExpressionTree callExpression) {
+    return CheckUtils.removeParenthesis(callExpression.callee()); 
+  }
+
+  private boolean mustProcess(CallExpressionTree callExpression) {
+    if (withIssue.contains(callExpression)) {
+      return false;
+    } else {
+      ExpressionTree callee = getCallee(callExpression);
+      if (callee.is(Kind.DOT_MEMBER_EXPRESSION)) {
+        String functionName = ((DotMemberExpressionTree)callee).property().name();
+        return !ignoredBuiltInFunctions.contains(functionName);
+      }
+      return true;
+    }
   }
 
   /*
@@ -76,7 +171,6 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
     List<Tree> parameters = functionTree.parameterList();
     return !parameters.isEmpty() && (parameters.get(parameters.size() - 1).is(Tree.Kind.REST_ELEMENT));
   }
-
 
   @Nullable
   private static FunctionTree getFunction(CallExpressionTree tree) {
@@ -89,9 +183,8 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
     return null;
   }
 
-
-  private boolean builtInArgumentsUsed(FunctionTree tree) {
-    Scope scope = getContext().getSymbolModel().getScope(tree);
+  private static boolean builtInArgumentsUsed(FunctionTree tree, SymbolModel symbolModel) {
+    Scope scope = symbolModel.getScope(tree);
     if (scope == null) {
       throw new IllegalStateException("No scope found for FunctionTree");
     }
@@ -107,6 +200,27 @@ public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
 
     boolean isUsed = !argumentsBuiltInVariable.usages().isEmpty();
     return argumentsBuiltInVariable.builtIn() && isUsed;
+  }
+
+  /**
+   * An object to collect all call expressions.
+   * In particular it collects the call expressions located out of any function (that is,
+   * in the global scope), which cannot be performed by the symbolic execution engine.
+   */
+  private static class CallExpressionGatherer extends SubscriptionVisitor {
+
+    Set<CallExpressionTree> callExpressions = new HashSet<>(); 
+
+    @Override
+    public List<Kind> nodesToVisit() {
+      return Lists.newArrayList(Kind.CALL_EXPRESSION);
+    }
+
+    @Override
+    public void visitNode(Tree tree) {
+      callExpressions.add((CallExpressionTree) tree);
+    }
+
   }
 
 }

javascript-checks/src/test/resources/checks/TooManyArguments.js
@@ -56,3 +56,54 @@ x=function(a,b){
 
 var arrow_function = (a, b) => {};
 arrow_function(1, 2, 3); // Noncompliant
+
+
+function builtIn_GlobalFunction() {
+  isNaN();                                  // OK, too few arguments
+  isNaN(obj);                               // OK
+  isNaN(obj, true);                         // Noncompliant {{"isNaN" expects 1 argument, but 2 were provided.}}
+}
+
+function builtIn_StringFunctions() {
+  "hello".charAt();                         // OK, too few arguments
+  "hello".charAt(1);                        // OK
+  "hello".charAt(1, 2, 3);                  // Noncompliant {{"charAt" expects 1 argument, but 3 were provided.}}
+  new String("hello").charAt(1, 2, 3);      // Noncompliant
+}
+
+function builtIn_FunctionWithNoParams() {
+  new Array(1, 2).reverse();                // OK
+  new Array(1, 2).reverse(a, b, c);         // Noncompliant {{"reverse" expects 0 arguments, but 3 were provided.}}
+}
+
+function builtIn_FunctionWithRestParams() {
+  Array.of();                               // OK
+  Array.of(1, 2, 3, 4);                     // OK
+}
+
+function builtIn_WithUselessParentheses() {
+  (isNaN)(obj, true);                       // Noncompliant {{"isNaN" expects 1 argument, but 2 were provided.}}
+}
+
+function builtIn_ReferenceToBuiltIn() {
+  var isNaN2 = isNaN;
+  isNaN2(obj, true);                        // Noncompliant {{"isNaN2" expects 1 argument, but 2 were provided.}}  // no secondary location, unfortunately
+}
+
+function builtIn_IgnoredSpecificCases() {
+  new Buffer().toString('base64');          // OK, specifically ignored Object function
+  new Buffer().toLocaleString('base64');    // OK, specifically ignored Object function
+  new Buffer().hasOwnProperty(a, b);        // Noncompliant, not ignored Object function
+  new Buffer().hello(a, b);                 // OK, not an Object function
+}
+
+function builtIn_OneIssueAtMost(cond) {
+  var s = "hello";
+  if (cond) {
+    s = "world";
+  }
+  s.charAt(1, 2);                           // Noncompliant (1 issue here, not 2)
+  foo(cond);                                // to keep cond alive and thus force 2 execution paths
+}
+
+isNaN(obj, true);                           // OK, too many arguments but symbolic execution doesn't check global scope 

SonarSource/SonarJS

javascript-checks/src/main/java/org/sonar/javascript/checks/TooManyArgumentsCheck.java
@@ -19,94 +19,193 @@
  */
 package org.sonar.javascript.checks;
 
+import com.google.common.collect.Sets;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.function.IntFunction;
 import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.utils.CheckUtils;
+import org.sonar.javascript.se.Constraint;
+import org.sonar.javascript.se.ProgramState;
+import org.sonar.javascript.se.SeCheck;
+import org.sonar.javascript.se.sv.BuiltInFunctionSymbolicValue;
+import org.sonar.javascript.se.sv.SymbolicValue;
+import org.sonar.javascript.tree.impl.SeparatedList;
 import org.sonar.javascript.tree.symbols.Scope;
 import org.sonar.javascript.tree.symbols.type.FunctionType;
 import org.sonar.plugins.javascript.api.symbols.Symbol;
 import org.sonar.plugins.javascript.api.symbols.Type;
+import org.sonar.plugins.javascript.api.tree.ScriptTree;
 import org.sonar.plugins.javascript.api.tree.Tree;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
 import org.sonar.plugins.javascript.api.tree.declaration.FunctionTree;
 import org.sonar.plugins.javascript.api.tree.expression.CallExpressionTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
+import org.sonar.plugins.javascript.api.tree.expression.DotMemberExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitor;
 
 @Rule(key = "S930")
-public class TooManyArgumentsCheck extends DoubleDispatchVisitorCheck {
+public class TooManyArgumentsCheck extends SeCheck {
 
   private static final String MESSAGE = "%s expects %s argument%s, but %s %s provided.";
+  
+  private static final Set<String> BUILT_IN_FUNCTIONS_TO_IGNORE = Sets.newHashSet("toString", "toLocaleString");
+
+  /**
+   * Expressions for which an issue has already been raised. Used to avoid multiple identical issues due to different execution paths.
+   */
+  private Set<CallExpressionTree> withIssue = new HashSet<>();
 
   @Override
-  public void visitCallExpression(CallExpressionTree tree) {
-    FunctionTree functionTree = getFunction(tree);
+  public void startOfExecution(Scope functionScope) {
+    withIssue.clear();
+  }
 
-    if (functionTree != null) {
+  @Override
+  public void endOfFile(ScriptTree scriptTree) {
+    TreeVisitorCheck visitor = new TreeVisitorCheck();
+    visitor.visitScript(scriptTree);
+  }
 
-      int parametersNumber = functionTree.parameterList().size();
-      int argumentsNumber = tree.arguments().parameters().size();
+  /**
+   * Gets the issues for built-in functions.
+   */
+  @Override
+  public void beforeBlockElement(ProgramState currentState, Tree element) {
+    if (element.is(Kind.CALL_EXPRESSION)) {
+      CallExpressionTree callExpression = (CallExpressionTree) element;
+      SeparatedList<Tree> actualArguments = callExpression.arguments().parameters();
+      int nbActualArguments = actualArguments.size();
 
-      if (!hasRestParameter(functionTree) && !builtInArgumentsUsed(functionTree) && argumentsNumber > parametersNumber) {
-        String message = getMessage(tree, parametersNumber, argumentsNumber);
-        addIssue(tree.arguments(), message)
-          .secondary(functionTree.parameterClause(), "Formal parameters");
+      SymbolicValue calleeValue = currentState.peekStack(nbActualArguments);
+      if (nbActualArguments > 0 
+        && calleeValue instanceof BuiltInFunctionSymbolicValue 
+        && shouldCheck(callExpression)
+        && hasTooManyArguments((BuiltInFunctionSymbolicValue) calleeValue, nbActualArguments)) {
+       
+        int nbExpectedArguments = getNbParameters((BuiltInFunctionSymbolicValue) calleeValue);
+        String message = getMessage(callExpression, nbExpectedArguments, nbActualArguments);
+        addIssue(callExpression.arguments(), message);
+        withIssue.add(callExpression);
       }
-
     }
+  }
+  
+  private static boolean hasTooManyArguments(BuiltInFunctionSymbolicValue calleeValue, int nbActualArguments) {
+    return calleeValue.signature().apply(nbActualArguments - 1) == null;
+  }
 
-    super.visitCallExpression(tree);
+  /**
+   * Returns the number of parameters of the specified built-in function.
+   * @param calleeValue a built-in function assumed to have a fixed number of parameters (that is, no rest parameters).
+   */
+  private static int getNbParameters(BuiltInFunctionSymbolicValue calleeValue) {
+    IntFunction<Constraint> signature = calleeValue.signature();
+    int index = 0;
+    while (true) {
+      // for a non-existing parameter, method "apply" returns null
+      if (signature.apply(index) == null) {
+        break;
+      }
+      index++;
+    }
+    return index;
   }
 
   private static String getMessage(CallExpressionTree tree, int parametersNumber, int argumentsNumber) {
-    String callee;
-    if (CheckUtils.removeParenthesis(tree.callee()).is(Kind.FUNCTION_EXPRESSION)) {
-      callee = "This function";
+    ExpressionTree callee = getCallee(tree); 
+    String calleeName;
+    if (callee.is(Kind.FUNCTION_EXPRESSION)) {
+      calleeName = "This function";
+    } else if (callee.is(Kind.DOT_MEMBER_EXPRESSION)) {
+      calleeName = "\"" + ((DotMemberExpressionTree)callee).property().name() + "\"";
     } else {
-      callee = "\"" + CheckUtils.asString(tree.callee()) + "\"";
+      calleeName = "\"" + CheckUtils.asString(callee) + "\"";
     }
-    return String.format(MESSAGE, callee, parametersNumber, parametersNumber == 1 ? "" : "s", argumentsNumber, argumentsNumber > 1 ? "were" : "was");
+    return String.format(MESSAGE, calleeName, parametersNumber, parametersNumber == 1 ? "" : "s", argumentsNumber, argumentsNumber > 1 ? "were" : "was");
   }
 
-  /*
-   * @return true if function's last parameter has "... p" format and stands for all rest parameters
-   */
-  private static boolean hasRestParameter(FunctionTree functionTree) {
-    List<Tree> parameters = functionTree.parameterList();
-    return !parameters.isEmpty() && (parameters.get(parameters.size() - 1).is(Tree.Kind.REST_ELEMENT));
+  private static ExpressionTree getCallee(CallExpressionTree callExpression) {
+    return CheckUtils.removeParenthesis(callExpression.callee()); 
   }
 
-
-  @Nullable
-  private static FunctionTree getFunction(CallExpressionTree tree) {
-    Set<Type> types = tree.callee().types();
-
-    if (types.size() == 1 && types.iterator().next().kind().equals(Type.Kind.FUNCTION)) {
-      return ((FunctionType) types.iterator().next()).functionTree();
-    }
-
-    return null;
-  }
-
-
-  private boolean builtInArgumentsUsed(FunctionTree tree) {
-    Scope scope = getContext().getSymbolModel().getScope(tree);
-    if (scope == null) {
-      throw new IllegalStateException("No scope found for FunctionTree");
-    }
-
-    Symbol argumentsBuiltInVariable = scope.lookupSymbol("arguments");
-    if (argumentsBuiltInVariable == null) {
-      if (!tree.is(Kind.ARROW_FUNCTION)) {
-        throw new IllegalStateException("No 'arguments' symbol found for function scope");
-      } else {
-        return false;
+  private boolean shouldCheck(CallExpressionTree callExpression) {
+    if (withIssue.contains(callExpression)) {
+      return false;
+    } else {
+      ExpressionTree callee = getCallee(callExpression);
+      if (callee.is(Kind.DOT_MEMBER_EXPRESSION)) {
+        String functionName = ((DotMemberExpressionTree)callee).property().name();
+        return !BUILT_IN_FUNCTIONS_TO_IGNORE.contains(functionName);
       }
+      return true;
+    }
+  }
+
+  /**
+   * This visitor is responsible for raising issues based on symbol model and syntax tree.
+   * Thus TooManyArgumentsCheck based on symbolic execution is checking built-in function calls while this one checks user-defined functions.
+   */
+  private class TreeVisitorCheck extends DoubleDispatchVisitor {
+
+    @Override
+    public void visitCallExpression(CallExpressionTree tree) {
+      FunctionTree functionTree = getFunction(tree);
+
+      if (functionTree != null) {
+        int parametersNumber = functionTree.parameterList().size();
+        int argumentsNumber = tree.arguments().parameters().size();
+
+        if (!hasRestParameter(functionTree) && !builtInArgumentsUsed(functionTree) && argumentsNumber > parametersNumber) {
+          String message = getMessage(tree, parametersNumber, argumentsNumber);
+          addIssue(tree.arguments(), message)
+            .secondary(functionTree.parameterClause(), "Formal parameters");
+        }
+      }
+
+      super.visitCallExpression(tree);
     }
 
-    boolean isUsed = !argumentsBuiltInVariable.usages().isEmpty();
-    return argumentsBuiltInVariable.builtIn() && isUsed;
+    /*
+    * @return true if function's last parameter has "... p" format and stands for all rest parameters
+    */
+    private boolean hasRestParameter(FunctionTree functionTree) {
+      List<Tree> parameters = functionTree.parameterList();
+      return !parameters.isEmpty() && (parameters.get(parameters.size() - 1).is(Tree.Kind.REST_ELEMENT));
+    }
+
+    @Nullable
+    private FunctionTree getFunction(CallExpressionTree tree) {
+      Set<Type> types = tree.callee().types();
+
+      if (types.size() == 1 && types.iterator().next().kind().equals(Type.Kind.FUNCTION)) {
+        return ((FunctionType) types.iterator().next()).functionTree();
+      }
+
+      return null;
+    }
+
+    private boolean builtInArgumentsUsed(FunctionTree tree) {
+      Scope scope = TooManyArgumentsCheck.this.getContext().getSymbolModel().getScope(tree);
+      if (scope == null) {
+        throw new IllegalStateException("No scope found for FunctionTree");
+      }
+
+      Symbol argumentsBuiltInVariable = scope.lookupSymbol("arguments");
+      if (argumentsBuiltInVariable == null) {
+        if (!tree.is(Kind.ARROW_FUNCTION)) {
+          throw new IllegalStateException("No 'arguments' symbol found for function scope");
+        } else {
+          return false;
+        }
+      }
+
+      boolean isUsed = !argumentsBuiltInVariable.usages().isEmpty();
+      return argumentsBuiltInVariable.builtIn() && isUsed;
+    }
+
   }
 
 }

javascript-checks/src/test/resources/checks/TooManyArguments.js
@@ -56,3 +56,63 @@ x=function(a,b){
 
 var arrow_function = (a, b) => {};
 arrow_function(1, 2, 3); // Noncompliant
+
+
+function builtIn_GlobalFunction() {
+  isNaN();                                  // OK, too few arguments
+  isNaN(obj);                               // OK
+  isNaN(obj, true);                         // Noncompliant {{"isNaN" expects 1 argument, but 2 were provided.}}
+}
+
+function builtIn_StringFunctions() {
+  "hello".charAt();                         // OK, too few arguments
+  "hello".charAt(1);                        // OK
+  "hello".charAt(1, 2, 3);                  // Noncompliant {{"charAt" expects 1 argument, but 3 were provided.}}
+  new String("hello").charAt(1, 2, 3);      // Noncompliant
+}
+
+function builtIn_FunctionWithNoParams() {
+  new Array(1, 2).reverse();                // OK
+  new Array(1, 2).reverse(a, b, c);         // Noncompliant {{"reverse" expects 0 arguments, but 3 were provided.}}
+}
+
+function builtIn_FunctionWithRestParams() {
+  Array.of();                               // OK
+  Array.of(1, 2, 3, 4);                     // OK
+}
+
+function builtIn_WithUselessParentheses() {
+  (isNaN)(obj, true);                       // Noncompliant {{"isNaN" expects 1 argument, but 2 were provided.}}
+}
+
+function builtIn_ReferenceToBuiltIn() {
+  var isNaN2 = isNaN;
+  isNaN2(obj, true);                        // Noncompliant {{"isNaN2" expects 1 argument, but 2 were provided.}}  // no secondary location, unfortunately
+}
+
+function builtIn_IgnoredSpecificCases() {
+  new Buffer().toString('base64');          // OK, specifically ignored Object function
+  new Buffer().toLocaleString('base64');    // OK, specifically ignored Object function
+  new Buffer().hasOwnProperty(a, b);        // Noncompliant, not ignored Object function
+  new Buffer().hello(a, b);                 // OK, not an Object function
+}
+
+function builtIn_OneIssueAtMost(cond) {
+  var s = "hello";
+  if (cond) {
+    s = "world";
+  }
+  s.charAt(1, 2);                           // Noncompliant (1 issue here, not 2)
+  foo(cond);                                // to keep cond alive and thus force 2 execution paths
+}
+
+function builtIn_OnePathRaisesIssue(cond) {
+  var s = unknown();
+  if (cond) {
+    s = "world";
+  }
+  s.charAt(1, 2);                           // Noncompliant (s is a string on one path, s is unknown on the other path)
+  foo(cond);                                // to keep cond alive and thus force 2 execution paths
+}
+
+isNaN(obj, true);                           // FN, too many arguments but symbolic execution doesn't check global scope 