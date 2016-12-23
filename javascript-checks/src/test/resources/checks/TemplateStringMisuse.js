e511ef13e77f4667886211a90ebc9a0d77419d28
Elena Vilchik <elena.vilchik@sonarsource.com>
2016-12-23 16:58:14 +0100

    Add unit test

--- javascript-checks/src/test/resources/checks/TemplateStringMisuse.js
+++ javascript-checks/src/test/resources/checks/TemplateStringMisuse.js
@@ -1,6 +1,8 @@
 "Hello ${name}"; // Noncompliant {{Replace the quotes (") with back-ticks (`).}}
 'Hello ${name}'; // Noncompliant {{Replace the quotes (') with back-ticks (`).}}
 
+'Hello ${firstName} ${lastName}'; // Noncompliant
+
 `Hello ${name}`; // OK
 `Hello name`; // OK
 `Hello {name}`; // OK
