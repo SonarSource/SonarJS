1ed1edaee0192a45166c7936390439909107fb0d
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-01-18 09:09:23 +0100

    SONARJS-862 Rule: Cognitive Complexity of functions should not be too high

---
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/CognitiveComplexityFunctionCheck.java
@@ -0,0 +1,361 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import javax.annotation.Nullable;
+import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
+import org.sonar.javascript.tree.TreeKinds;
+import org.sonar.javascript.tree.impl.JavaScriptTree;
+import org.sonar.plugins.javascript.api.tree.Tree;
+import org.sonar.plugins.javascript.api.tree.Tree.Kind;
+import org.sonar.plugins.javascript.api.tree.declaration.FunctionDeclarationTree;
+import org.sonar.plugins.javascript.api.tree.declaration.FunctionTree;
+import org.sonar.plugins.javascript.api.tree.declaration.MethodDeclarationTree;
+import org.sonar.plugins.javascript.api.tree.expression.ArrowFunctionTree;
+import org.sonar.plugins.javascript.api.tree.expression.BinaryExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.ConditionalExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.FunctionExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
+import org.sonar.plugins.javascript.api.tree.lexical.SyntaxToken;
+import org.sonar.plugins.javascript.api.tree.statement.BreakStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.CatchBlockTree;
+import org.sonar.plugins.javascript.api.tree.statement.ContinueStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.DoWhileStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.ElseClauseTree;
+import org.sonar.plugins.javascript.api.tree.statement.ForObjectStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.ForStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.IfStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.SwitchStatementTree;
+import org.sonar.plugins.javascript.api.tree.statement.WhileStatementTree;
+import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitor;
+import org.sonar.plugins.javascript.api.visitors.IssueLocation;
+import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
+import org.sonar.plugins.javascript.api.visitors.SubscriptionVisitorCheck;
+
+import static org.sonar.plugins.javascript.api.tree.Tree.Kind.ARROW_FUNCTION;
+import static org.sonar.plugins.javascript.api.tree.Tree.Kind.CONDITIONAL_AND;
+import static org.sonar.plugins.javascript.api.tree.Tree.Kind.CONDITIONAL_OR;
+
+@Rule(key = "S3776")
+public class CognitiveComplexityFunctionCheck extends SubscriptionVisitorCheck {
+
+  private static final String MESSAGE = "Refactor this function to reduce its Cognitive Complexity from %s to the %s allowed.";
+  private static final int DEFAULT_THRESHOLD = 15;
+
+  @RuleProperty(
+    key = "threshold",
+    description = "The maximum authorized complexity.",
+    defaultValue = "" + DEFAULT_THRESHOLD)
+  private int threshold = DEFAULT_THRESHOLD;
+
+  /* When complexity of nesting function is considered for this check, we ignore all nested in it functions */
+  private Set<Tree> ignoredNestedFunctions = new HashSet<>();
+  private final CognitiveComplexity cognitiveComplexity = new CognitiveComplexity();
+
+  @Override
+  public List<Kind> nodesToVisit() {
+    return TreeKinds.functionKinds();
+  }
+
+  @Override
+  public void visitFile(Tree scriptTree) {
+    ignoredNestedFunctions.clear();
+    super.visitFile(scriptTree);
+  }
+
+  @Override
+  public void visitNode(Tree tree) {
+    if (!ignoredNestedFunctions.contains(tree)) {
+      tree.accept(cognitiveComplexity);
+    }
+  }
+
+  private class CognitiveComplexity extends DoubleDispatchVisitor {
+    private int nestingLevel;
+    private int ownComplexity;
+    private int nestedFunctionComplexity;
+
+    private boolean functionContainsStructuralComplexity;
+
+    private Set<IssueLocation> secondaryIssueLocations = new HashSet<>();
+
+    private Tree currentFunction = null;
+    private Set<Tree> nestedFunctions = new HashSet<>();
+    private Deque<Tree> functionStack = new ArrayDeque<>();
+
+
+    private void visitFunction(FunctionTree tree) {
+      if (currentFunction == null) {
+        initComplexityCalculation(tree);
+
+      } else {
+        nestedFunctions.add(tree);
+        functionStack.push(tree);
+        nestingLevel++;
+      }
+    }
+
+    private void initComplexityCalculation(FunctionTree tree) {
+      nestingLevel = 0;
+      secondaryIssueLocations.clear();
+      ownComplexity = 0;
+      nestedFunctionComplexity = 0;
+      functionContainsStructuralComplexity = false;
+      currentFunction = tree;
+      nestedFunctions.clear();
+
+      functionStack.clear();
+      functionStack.push(currentFunction);
+    }
+
+    private void leaveFunction(FunctionTree tree) {
+      if (tree.equals(currentFunction)) {
+        checkComplexity();
+        currentFunction = null;
+
+      } else {
+        nestingLevel--;
+        functionStack.pop();
+      }
+    }
+
+    private void checkComplexity() {
+      int complexity;
+
+      if (functionContainsStructuralComplexity) {
+        complexity = ownComplexity + nestedFunctionComplexity;
+        ignoredNestedFunctions.addAll(nestedFunctions);
+
+      } else {
+        complexity = ownComplexity;
+      }
+
+      if (complexity > threshold) {
+        raiseIssue(complexity);
+      }
+    }
+
+    @Override
+    public void visitIfStatement(IfStatementTree tree) {
+      if (isElseIf(tree)) {
+        addComplexityWithoutNesting(tree.ifKeyword());
+
+      } else {
+        addComplexityWithNesting(tree.ifKeyword());
+      }
+
+      visit(tree.condition());
+      visitWithNesting(tree.statement());
+      visit(tree.elseClause());
+    }
+
+    @Override
+    public void visitElseClause(ElseClauseTree tree) {
+      if (tree.statement().is(Kind.IF_STATEMENT)) {
+        visit(tree.statement());
+
+      } else {
+        addComplexityWithoutNesting(tree.elseKeyword());
+        visitWithNesting(tree.statement());
+      }
+    }
+
+    @Override
+    public void visitWhileStatement(WhileStatementTree tree) {
+      visitLoop(tree.whileKeyword(), tree.statement(), tree.condition());
+    }
+
+    @Override
+    public void visitDoWhileStatement(DoWhileStatementTree tree) {
+      visitLoop(tree.doKeyword(), tree.statement(), tree.condition());
+    }
+
+    @Override
+    public void visitForStatement(ForStatementTree tree) {
+      visitLoop(tree.forKeyword(), tree.statement(), tree.init(), tree.condition(), tree.update());
+    }
+
+    @Override
+    public void visitForObjectStatement(ForObjectStatementTree tree) {
+      visitLoop(tree.forKeyword(), tree.statement(), tree.variableOrExpression(), tree.expression());
+    }
+
+    private void visitLoop(SyntaxToken secondaryLocationToken, Tree loopBody, Tree ... notNestedElements) {
+      addComplexityWithNesting(secondaryLocationToken);
+      visit(notNestedElements);
+      visitWithNesting(loopBody);
+    }
+
+    @Override
+    public void visitCatchBlock(CatchBlockTree tree) {
+      addComplexityWithNesting(tree.catchKeyword());
+      visitWithNesting(tree.block());
+    }
+
+    @Override
+    public void visitSwitchStatement(SwitchStatementTree tree) {
+      addComplexityWithNesting(tree.switchKeyword());
+      nestingLevel++;
+      super.visitSwitchStatement(tree);
+      nestingLevel--;
+    }
+
+    @Override
+    public void visitBinaryExpression(BinaryExpressionTree tree) {
+      if (tree.is(CONDITIONAL_AND, CONDITIONAL_OR)) {
+        JavaScriptTree javaScriptTree = (JavaScriptTree) tree;
+        boolean parentOfSameKind = javaScriptTree.getParent().is(javaScriptTree.getKind());
+        if (!parentOfSameKind) {
+          addComplexityWithoutNesting(tree.operator());
+        }
+      }
+
+      super.visitBinaryExpression(tree);
+    }
+
+    @Override
+    public void visitConditionalExpression(ConditionalExpressionTree tree) {
+      addComplexityWithNesting(tree.query());
+
+      visit(tree.condition());
+
+      visitWithNesting(tree.trueExpression());
+      visitWithNesting(tree.falseExpression());
+    }
+
+    @Override
+    public void visitBreakStatement(BreakStatementTree tree) {
+      visitJumpStatement(tree.breakKeyword(), tree.label());
+      super.visitBreakStatement(tree);
+    }
+
+    @Override
+    public void visitContinueStatement(ContinueStatementTree tree) {
+      visitJumpStatement(tree.continueKeyword(), tree.label());
+      super.visitContinueStatement(tree);
+    }
+
+    private void visitJumpStatement(SyntaxToken keyword, @Nullable IdentifierTree label) {
+      if (label != null) {
+        addComplexityWithoutNesting(keyword);
+      }
+    }
+
+    private void visit(Tree ... trees) {
+      for (Tree tree : trees) {
+        if (tree != null) {
+          tree.accept(this);
+        }
+      }
+    }
+
+    private void visitWithNesting(Tree tree) {
+      nestingLevel++;
+      tree.accept(this);
+      nestingLevel--;
+    }
+
+    private void addComplexityWithNesting(SyntaxToken secondaryLocationToken) {
+      if (functionStack.peek().equals(currentFunction)) {
+        functionContainsStructuralComplexity = true;
+      }
+      addComplexity(nestingLevel + 1, secondaryLocationToken);
+    }
+
+    private void addComplexityWithoutNesting(SyntaxToken secondaryLocationToken) {
+      addComplexity(1, secondaryLocationToken);
+    }
+
+    private void addComplexity(int addedComplexity, SyntaxToken secondaryLocationToken) {
+      if (functionStack.peek().equals(currentFunction)) {
+        ownComplexity += addedComplexity;
+      } else {
+        nestedFunctionComplexity += addedComplexity;
+      }
+
+      secondaryIssueLocations.add(new IssueLocation(secondaryLocationToken, secondaryMessage(addedComplexity)));
+    }
+
+    private void raiseIssue(int complexity) {
+      String message = String.format(MESSAGE, complexity, threshold);
+
+      SyntaxToken primaryLocation = ((JavaScriptTree) currentFunction).getFirstToken();
+      if (currentFunction.is(ARROW_FUNCTION)) {
+        primaryLocation = ((ArrowFunctionTree) currentFunction).doubleArrow();
+      }
+
+      PreciseIssue issue = addIssue(primaryLocation, message).cost(complexity - (double)threshold);
+
+      secondaryIssueLocations.forEach(issue::secondary);
+    }
+
+
+    private String secondaryMessage(int complexity) {
+      if (complexity == 1) {
+        return "+1";
+
+      } else{
+        return String.format("+%s (incl %s for nesting)", complexity, complexity - 1);
+      }
+    }
+
+    @Override
+    public void visitFunctionDeclaration(FunctionDeclarationTree tree) {
+      visitFunction(tree);
+      super.visitFunctionDeclaration(tree);
+      leaveFunction(tree);
+    }
+
+    @Override
+    public void visitArrowFunction(ArrowFunctionTree tree) {
+      visitFunction(tree);
+      super.visitArrowFunction(tree);
+      leaveFunction(tree);
+    }
+
+    @Override
+    public void visitFunctionExpression(FunctionExpressionTree tree) {
+      visitFunction(tree);
+      super.visitFunctionExpression(tree);
+      leaveFunction(tree);
+    }
+
+    @Override
+    public void visitMethodDeclaration(MethodDeclarationTree tree) {
+      visitFunction(tree);
+      super.visitMethodDeclaration(tree);
+      leaveFunction(tree);
+    }
+
+    private boolean isElseIf(IfStatementTree tree) {
+      return ((JavaScriptTree)tree).getParent().is(Kind.ELSE_CLAUSE);
+    }
+
+  }
+  public void setThreshold(int threshold) {
+    this.threshold = threshold;
+  }
+}
---
+++ javascript-checks/src/test/resources/checks/CognitiveComplexityFunctionCheck/zeroThreshold.js
@@ -0,0 +1,268 @@
+function zero_complexity(){
+}
+
+  function if_else_complexity() { // Noncompliant [[effortToFix=3;id=IF_ELSE]] {{Refactor this function to reduce its Cognitive Complexity from 3 to the 0 allowed.}}
+//^^^^^^^^
+    if (condition) {        // +1
+//S ^^ IF_ELSE {{+1}}
+    } else if (condition) { // +1
+//S        ^^ IF_ELSE {{+1}}
+    } else {                // +1
+//S   ^^^^ IF_ELSE {{+1}}
+    }
+}
+
+
+function else_nesting() {    // Noncompliant [[effortToFix=4]]
+    if (condition) {      // +1
+
+    } else {              // +1 (nesting level +1)
+        if (condition) {} // +2
+    }
+}
+
+
+function else_nested() {    // Noncompliant [[effortToFix=4]]
+    if (condition) {      // +1 (nesting level +1)
+
+        if (condition) {  // +2
+
+        } else {          // +1
+
+        }
+    }
+}
+
+function if_nested() { // Noncompliant [[effortToFix=6]]
+    if (condition)          // +1 (nesting level +1)
+        if (condition)      // +2 (nesting level +1)
+            if (condition) {// +3
+            }
+}
+
+function else_if_nesting() {    // Noncompliant [[effortToFix=4]]
+    if (condition) {         // +1
+
+    } else if (condition)    // +1 (nesting level +1)
+        if (condition) {     // +2
+        }
+}
+
+
+
+function loops_complexity() {    // Noncompliant [[effortToFix=15;id=LOOPS]]
+    while (condition) {             // +1 (nesting level +1)
+//S ^^^^^ LOOPS {{+1}}
+        if (condition) {            // +2
+//S     ^^ LOOPS
+        }
+    }
+
+    do {                            // +1 (nesting level +1)
+//S ^^ LOOPS {{+1}}
+        if (condition) {            // +2
+//S     ^^ LOOPS
+        }
+    } while (condition)
+
+    for (i = 0; i < length; i++) {  // +1 (nesting level +1)
+//S ^^^ LOOPS {{+1}}
+        if (condition) {            // +2
+//S     ^^ LOOPS
+        }
+    }
+
+    for (x in obj) {                // +1 (nesting level +1)
+//S ^^^ LOOPS {{+1}}
+        if (condition) {            // +2
+//S     ^^ LOOPS
+        }
+    }
+
+    for (x of obj) {                // +1 (nesting level +1)
+//S ^^^ LOOPS {{+1}}
+        if (condition) {            // +2
+//S     ^^ LOOPS
+        }
+    }
+}
+
+function switch_statement() {   // Noncompliant [[effortToFix=6;id=SWITCH]]
+    if (condition) {                 // +1 (nesting level +1)
+//S ^^ SWITCH
+
+    switch (expr) {                // +2 (nesting level +1)
+//S ^^^^^^ SWITCH {{+2 (incl 1 for nesting)}}
+       case "1":
+          if (condition) {}        // +3
+//S       ^^ SWITCH {{+3 (incl 2 for nesting)}}
+          break;
+       case "2":
+          break;
+       default:
+          foo();
+    }
+  }
+}
+
+function try_catch() {    // Noncompliant [[effortToFix=5;id=TRY_CATCH]]
+    try {
+        if (condition) { }   // +1
+//S     ^^ TRY_CATCH
+    } catch (someError) {    // +1 (nesting level +1)
+//S   ^^^^^ TRY_CATCH {{+1}}
+        if (condition)  { }  // +2
+//S     ^^ TRY_CATCH
+    } finally {
+        if (condition) { }   // +1
+//S     ^^ TRY_CATCH
+    }
+}
+
+function try_finally() {    // Noncompliant [[effortToFix=2]]
+    try {
+        if (condition) { } // +1
+
+    } finally {
+        if (condition) { } // +1
+    }
+}
+
+function jump_statements_no_complexity() {   // Noncompliant [[effortToFix=6]]
+    if (condition)         // +1
+        return;
+    else if (condition)    // +1
+        return 42;
+
+    label:
+    while (condition) {    // +1 (nesting level +1)
+        if (condition)     // +2
+            break;
+        else if (condition)// +1
+            continue;
+    }
+}
+
+function break_continue_with_label() {   // Noncompliant [[effortToFix=3;id=BREAK_CONTINUE]]
+    label:
+    while (condition) {
+//S ^^^^^ BREAK_CONTINUE
+        break label;   // +1
+//S     ^^^^^ BREAK_CONTINUE {{+1}}
+        continue label;// +1
+//S     ^^^^^^^^ BREAK_CONTINUE {{+1}}
+    }
+}
+
+// TODO recursion +1
+function recursion() {  // Noncompliant [[effortToFix=2]]
+    if (condition)
+        return 42;
+    else
+        return recursion();
+}
+
+function nesting_func_no_complexity() {  // Ok
+    function nested_func() { // Noncompliant [[effortToFix=1]]
+        if (condition) { }   // +1
+    }
+}
+
+function nesting_func_with_complexity() {  // Noncompliant [[effortToFix=3]]
+    if (condition) {}          // +1
+    function nested_func() {   // (nesting level +1)
+        if (condition) { }     // +2
+    }
+}
+
+function nesting_func_with_not_structural_complexity() {  // Noncompliant [[effortToFix=1]]
+    return a && b;
+    function nested_func() {   // Noncompliant [[effortToFix=1]]
+        if (condition) { }     // +1
+    }
+}
+
+function two_level_nesting() {
+    function nested1() {
+      function nested2() {   // Noncompliant [[effortToFix=1]]
+          if (condition) { }     // +1
+      }
+    }
+}
+
+function two_level_nesting_2() {
+    function nested1() {     // Noncompliant [[effortToFix=3]]
+      if (condition) {}      // +1
+      function nested2() {   // (nesting +1)
+          if (condition) { } // +2
+      }
+    }
+}
+
+function with_complexity_after_nested_function() { // Noncompliant [[effortToFix=3]]
+    function nested_func() {   // (nesting level +1)
+        if (condition) { }     // +2
+    }
+
+   if (condition) {}           // +1
+ }
+
+function and_or() {  // Noncompliant [[effortToFix=7;id=LOGICAL_OPS]]
+    foo(1 && 2 && 3 && 4); // +1
+//S                 ^^ LOGICAL_OPS {{+1}}
+
+    foo(1 || 2 || 3 || 4); // +1
+//S                 ^^ LOGICAL_OPS {{+1}}
+
+    foo(1 && 2
+//S       ^^ LOGICAL_OPS {{+1}}
+    || 3 || 4); // +2
+//S      ^^ LOGICAL_OPS {{+1}}
+
+
+    foo(1 && 2
+//S       ^^ LOGICAL_OPS {{+1}}
+          || 3
+//S       ^^ LOGICAL_OPS {{+1}}
+          && 4); // +3
+//S       ^^ LOGICAL_OPS {{+1}}
+}
+
+function conditional_expression() { // Noncompliant [[effortToFix=1;id=TERNARY]]
+    return condition ? trueValue : falseValue;
+//S                  ^ TERNARY {{+1}}
+}
+
+function nested_conditional_expression() { // Noncompliant [[effortToFix=6]]
+    x = condition1 ? (condition2 ? trueValue2 : falseValue2) : falseValue1 ; // +3
+    x = condition1 ? trueValue1 : (condition2 ? trueValue2 : falseValue2)  ; // +3
+}
+
+class A {
+    method() {  // Noncompliant [[effortToFix=1]]
+//  ^^^^^^
+        if (condition)  // +1
+            class B {
+            }
+    }
+}
+
+var arrowFunction = (a, b) => a && b; // Noncompliant [[effortToFix=1]]
+//                         ^^
+var functionExpression = function(a, b) { return a && b; } // Noncompliant [[effortToFix=1]]
+//                       ^^^^^^^^
+
+function complexity_in_conditions(a, b) { // Noncompliant [[effortToFix=11]]
+  if (a && b) {                      // +1(if) +1(&&)
+    a && b;                          // +1 (no nesting)
+  }
+
+  while (a && b) {                    // +1(while) +1(&&)
+  }
+
+  do {                                // +1
+  } while (a && b)                     // +1
+
+  for (var i = a && b; a && b; a && b) { // +1(for) +1(&&)  +1(&&)  +1(&&)
+  }
+}
