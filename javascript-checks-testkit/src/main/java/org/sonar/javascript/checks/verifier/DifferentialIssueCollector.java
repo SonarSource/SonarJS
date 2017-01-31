SymphonyOSF/sonar-javascript

SONARJS-885 Fast Ruling
The BulkTestOnSingleRule utility allows running a rule
on all ruling sample files.
The first time it is run it builds an expected file.
Afterwards it only shows differences introduced since
the first run.

javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/DifferentialIssueCollector.java
@@ -1,0 +1,53 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+
+public class DifferentialIssueCollector implements IssueCollector {
+
+  private final ExpectedIssues expectedIssues;
+  private final Consumer<String> output;
+
+  public DifferentialIssueCollector(ExpectedIssues expectedIssues, Consumer<String> output) {
+    this.expectedIssues = expectedIssues;
+    this.output = output;
+  }
+
+  @Override
+  public synchronized void writeIssues(Iterator<Issue> issues, File file) {
+    while (issues.hasNext()) {
+      Issue issue = issues.next();
+      IssueEntry entry = IssueEntry.from(issue, file);
+      if (expectedIssues.expects(entry)) {
+        continue;
+      }
+      output.accept(entry.createExcerpt());
+    }
+  }
+
+  @Override
+  public void writeSummary() {
+    output.accept(expectedIssues.printMissingExpectations());
+  }
+}

javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/ExpectedIssueCollector.java
@@ -1,0 +1,59 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Iterator;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+
+public class ExpectedIssueCollector implements IssueCollector {
+
+  private static final Logger LOGGER = Loggers.get(DifferentialIssueCollector.class);
+
+  private final FileWriter writer;
+  private final File expectedIssuesFile;
+  private int nbIssues = 0;
+  private int nbFiles = 0;
+
+  public ExpectedIssueCollector(File expectedIssuesFile) throws IOException {
+    this.writer = new FileWriter(expectedIssuesFile);
+    this.expectedIssuesFile = expectedIssuesFile;
+  }
+
+  @Override
+  public synchronized void writeIssues(Iterator<Issue> issues, File file) throws IOException {
+    nbFiles++;
+    while (issues.hasNext()) {
+      final Issue issue = issues.next();
+      nbIssues++;
+      writer.write(IssueEntry.from(issue, file).createExcerpt());
+    }
+  }
+
+  @Override
+  public void writeSummary() throws IOException {
+    writer.write("\n\nTotal number of issues: " + nbIssues + " (on " + nbFiles + " scanned files)\n");
+    LOGGER.info("Output dumped into file " + expectedIssuesFile.getAbsolutePath());
+  }
+}

javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/ExpectedIssues.java
@@ -1,0 +1,69 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ExpectedIssues {
+  private Map<String, Boolean> blacklist = new HashMap<>();
+
+  public static ExpectedIssues parse(File expectedIssuesFile) throws IOException {
+    final ExpectedIssues expectedIssues = new ExpectedIssues();
+    final Pattern pattern = Pattern.compile(".*" + IssueEntry.TAG_START + "(.*)" + IssueEntry.TAG_END + ".*");
+    if (expectedIssuesFile.exists()) {
+      Stream<String> lines = Files.lines(expectedIssuesFile.toPath());
+      lines.forEach(line -> {
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.matches()) {
+          expectedIssues.addExpectation(matcher.group(1));
+        }
+      });
+    } else {
+      expectedIssuesFile.createNewFile();
+    }
+    return expectedIssues;
+  }
+
+  final void addExpectation(String id) {
+    blacklist.put(id, false);
+  }
+
+  public boolean expects(IssueEntry issueEntry) {
+    if (blacklist.containsKey(issueEntry.toId())) {
+      blacklist.put(issueEntry.toId(), true);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String printMissingExpectations() {
+    final String missingExpectations = blacklist.entrySet().stream().filter(entry -> !entry.getValue()).map(Map.Entry::getKey).collect(Collectors.joining("\n"));
+    return "MISSING EXPECTATIONS : \n" + missingExpectations;
+  }
+}

javascript-checks-testkit/src/test/java/org/sonar/javascript/checks/verifier/DifferentialIssueCollectorTest.java
@@ -1,0 +1,80 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+import org.sonar.plugins.javascript.api.visitors.LineIssue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DifferentialIssueCollectorTest extends DoubleDispatchVisitorCheck {
+
+  private LineIssue issue1;
+  private LineIssue issue2;
+  private List<String> output;
+  private File file;
+  private ArrayList<Issue> issues;
+  private DifferentialIssueCollector collector;
+  private ExpectedIssues expectedIssues;
+
+  @Before
+  public void setUp() throws Exception {
+    file = new File("src/test/resources/bulk-verifier-test/bulk-file-2.js");
+    output = new ArrayList<>();
+    issues = new ArrayList<>();
+    issue1 = new LineIssue(this, 1, "an issue");
+    issue2 = new LineIssue(this, 2, "another issue");
+    expectedIssues = new ExpectedIssues();
+    collector = new DifferentialIssueCollector(expectedIssues, output::add);
+  }
+
+  @Test
+  public void logsUnexpectedIssue() throws Exception {
+    issues.add(issue1);
+    collector.writeIssues(issues.iterator(), file);
+    assertThat(output).contains(IssueEntry.from(issue1, file).createExcerpt());
+  }
+
+  @Test
+  public void skipsExpectedIssue() throws Exception {
+    issues.add(issue1);
+    issues.add(issue2);
+    expectedIssues.addExpectation(IssueEntry.from(issue1, file).toId());
+    collector.writeIssues(issues.iterator(), file);
+    assertThat(output).contains(IssueEntry.from(issue2, file).createExcerpt());
+    assertThat(output).doesNotContain(IssueEntry.from(issue1, file).createExcerpt());
+  }
+
+  @Test
+  public void reportsMissingExpectedIssue() throws Exception {
+    final IssueEntry expectedEntry = IssueEntry.from(issue1, file);
+    expectedIssues.addExpectation(expectedEntry.toId());
+    issues.add(issue2);
+    collector.writeIssues(issues.iterator(), file);
+    collector.writeSummary();
+    assertThat(output).contains(expectedIssues.printMissingExpectations());
+  }
+}

javascript-checks/src/test/java/org/sonar/javascript/checks/utils/BulkTestOnSingleRule.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube JavaScript Plugin
- * Copyright (C) 2011-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,34 +20,56 @@
 package org.sonar.javascript.checks.utils;
 
 import java.io.File;
+import java.io.IOException;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 import org.sonar.javascript.checks.verifier.BulkVerifier;
+import org.sonar.javascript.checks.verifier.DifferentialIssueCollector;
+import org.sonar.javascript.checks.verifier.ExpectedIssueCollector;
+import org.sonar.javascript.checks.verifier.ExpectedIssues;
+import org.sonar.javascript.checks.verifier.IssueCollector;
+import org.sonar.plugins.javascript.api.JavaScriptCheck;
 
 /**
  * Class to test one single rule on a set of JavaScript files.
  */
 public class BulkTestOnSingleRule {
-  
+
   private static final Logger LOGGER = Loggers.get(BulkTestOnSingleRule.class);
 
-  /**
-   * Run a specific rule.
-   * @param ruleClassName, e.g., "org.sonar.javascript.checks.ComparisonReturningFalseCheck"
-   * @param sourceDirectory. If not provided, the ruling directory is used
-   */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (args.length == 1) {
-      execute(args[0], "../its/sources/src");
-    } else if (args.length == 2) {
-      execute(args[0], args[1]);
+      execute(args[0], "javascript-checks-testkit/target/expected.txt", "its/sources/src");
+    } else if (args.length == 3) {
+      execute(args[0], args[1], args[2]);
     } else {
-      LOGGER.error("First argument = fully-qualified check class name. Second argument (optional) = JS files directory"); 
+      LOGGER.error("First argument = fully-qualified check class name. Second argument (optional) = ignored issues file. Second argument (optional) = JS files directory");
     }
   }
 
-  private static void execute(String checkClassName, String sourceDirectory) {
-    new BulkVerifier().scanDirectory(checkClassName, new File(sourceDirectory));
+  private static void execute(String checkClassName, String expectedIssuesFileName, String sourceDirectory) throws IOException {
+    final File expectedIssuesFile = new File(expectedIssuesFileName);
+    IssueCollector issueCollector;
+    if (expectedIssuesFile.exists()) {
+      issueCollector = new DifferentialIssueCollector(ExpectedIssues.parse(expectedIssuesFile), LOGGER::warn);
+    } else {
+      issueCollector = new ExpectedIssueCollector(expectedIssuesFile);
+    }
+    new BulkVerifier(getCheckClass(checkClassName), issueCollector).scanDirectory(new File(sourceDirectory));
+  }
+
+  private static Class<JavaScriptCheck> getCheckClass(String checkClassName) {
+    Class<?> clazz;
+    try {
+      clazz = Class.forName(checkClassName);
+    } catch (ClassNotFoundException e) {
+      throw new IllegalArgumentException("Exception when attempting to load : " + checkClassName, e);
+    }
+    if (JavaScriptCheck.class.isAssignableFrom(clazz)) {
+      return (Class<JavaScriptCheck>) clazz;
+    } else {
+      throw new IllegalArgumentException("Class " + checkClassName + " is not a JavaScriptCheck");
+    }
   }
 
 }

SonarSource/SonarJS

javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/DifferentialIssueCollector.java
@@ -1,0 +1,52 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.function.Consumer;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+
+public class DifferentialIssueCollector implements IssueCollector {
+
+  private final ExpectedIssues expectedIssues;
+  private final Consumer<String> output;
+
+  public DifferentialIssueCollector(ExpectedIssues expectedIssues, Consumer<String> output) {
+    this.expectedIssues = expectedIssues;
+    this.output = output;
+  }
+
+  @Override
+  public synchronized void writeIssues(Iterator<Issue> issues, File file) {
+    while (issues.hasNext()) {
+      Issue issue = issues.next();
+      IssueEntry entry = IssueEntry.from(issue, file);
+      if (!expectedIssues.expects(entry)) {
+        output.accept(entry.createExcerpt());
+      }
+    }
+  }
+
+  @Override
+  public void writeSummary() {
+    output.accept(expectedIssues.describeMissingExpectations());
+  }
+}

javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/ExpectedIssueCollector.java
@@ -1,0 +1,63 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Iterator;
+import org.sonar.api.utils.log.Logger;
+import org.sonar.api.utils.log.Loggers;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+
+public class ExpectedIssueCollector implements IssueCollector {
+
+  private static final Logger LOGGER = Loggers.get(DifferentialIssueCollector.class);
+
+  private final OutputStreamWriter writer;
+  private final File expectedIssuesFile;
+  private int nbIssues = 0;
+  private int nbFiles = 0;
+
+  public ExpectedIssueCollector(File expectedIssuesFile) throws IOException {
+    this.writer = new OutputStreamWriter(new FileOutputStream(expectedIssuesFile), "UTF-8");
+    this.expectedIssuesFile = expectedIssuesFile;
+  }
+
+  @Override
+  public synchronized void writeIssues(Iterator<Issue> issues, File file) throws IOException {
+    nbFiles++;
+    while (issues.hasNext()) {
+      final Issue issue = issues.next();
+      nbIssues++;
+      final String excerpt = IssueEntry.from(issue, file).createExcerpt();
+      writer.write(excerpt);
+      LOGGER.info(excerpt);
+    }
+  }
+
+  @Override
+  public void writeSummary() throws IOException {
+    writer.write("\n\nTotal number of issues: " + nbIssues + " (on " + nbFiles + " scanned files)\n");
+    LOGGER.info("Output dumped into file " + expectedIssuesFile.getAbsolutePath());
+    writer.close();
+  }
+}

javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/ExpectedIssues.java
@@ -1,0 +1,69 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class ExpectedIssues {
+  private Map<String, Boolean> expectedEntries = new HashMap<>();
+
+  public static ExpectedIssues parse(File expectedIssuesFile) throws IOException {
+    final ExpectedIssues expectedIssues = new ExpectedIssues();
+    final Pattern pattern = Pattern.compile(".*" + IssueEntry.TAG_START + "(.*)" + IssueEntry.TAG_END + ".*");
+    if (expectedIssuesFile.exists()) {
+      Stream<String> lines = Files.lines(expectedIssuesFile.toPath());
+      lines.forEach(line -> {
+        Matcher matcher = pattern.matcher(line);
+        if (matcher.matches()) {
+          expectedIssues.addExpectation(matcher.group(1));
+        }
+      });
+    } else {
+      expectedIssuesFile.createNewFile();
+    }
+    return expectedIssues;
+  }
+
+  final void addExpectation(String id) {
+    expectedEntries.put(id, false);
+  }
+
+  public boolean expects(IssueEntry issueEntry) {
+    if (expectedEntries.containsKey(issueEntry.toId())) {
+      expectedEntries.put(issueEntry.toId(), true);
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+  public String describeMissingExpectations() {
+    final String missingExpectations = expectedEntries.entrySet().stream().filter(entry -> !entry.getValue()).map(Map.Entry::getKey).collect(Collectors.joining("\n"));
+    return "MISSING EXPECTATIONS : \n" + missingExpectations;
+  }
+}

javascript-checks-testkit/src/test/java/org/sonar/javascript/checks/verifier/DifferentialIssueCollectorTest.java
@@ -1,0 +1,80 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.javascript.checks.verifier;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import org.junit.Before;
+import org.junit.Test;
+import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
+import org.sonar.plugins.javascript.api.visitors.Issue;
+import org.sonar.plugins.javascript.api.visitors.LineIssue;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DifferentialIssueCollectorTest extends DoubleDispatchVisitorCheck {
+
+  private LineIssue issue1;
+  private LineIssue issue2;
+  private List<String> output;
+  private File file;
+  private ArrayList<Issue> issues;
+  private DifferentialIssueCollector collector;
+  private ExpectedIssues expectedIssues;
+
+  @Before
+  public void setUp() throws Exception {
+    file = new File("src/test/resources/bulk-verifier-test/bulk-file-2.js");
+    output = new ArrayList<>();
+    issues = new ArrayList<>();
+    issue1 = new LineIssue(this, 1, "an issue");
+    issue2 = new LineIssue(this, 2, "another issue");
+    expectedIssues = new ExpectedIssues();
+    collector = new DifferentialIssueCollector(expectedIssues, output::add);
+  }
+
+  @Test
+  public void logsUnexpectedIssue() throws Exception {
+    issues.add(issue1);
+    collector.writeIssues(issues.iterator(), file);
+    assertThat(output).contains(IssueEntry.from(issue1, file).createExcerpt());
+  }
+
+  @Test
+  public void skipsExpectedIssue() throws Exception {
+    issues.add(issue1);
+    issues.add(issue2);
+    expectedIssues.addExpectation(IssueEntry.from(issue1, file).toId());
+    collector.writeIssues(issues.iterator(), file);
+    assertThat(output).contains(IssueEntry.from(issue2, file).createExcerpt());
+    assertThat(output).doesNotContain(IssueEntry.from(issue1, file).createExcerpt());
+  }
+
+  @Test
+  public void reportsMissingExpectedIssue() throws Exception {
+    final IssueEntry expectedEntry = IssueEntry.from(issue1, file);
+    expectedIssues.addExpectation(expectedEntry.toId());
+    issues.add(issue2);
+    collector.writeIssues(issues.iterator(), file);
+    collector.writeSummary();
+    assertThat(output).contains(expectedIssues.describeMissingExpectations());
+  }
+}

javascript-checks/src/test/java/org/sonar/javascript/checks/utils/BulkTestOnSingleRule.java
@@ -1,7 +1,7 @@
 /*
  * SonarQube JavaScript Plugin
- * Copyright (C) 2011-2016 SonarSource SA
- * mailto:contact AT sonarsource DOT com
+ * Copyright (C) 2011-2017 SonarSource SA
+ * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -20,34 +20,56 @@
 package org.sonar.javascript.checks.utils;
 
 import java.io.File;
+import java.io.IOException;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
 import org.sonar.javascript.checks.verifier.BulkVerifier;
+import org.sonar.javascript.checks.verifier.DifferentialIssueCollector;
+import org.sonar.javascript.checks.verifier.ExpectedIssueCollector;
+import org.sonar.javascript.checks.verifier.ExpectedIssues;
+import org.sonar.javascript.checks.verifier.IssueCollector;
+import org.sonar.plugins.javascript.api.JavaScriptCheck;
 
 /**
  * Class to test one single rule on a set of JavaScript files.
  */
 public class BulkTestOnSingleRule {
-  
+
   private static final Logger LOGGER = Loggers.get(BulkTestOnSingleRule.class);
 
-  /**
-   * Run a specific rule.
-   * @param ruleClassName, e.g., "org.sonar.javascript.checks.ComparisonReturningFalseCheck"
-   * @param sourceDirectory. If not provided, the ruling directory is used
-   */
-  public static void main(String[] args) {
+  public static void main(String[] args) throws IOException {
     if (args.length == 1) {
-      execute(args[0], "../its/sources/src");
-    } else if (args.length == 2) {
-      execute(args[0], args[1]);
+      execute(args[0], "javascript-checks-testkit/target/expected.txt", "its/sources/src");
+    } else if (args.length == 3) {
+      execute(args[0], args[1], args[2]);
     } else {
-      LOGGER.error("First argument = fully-qualified check class name. Second argument (optional) = JS files directory"); 
+      LOGGER.error("First argument = fully-qualified check class name. Second argument (optional) = ignored issues file. Third argument (optional) = JS files directory");
     }
   }
 
-  private static void execute(String checkClassName, String sourceDirectory) {
-    new BulkVerifier().scanDirectory(checkClassName, new File(sourceDirectory));
+  private static void execute(String checkClassName, String expectedIssuesFileName, String sourceDirectory) throws IOException {
+    final File expectedIssuesFile = new File(expectedIssuesFileName);
+    IssueCollector issueCollector;
+    if (expectedIssuesFile.exists()) {
+      issueCollector = new DifferentialIssueCollector(ExpectedIssues.parse(expectedIssuesFile), LOGGER::warn);
+    } else {
+      issueCollector = new ExpectedIssueCollector(expectedIssuesFile);
+    }
+    new BulkVerifier(getCheckClass(checkClassName), issueCollector).scanDirectory(new File(sourceDirectory));
+  }
+
+  private static Class<JavaScriptCheck> getCheckClass(String checkClassName) {
+    Class<?> clazz;
+    try {
+      clazz = Class.forName(checkClassName);
+    } catch (ClassNotFoundException e) {
+      throw new IllegalArgumentException("Exception when attempting to load : " + checkClassName, e);
+    }
+    if (JavaScriptCheck.class.isAssignableFrom(clazz)) {
+      return (Class<JavaScriptCheck>) clazz;
+    } else {
+      throw new IllegalArgumentException("Class " + checkClassName + " is not a JavaScriptCheck");
+    }
   }
 
 }