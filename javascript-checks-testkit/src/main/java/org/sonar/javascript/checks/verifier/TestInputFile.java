159f5dd3c0ef174dd90e88b1913c4aaa8d60bb3e
Janos Gyerik <janos.gyerik@sonarsource.com>
2017-01-31 16:44:24 +0100

    Remove unused constructor

--- javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/TestInputFile.java
+++ javascript-checks-testkit/src/main/java/org/sonar/javascript/checks/verifier/TestInputFile.java
@@ -20,29 +20,11 @@
 package org.sonar.javascript.checks.verifier;
 
 import java.io.File;
-import java.io.IOException;
-import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import org.sonar.api.batch.fs.internal.DefaultInputFile;
-import org.sonar.api.batch.fs.internal.FileMetadata;
-
-import com.google.common.base.Throwables;
 
 public class TestInputFile extends DefaultInputFile {
-  public TestInputFile(File file, String contents, Charset encoding) {
-    this("", file.getAbsolutePath());
-    this.setCharset(encoding);
-    try {
-      Files.write(file.toPath(), contents.getBytes(encoding));
-      this.initMetadata(new FileMetadata().readMetadata(file, encoding));
-    } catch (IOException e) {
-      throw Throwables.propagate(e);
-    }
-  }
-
   public TestInputFile(String relativePath) {
     this("", relativePath);
   }
