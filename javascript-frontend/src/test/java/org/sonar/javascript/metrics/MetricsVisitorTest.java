hetzijzo/sonar-javascript

Get file content through InputFile

javascript-frontend/src/test/java/org/sonar/javascript/metrics/MetricsVisitorTest.java
@@ -59,7 +59,6 @@ public class MetricsVisitorTest extends JavaScriptTreeModelTest {
     HashMap<InputFile, Set<Integer>> projectLinesOfCode = new HashMap<>();
 
     MetricsVisitor metricsVisitor = new MetricsVisitor(
-      context.fileSystem(),
       context,
       mock(NoSonarFilter.class),
       false,
@@ -67,7 +66,7 @@ public class MetricsVisitorTest extends JavaScriptTreeModelTest {
       projectLinesOfCode);
 
     TreeVisitorContext treeVisitorContext = mock(TreeVisitorContext.class);
-    when(treeVisitorContext.getFile()).thenReturn(inputFile.file());
+    when(treeVisitorContext.getFile()).thenReturn(inputFile);
     when(treeVisitorContext.getTopTree()).thenReturn((ScriptTree) p.parse(inputFile.file()));
 
     metricsVisitor.scanTree(treeVisitorContext);

SonarSource/SonarJS

javascript-frontend/src/test/java/org/sonar/javascript/metrics/MetricsVisitorTest.java
@@ -85,7 +85,7 @@ public class MetricsVisitorTest extends JavaScriptTreeModelTest {
       saveExecutableLines);
 
     TreeVisitorContext treeVisitorContext = mock(TreeVisitorContext.class);
-    when(treeVisitorContext.getFile()).thenReturn(INPUT_FILE.file());
+    when(treeVisitorContext.getFile()).thenReturn(INPUT_FILE);
     when(treeVisitorContext.getTopTree()).thenReturn((ScriptTree) p.parse(INPUT_FILE.file()));
 
     metricsVisitor.scanTree(treeVisitorContext);