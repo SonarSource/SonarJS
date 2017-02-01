3403271d77f0ceaf7052b8d32445520fc2fabf85
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-02-01 16:54:27 +0100

    SONARJS-900 Add the rule to ruling IT

--- its/ruling/src/test/java/org/sonar/javascript/it/JavaScriptTest.java
+++ its/ruling/src/test/java/org/sonar/javascript/it/JavaScriptTest.java
@@ -19,20 +19,28 @@
  */
 package org.sonar.javascript.it;
 
+import com.google.common.collect.ImmutableMap;
 import com.google.common.io.Files;
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.SonarScanner;
 import com.sonar.orchestrator.locator.FileLocation;
 import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.sonar.wsclient.SonarClient;
 
 import static junit.framework.TestCase.assertTrue;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaScriptTest {
+
+  private static final Logger LOG = LoggerFactory.getLogger(JavaScriptTest.class);
 
   @ClassRule
   public static Orchestrator orchestrator = Orchestrator.builderEnv()
@@ -55,7 +63,7 @@ public class JavaScriptTest {
     File litsDifferencesFile = FileLocation.of("target/differences").getFile();
     orchestrator.getServer().provisionProject("project", "project");
     orchestrator.getServer().associateProjectToQualityProfile("project", "js", "rules");
-    SonarScanner build = (SonarScanner) SonarScanner.create(FileLocation.of("../sources/src").getFile())
+    SonarScanner build = SonarScanner.create(FileLocation.of("../sources/src").getFile())
       .setProjectKey("project")
       .setProjectName("project")
       .setProjectVersion("1")
@@ -69,9 +77,39 @@ public class JavaScriptTest {
       .setProperty("lits.differences", litsDifferencesFile.getAbsolutePath())
       .setProperty("sonar.cpd.skip", "true")
       .setEnvironmentVariable("SONAR_RUNNER_OPTS", "-Xmx1024m");
+
+    instantiateTemplateRule("CommentRegularExpression", "CommentRegexTest", "regularExpression=\"(?i).*TODO.*\";message=\"bad user\"");
+
     orchestrator.executeBuild(build);
 
     assertThat(Files.toString(litsDifferencesFile, StandardCharsets.UTF_8)).isEmpty();
+  }
+
+  private static void instantiateTemplateRule(String ruleTemplateKey, String instantiationKey, String params) {
+    SonarClient sonarClient = orchestrator.getServer().adminWsClient();
+    sonarClient.post("/api/rules/create", ImmutableMap.<String, Object>builder()
+      .put("name", instantiationKey)
+      .put("markdown_description", instantiationKey)
+      .put("severity", "INFO")
+      .put("status", "READY")
+      .put("template_key", "javascript:" + ruleTemplateKey)
+      .put("custom_key", instantiationKey)
+      .put("prevent_reactivation", "true")
+      .put("params", "name=\"" + instantiationKey + "\";key=\"" + instantiationKey + "\";markdown_description=\"" + instantiationKey + "\";" + params)
+      .build());
+    String post = sonarClient.get("api/rules/app");
+    Pattern pattern = Pattern.compile("js-rules-\\d+");
+    Matcher matcher = pattern.matcher(post);
+    if (matcher.find()) {
+      String profilekey = matcher.group();
+      sonarClient.post("api/qualityprofiles/activate_rule", ImmutableMap.<String, Object>of(
+        "profile_key", profilekey,
+        "rule_key", "javascript:" + instantiationKey,
+        "severity", "INFO",
+        "params", ""));
+    } else {
+      LOG.error("Could not retrieve profile key : Template rule " + ruleTemplateKey + " has not been activated");
+    }
   }
 
 }
