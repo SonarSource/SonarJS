3c6453d08e7731035c5b95621b5d7cb5b6eb297b
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-02-16 09:43:55 +0100

    Update IT perf expectations for SE
    After SONARJS-902

--- its/performancing/src/test/java/JavaScriptPerformanceTest.java
+++ its/performancing/src/test/java/JavaScriptPerformanceTest.java
@@ -52,7 +52,7 @@ public class JavaScriptPerformanceTest {
 
   @Test
   public void test_symbolic_engine_performance() throws IOException {
-    test_performance("se-project", "se-profile", true, 179.0);
+    test_performance("se-project", "se-profile", true, 185.0);
   }
 
   private static void test_performance(String projectKey, String profile, boolean shouldRaiseAnyIssue, double expectedTime) throws IOException {
