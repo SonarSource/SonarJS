e8a2512c12e6bf5ee8bf3e3044c909243a2c934f
Elena Vilchik <vilchik.elena@gmail.com>
2017-02-17 18:32:04 +0100

    SONARJS-909 Extend CommaOperatorInSwitchCaseCheck (S3616): cover 'or' (#488)

--- javascript-checks/src/test/resources/checks/commaOperatorInSwitchCase.js
+++ javascript-checks/src/test/resources/checks/commaOperatorInSwitchCase.js
@@ -1,4 +1,4 @@
-function foo() {
+function commaOperator() {
   switch (a) {
     case 0:         // OK
     case 1:         // OK
@@ -14,6 +14,28 @@ function foo() {
       break;
     case bar(), baz() : // Noncompliant {{Explicitly specify 2 separate cases that fall through; currently this case clause only works for "baz()".}}
 //       ^^^^^^^^^^^^
+      foo();
+      break;
+    default:
+      foo4();
+  }
+}
+
+function orOperator() {
+  switch (a) {
+    case 0:         // OK
+    case 1 && 2:    // OK
+      foo1();
+      break;
+    case 2 || 3:       // Noncompliant {{Explicitly specify 2 separate cases that fall through; currently this case clause only works for "2".}}
+//       ^^^^^^
+      foo2();
+      break;
+    case "a" || "b" || "c" || "d": // Noncompliant {{Explicitly specify 4 separate cases that fall through; currently this case clause only works for ""a"".}}
+//       ^^^^^^^^^^^^^^^^^^^^^^^^
+      foo3();
+      break;
+    case bar() || baz() : // OK, not literals
       foo();
       break;
     default:
