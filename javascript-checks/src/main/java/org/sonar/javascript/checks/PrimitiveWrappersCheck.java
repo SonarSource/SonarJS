5ea308d3cceeb7293f12a2c2f835c4860b803206
Elena Vilchik <vilchik.elena@gmail.com>
2017-03-22 18:24:40 +0100

    SONARJS-947 Improve PrimitiveWrappersCheck: ignore when argument of same type (#514)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/PrimitiveWrappersCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/PrimitiveWrappersCheck.java
@@ -19,10 +19,16 @@
  */
 package org.sonar.javascript.checks;
 
-import com.google.common.collect.ImmutableSet;
-import java.util.Set;
+import com.google.common.collect.ImmutableMap;
+import java.util.Map;
+import javax.annotation.Nullable;
 import org.sonar.check.Rule;
-import org.sonar.javascript.checks.utils.CheckUtils;
+import org.sonar.plugins.javascript.api.tree.Tree;
+import org.sonar.plugins.javascript.api.tree.Tree.Kind;
+import org.sonar.plugins.javascript.api.tree.declaration.ParameterListTree;
+import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
+import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
+import org.sonar.plugins.javascript.api.tree.expression.LiteralTree;
 import org.sonar.plugins.javascript.api.tree.expression.NewExpressionTree;
 import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
 
@@ -30,15 +36,38 @@ import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
 public class PrimitiveWrappersCheck extends DoubleDispatchVisitorCheck {
 
   private static final String MESSAGE = "Use a literal value for this.";
-  private static final Set<String> WRAPPERS = ImmutableSet.of("Boolean", "Number", "String");
+
+  private static final Map<String, Kind> ALLOWED_ARGUMENT_PER_WRAPPER = ImmutableMap.of(
+    "Boolean", Kind.BOOLEAN_LITERAL,
+    "Number", Kind.NUMERIC_LITERAL,
+    "String", Kind.STRING_LITERAL
+  );
 
   @Override
   public void visitNewExpression(NewExpressionTree tree) {
-    if (WRAPPERS.contains(CheckUtils.asString(tree.expression()))) {
-      addIssue(tree, MESSAGE);
+    ExpressionTree constructor = tree.expression();
+    ParameterListTree arguments = tree.arguments();
+
+    if (constructor.is(Kind.IDENTIFIER_REFERENCE)) {
+      Kind allowedArgument = ALLOWED_ARGUMENT_PER_WRAPPER.get(((IdentifierTree) constructor).name());
+
+      if (allowedArgument != null && !isAllowedUsage(arguments, allowedArgument)) {
+        addIssue(tree, MESSAGE);
+      }
     }
 
     super.visitNewExpression(tree);
+  }
+
+  private static boolean isAllowedUsage(@Nullable ParameterListTree arguments, Kind allowedArgument) {
+    if (arguments != null && arguments.parameters().size() == 1) {
+      Tree argument = arguments.parameters().get(0);
+      if (argument.is(allowedArgument)) {
+        return !"false".equals(((LiteralTree) argument).value());
+      }
+    }
+
+    return false;
   }
 
 }
--- javascript-checks/src/test/resources/checks/primitiveWrappers.js
+++ javascript-checks/src/test/resources/checks/primitiveWrappers.js
@@ -1,17 +1,31 @@
-function sayHello() {
-  var x = new Boolean(false); // Noncompliant {{Use a literal value for this.}}
-//        ^^^^^^^^^^^^^^^^^^
-  if (x) {
-    alert('hi');  // Shows 'hi'.
-  }
+function sayHello(y) {
+  var x = new Boolean(y); // Noncompliant {{Use a literal value for this.}}
+//        ^^^^^^^^^^^^^^
 
-  var x = Boolean(0); // OK
-  if (x) {
-    alert('hi');  // This will never be alerted.
-  }
+  x = new Number; // Noncompliant
+  x = new Number(y); // Noncompliant
+  x = new Number(true); // Noncompliant
+  x = new Number(false); // Noncompliant
 
-  new Number(1); // Noncompliant
-  new String('2'); // Noncompliant
+  x = new String(y); // Noncompliant
+  x = new String(42); // Noncompliant
+  x = new String(); // Noncompliant
+  x = new String("str", 42); // Noncompliant
 
-  new MyObject(); // OK
+  // not primitive wrapper constructors
+  x = new Array();
+  x = new MyObject();
+  x = new Foo.MyObject();
+  x = new MyObject;
+
+  // OK with literals of the same type, except "new Boolean(false)"
+  x = new Boolean(true);
+  x = new Boolean(false); // Noncompliant
+  x = new String("");
+  x = new Number(0);
+
+  // OK without "new"
+  x = Boolean(y);
+  x = Number(y);
+  x = String(y);
 }
