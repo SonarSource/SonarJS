53c3f24165f2ab98c9ec8daec2b5f347d3b47394
Elena Vilchik <vilchik.elena@gmail.com>
2017-03-31 15:19:51 +0200

    SONARJS-966 Improve SelfAssignmentCheck: ignore properties (#540)

--- javascript-checks/src/test/resources/checks/selfAssignment.js
+++ javascript-checks/src/test/resources/checks/selfAssignment.js
@@ -1,8 +1,10 @@
 x = 1;
 x = y;
-x = x; // Noncompliant {{Remove or correct this useless self-assignment.}}
+  x = x; // Noncompliant {{Remove or correct this useless self-assignment.}}
+//^^^^^
 x = this.x;
-  this.x = this.x; // Noncompliant
-//^^^^^^^^^^^^^^^
+this.x = this.x; // OK
+a.b.x = a.b.x; // OK
+x[i] = x[i]; // OK
 x += x;
 x = x + x;
