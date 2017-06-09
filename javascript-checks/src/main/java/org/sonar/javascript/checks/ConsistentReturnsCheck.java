a0a76ed4f772e4580780091cc152641cbee68265
Elena Vilchik <vilchik.elena@gmail.com>
2017-06-09 18:08:04 +0200

    Rule: Method returns should not be invariant (#598)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/ConsistentReturnsCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/ConsistentReturnsCheck.java
@@ -20,12 +20,9 @@
 package org.sonar.javascript.checks;
 
 import com.google.common.collect.ImmutableSet;
-import java.util.HashSet;
 import java.util.Set;
 import org.sonar.check.Rule;
-import org.sonar.javascript.cfg.CfgBlock;
-import org.sonar.javascript.cfg.CfgBranchingBlock;
-import org.sonar.javascript.cfg.ControlFlowGraph;
+import org.sonar.javascript.checks.utils.FunctionReturns;
 import org.sonar.javascript.tree.KindSet;
 import org.sonar.plugins.javascript.api.tree.Tree;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
@@ -36,6 +33,8 @@ import org.sonar.plugins.javascript.api.tree.statement.BlockTree;
 import org.sonar.plugins.javascript.api.tree.statement.ReturnStatementTree;
 import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
 import org.sonar.plugins.javascript.api.visitors.SubscriptionVisitorCheck;
+
+import static org.sonar.javascript.checks.utils.FunctionReturns.getFunctionReturns;
 
 @Rule(key = "S3801")
 public class ConsistentReturnsCheck extends SubscriptionVisitorCheck {
@@ -55,7 +54,7 @@ public class ConsistentReturnsCheck extends SubscriptionVisitorCheck {
       BlockTree body = (BlockTree) functionTree.body();
       FunctionReturns functionReturns = getFunctionReturns(body);
 
-      if (functionReturns.containsReturnWithoutValue && functionReturns.containsReturnWithValue) {
+      if (functionReturns.containsReturnWithoutValue() && functionReturns.containsReturnWithValue()) {
         raiseIssue(tree, functionReturns, body);
       }
     }
@@ -68,72 +67,13 @@ public class ConsistentReturnsCheck extends SubscriptionVisitorCheck {
     }
 
     PreciseIssue issue = addIssue(tokenToRaiseIssue, MESSAGE);
-    for (ReturnStatementTree returnStatement : functionReturns.returnStatements) {
+    for (ReturnStatementTree returnStatement : functionReturns.returnStatements()) {
       issue.secondary(returnStatement.returnKeyword(), returnStatement.expression() == null ? "Return without value" : "Return with value");
     }
 
-    if (functionReturns.containsImplicitReturn) {
+    if (functionReturns.containsImplicitReturn()) {
       issue.secondary(body.closeCurlyBraceToken(), "Implicit return without value");
     }
-  }
-
-  private static FunctionReturns getFunctionReturns(BlockTree functionBody) {
-    FunctionReturns functionReturns = new FunctionReturns();
-
-    ControlFlowGraph cfg = ControlFlowGraph.build(functionBody);
-    if (containsTry(cfg)) {
-      return functionReturns;
-    }
-
-    CfgBlock endBlock = cfg.end();
-
-    // Possible predecessors for end block:
-    // * return statement -> check for expression
-    // * last statement in function -> implicit return without value
-    // * throw statement -> ignore in the scope of this rule
-    for (CfgBlock cfgBlock : endBlock.predecessors()) {
-      Tree lastElement = cfgBlock.elements().get(cfgBlock.elements().size() - 1);
-      if (lastElement.is(Kind.RETURN_STATEMENT)) {
-        ReturnStatementTree returnStatement = (ReturnStatementTree) lastElement;
-        if (returnStatement.expression() == null) {
-          functionReturns.containsReturnWithoutValue = true;
-        } else {
-          functionReturns.containsReturnWithValue = true;
-        }
-
-        functionReturns.returnStatements.add(returnStatement);
-
-      } else if (!isThrowStatement(lastElement) && isReachableBlock(cfgBlock, cfg)) {
-        functionReturns.containsReturnWithoutValue = true;
-        functionReturns.containsImplicitReturn = true;
-      }
-    }
-
-    return functionReturns;
-  }
-
-  private static boolean containsTry(ControlFlowGraph cfg) {
-    for (CfgBlock cfgBlock : cfg.blocks()) {
-      if (cfgBlock instanceof CfgBranchingBlock && ((CfgBranchingBlock) cfgBlock).branchingTree().is(Kind.TRY_STATEMENT)) {
-        return true;
-      }
-    }
-    return false;
-  }
-
-  private static boolean isThrowStatement(Tree lastElement) {
-    return lastElement.parent().is(Kind.THROW_STATEMENT);
-  }
-
-  private static boolean isReachableBlock(CfgBlock cfgBlock, ControlFlowGraph cfg) {
-    return !cfgBlock.predecessors().isEmpty() || cfgBlock.equals(cfg.start());
-  }
-
-  private static class FunctionReturns {
-    boolean containsReturnWithValue = false;
-    boolean containsReturnWithoutValue = false;
-    boolean containsImplicitReturn = false;
-    Set<ReturnStatementTree> returnStatements = new HashSet<>();
   }
 
 }
