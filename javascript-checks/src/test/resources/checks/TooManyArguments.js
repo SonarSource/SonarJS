b1f63d2cd4258f95e469ae699d6fb6cea113371f
Elena Vilchik <vilchik.elena@gmail.com>
2017-06-09 17:51:16 +0200

    Update message TooManyArgumentsCheck: include declaration line number (#602)
    Fixes #569

--- javascript-checks/src/test/resources/checks/TooManyArguments.js
+++ javascript-checks/src/test/resources/checks/TooManyArguments.js
@@ -1,15 +1,15 @@
 function foo1(p1, p2){}
 
-foo1(1, 2, 3);  // Noncompliant [[sc=5;ec=14;el=+0;secondary=-2]] {{"foo1" expects 2 arguments, but 3 were provided.}}
+foo1(1, 2, 3);  // Noncompliant [[sc=5;ec=14;el=+0;secondary=-2]] {{"foo1" declared at line 1 expects 2 arguments, but 3 were provided.}}
 
 function foo2(){
-  foo1(1, 2, 3, 4);  // Noncompliant {{"foo1" expects 2 arguments, but 4 were provided.}}
+  foo1(1, 2, 3, 4);  // Noncompliant {{"foo1" declared at line 1 expects 2 arguments, but 4 were provided.}}
   foo1(1, 2);
   foo1(1);
 
   var foo3 = function(){}
 
-  foo3(1)  // Noncompliant {{"foo3" expects 0 arguments, but 1 was provided.}}
+  foo3(1)  // Noncompliant {{"foo3" declared at line 10 expects 0 arguments, but 1 was provided.}}
 }
 
 function foo4(){
@@ -43,12 +43,12 @@ function foo7(arguments){
   console.log(arguments)
 }
 
-foo7(1, 2); // Noncompliant {{"foo7" expects 1 argument, but 2 were provided.}}
+foo7(1, 2); // Noncompliant {{"foo7" declared at line 42 expects 1 argument, but 2 were provided.}}
 
 (function(p1, p2){
   doSomething1();
   doSomething2();
-})(1, 2, 3);     // Noncompliant {{This function expects 2 arguments, but 3 were provided.}}
+})(1, 2, 3);     // Noncompliant
 
 x=function(a,b){
   return a + b;
--- javascript-checks/src/test/resources/checks/TooManyArguments2.js
+++ javascript-checks/src/test/resources/checks/TooManyArguments2.js
@@ -1,6 +1,6 @@
 function bar(p1, p2){}
 
-bar(1, 2, 3);                          // Noncompliant [[sc=4;ec=13;el=+0;secondary=-2]] {{"bar" expects 2 arguments, but 3 were provided.}}
+bar(1, 2, 3);                          // Noncompliant [[sc=4;ec=13;el=+0;secondary=-2]] {{"bar" declared at line 1 expects 2 arguments, but 3 were provided.}}
 
 
 function builtIn_StringFunctions() {
