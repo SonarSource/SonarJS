5885226c230496105949f6085d0ba28cd1615721
David Rautureau <david.rautureau@sonarsource.com>
2017-10-09 15:58:25 +0200

    WIP use Java 9 for integration tests

--- cix.sh
+++ cix.sh
@@ -2,6 +2,9 @@
 set -euo pipefail
 echo "Running $TEST with SQ=$SQ_VERSION"
 
+export JAVA_HOME=/opt/sonarsource/jvm/java-1.9.0-sun-x64
+export PATH=$JAVA_HOME/bin:$PATH
+
 case "$TEST" in
   plugin|ruling|type-inference|performancing)
 
