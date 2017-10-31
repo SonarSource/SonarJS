b7cb5c807e7d7dd7e5e0fb28c6730b54f7d56956
Elena Vilchik <elena.vilchik@sonarsource.com>
2017-10-31 11:19:41 +0100

    Add rule: Conditionals should start on new lines (#810)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/CheckList.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/CheckList.java
@@ -167,6 +167,7 @@ public final class CheckList {
       ReturnInSetterCheck.class,
       ReturnOfBooleanExpressionCheck.class,
       ReturnValueNotIgnoredCheck.class,
+      SameLineConditionalCheck.class,
       SelectionTestedWithoutLengthCheck.class,
       SelfAssignmentCheck.class,
       SemicolonCheck.class,
