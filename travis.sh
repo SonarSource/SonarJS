690e4ac18a929c1aa7254abbc39da4d907430b31
Carlo Bottiglieri <carlo.bottiglieri@gmail.com>
2017-12-14 10:58:55 +0100

    Fix travis build by forcing maven version

--- travis.sh
+++ travis.sh
@@ -4,10 +4,11 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v39 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v42 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
+. ~/.local/bin/installMaven35
 
 export DEPLOY_PULL_REQUEST=true
 regular_mvn_build_deploy_analyze
