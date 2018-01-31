95d289929eb948dc67407a73a3ec5ca0fedc4c23
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-01-31 12:13:59 +0100

    feedback round2

--- javascript-checks/src/test/resources/checks/className.js
+++ javascript-checks/src/test/resources/checks/className.js
@@ -1,5 +1,6 @@
 
-class my_class { // Noncompliant [[sc=7;ec=15]] {{Rename class "my_class" to match the regular expression ^[A-Z][a-zA-Z0-9]*$.}}
+class my_class { // Noncompliant {{Rename class "my_class" to match the regular expression ^[A-Z][a-zA-Z0-9]*$.}}
+//    ^^^^^^^^
 
 }
 
@@ -7,5 +8,5 @@ class MyClass {
 
 }
 
-var x = class y { // Compliant rule doesn't check class expressions
+var x = class y { // Compliant, rule doesn't check class expressions
 }
--- javascript-checks/src/test/resources/checks/classNameCustomFormat.js
+++ javascript-checks/src/test/resources/checks/classNameCustomFormat.js
@@ -3,7 +3,8 @@ class _MyClass { // Compliant
 
 }
 
-class __MyClass { // Noncompliant
+class __MyClass { // Noncompliant {{Rename class "__MyClass" to match the regular expression ^[_A-Z][a-zA-Z0-9]*$.}}
+//    ^^^^^^^^^
 
 }
 
