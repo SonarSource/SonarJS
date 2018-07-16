11f9f3e2397eb06a82a8690dd85888e860f6e415
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2018-07-16 16:21:22 +0200

    Use analyzer-commons to load rule metadata

--- sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/rules/JavaScriptRulesDefinition.java
+++ sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/rules/JavaScriptRulesDefinition.java
@@ -19,27 +19,15 @@
  */
 package org.sonar.plugins.javascript.rules;
 
-import com.google.common.base.Charsets;
-import com.google.common.io.Resources;
-import com.google.gson.Gson;
-import java.io.IOException;
-import java.net.URL;
-import java.util.Locale;
-import java.util.Set;
-import javax.annotation.Nullable;
-import org.sonar.api.rule.RuleStatus;
-import org.sonar.api.rules.RuleType;
-import org.sonar.api.server.debt.DebtRemediationFunction;
 import org.sonar.api.server.rule.RulesDefinition;
-import org.sonar.api.server.rule.RulesDefinitionAnnotationLoader;
 import org.sonar.javascript.checks.CheckList;
 import org.sonar.plugins.javascript.JavaScriptLanguage;
-import org.sonar.plugins.javascript.JsonProfileReader;
 import org.sonar.plugins.javascript.SonarWayProfile;
+import org.sonarsource.analyzer.commons.RuleMetadataLoader;
 
 public class JavaScriptRulesDefinition implements RulesDefinition {
 
-  private final Gson gson = new Gson();
+  private static final String METADATA_LOCATION = "org/sonar/l10n/javascript/rules/javascript";
 
   @Override
   public void define(Context context) {
@@ -47,23 +35,8 @@ public class JavaScriptRulesDefinition implements RulesDefinition {
       .createRepository(CheckList.REPOSITORY_KEY, JavaScriptLanguage.KEY)
       .setName(CheckList.REPOSITORY_NAME);
 
-    RulesDefinitionAnnotationLoader rulesDefinitionAnnotationLoader = new RulesDefinitionAnnotationLoader();
-    for (Class ruleClass : CheckList.getChecks()) {
-      rulesDefinitionAnnotationLoader.load(repository, ruleClass);
-    }
-
-    for (NewRule rule : repository.rules()) {
-      String metadataKey = rule.key();
-      // Setting internal key is essential for rule templates (see SONAR-6162), and it is not done by AnnotationBasedRulesDefinition from sslr-squid-bridge version 2.5.1:
-      rule.setInternalKey(metadataKey);
-      rule.setHtmlDescription(readRuleDefinitionResource(metadataKey + ".html"));
-      addMetadata(rule, metadataKey);
-    }
-
-    Set<String> activatedRuleKeys = JsonProfileReader.ruleKeys(SonarWayProfile.PATH_TO_JSON);
-    for (NewRule rule : repository.rules()) {
-      rule.setActivatedByDefault(activatedRuleKeys.contains(rule.key()));
-    }
+    RuleMetadataLoader ruleMetadataLoader = new RuleMetadataLoader(METADATA_LOCATION, SonarWayProfile.PATH_TO_JSON);
+    ruleMetadataLoader.addRulesByAnnotatedClass(repository, CheckList.getChecks());
 
     NewRule commentRegularExpression = repository.rule("CommentRegularExpression");
     commentRegularExpression.setTemplate(true);
@@ -71,64 +44,5 @@ public class JavaScriptRulesDefinition implements RulesDefinition {
     repository.done();
   }
 
-  @Nullable
-  private static String readRuleDefinitionResource(String fileName) {
-    URL resource = JavaScriptRulesDefinition.class.getResource("/org/sonar/l10n/javascript/rules/javascript/" + fileName);
-    if (resource == null) {
-      return null;
-    }
-    try {
-      return Resources.toString(resource, Charsets.UTF_8);
-    } catch (IOException e) {
-      throw new IllegalStateException("Failed to read: " + resource, e);
-    }
-  }
 
-
-  private void addMetadata(NewRule rule, String metadataKey) {
-    String json = readRuleDefinitionResource(metadataKey + ".json");
-    if (json != null) {
-      RuleMetadata metadata = gson.fromJson(json, RuleMetadata.class);
-      rule.setSeverity(metadata.defaultSeverity.toUpperCase(Locale.US));
-      rule.setName(metadata.title);
-      rule.setTags(metadata.tags);
-      rule.setType(RuleType.valueOf(metadata.type));
-      rule.setStatus(RuleStatus.valueOf(metadata.status.toUpperCase(Locale.US)));
-
-      if (metadata.remediation != null) {
-        // metadata.remediation is null for template rules
-        rule.setDebtRemediationFunction(metadata.remediation.remediationFunction(rule.debtRemediationFunctions()));
-        rule.setGapDescription(metadata.remediation.linearDesc);
-      }
-    }
-  }
-
-  private static class RuleMetadata {
-    String title;
-    String status;
-    String type;
-    @Nullable
-    Remediation remediation;
-
-    String[] tags;
-    String defaultSeverity;
-  }
-
-  private static class Remediation {
-    String func;
-    String constantCost;
-    String linearDesc;
-    String linearOffset;
-    String linearFactor;
-
-    private DebtRemediationFunction remediationFunction(DebtRemediationFunctions drf) {
-      if (func.startsWith("Constant")) {
-        return drf.constantPerIssue(constantCost.replace("mn", "min"));
-      }
-      if ("Linear".equals(func)) {
-        return drf.linear(linearFactor.replace("mn", "min"));
-      }
-      return drf.linearWithOffset(linearFactor.replace("mn", "min"), linearOffset.replace("mn", "min"));
-    }
-  }
 }
