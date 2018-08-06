68e44311b3eb401b65aa537e2a14b5a3b1d8468a
Elena <elena.vilchik@sonarsource.com>
2018-08-06 14:47:00 +0200

    Undo change to AlertUseCheck

--- javascript-checks/src/main/java/org/sonar/javascript/checks/AlertUseCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/AlertUseCheck.java
@@ -20,7 +20,6 @@
 package org.sonar.javascript.checks;
 
 import com.google.common.collect.ImmutableSet;
-import java.util.List;
 import java.util.Set;
 import org.sonar.check.Rule;
 import org.sonar.plugins.javascript.api.tree.Tree.Kind;
@@ -28,24 +27,12 @@ import org.sonar.plugins.javascript.api.tree.expression.CallExpressionTree;
 import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
 import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
 import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
-import org.sonar.plugins.javascript.api.visitors.Issue;
-import org.sonar.plugins.javascript.api.visitors.JavaScriptFile;
-import org.sonar.plugins.javascript.api.visitors.TreeVisitorContext;
 
 @Rule(key = "S1442")
 public class AlertUseCheck extends DoubleDispatchVisitorCheck {
 
   private static final String MESSAGE = "Remove this usage of %s(...).";
   private static final Set<String> OPEN_DIALOG_METHODS = ImmutableSet.of("alert", "prompt", "confirm");
-
-  @Override
-  public List<Issue> scanFile(TreeVisitorContext context) {
-    JavaScriptFile jsFile = context.getJavaScriptFile();
-    if (jsFile.fileName().equals("file.js")) {
-      addIssue(context.getTopTree(), "Issue on specific file.");
-    }
-    return super.scanFile(context);
-  }
 
   @Override
   public void visitCallExpression(CallExpressionTree tree) {
