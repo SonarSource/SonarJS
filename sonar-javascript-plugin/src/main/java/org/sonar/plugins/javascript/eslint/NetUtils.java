03a56e631823b7f72943100a5ba12bb993d03893
vilchik-elena <elena.vilchik@sonarsource.com>
2018-09-19 18:07:30 +0200

    Connect eslint-bridge with SonarJS plugin

---
+++ sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/eslint/NetUtils.java
@@ -0,0 +1,67 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2018 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package org.sonar.plugins.javascript.eslint;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class NetUtils {
+
+  private NetUtils(){
+  }
+
+  public static int findOpenPort() {
+    try (ServerSocket socket = new ServerSocket(0)) {
+      return socket.getLocalPort();
+    } catch (IOException e) {
+      return 3000;
+    }
+  }
+
+  public static boolean waitServerToStart(String host, int port, int timeoutMs) {
+    int leftTimeout = timeoutMs;
+    int sleepStep = 500;
+    try {
+      while (!serverListening(host, port)) {
+        if (leftTimeout > 0) {
+          leftTimeout -= sleepStep;
+          Thread.sleep(sleepStep);
+        } else {
+          return false;
+        }
+      }
+    } catch (InterruptedException e) {
+      Thread.currentThread().interrupt();
+    }
+
+    return true;
+  }
+
+  private static boolean serverListening(String host, int port) {
+    try (Socket s = new Socket(host, port)) {
+      return true;
+    } catch (Exception e) {
+      return false;
+    }
+  }
+
+
+}
---
+++ sonar-javascript-plugin/src/test/resources/eslint-bridge/no-response.js
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+
+const http = require('http')
+const port = process.argv[2]
+
+const requestHandler = (request, response) => {
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
