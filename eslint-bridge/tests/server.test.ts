036e4b3ad2acd39dbf636d83e2f75cce1311644c
Elena <elena.vilchik@sonarsource.com>
2018-09-24 11:02:28 +0200

    Connect eslint-bridge with SonarJS plugin

---
+++ eslint-bridge/tests/server.test.ts
@@ -0,0 +1,74 @@
+import { start } from "../src/server";
+import * as http from "http";
+import { promisify } from "util";
+import * as getPort from "get-port";
+
+describe("server", () => {
+  let port;
+  let server;
+  let close;
+
+  beforeEach(async () => {
+    port = await getPort();
+    server = await start(port);
+    close = promisify(server.close.bind(server));
+  });
+
+  afterEach(async () => {
+    await close();
+  });
+
+  it("should respond with issues", async () => {
+    expect.assertions(2);
+    expect(server.listening).toEqual(true);
+
+    const response = await post(
+      JSON.stringify({
+        filepath: "dir/file.js",
+        fileContent: "if (true) 42; else 42;",
+        rules: ["no-all-duplicated-branches"],
+      }),
+    );
+
+    expect(JSON.parse(response)).toEqual([
+      {
+        column: 1,
+        endColumn: 23,
+        endLine: 1,
+        line: 1,
+        message:
+          "Remove this conditional structure or edit its code blocks so that they're not all the same.",
+        ruleId: "no-all-duplicated-branches",
+      },
+    ]);
+  });
+
+  function post(data): Promise<string> {
+    const options = {
+      host: "localhost",
+      port,
+      path: "/analyze",
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    };
+
+    return new Promise((resolve, reject) => {
+      let response = "";
+
+      const req = http.request(options, res => {
+        res.on("data", chunk => {
+          response += chunk;
+        });
+
+        res.on("end", () => resolve(response));
+      });
+
+      req.on("error", reject);
+
+      req.write(data);
+      req.end();
+    });
+  }
+});
---
+++ sonar-javascript-plugin/src/test/resources/eslint-bridge/startServer.js
@@ -0,0 +1,34 @@
+#!/usr/bin/env node
+
+const http = require('http')
+const port = process.argv[2]
+
+const issues = [{
+  line: 1,
+  column: 2,
+  endLine: 3,
+  endColumn: 4,
+  ruleId: "no-all-duplicated-branches",
+  message: "Issue message"
+},
+{
+  line: 1,
+  column: 2,
+  ruleId: "no-all-duplicated-branches",
+  message: "Line issue message"
+}
+]
+
+const requestHandler = (request, response) => {
+  response.end(JSON.stringify(issues))
+}
+
+const server = http.createServer(requestHandler)
+
+server.listen(port, (err) => {
+  if (err) {
+    return console.log('something bad happened', err)
+  }
+
+  console.log(`server is listening on ${port}`)
+})
