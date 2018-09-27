2e25a1c7010ac7cc58ee36c89516d1c9ad6cb246
vilchik-elena <elena.vilchik@sonarsource.com>
2018-09-27 17:11:54 +0200

    Replace TooManyArgumentsCheck Some issues on ruling are missing because of different symbol information between ESLint and our frontend. More specifically, ESLint semantic engine is missing symbol information on function declarations.

--- javascript-checks/src/test/resources/checks/TooManyArguments2.js
+++
@@ -1,9 +0,0 @@
-function bar(p1, p2){}
-
-bar(1, 2, 3);                          // Noncompliant [[sc=4;ec=13;el=+0;secondary=-2]] {{"bar" declared at line 1 expects 2 arguments, but 3 were provided.}}
-
-
-function builtIn_StringFunctions() {
-  "hello".charAt();                    // OK, too few arguments
-  "hello".charAt(1, 2, 3);             // Noncompliant {{"charAt" expects 1 argument, but 3 were provided.}}
-}
