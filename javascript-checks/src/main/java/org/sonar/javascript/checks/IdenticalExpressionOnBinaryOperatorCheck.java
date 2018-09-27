ce3f718cd09756336df88310401a80ce067949c1
vilchik-elena <elena.vilchik@sonarsource.com>
2018-09-27 17:16:02 +0200

    Replace IdenticalExpressionOnBinaryOperatorCheck Change of implementation and scope in ESLint-based rule. Java implementation: logic to exclude potential NaN comparison ESLint-based implementation: logic to exclude simple expression (made by a single identifier)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/IdenticalExpressionOnBinaryOperatorCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/IdenticalExpressionOnBinaryOperatorCheck.java
@@ -20,49 +20,12 @@
 package org.sonar.javascript.checks;
 
 import org.sonar.check.Rule;
-import org.sonar.javascript.tree.KindSet;
-import org.sonar.javascript.tree.SyntacticEquivalence;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.expression.BinaryExpressionTree;
-import org.sonar.plugins.javascript.api.tree.expression.LiteralTree;
-import org.sonar.plugins.javascript.api.tree.expression.UnaryExpressionTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
 
 @Rule(key = "S1764")
-public class IdenticalExpressionOnBinaryOperatorCheck extends DoubleDispatchVisitorCheck {
-
-  private static final String MESSAGE = "Correct one of the identical sub-expressions on both sides of operator \"%s\"";
+public class IdenticalExpressionOnBinaryOperatorCheck extends EslintBasedCheck {
 
   @Override
-  public void visitBinaryExpression(BinaryExpressionTree tree) {
-    if (!tree.is(Kind.MULTIPLY, Kind.PLUS, Kind.ASSIGNMENT)
-      && SyntacticEquivalence.areEquivalent(tree.leftOperand(), tree.rightOperand()) && !isExcluded(tree)) {
-
-      String message = String.format(MESSAGE, tree.operatorToken().text());
-      addIssue(tree.rightOperand(), message)
-        .secondary(tree.leftOperand());
-    }
-
-    super.visitBinaryExpression(tree);
+  public String eslintKey() {
+    return "no-identical-expressions";
   }
-
-  private static boolean isExcluded(BinaryExpressionTree tree) {
-    return tree.is(Kind.COMMA_OPERATOR, Kind.INSTANCE_OF) || isOneOntoOneShifting(tree) || isPotentialNanComparison(tree);
-  }
-
-  private static boolean isPotentialNanComparison(BinaryExpressionTree tree) {
-    return tree.is(KindSet.EQUALITY_KINDS)
-      && (tree.leftOperand().is(
-      Kind.IDENTIFIER_REFERENCE,
-      Kind.BRACKET_MEMBER_EXPRESSION,
-      Kind.DOT_MEMBER_EXPRESSION) || tree.leftOperand() instanceof UnaryExpressionTree);
-
-  }
-
-  private static boolean isOneOntoOneShifting(BinaryExpressionTree tree) {
-    return tree.is(Kind.LEFT_SHIFT)
-      && tree.leftOperand().is(Kind.NUMERIC_LITERAL)
-      && "1".equals(((LiteralTree) tree.leftOperand()).value());
-  }
-
 }
