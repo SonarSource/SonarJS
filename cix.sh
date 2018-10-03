b5bdd8a693261dd6bf8796fe211fe9ecfd88a68c
Elena Vilchik <elena.vilchik@sonarsource.com>
2018-10-03 16:27:25 +0200

    Add execution of CI on Windows (#1101)

--- cix.sh
+++ cix.sh
@@ -3,7 +3,29 @@ set -euo pipefail
 echo "Running $TEST with SQ=$SQ_VERSION"
 
 case "$TEST" in
-  plugin|ruling|type-inference|performancing)
+  ci)
+
+  #install npm
+  node_home=$(pwd)/node-v8.9.0-win-x64
+  node_archive=node.7z
+  if [ ! -d "$node_home" ]; then
+    echo "=== Install Node.js ===";
+    curl --insecure --silent --show-error -o $node_archive https://nodejs.org/dist/v8.9.0/node-v8.9.0-win-x64.7z;
+    7z x $node_archive;
+    rm $node_archive;
+  fi
+
+  chmod 755 $node_home/node;
+
+  export PATH=$node_home:$PATH;
+
+  echo "Node version"
+  node -v
+
+  mvn clean package
+  ;;
+
+  plugin|ruling|performancing)
 
   cd its/$TEST
   mvn -B -e -Dsonar.runtimeVersion="$SQ_VERSION" -Dmaven.test.redirectTestOutputToFile=false package
