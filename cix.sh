794fc0ae0704fd66f8c3d6d0e0153fd5dffd1ccf
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-02-15 15:33:34 +0100

    Install NodeJS on windows nodes for QA

--- cix.sh
+++ cix.sh
@@ -2,9 +2,11 @@
 set -euo pipefail
 echo "Running $TEST with SQ=$SQ_VERSION"
 
-case "$TEST" in
-  ci)
-
+function install_nodejs_win() {
+  if [[ "$OSTYPE" != "msys" && "$OSTYPE" != "cygwin" ]]; then
+    echo "Skipping NodeJS installation for nix OS"
+    return
+  fi
   #install npm
   node_home=$(pwd)/node-v8.9.0-win-x64
   node_archive=node.7z
@@ -21,6 +23,12 @@ case "$TEST" in
 
   echo "Node version"
   node -v
+}
+
+install_nodejs_win
+
+case "$TEST" in
+  ci)
 
   mvn clean package
   ;;
