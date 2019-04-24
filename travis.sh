1d24653c741e0dfa8d0e627bbc4ce5eec4c5e6b1
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-04-24 10:11:20 +0200

    Use JDK 11 for CI

--- travis.sh
+++ travis.sh
@@ -4,11 +4,10 @@ set -euo pipefail
 
 function configureTravis {
   mkdir -p ~/.local
-  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v55 | tar zx --strip-components 1 -C ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v56 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
 configureTravis
-. ~/.local/bin/installMaven35
 
 export DEPLOY_PULL_REQUEST=true
 regular_mvn_build_deploy_analyze
