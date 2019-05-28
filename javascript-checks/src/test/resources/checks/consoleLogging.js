88cd23103bdee62cb4ce1f03acd0b4d1e0df7325
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-05-28 17:06:27 +0200

    Migrate S2228 to S106

--- javascript-checks/src/test/resources/checks/consoleLogging.js
+++ javascript-checks/src/test/resources/checks/consoleLogging.js
@@ -1,7 +1,7 @@
-  console.log("Foo"); // Noncompliant {{Remove this logging statement.}}
+  console.log("Foo"); // Noncompliant {{Replace this usage of console.log by a logger.}}
 //^^^^^^^^^^^
-  console.warn("Foo"); // Noncompliant {{Remove this logging statement.}}
-  console.error("Foo"); // Noncompliant {{Remove this logging statement.}}
+  console.warn("Foo"); // Noncompliant {{Replace this usage of console.warn by a logger.}}
+  console.error("Foo"); // Noncompliant {{Replace this usage of console.error by a logger.}}
 console.group();    // OK
 
 myObject.log();     // OK
