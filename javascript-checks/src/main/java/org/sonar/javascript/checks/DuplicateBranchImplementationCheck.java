e243ac61f0095cb8209016a459f151c585c49cd6
Andrea Guarino <andrea.guarino@sonarsource.com>
2019-05-31 16:47:23 +0200

    Make DuplicateBranchImplementationCheck(S1871) eslint-based rule (no-duplicated-branches)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/DuplicateBranchImplementationCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/DuplicateBranchImplementationCheck.java
@@ -19,62 +19,13 @@
  */
 package org.sonar.javascript.checks;
 
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.sonar.check.Rule;
-import org.sonar.javascript.metrics.LineVisitor;
-import org.sonar.plugins.javascript.api.tree.Tree;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.statement.BlockTree;
-import org.sonar.plugins.javascript.api.tree.statement.StatementTree;
-import org.sonar.plugins.javascript.api.tree.statement.SwitchClauseTree;
-import org.sonar.plugins.javascript.api.visitors.IssueLocation;
 
 @Rule(key = "S1871")
-public class DuplicateBranchImplementationCheck extends AbstractDuplicateBranchImplementationCheck {
-
-  private static final String MESSAGE = "Either merge this %s with the identical one on line \"%s\" or change one of the implementations.";
+public class DuplicateBranchImplementationCheck extends EslintBasedCheck {
 
   @Override
-  protected void checkDuplicatedBranches(List<Tree> branches) {
-    Set<Tree> withIssue = new HashSet<>();
-
-    for (int i = 0; i < branches.size(); i++) {
-      Tree currentBranch = branches.get(i);
-
-      for (int j = i + 1; j < branches.size(); j++) {
-        Tree comparedBranch = branches.get(j);
-
-        if (!withIssue.contains(comparedBranch)
-          && syntacticallyEqual(currentBranch, comparedBranch)
-          && linesOfCodeForBranch(comparedBranch) > 1) {
-
-          IssueLocation secondary = new IssueLocation(currentBranch, "Original");
-          String branchType = currentBranch instanceof SwitchClauseTree ? "case" : "branch";
-          String message = String.format(MESSAGE, branchType, secondary.startLine());
-          addIssue(comparedBranch, message).secondary(secondary);
-          withIssue.add(comparedBranch);
-        }
-      }
-    }
+  public String eslintKey() {
+    return "no-duplicated-branches";
   }
-
-
-  @Override
-  protected void allBranchesDuplicated(Tree tree) {
-    // do nothing, covered with S3923
-  }
-
-  private static <T> int linesOfCodeForBranch(T branch) {
-    if (branch instanceof SwitchClauseTree) {
-      return new LineVisitor(normalize((SwitchClauseTree) branch)).getLinesOfCodeNumber();
-
-    } else {
-      StatementTree statementBranch = (StatementTree) branch;
-      LineVisitor lineVisitor = statementBranch.is(Kind.BLOCK) ? new LineVisitor(((BlockTree) statementBranch).statements()) : new LineVisitor(statementBranch);
-      return lineVisitor.getLinesOfCodeNumber();
-    }
-  }
-
 }
