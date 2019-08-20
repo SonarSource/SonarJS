3741e4a29c6c201365b03193fc512f18c3c1240b
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-08-20 09:34:17 +0200

    Declare TypeScript language (#1295)

--- sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/JavaScriptPluginTest.java
+++ sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/JavaScriptPluginTest.java
@@ -34,7 +34,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 public class JavaScriptPluginTest {
 
-  private static final int BASE_EXTENSIONS = 16;
+  private static final int BASE_EXTENSIONS = 18;
   public static final Version LTS_VERSION = Version.create(7, 9);
 
   @Test
@@ -45,7 +45,7 @@ public class JavaScriptPluginTest {
 
   @Test
   public void should_contain_right_properties_number() throws Exception {
-    assertThat(properties()).hasSize(8);
+    assertThat(properties()).hasSize(9);
   }
 
   @Test
@@ -62,7 +62,7 @@ public class JavaScriptPluginTest {
       if (propertyDefinition.key().equals("sonar.eslint.reportPaths")) {
         assertThat(propertyDefinition.category()).isEqualTo("External Analyzers");
       } else {
-        assertThat(propertyDefinition.category()).isEqualTo("JavaScript");
+        assertThat(propertyDefinition.category()).isIn("JavaScript", "TypeScript");
       }
     }
   }
