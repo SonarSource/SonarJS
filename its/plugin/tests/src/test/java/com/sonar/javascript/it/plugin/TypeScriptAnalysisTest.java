716c77908f9e38368dfedf40940a3ae05ef8d1f5
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2019-08-23 12:03:32 +0200

    Remove node_modules from tsproject

--- its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/TypeScriptAnalysisTest.java
+++ its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/TypeScriptAnalysisTest.java
@@ -23,11 +23,13 @@ import com.google.common.collect.ImmutableList;
 import com.sonar.orchestrator.Orchestrator;
 import com.sonar.orchestrator.build.SonarScanner;
 import java.io.File;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.sonar.api.utils.System2;
 import org.sonarqube.ws.Issues;
 import org.sonarqube.ws.client.issues.SearchRequest;
 
@@ -43,7 +45,7 @@ public class TypeScriptAnalysisTest {
   private static final File PROJECT_DIR = TestUtils.projectDir(TSPROJECT);
 
   @BeforeClass
-  public static void startServer() {
+  public static void startServer() throws Exception {
     orchestrator.resetData();
 
     String projectKey = TSPROJECT;
@@ -56,7 +58,19 @@ public class TypeScriptAnalysisTest {
 
     Tests.setProfile(projectKey, "eslint-based-rules-profile");
 
+    installTypesScript();
+
     orchestrator.executeBuild(build);
+  }
+
+  private static void installTypesScript() throws IOException, InterruptedException {
+    String npm = System2.INSTANCE.isOsWindows() ? "npm.cmd" : "npm";
+    String[] cmd = {npm, "install", "typescript"};
+    Process process = Runtime.getRuntime().exec(cmd, null, PROJECT_DIR);
+    int returnValue = process.waitFor();
+    if (returnValue != 0) {
+      throw new IllegalStateException("Failed to install TypeScript");
+    }
   }
 
   @Test
