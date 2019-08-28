fcdc6f28374d058a1dd0d4686483a3810b1e0f0d
Yassin Kammoun <yassin.kammoun@sonarsource.com>
2019-08-28 10:22:25 +0200

    Provide copy-paste detection and size metrics for TypeScript

---
+++ eslint-bridge/tests/runner/cpd.test.ts
@@ -0,0 +1,115 @@
+import getCpdTokens, { CpdToken } from "../../src/runner/cpd";
+import { parseTypeScriptSourceFile } from "../../src/parser";
+import { join } from "path";
+
+it("should not skip any token", () => {
+  const result = actual(
+    `
+    // comment
+    "str1"
+    'str2'
+    \`template\`
+    identifier
+    `,
+  );
+  expect(result.length).toBe(4);
+});
+
+it("should skip comments", () => {
+  const result = actual(
+    `a // comment1
+  /*comment2*/
+  // comment3
+  b // comment4
+  /**
+   * comment5
+   */
+/**
+ * Shift down
+ * @param  {Array} array
+ * @param  {Number} i
+ * @param  {Number} j
+ */
+  c
+  // comment6`,
+  );
+  expect(result.length).toBe(3);
+});
+
+it("should provide correct position", () => {
+  const result = actual(
+    ` foo
+bar
+/**/foobar
+\`
+multiline string
+\`
+  `,
+  );
+  expect(result.length).toBe(4);
+  expect(result).toContainEqual(token(1, 1, 1, 4, "foo"));
+  expect(result).toContainEqual(token(2, 0, 2, 3, "bar"));
+  expect(result).toContainEqual(token(3, 4, 3, 10, "foobar"));
+  expect(result).toContainEqual(token(4, 0, 6, 1, "LITERAL"));
+});
+
+it("should replace strings", () => {
+  expect(actual("'string'")[0].image).toBe("LITERAL");
+  expect(actual("`string`")[0].image).toBe("LITERAL");
+  expect(actual('"string"')[0].image).toBe("LITERAL");
+  expect(actual("42")[0].image).toBe("42");
+  expect(actual("true")[0].image).toBe("true");
+});
+
+it("should process JSX syntax", () => {
+  const result = actual("<foo/>");
+  expect(result.length).toBe(4);
+  expect(result).toContainEqual(token(1, 0, 1, 1, "<"));
+  expect(result).toContainEqual(token(1, 1, 1, 4, "foo"));
+  expect(result).toContainEqual(token(1, 4, 1, 5, "/"));
+  expect(result).toContainEqual(token(1, 5, 1, 6, ">"));
+});
+
+it("should process JSX syntax with empty text elements", () => {
+  const result = actual(`<foo>
+      </foo>`);
+  expect(result.length).toBe(7);
+  expect(result).toContainEqual(token(1, 0, 1, 1, "<"));
+  expect(result).toContainEqual(token(1, 1, 1, 4, "foo"));
+  expect(result).toContainEqual(token(1, 4, 1, 5, ">"));
+  expect(result).toContainEqual(token(2, 6, 2, 7, "<"));
+});
+
+it("should process JSX syntax with not empty text elements", () => {
+  const result = actual(`<foo> hello
+    world </foo>`);
+  expect(result.length).toBe(8);
+  expect(result).toContainEqual(token(1, 0, 1, 1, "<"));
+  expect(result).toContainEqual(token(1, 1, 1, 4, "foo"));
+  expect(result).toContainEqual(token(1, 4, 1, 5, ">"));
+  expect(result).toContainEqual(token(1, 5, 2, 10, " hello\n    world "));
+  expect(result).toContainEqual(token(2, 10, 2, 11, "<"));
+});
+
+function token(
+  startLine: number,
+  startCol: number,
+  endLine: number,
+  endCol: number,
+  image: string,
+): CpdToken {
+  return {
+    startLine,
+    startCol,
+    endLine,
+    endCol,
+    image,
+  };
+}
+
+function actual(code: string): CpdToken[] {
+  const fileUri = join(__dirname, "/../fixtures/tsx-project/sample.lint.tsx");
+  const tsConfig = join(__dirname, "/../fixtures/tsx-project/tsconfig.json");
+  const sourceCode = parseTypeScriptSourceFile(code, fileUri, [tsConfig]);
+  return getCpdTokens(sourceCode).cpdTokens;
+}
