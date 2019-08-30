aa0c7685e5ebee457ff0f7680f346c1c920fe943
Elena Vilchik <elena.vilchik@sonarsource.com>
2019-08-30 11:07:25 +0200

    Add rule S4325 'no-unnecessary-type-assertion' [TS]

--- its/ruling/src/test/resources/convert-expectations.js
+++ its/ruling/src/test/resources/convert-expectations.js
@@ -1,22 +1,40 @@
 // absolute path to a snapshot file from SonarTS
 const source = process.argv[2];
-// absolute path to a file to create for ruling IT in SonarJS
+// absolute path to a directory with expectation for TS projects
 const destination = process.argv[3];
+
+const ruleKey = process.argv[4];
 
 const fs = require('fs');
 
 const data  = fs.readFileSync(source).toString().trim();
-const result = {};
+const resultsByProject = {};
+const projects = new Set();
+
 data.split("\n").forEach(line => {
   const [filepath, lineNumbers] = line.split(":");
   var lineNumbersAsArray = lineNumbers.split(',').map(function(item) {
     return parseInt(item, 10);
   });
-  result["ts-project:" + filepath.substring(4)] = lineNumbersAsArray;
+  const [ , projectKey] = filepath.split("/", 3);
+  if (!resultsByProject[projectKey]) {
+    resultsByProject[projectKey] = {};
+  }
+  const result = resultsByProject[projectKey];
+  projects.add(projectKey);
+
+  result[projectKey + ":" + filepath.substring(5 + projectKey.length)] = lineNumbersAsArray;
 });
 
-const json = JSON.stringify(result, null, 2);
-console.log(json);
-fs.writeFileSync(destination, json);
-
-
+projects.forEach(projectKey => {
+  const result = resultsByProject[projectKey];
+  let json = JSON.stringify(result, null, 2);
+  const dir = destination + projectKey;
+  if (!fs.existsSync(dir)) {
+    fs.mkdirSync(dir);
+  }
+  json = json.split(" ").join("");
+  json = json.split("\"").join("'");
+  json = json.split("\n]").join(",\n]");
+  fs.writeFileSync(dir + "/typescript-" + ruleKey + ".json", json);
+});
