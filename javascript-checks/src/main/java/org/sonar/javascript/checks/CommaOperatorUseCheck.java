955b20b014beb6b2f6d29bd085ff34d53712dd79
quentin-jaquier-sonarsource <43733433+quentin-jaquier-sonarsource@users.noreply.github.com>
2019-09-11 13:52:45 +0200

    Add rule S878 (based on 'no-sequences') for TS/JS (#1364)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/CommaOperatorUseCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/CommaOperatorUseCheck.java
@@ -19,89 +19,18 @@
  */
 package org.sonar.javascript.checks;
 
-import com.google.common.collect.Lists;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.List;
-import javax.annotation.Nullable;
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.annotations.JavaScriptRule;
-import org.sonar.plugins.javascript.api.tree.Tree;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.expression.BinaryExpressionTree;
-import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
-import org.sonar.plugins.javascript.api.tree.lexical.SyntaxToken;
-import org.sonar.plugins.javascript.api.tree.statement.ForStatementTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
-import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
+import org.sonar.javascript.checks.annotations.TypeScriptRule;
 
+@TypeScriptRule
 @JavaScriptRule
 @Rule(key = "S878")
-public class CommaOperatorUseCheck extends DoubleDispatchVisitorCheck {
-
-  private static final String MESSAGE_ONE_COMMA = "Remove use of this comma operator.";
-  private static final String MESSAGE_MANY_COMMAS = "Remove use of all comma operators in this expression.";
+public class CommaOperatorUseCheck extends EslintBasedCheck {
 
   @Override
-  public void visitBinaryExpression(BinaryExpressionTree tree) {
-
-    if (!tree.is(Kind.COMMA_OPERATOR)) {
-      super.visitBinaryExpression(tree);
-      return;
-    }
-
-    raiseIssue(tree);
-
-    getAllSubExpressions(tree).forEach(expression -> super.scan(expression));
-  }
-
-  private void raiseIssue(BinaryExpressionTree tree) {
-    List<SyntaxToken> commas = getCommas(tree);
-    String message = commas.size() > 1 ? MESSAGE_MANY_COMMAS : MESSAGE_ONE_COMMA;
-    PreciseIssue issue = addIssue(commas.get(0), message);
-    commas.subList(1, commas.size()).forEach(issue::secondary);
-  }
-
-  @Override
-  public void visitForStatement(ForStatementTree tree) {
-    visitPossibleException(tree.init());
-    super.scan(tree.condition());
-    visitPossibleException(tree.update());
-    super.scan(tree.statement());
-  }
-
-  private void visitPossibleException(@Nullable Tree tree) {
-    if (tree != null && tree.is(Kind.COMMA_OPERATOR)) {
-      List<ExpressionTree> expressions = getAllSubExpressions((BinaryExpressionTree) tree);
-      for (ExpressionTree expression : expressions) {
-        super.scan(expression);
-      }
-    } else {
-      super.scan(tree);
-    }
-  }
-
-  private static List<ExpressionTree> getAllSubExpressions(BinaryExpressionTree tree) {
-    List<ExpressionTree> result = new LinkedList<>();
-    result.add(tree.rightOperand());
-    ExpressionTree currentExpression = tree.leftOperand();
-    while (currentExpression.is(Kind.COMMA_OPERATOR)) {
-      result.add(((BinaryExpressionTree) currentExpression).rightOperand());
-      currentExpression = ((BinaryExpressionTree) currentExpression).leftOperand();
-    }
-    result.add(currentExpression);
-    return result;
-  }
-
-  private static List<SyntaxToken> getCommas(BinaryExpressionTree tree) {
-    List<SyntaxToken> commas = new ArrayList<>();
-    commas.add(tree.operatorToken());
-    ExpressionTree currentExpression = tree.leftOperand();
-    while (currentExpression.is(Kind.COMMA_OPERATOR)) {
-      commas.add(((BinaryExpressionTree) currentExpression).operatorToken());
-      currentExpression = ((BinaryExpressionTree) currentExpression).leftOperand();
-    }
-    return Lists.reverse(commas);
+  public String eslintKey() {
+    return "no-sequences";
   }
 
 }
