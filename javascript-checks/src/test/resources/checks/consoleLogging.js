227efe64b3941ea79f603d02e97930571a389371
quentin-jaquier-sonarsource <43733433+quentin-jaquier-sonarsource@users.noreply.github.com>
2019-09-18 15:30:19 +0200

    Add rule S2228 (based on 'no-console') for TS/JS (#1501)

--- javascript-checks/src/test/resources/checks/consoleLogging.js
+++
@@ -1,8 +0,0 @@
-  console.log("Foo"); // Noncompliant {{Replace this usage of console.log by a logger.}}
-//^^^^^^^^^^^
-  console.warn("Foo"); // Noncompliant {{Replace this usage of console.warn by a logger.}}
-  console.error("Foo"); // Noncompliant {{Replace this usage of console.error by a logger.}}
-console.group();    // OK
-
-myObject.log();     // OK
-
