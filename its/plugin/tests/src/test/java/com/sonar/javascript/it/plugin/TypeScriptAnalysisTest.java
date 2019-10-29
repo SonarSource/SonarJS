23f6f50958bb15273dcbb6bb460220a87fae2c4c
Elena Vilchik <elena.vilchik@sonarsource.com>
2019-10-29 17:45:39 +0100

    Update typescript-eslint to 2.6.0 (#1702)

--- its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/TypeScriptAnalysisTest.java
+++ its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/TypeScriptAnalysisTest.java
@@ -28,7 +28,6 @@ import java.util.List;
 import org.apache.commons.io.FileUtils;
 import org.junit.BeforeClass;
 import org.junit.ClassRule;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.sonarqube.ws.Issues.Issue;
 
@@ -153,7 +152,7 @@ public class TypeScriptAnalysisTest {
   @Test
   public void test_new_typescript() throws Exception {
     File dir = TestUtils.projectDir("tsproject-no-typescript");
-    TestUtils.npmInstall(dir, "typescript@3.6.2", "--no-save");
+    TestUtils.npmInstall(dir, "typescript@3.8.0-dev.20191026", "--no-save");
     String projectKey = "tsproject-new-typescript";
     SonarScanner build = SonarScanner.create()
       .setProjectKey(projectKey)
@@ -164,7 +163,7 @@ public class TypeScriptAnalysisTest {
     Tests.setProfile(projectKey, "eslint-based-rules-profile", "ts");
     BuildResult result = orchestrator.executeBuild(build);
     assertThat(result.isSuccess()).isTrue();
-    assertThat(result.getLogsLines(l -> l.contains("You are using version of TypeScript 3.6.2 which is not officially supported; supported versions >=3.2.1 <3.6.0"))).hasSize(1);
+    assertThat(result.getLogsLines(l -> l.contains("You are using version of TypeScript 3.8.0-dev.20191026 which is not officially supported; supported versions >=3.2.1 <3.8.0"))).hasSize(1);
   }
 
   @Test
