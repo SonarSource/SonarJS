dc00233040bc19e241243244e908dfb387705090
Christophe Zürn <christophe.zurn@sonarsource.com>
2019-10-29 13:05:34 +0100

    Now relying on Noncompliant comments for unit tests with multiple issues

--- eslint-bridge/tests/rules/arguments-order.typescript.test.ts
+++ eslint-bridge/tests/rules/arguments-order.typescript.test.ts
@@ -13,6 +13,25 @@ const ruleTester = new RuleTester({
 });
 const placeHolderFilePath = path.resolve(`${__dirname}/../fixtures/rule-tester-project/file.ts`);
 
+function invalid(code: string) {
+  const errors: RuleTester.TestCaseError[] = [];
+  const lines = code.split("\n");
+  for (let i = 1; i <= lines.length; i++) {
+    const line = lines[i - 1];
+    if (line.includes("// Noncompliant")) {
+      errors.push({
+        line: i,
+        endLine: i,
+      });
+    }
+  }
+  return {
+    filename: placeHolderFilePath,
+    code: code,
+    errors,
+  };
+}
+
 ruleTester.run("Parameters should be passed in the correct order", rule, {
   valid: [
     {
@@ -71,7 +90,7 @@ ruleTester.run("Parameters should be passed in the correct order", rule, {
       code: `
         function differentTypes(x: string, y: number, z = 42) {}
         function nokForSameType(z: number, y: number) {
-          differentTypes("hello", z, y);
+          differentTypes("hello", z, y); // Noncompliant
         }`,
       errors: [
         {
@@ -91,7 +110,7 @@ ruleTester.run("Parameters should be passed in the correct order", rule, {
         }
         function objectType(a1: A, a2: A) {
           ((a1: A, a2: A) => {})
-                                (a2, a1);
+                                (a2, a1); // Noncompliant
         }`,
       errors: [
         {
@@ -103,48 +122,30 @@ ruleTester.run("Parameters should be passed in the correct order", rule, {
         },
       ],
     },
-    {
-      filename: placeHolderFilePath,
-      code: `
+    invalid(`
         const from = 1, length = 2;
-        const standardMethod = "str".substr(length, from);`,
-      errors: [
-        {
-          message: `{"message":"Arguments 'length' and 'from' have the same names but not the same order as the function parameters.","secondaryLocations":[]}`,
-          line: 3,
-          endLine: 3,
-          column: 45,
-          endColumn: 57,
-        },
-      ],
-    },
-    {
-      filename: placeHolderFilePath,
-      code: `
-        export function sameType(a: number, b: number, c = 3) {}
-        const a = 1, b = 2, c = 3, d = 4;
+        const standardMethod = "str".substr(length, from); // Noncompliant
+        `),
+    invalid(`
+        export function sameType(a: Number, b: number, c = 3) {}
+        const a = 1, c = 3, d = 4;
+        const b: Number = 2;
         
-        sameType(b, a, d);
-        sameType(b, a, c);
-        sameType(c, 2, a);
-        sameType(b, a);
-        sameType(42, c, b);`,
-      errors: 5,
-    },
-    {
-      filename: placeHolderFilePath,
-      code: `
+        sameType(b, a, d); // Noncompliant
+        sameType(b, a, c); // Noncompliant
+        sameType(c, 2, a); // Noncompliant
+        sameType(b, a); // Noncompliant
+        sameType(42, c, b); // Noncompliant
+        `),
+    invalid(`
       /**
        * @param a number
        * @param b string
        */
       function withJsDoc(a, b){}
-      withJsDoc(b, a);`,
-      errors: 1,
-    },
-    {
-      filename: placeHolderFilePath,
-      code: `
+      withJsDoc(b, a); // Noncompliant
+      `),
+    invalid(`
         class A {
             constructor(x: string, y: number, z = 42) {};
             sameType(a: number, b: number, c = 3) {};
@@ -153,19 +154,18 @@ ruleTester.run("Parameters should be passed in the correct order", rule, {
         
           class B extends A {
               constructor(x: string, y: number, z = 42) {
-                  super(x, z, y);
+                  super(x, z, y); // Noncompliant
               };
           }
         
-        new A(a, z, y);
-        new B("", z, y);
-        new A().sameType(b, a, d);
-        new A().sameType(b, a, c);
-        new A().sameType(c, 2, a);
-        new A().sameType(b, a);
-        new A().sameType(42, c, b)
-        new A().differentTypes("hello", z, y);`,
-      errors: 9,
-    },
+        new A(a, z, y); // Noncompliant
+        new B("", z, y); // Noncompliant
+        new A().sameType(b, a, d); // Noncompliant
+        new A().sameType(b, a, c); // Noncompliant
+        new A().sameType(c, 2, a); // Noncompliant
+        new A().sameType(b, a); // Noncompliant
+        new A().sameType(42, c, b) // Noncompliant
+        new A().differentTypes("hello", z, y); // Noncompliant
+        `),
   ],
 });
