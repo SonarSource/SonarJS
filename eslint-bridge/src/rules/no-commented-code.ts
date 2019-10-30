71f1bb996c4f906260181d56779a94fbc465bb00
Christophe Zurn <christophe.zurn@sonarsource.com>
2019-10-30 12:07:24 +0100

    Add rule S125 for TS/JS (`no-commented-code`)

---
+++ eslint-bridge/src/rules/no-commented-code.ts
@@ -0,0 +1,180 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-125
+
+import { Rule, SourceCode } from "eslint";
+import * as estree from "estree";
+import { TSESTree } from "@typescript-eslint/experimental-utils";
+import { parseJavaScriptSourceFile } from "../parser";
+import { ParsingError } from "../analyzer";
+
+const EXCLUDED_STATEMENTS = ["BreakStatement", "LabeledStatement", "ContinueStatement"];
+
+interface GroupComment {
+  value: string;
+  nodes: TSESTree.Comment[];
+}
+
+export const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    function hasNoSemicolon(code: string) {
+      return code[code.length - 1] !== ";";
+    }
+
+    function isExpressionExclusion(statement: estree.Node, code: string) {
+      if (statement.type === "ExpressionStatement") {
+        const expression = statement.expression;
+        if (
+          expression.type === "Identifier" ||
+          expression.type === "SequenceExpression" ||
+          isUnaryPlusOrMinus(expression) ||
+          isStringLiteral(expression) ||
+          hasNoSemicolon(code)
+        ) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function isExclusion(parsedBody: Array<estree.Node>, code: string) {
+      if (parsedBody.length === 1) {
+        const singleStatement = parsedBody[0];
+        return (
+          EXCLUDED_STATEMENTS.includes(singleStatement.type) ||
+          isReturnThrowExclusion(singleStatement) ||
+          isExpressionExclusion(singleStatement, code)
+        );
+      }
+      return false;
+    }
+
+    function containsCode(value: string) {
+      const parseResult = parseJavaScriptSourceFile(value);
+      return (
+        isSourceCode(parseResult) &&
+        parseResult.ast.body.length > 0 &&
+        !isExclusion(parseResult.ast.body, value)
+      );
+    }
+
+    return {
+      "Program:exit": () => {
+        const groupedComments = getGroupedComments(
+          context.getSourceCode().getAllComments() as TSESTree.Comment[],
+        );
+        groupedComments.forEach(groupComment => {
+          const rawTextTrimmed = groupComment.value.trim();
+          if (rawTextTrimmed !== "}" && containsCode(injectMissingBraces(rawTextTrimmed))) {
+            context.report({
+              message: "Remove this commented out code.",
+              loc: getCommentLocation(groupComment.nodes),
+            });
+          }
+        });
+      },
+    };
+  },
+};
+
+function getGroupedComments(comments: TSESTree.Comment[]): GroupComment[] {
+  const groupedComments: GroupComment[] = [];
+  let currentGroup: TSESTree.Comment[] = [];
+  for (const comment of comments) {
+    if (comment.type === "Block") {
+      groupedComments.push({ value: comment.value, nodes: [comment] });
+    } else if (
+      currentGroup.length === 0 ||
+      areAdjacent(currentGroup[currentGroup.length - 1], comment)
+    ) {
+      currentGroup.push(comment);
+    } else {
+      groupedComments.push({
+        value: currentGroup.map(lineComment => lineComment.value).join("\n"),
+        nodes: currentGroup,
+      });
+      currentGroup = [comment];
+    }
+  }
+
+  if (currentGroup.length > 0) {
+    groupedComments.push({
+      value: currentGroup.map(lineComment => lineComment.value).join("\n"),
+      nodes: currentGroup,
+    });
+  }
+
+  return groupedComments;
+}
+
+function areAdjacent(previous: TSESTree.Comment, next: TSESTree.Comment) {
+  return previous.loc.start.line + 1 === next.loc.start.line;
+}
+
+function injectMissingBraces(value: string) {
+  const openCurlyBraceNum = (value.match(/{/g) || []).length;
+  const closeCurlyBraceNum = (value.match(/}/g) || []).length;
+  const missingBraces = openCurlyBraceNum - closeCurlyBraceNum;
+  if (missingBraces > 0) {
+    return (
+      value +
+      Array(missingBraces)
+        .fill("}")
+        .join("")
+    );
+  } else if (missingBraces < 0) {
+    return (
+      Array(-missingBraces)
+        .fill("{")
+        .join("") + value
+    );
+  } else {
+    return value;
+  }
+}
+
+function getCommentLocation(nodes: TSESTree.Comment[]) {
+  return {
+    start: nodes[0].loc.start,
+    end: nodes[nodes.length - 1].loc.end,
+  };
+}
+
+function isSourceCode(parseResult: SourceCode | ParsingError): parseResult is SourceCode {
+  return !!(parseResult as SourceCode).ast;
+}
+
+function isReturnThrowExclusion(statement: estree.Node) {
+  if (statement.type === "ReturnStatement" || statement.type === "ThrowStatement") {
+    return statement.argument == null || statement.argument.type === "Identifier";
+  }
+  return false;
+}
+
+function isUnaryPlusOrMinus(expression: estree.Expression) {
+  return (
+    expression.type === "UnaryExpression" &&
+    (expression.operator === "+" || expression.operator === "-")
+  );
+}
+
+function isStringLiteral(expression: estree.Expression) {
+  return expression.type === "Literal" && typeof expression.value === "string";
+}
---
+++ eslint-bridge/tests/rules/no-commented-code.test.ts
@@ -0,0 +1,193 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import { RuleTester } from "eslint";
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018 } });
+import { rule } from "../../src/rules/no-commented-code";
+
+ruleTester.run("Sections of code should not be commented out", rule, {
+  valid: [
+    {
+      code: `
+        //
+      
+        //    
+        
+        /* */
+        
+        //
+        //  // nested comment
+        //
+      
+        /**
+         * // this should be ignored
+         * if (something) { return true;}
+         */
+      
+        /*jslint bitwise: false, browser: true, continue: false, devel: true, eqeq: false, evil: false, forin: false, newcap: false, nomen: false, plusplus: true, regexp: true, stupid: false, sub: false, undef: false, vars: false */
+      
+        /*jshint bitwise: false, curly: true, eqeqeq: true, forin: true, immed: true, latedef: true, newcap: true, noarg: true, noempty: false, nonew: true, plusplus: false, regexp: false, undef: true, strict: true, trailing: true, expr: true, regexdash: true, browser: true, jquery: true, onevar: true, nomen: true */
+      
+        /*global myGlobal: true */
+      
+        // ====
+      
+        // ----
+      
+        // ++++
+      
+        // some text with semicolon at the end;
+      
+        // http://www.example.com/ = http://www.example.com/
+      
+        // labelName : id
+      
+        // foo(), bar();
+      
+        // continue
+      
+        // return blabla
+      
+        // break something
+      
+        // throw exception
+      
+        // throw exception;
+      
+        // labelName : id;
+      
+        //\t\t\t\tbreak;
+      
+        // foo.bar
+      
+        // a + b
+      
+        // foo (see [123])
+      
+        // IE
+      
+        // shift
+      
+        // reduce
+      
+        //Object;
+      
+        //+ 10;
+      
+        //"gradientunscaled";
+      
+        // some text with some code is ok
+        // if (condition) {
+        // }
+      
+      
+        /*
+         some text with some code is ok
+         if (condition) {
+         }
+        */
+      
+        // }
+        `,
+    },
+  ],
+  invalid: [
+    {
+      code: `// if (something) {}`,
+      errors: [
+        {
+          message: "Remove this commented out code.",
+          line: 1,
+          endLine: 1,
+          column: 1,
+          endColumn: 21,
+        },
+      ],
+    },
+    {
+      code: `
+        // // nested comment
+        // foo(a, function(){
+        //     doSmth();
+        // });`,
+      errors: [
+        {
+          message: "Remove this commented out code.",
+          line: 2,
+          column: 9,
+          endLine: 5,
+          endColumn: 15,
+        },
+      ],
+    },
+    {
+      code: `
+        /* // nested comment
+         foo(a, function(){
+             doSmth();
+         });
+         */`,
+      errors: [
+        {
+          message: "Remove this commented out code.",
+          line: 2,
+          column: 9,
+          endLine: 6,
+          endColumn: 12,
+        },
+      ],
+    },
+    {
+      code: `
+        // var object = {};
+      
+        // return foo().bar();
+      
+        // return foo().bar()
+      
+        // throw foo().bar()
+        
+        // foo();
+        // bar();
+        
+        /* foo();
+           bar(); */
+           
+        /* throw foo().bar() */
+         
+        // if (condition) {
+        //   while (condition) {
+        //     doSomething();
+        
+        //   while (condition) {
+        //     doSomething();
+        //   }
+        // }
+      
+        // }}
+        
+        // {{
+      
+        //   }
+        // }`,
+      errors: 12,
+    },
+  ],
+});
