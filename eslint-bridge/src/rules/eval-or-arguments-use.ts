f2654e108e4ecd3b4275a2a9063f311cda6d5d40
quentin-jaquier-sonarsource <43733433+quentin-jaquier-sonarsource@users.noreply.github.com>
2019-12-20 15:22:26 +0100

    Add rule S1514 (BoundOrAssignedEvalOrArguments) for JS/TS (new rule for TS) (#1827)

---
+++ eslint-bridge/src/rules/eval-or-arguments-use.ts
@@ -0,0 +1,99 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2019 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-1514
+
+import { Rule } from "eslint";
+import * as estree from "estree";
+
+type FunctionNodeInterType = estree.FunctionDeclaration &
+  estree.FunctionExpression &
+  estree.ArrowFunctionExpression;
+
+const illegalName = ["eval", "arguments"];
+
+const getDeclareMessage = (redeclareType: string) => (name: string) =>
+  `Do not use "${name}" to declare a ${redeclareType} - use another name.`;
+
+const getModificationMessage = (functionName: string) =>
+  `Remove the modification of "${functionName}".`;
+
+export const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    return {
+      ":function": function(node: estree.Node) {
+        const func = node as FunctionNodeInterType;
+        reportBadUsage(func.id, getDeclareMessage("function"), context);
+        func.params.forEach(p => {
+          reportBadUsage(p, getDeclareMessage("parameter"), context);
+        });
+      },
+      VariableDeclaration(node: estree.Node) {
+        (node as estree.VariableDeclaration).declarations.forEach(decl => {
+          reportBadUsage(decl.id, getDeclareMessage("variable"), context);
+        });
+      },
+      UpdateExpression(node: estree.Node) {
+        reportBadUsage((node as estree.UpdateExpression).argument, getModificationMessage, context);
+      },
+      AssignmentExpression(node: estree.Node) {
+        reportBadUsage((node as estree.AssignmentExpression).left, getModificationMessage, context);
+      },
+      CatchClause(node: estree.Node) {
+        reportBadUsage((node as estree.CatchClause).param, getDeclareMessage("variable"), context);
+      },
+    };
+  },
+};
+
+function reportBadUsage(
+  node: estree.Node | null | undefined,
+  buildMessage: (name: string) => string,
+  context: Rule.RuleContext,
+) {
+  if (node) {
+    switch (node.type) {
+      case "Identifier": {
+        if (illegalName.includes(node.name)) {
+          context.report({
+            message: buildMessage(node.name),
+            node: node,
+          });
+        }
+        break;
+      }
+      case "RestElement":
+        reportBadUsage(node.argument, buildMessage, context);
+        break;
+      case "ObjectPattern":
+        node.properties.forEach(prop => {
+          reportBadUsage(prop.value, buildMessage, context);
+        });
+        break;
+      case "ArrayPattern":
+        node.elements.forEach(elem => {
+          reportBadUsage(elem, buildMessage, context);
+        });
+        break;
+      case "AssignmentPattern":
+        reportBadUsage(node.left, buildMessage, context);
+        break;
+    }
+  }
+}
