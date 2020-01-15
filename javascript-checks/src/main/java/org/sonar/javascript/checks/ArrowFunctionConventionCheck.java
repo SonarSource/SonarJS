34d3f84609e18ba0822be9669dc7316637e66826
Alban Auzeill <alban.auzeill@sonarsource.com>
2020-01-15 08:48:47 +0100

    Add rule S3524 (ArrowFunctionConventionCheck) for JS/TS (new rule for TS) (#1890)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/ArrowFunctionConventionCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/ArrowFunctionConventionCheck.java
@@ -19,29 +19,17 @@
  */
 package org.sonar.javascript.checks;
 
+import java.util.Collections;
 import java.util.List;
 import org.sonar.check.Rule;
 import org.sonar.check.RuleProperty;
 import org.sonar.javascript.checks.annotations.JavaScriptRule;
-import org.sonar.plugins.javascript.api.tree.SeparatedList;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.declaration.BindingElementTree;
-import org.sonar.plugins.javascript.api.tree.declaration.ParameterListTree;
-import org.sonar.plugins.javascript.api.tree.expression.ArrowFunctionTree;
-import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
-import org.sonar.plugins.javascript.api.tree.statement.BlockTree;
-import org.sonar.plugins.javascript.api.tree.statement.ReturnStatementTree;
-import org.sonar.plugins.javascript.api.tree.statement.StatementTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
+import org.sonar.javascript.checks.annotations.TypeScriptRule;
 
 @JavaScriptRule
+@TypeScriptRule
 @Rule(key = "S3524")
-public class ArrowFunctionConventionCheck extends DoubleDispatchVisitorCheck {
-
-  private static final String MESSAGE_ADD_PARAMETER = "Add parentheses around the parameter of this arrow function.";
-  private static final String MESSAGE_ADD_BODY = "Add curly braces and \"return\" to this arrow function body.";
-  private static final String MESSAGE_REMOVE_PARAMETER = "Remove parentheses around the parameter of this arrow function.";
-  private static final String MESSAGE_REMOVE_BODY = "Remove curly braces and \"return\" from this arrow function body.";
+public class ArrowFunctionConventionCheck extends EslintBasedCheck {
 
   private static final boolean DEFAULT_PARAMETER_PARENS = false;
   private static final boolean DEFAULT_BODY_BRACES = false;
@@ -50,80 +38,32 @@ public class ArrowFunctionConventionCheck extends DoubleDispatchVisitorCheck {
     key = "parameter_parens",
     description = "True to require parentheses around parameters. False to forbid them for single parameter.",
     defaultValue = "" + DEFAULT_PARAMETER_PARENS)
-  private boolean parameterParens = DEFAULT_PARAMETER_PARENS;
+  boolean parameterParens = DEFAULT_PARAMETER_PARENS;
 
   @RuleProperty(
     key = "body_braces",
     description = "True to require curly braces around function body. False to forbid them for single-return bodies.",
     defaultValue = "" + DEFAULT_BODY_BRACES)
-  private boolean bodyBraces = DEFAULT_BODY_BRACES;
+  boolean bodyBraces = DEFAULT_BODY_BRACES;
 
-  public void setParameterParens(boolean value) {
-    parameterParens = value;
-  }
-  public void setBodyBraces(boolean value) {
-    bodyBraces = value;
+  @Override
+  public String eslintKey() {
+    return "arrow-function-convention";
   }
 
   @Override
-  public void visitArrowFunction(ArrowFunctionTree tree) {
-    checkParameterClause(tree);
-    checkBody(tree);
-
-    super.visitArrowFunction(tree);
+  public List<Object> configurations() {
+    return Collections.singletonList(new Config(parameterParens, bodyBraces));
   }
 
-  private void checkBody(ArrowFunctionTree tree) {
-    boolean hasBodyBraces = tree.body().is(Kind.BLOCK);
+  private static class Config {
+    boolean requireParameterParentheses;
+    boolean requireBodyBraces;
 
-    if (bodyBraces && !hasBodyBraces) {
-      addIssue(tree.body(), MESSAGE_ADD_BODY);
-    }
-
-    if (!bodyBraces && hasBodyBraces) {
-      List<StatementTree> statements = ((BlockTree) tree.body()).statements();
-
-      if (statements.size() == 1) {
-        StatementTree statement = statements.get(0);
-
-        if (isRemovableReturn(statement)) {
-          addIssue(tree.body(), MESSAGE_REMOVE_BODY);
-        }
-      }
+    Config(boolean requireParameterParentheses, boolean requireBodyBraces) {
+      this.requireParameterParentheses = requireParameterParentheses;
+      this.requireBodyBraces = requireBodyBraces;
     }
   }
 
-  private static boolean isRemovableReturn(StatementTree tree) {
-    if (tree.is(Kind.RETURN_STATEMENT)) {
-      ExpressionTree expression = ((ReturnStatementTree) tree).expression();
-
-      if (expression != null && !expression.is(Kind.OBJECT_LITERAL)) {
-        int firstLine = expression.firstToken().line();
-        int lastLine = expression.lastToken().line();
-        return firstLine == lastLine;
-      }
-    }
-
-    return false;
-  }
-
-  private void checkParameterClause(ArrowFunctionTree tree) {
-    boolean hasParameterParens = tree.parameterClause().is(Kind.PARAMETER_LIST);
-
-    if (parameterParens && !hasParameterParens) {
-      addIssue(tree.parameterClause(), MESSAGE_ADD_PARAMETER);
-    }
-
-    if (!parameterParens && hasParameterParens) {
-      ParameterListTree parameterListTree = (ParameterListTree) tree.parameterClause();
-      SeparatedList<BindingElementTree> parameters = parameterListTree.parameters();
-      if (parameters.size() == 1 && parameters.get(0).is(Kind.BINDING_IDENTIFIER) && !hasCommentInside(parameterListTree)) {
-        addIssue(tree.parameterClause(), MESSAGE_REMOVE_PARAMETER);
-      }
-    }
-  }
-
-  private static boolean hasCommentInside(ParameterListTree parameterListTree) {
-    return !parameterListTree.closeParenthesisToken().trivias().isEmpty();
-  }
 }
