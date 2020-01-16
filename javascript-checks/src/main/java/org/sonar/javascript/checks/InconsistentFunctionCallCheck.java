a645cf25cbaed7feb39e6b97493495433e30310b
quentin-jaquier-sonarsource <43733433+quentin-jaquier-sonarsource@users.noreply.github.com>
2020-01-16 09:06:13 +0100

    Add rule S3686 (InconsistentFunctionCallCheck) for JS (#1897)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/InconsistentFunctionCallCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/InconsistentFunctionCallCheck.java
@@ -19,107 +19,16 @@
  */
 package org.sonar.javascript.checks;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
-import javax.annotation.CheckForNull;
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.annotations.JavaScriptRule;
-import org.sonar.plugins.javascript.api.symbols.Symbol;
-import org.sonar.plugins.javascript.api.tree.ScriptTree;
-import org.sonar.plugins.javascript.api.tree.Tree;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.expression.CallExpressionTree;
-import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
-import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
-import org.sonar.plugins.javascript.api.tree.expression.NewExpressionTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
-import org.sonar.plugins.javascript.api.visitors.IssueLocation;
-import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
 
 @JavaScriptRule
 @Rule(key = "S3686")
-public class InconsistentFunctionCallCheck extends DoubleDispatchVisitorCheck {
-
-  private static final String MESSAGE = "Correct the use of this function; on line %s it was called with%s \"new\".";
-  private static final String SECONDARY_MESSAGE = "Called with%s \"new\"";
-
-  private Set<Symbol> hasIssue = new HashSet<>();
-
-  private Map<Symbol, CallExpressionTree> usedInCallExpression = new HashMap<>();
-  private Map<Symbol, NewExpressionTree> usedInNewExpression = new HashMap<>();
+public class InconsistentFunctionCallCheck extends EslintBasedCheck {
 
   @Override
-  public void visitScript(ScriptTree tree) {
-    super.visitScript(tree);
-
-    usedInCallExpression.clear();
-    usedInNewExpression.clear();
+  public String eslintKey() {
+    return "inconsistent-function-call";
   }
 
-  @Override
-  public void visitCallExpression(CallExpressionTree tree) {
-    checkExpression(tree, tree.callee(), usedInNewExpression, usedInCallExpression, "");
-    super.visitCallExpression(tree);
-  }
-
-  @Override
-  public void visitNewExpression(NewExpressionTree tree) {
-    checkExpression(tree, tree.expression(), usedInCallExpression, usedInNewExpression, "out");
-    super.visitNewExpression(tree);
-  }
-
-  private <T1 extends Tree, T2 extends Tree> void checkExpression(
-    T1 tree, ExpressionTree symbolTree,
-    Map<Symbol, T2> otherTypeUsageMap, Map<Symbol, T1> thisTypeUsageMap, String tail
-  ) {
-    Symbol symbol = getSymbol(symbolTree);
-    if (symbol == null || symbol.external()) {
-      return;
-    }
-
-    T2 otherTypeUsage = otherTypeUsageMap.get(symbol);
-
-    if (otherTypeUsage != null && !hasIssue.contains(symbol)) {
-
-      String message = String.format(MESSAGE, otherTypeUsage.firstToken().line(), tail);
-      String secondaryMessage = String.format(SECONDARY_MESSAGE, tail);
-
-      addIssue(new PreciseIssue(this, issueLocation(tree, message)))
-        .secondary(issueLocation(otherTypeUsage, secondaryMessage));
-
-      hasIssue.add(symbol);
-
-    } else {
-      thisTypeUsageMap.put(symbol, tree);
-    }
-
-  }
-
-  private static <T extends Tree> IssueLocation issueLocation(T tree, String message) {
-    if (tree.is(Kind.NEW_EXPRESSION)) {
-      NewExpressionTree newExpressionTree = (NewExpressionTree) tree;
-      return new IssueLocation(
-        newExpressionTree.newKeyword(),
-        newExpressionTree.expression(),
-        message
-      );
-
-    } else {
-      CallExpressionTree callExpressionTree = (CallExpressionTree) tree;
-      return new IssueLocation(
-        callExpressionTree.callee(),
-        message
-      );
-    }
-  }
-
-  @CheckForNull
-  private static Symbol getSymbol(ExpressionTree expression) {
-    if (expression.is(Kind.IDENTIFIER_REFERENCE)) {
-      return ((IdentifierTree) expression).symbol().orElse(null);
-    }
-    return null;
-  }
 }
