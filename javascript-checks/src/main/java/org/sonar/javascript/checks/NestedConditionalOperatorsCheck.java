ef7ae8ad50c86f5a42d7121a733ec89d53925aa5
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2020-09-04 09:45:06 +0200

    Migrate S3358 to eslint implementation (no-nested-ternary) (#2096)

--- javascript-checks/src/main/java/org/sonar/javascript/checks/NestedConditionalOperatorsCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/NestedConditionalOperatorsCheck.java
@@ -21,58 +21,15 @@ package org.sonar.javascript.checks;
 
 import org.sonar.check.Rule;
 import org.sonar.javascript.checks.annotations.JavaScriptRule;
-import org.sonar.javascript.tree.KindSet;
-import org.sonar.plugins.javascript.api.tree.ScriptTree;
-import org.sonar.plugins.javascript.api.tree.Tree;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.expression.ConditionalExpressionTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
+import org.sonar.javascript.checks.annotations.TypeScriptRule;
 
 @JavaScriptRule
+@TypeScriptRule
 @Rule(key = "S3358")
-public class NestedConditionalOperatorsCheck extends DoubleDispatchVisitorCheck {
-
-  private static final String MESSAGE = "Extract this nested ternary operation into an independent statement.";
-
-  private int nestingLevel;
+public class NestedConditionalOperatorsCheck extends EslintBasedCheck {
 
   @Override
-  public void visitScript(ScriptTree tree) {
-    nestingLevel = 0;
-
-    super.visitScript(tree);
+  public String eslintKey() {
+    return "no-nested-conditional";
   }
-
-  @Override
-  public void visitConditionalExpression(ConditionalExpressionTree conditionalExpression) {
-    if (nestingLevel > 0 && !isNestingBroken(conditionalExpression)) {
-      addIssue(conditionalExpression, MESSAGE);
-    }
-    nestingLevel++;
-
-    super.visitConditionalExpression(conditionalExpression);
-
-    nestingLevel--;
-  }
-
-  /**
-   * Returns true if, between the ConditionalExpression and its nesting ConditionExpression,
-   * there is a construct that functionally "disconnects" the two ConditionExpressions. 
-   */
-  private static boolean isNestingBroken(ConditionalExpressionTree conditionalExpression) {
-    Tree parent = conditionalExpression.parent();
-    while (!parent.is(Kind.CONDITIONAL_EXPRESSION)) {
-      if (breaksNesting(parent)) {
-        return true;
-      }
-      parent = parent.parent();
-    }
-    return false;
-  }
-
-  private static boolean breaksNesting(Tree tree) {
-    return tree.is(Kind.ARRAY_LITERAL, Kind.OBJECT_LITERAL) ||
-      tree.is(KindSet.FUNCTION_KINDS);
-  }
-
 }
