e17d763850193fc8b47365b5e502fdad692bbc2e
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2020-09-07 12:41:18 +0200

    Switch to a custom eslint-based implementation

---
+++ eslint-bridge/src/rules/no-const-assign.ts
@@ -0,0 +1,58 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-3500
+
+import { Rule, Scope } from 'eslint';
+import * as estree from 'estree';
+import { toEncodedMessage } from './utils';
+
+export const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    return {
+      'VariableDeclaration[kind="const"]': (node: estree.Node) => {
+        context.getDeclaredVariables(node).forEach(variable =>
+          variable.references.filter(isModifyingReference).forEach(reference =>
+            context.report({
+              message: toEncodedMessage(
+                `Correct this attempt to modify "${reference.identifier.name}" or use "let" in its declaration.`,
+                [node],
+                ['Const declaration'],
+              ),
+              node: reference.identifier,
+            }),
+          ),
+        );
+      },
+    };
+  },
+};
+
+function isModifyingReference(
+  reference: Scope.Reference,
+  index: number,
+  references: Scope.Reference[],
+) {
+  const identifier = reference.identifier;
+  const modifyingDifferentIdentifier =
+    index === 0 || references[index - 1].identifier !== identifier;
+  return (
+    identifier && reference.init === false && reference.isWrite() && modifyingDifferentIdentifier
+  );
+}
