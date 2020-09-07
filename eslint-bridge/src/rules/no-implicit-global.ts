cb34455e2b24b9857ba44f3ff6f327390e9d2874
Andrea Guarino <andrea.guarino@sonarsource.com>
2020-09-07 08:59:19 +0200

    Migrate S2703 to eslint implementation (no-implicit-global)

---
+++ eslint-bridge/src/rules/no-implicit-global.ts
@@ -0,0 +1,52 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-2703
+
+import { Rule } from 'eslint';
+import { globalsByLibraries } from '../utils/globals';
+import { flatMap } from './utils';
+
+const message = (variable: string) =>
+  `Add the "let", "const" or "var" keyword to this declaration of "${variable}" to make it explicit.`;
+
+const excludedNames = flatMap(Object.values(globalsByLibraries), globals => globals);
+
+export const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    return {
+      'Program:exit'() {
+        const globalScope = context.getScope();
+        const alreadyReported: string[] = [];
+        globalScope.through
+          .filter(ref => ref.isWrite())
+          .forEach(ref => {
+            const name = ref.identifier.name;
+            if (!alreadyReported.includes(name) && !excludedNames.includes(name)) {
+              alreadyReported.push(name);
+              context.report({
+                message: message(name),
+                node: ref.identifier,
+              });
+            }
+          });
+      },
+    };
+  },
+};
