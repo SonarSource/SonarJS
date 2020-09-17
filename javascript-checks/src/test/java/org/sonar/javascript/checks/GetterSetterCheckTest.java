3f3a016b2d42aace1d266c0b119c9e5d12b0ed12
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2020-09-17 15:59:27 +0200

    Migrate S2376 to eslint implementation (accessor-pairs)

--- javascript-checks/src/test/java/org/sonar/javascript/checks/GetterSetterCheckTest.java
+++ javascript-checks/src/test/java/org/sonar/javascript/checks/GetterSetterCheckTest.java
@@ -19,14 +19,16 @@
  */
 package org.sonar.javascript.checks;
 
-import java.io.File;
+import com.google.gson.Gson;
 import org.junit.Test;
-import org.sonar.javascript.checks.verifier.JavaScriptCheckVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class GetterSetterCheckTest {
 
   @Test
   public void test() {
-    JavaScriptCheckVerifier.verify(new GetterSetterCheck(), new File("src/test/resources/checks/GetterSetter.js"));
+    String configAsString = new Gson().toJson(new GetterSetterCheck().configurations());
+    assertThat(configAsString).isEqualTo("[{\"getWithoutSet\":true}]");
   }
 }
