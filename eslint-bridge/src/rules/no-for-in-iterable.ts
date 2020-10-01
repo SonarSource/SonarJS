58f1e5f46ed8038dc15eea7224356b3170cbba06
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2020-10-01 11:25:54 +0200

    Rule: "for in" should not be used with iterables (#2189)

---
+++ eslint-bridge/src/rules/no-for-in-iterable.ts
@@ -0,0 +1,80 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-4139
+
+import { Rule } from 'eslint';
+import * as estree from 'estree';
+import ts from 'typescript';
+import { isRequiredParserServices } from '../utils/isRequiredParserServices';
+import { getTypeFromTreeNode } from './utils';
+
+export const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    const services = context.parserServices;
+    if (!isRequiredParserServices(services)) {
+      return {};
+    }
+    return {
+      ForInStatement: (node: estree.Node) => {
+        const type = getTypeFromTreeNode((node as estree.ForInStatement).right, services);
+        if (isIterable(type)) {
+          const iterable = type.symbol ? type.symbol.name : 'String';
+          context.report({
+            message: `Use "for...of" to iterate over this "${iterable}".`,
+            loc: context.getSourceCode().getFirstToken(node)!.loc,
+          });
+        }
+      },
+    };
+  },
+};
+
+function isIterable(type: ts.Type) {
+  return isCollection(type) || isString(type);
+}
+
+function isCollection(type: ts.Type) {
+  return (
+    type.symbol !== undefined &&
+    [
+      'Array',
+      'Int8Array',
+      'Uint8Array',
+      'Uint8ClampedArray',
+      'Int16Array',
+      'Uint16Array',
+      'Int32Array',
+      'Uint32Array',
+      'Float32Array',
+      'Float64Array',
+      'BigInt64Array',
+      'BigUint64Array',
+      'Set',
+      'Map',
+    ].includes(type.symbol.name)
+  );
+}
+
+function isString(type: ts.Type) {
+  return (
+    (type.symbol !== undefined && type.symbol.name === 'String') ||
+    (type.flags & ts.TypeFlags.StringLike) !== 0
+  );
+}
