08e36d4b579055e563ac5863fe9fc8942976c4a1
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2020-10-02 12:28:04 +0200

    Migrate S2201 to eslint frontend for JS (#2194)

--- javascript-checks/src/test/java/org/sonar/javascript/checks/CheckListTest.java
+++ javascript-checks/src/test/java/org/sonar/javascript/checks/CheckListTest.java
@@ -86,7 +86,6 @@ public class CheckListTest {
     keys.remove("S3402");
     keys.remove("S3533");
     keys.remove("S3863");
-    keys.remove("S2201");
     keys.remove("S4043");
     
     // this rule has the same implementation for TS and JS, but defines a different rule property
