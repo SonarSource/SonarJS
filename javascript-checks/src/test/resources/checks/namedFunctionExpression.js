026bf7d9361ccd6c8971ca900b16f4eda7cbd27d
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-10-21 16:40:04 +0200

    Drop rule S1517

--- javascript-checks/src/test/resources/checks/namedFunctionExpression.js
+++
@@ -1,9 +0,0 @@
-f = function fun(){};  // Noncompliant {{Make this function anonymous by removing its name: 'function() {...}'.}}
-new function fun(){}; // Noncompliant [[sc=14;ec=17]]
-(function fun(){}); // Noncompliant
-
-f = function(){}; // function expression
-new function(){}; // function expression
-(function(){}); // function expression
-
-function fun(){} // function declaration
