a54e6e075cdc1214867f3937e069136db52eec29
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-11-04 17:51:41 +0100

    Rule S5743: Allowing browsers to perform DNS prefetching is security-sensitive (#2300)

---
+++ eslint-bridge/src/rules/dns-prefetching.ts
@@ -0,0 +1,56 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-5743
+
+import { Rule } from 'eslint';
+import * as estree from 'estree';
+import { checkSensitiveCall } from '../utils/sensitive-arguments';
+import { getModuleNameOfNode, isCallToFQN } from './utils';
+
+const MESSAGE = 'Make sure allowing browsers to perform DNS prefetching is safe here.';
+
+export const rule: Rule.RuleModule = {
+  meta: {
+    schema: [
+      {
+        // internal parameter for rules having secondary locations
+        enum: ['sonar-runtime'],
+      },
+    ],
+  },
+  create(context: Rule.RuleContext) {
+    return {
+      CallExpression: (node: estree.Node) => {
+        const callExpression = node as estree.CallExpression;
+        const { callee } = callExpression;
+        if (isCallToFQN(context, callExpression, 'helmet', 'dnsPrefetchControl')) {
+          checkSensitiveCall(context, callExpression, 0, 'allow', true, MESSAGE);
+        }
+        const calledModule = getModuleNameOfNode(context, callee);
+        if (calledModule?.value === 'helmet') {
+          checkSensitiveCall(context, callExpression, 0, 'dnsPrefetchControl', false, MESSAGE);
+        }
+        if (calledModule?.value === 'dns-prefetch-control') {
+          checkSensitiveCall(context, callExpression, 0, 'allow', true, MESSAGE);
+        }
+      },
+    };
+  },
+};
