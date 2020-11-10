3cd9bed7fc4226dd44fd77566f75926072292bd8
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-11-10 11:30:11 +0100

    Migrate S1774 to eslint frontend (new rule for TS)

--- javascript-checks/src/test/resources/checks/conditionalOperator.js
+++
@@ -1,4 +0,0 @@
-function sayHello() {
-  var a = (a === 'a') ? 'is a' : 'is not a'; // Noncompliant {{Convert this usage of the ternary operator to an "if"/"else" structure.}}
-//                    ^
-}
