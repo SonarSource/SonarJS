eeddc86ad3248cd925426dcaed3405b9f3ef16aa
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2020-11-11 09:08:01 +0100

    Fix rule template for MMF-1898 (#2324)

--- its/sources/file-for-rules/S5732.js
+++ its/sources/file-for-rules/S5732.js
@@ -1,4 +1,5 @@
 const csp = require('helmet-csp');
+const helmet = require('helmet');
 module.exports.sensitiveCspFrame = function(app) {
   app.use(
     csp({
@@ -6,5 +7,22 @@ module.exports.sensitiveCspFrame = function(app) {
         defaultSrc: ["'self'", 'example.com', 'code.jquery.com'],
       } // Sensitive: frame-ancestors is not set
     })
+  );
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+      }, // Sensitive: frameAncestors is not set
+    }),
+  );
+
+  app.use(
+    helmet.contentSecurityPolicy({
+      directives: {
+        defaultSrc: ["'self'"],
+        frameAncestors: ["'none'"],
+      }, // Sensitive: frameAncestors is set to none
+    }),
   );
 };
