dfcebe54b27ee3ca54f54c6b57203cf7c70433fa
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-11-19 17:30:40 +0100

    Migrate S3827 to eslint-based implementation

--- javascript-checks/src/main/java/org/sonar/javascript/checks/ReferenceErrorCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/ReferenceErrorCheck.java
@@ -19,81 +19,15 @@
  */
 package org.sonar.javascript.checks;
 
-import com.google.common.collect.ArrayListMultimap;
-import com.google.common.collect.ListMultimap;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
 import org.sonar.check.Rule;
+import org.sonar.plugins.javascript.api.EslintBasedCheck;
 import org.sonar.plugins.javascript.api.JavaScriptRule;
-import org.sonar.plugins.javascript.api.tree.ScriptTree;
-import org.sonar.plugins.javascript.api.tree.Tree.Kind;
-import org.sonar.plugins.javascript.api.tree.declaration.ExportClauseTree;
-import org.sonar.plugins.javascript.api.tree.expression.ExpressionTree;
-import org.sonar.plugins.javascript.api.tree.expression.IdentifierTree;
-import org.sonar.plugins.javascript.api.tree.expression.UnaryExpressionTree;
-import org.sonar.plugins.javascript.api.tree.statement.WithStatementTree;
-import org.sonar.plugins.javascript.api.visitors.DoubleDispatchVisitorCheck;
-import org.sonar.plugins.javascript.api.visitors.PreciseIssue;
 
 @JavaScriptRule
 @Rule(key = "S3827")
-public class ReferenceErrorCheck extends DoubleDispatchVisitorCheck {
-
-  private static final String MESSAGE = "\"%s\" does not exist. Change its name or declare it so that its usage doesn't result in a \"ReferenceError\".";
-
-  private final ListMultimap<String, IdentifierTree> undeclaredIdentifiersByName = ArrayListMultimap.create();
-  private final Set<String> excludedNames = new HashSet<>();
-
+public class ReferenceErrorCheck implements EslintBasedCheck {
   @Override
-  public void visitScript(ScriptTree tree) {
-    undeclaredIdentifiersByName.clear();
-    excludedNames.clear();
-
-    super.visitScript(tree);
-
-    for (String name : undeclaredIdentifiersByName.keySet()) {
-      List<IdentifierTree> identifiers = undeclaredIdentifiersByName.get(name);
-      PreciseIssue issue = addIssue(identifiers.get(0), String.format(MESSAGE, name));
-      identifiers.subList(1, identifiers.size()).stream().forEach(issue::secondary);
-    }
-  }
-
-  @Override
-  public void visitIdentifier(IdentifierTree identifier) {
-    if (identifier.is(Kind.IDENTIFIER_REFERENCE)
-      && !identifier.symbol().isPresent()
-      && !"undefined".equals(identifier.name())
-      && !excludedNames.contains(identifier.name())) {
-
-      undeclaredIdentifiersByName.put(identifier.name(), identifier);
-
-    }
-    super.visitIdentifier(identifier);
-  }
-
-  @Override
-  public void visitWithStatement(WithStatementTree tree) {
-    // Don't visit with statements, we cannot resolve
-  }
-
-  @Override
-  public void visitUnaryExpression(UnaryExpressionTree unaryExpression) {
-    if (unaryExpression.is(Kind.TYPEOF)) {
-      ExpressionTree expression = unaryExpression.expression();
-      if (expression.is(Kind.IDENTIFIER_REFERENCE)) {
-        excludedNames.add(((IdentifierTree) expression).name());
-      }
-    }
-    super.visitUnaryExpression(unaryExpression);
-  }
-
-  @Override
-  public void visitExportClause(ExportClauseTree tree) {
-    if (tree.fromClause() != null) {
-      // don't visit identifiers exported from another module
-      return;
-    }
-    super.visitExportClause(tree);
+  public String eslintKey() {
+    return "no-reference-error";
   }
 }
