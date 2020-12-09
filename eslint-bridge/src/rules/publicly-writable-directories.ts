bc86377fd84936d43a7775d491746a470490039d
Guillaume Dequenne <guillaume.dequenne@sonarsource.com>
2020-12-09 17:55:24 +0100

    Rule S5443: Using publicly writable directories is security-sensitive (#2418)

---
+++ eslint-bridge/src/rules/publicly-writable-directories.ts
@@ -0,0 +1,84 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2020 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://jira.sonarsource.com/browse/RSPEC-5443
+
+import { Rule } from 'eslint';
+import * as estree from 'estree';
+
+const UNIX_DIRECTORIES = [
+  '/tmp/',
+  '/var/tmp/',
+  '/usr/tmp/',
+  '/dev/shm/',
+  '/dev/mqueue/',
+  '/run/lock/',
+  '/var/run/lock/',
+  '/library/caches/',
+  '/users/shared/',
+  '/private/tmp/',
+  '/private/var/tmp/',
+].map(v => new RegExp(`^${v}`, 'i'));
+
+const WINDOWS_DIRECTORIES_PATTERN = new RegExp(
+  '^[^\\\\]*(\\\\){1,2}(Windows(\\\\){1,2}Temp|Temp|TMP)(\\\\.*|$)',
+  'i',
+);
+const SENSITIVE_ENV_VARIABLES = ['TMPDIR', 'TMP', 'TEMPDIR', 'TEMP'];
+
+const MESSAGE = 'Make sure publicly writable directories are used safely here.';
+export const rule: Rule.RuleModule = {
+  create(context: Rule.RuleContext) {
+    return {
+      Literal: (node: estree.Node) => {
+        const literal = node as estree.Literal;
+        // Using literal.raw instead of literal.value as the latter escapes backslashes
+        const value = literal.raw?.slice(1, literal.raw.length - 1);
+        if (
+          value &&
+          (UNIX_DIRECTORIES.find(i => value.match(i)) || value.match(WINDOWS_DIRECTORIES_PATTERN))
+        ) {
+          context.report({
+            node: literal,
+            message: MESSAGE,
+          });
+        }
+      },
+      MemberExpression: (node: estree.Node) => {
+        const memberExpression = node as estree.MemberExpression;
+        const { object, property } = memberExpression;
+        if (
+          property.type !== 'Identifier' ||
+          !SENSITIVE_ENV_VARIABLES.includes(property.name) ||
+          object.type !== 'MemberExpression'
+        ) {
+          return;
+        }
+        if (
+          object.property.type === 'Identifier' &&
+          object.property.name === 'env' &&
+          object.object.type === 'Identifier' &&
+          object.object.name === 'process'
+        ) {
+          context.report({ node: memberExpression, message: MESSAGE });
+        }
+      },
+    };
+  },
+};
