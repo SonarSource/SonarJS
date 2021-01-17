cc90e3f70957a4c85e396651d53d5bd41db9b512
Margarita <margarita.nedzelska@sonarsource.com>
2021-01-17 11:39:30 +0100

    Remove dependency on sslr-squid-bridge

--- javascript-frontend/src/main/java/org/sonar/plugins/javascript/api/CustomJavaScriptRulesDefinition.java
+++ javascript-frontend/src/main/java/org/sonar/plugins/javascript/api/CustomJavaScriptRulesDefinition.java
@@ -20,11 +20,10 @@
 package org.sonar.plugins.javascript.api;
 
 import com.google.common.annotations.Beta;
-import com.google.common.collect.ImmutableList;
 import org.sonar.api.ExtensionPoint;
 import org.sonar.api.scanner.ScannerSide;
 import org.sonar.api.server.rule.RulesDefinition;
-import org.sonar.squidbridge.annotations.AnnotationBasedRulesDefinition;
+import org.sonar.api.server.rule.RulesDefinitionAnnotationLoader;
 
 /**
  * Extension point to create custom rule repository for JavaScript.
@@ -47,7 +46,7 @@ public abstract class CustomJavaScriptRulesDefinition implements RulesDefinition
     RulesDefinition.NewRepository repo = context.createRepository(repositoryKey(), "js").setName(repositoryName());
 
     // Load metadata from check classes' annotations
-    new AnnotationBasedRulesDefinition(repo, "javascript").addRuleClasses(false, ImmutableList.copyOf(checkClasses()));
+    new RulesDefinitionAnnotationLoader().load(repo, checkClasses());
 
     repo.done();
   }
