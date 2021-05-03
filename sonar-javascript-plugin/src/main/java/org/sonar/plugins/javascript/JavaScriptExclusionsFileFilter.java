288a72ff137745d1d1054c704a03c2344ada0ea3
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2021-05-03 12:56:16 +0200

    Exclusion filter should exclude only JS/TS files and patterns should work only on relative path (#2615)
    * Match exclude patterns only on relative path of input file
    
    * Remove **/build/** pattern
    
    * Exclude only JS/TS files
    
    * update ruling
    
    * update docs

--- sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/JavaScriptExclusionsFileFilter.java
+++ sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/JavaScriptExclusionsFileFilter.java
@@ -38,7 +38,6 @@ public class JavaScriptExclusionsFileFilter implements InputFileFilter {
     "**/node_modules/**",
     "**/bower_components/**",
     "**/dist/**",
-    "**/build/**",
     "**/vendor/**",
     "**/external/**",
     "**/*.d.ts"};
@@ -80,13 +79,17 @@ public class JavaScriptExclusionsFileFilter implements InputFileFilter {
       JavaScriptLanguage.KEY.equals(inputFile.language()) ||
       TypeScriptLanguage.KEY.equals(inputFile.language());
 
-    if (isJsTs && SizeAssessor.hasExcessiveSize(inputFile, maxFileSizeKb * 1000)) {
+    // filter only JS/TS files
+    if (!isJsTs) {
+      return true;
+    }
+
+    if (SizeAssessor.hasExcessiveSize(inputFile, maxFileSizeKb * 1000)) {
       LOG.debug("File {} was excluded because of excessive size", inputFile);
       return false;
     }
 
-    String relativePath = inputFile.uri().toString();
-    if (WildcardPattern.match(excludedPatterns, relativePath)) {
+    if (WildcardPattern.match(excludedPatterns, inputFile.relativePath())) {
       LOG.debug("File {} was excluded by {} or {}", inputFile, JavaScriptPlugin.JS_EXCLUSIONS_KEY, JavaScriptPlugin.TS_EXCLUSIONS_KEY);
       return false;
     }
--- sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/JavaScriptExclusionsFileFilterTest.java
+++ sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/JavaScriptExclusionsFileFilterTest.java
@@ -19,10 +19,15 @@
  */
 package org.sonar.plugins.javascript;
 
+import java.io.File;
 import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.function.Function;
 import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.sonar.api.batch.fs.InputFile;
 import org.sonar.api.batch.fs.internal.DefaultInputFile;
 import org.sonar.api.batch.fs.internal.TestInputFileBuilder;
 import org.sonar.api.config.internal.MapSettings;
@@ -38,6 +43,10 @@ public class JavaScriptExclusionsFileFilterTest {
   @Rule
   public LogTester logTester = new LogTester();
 
+  @Rule
+  public TemporaryFolder temporaryFolder = new TemporaryFolder();
+
+
   @Test
   public void should_exclude_node_modules_and_bower_components_by_default() throws Exception {
     MapSettings settings = new MapSettings();
@@ -46,7 +55,7 @@ public class JavaScriptExclusionsFileFilterTest {
     assertThat(filter.accept(inputFile("some_app.ts"))).isTrue();
     assertThat(filter.accept(inputFile("node_modules/some_lib.js"))).isFalse();
     assertThat(filter.accept(inputFile("vendor/some_lib.js"))).isFalse();
-    assertThat(filter.accept(inputFile("build/some_lib.js"))).isFalse();
+    assertThat(filter.accept(inputFile("build/some_lib.js"))).isTrue();
     assertThat(filter.accept(inputFile("dist/some_lib.js"))).isFalse();
     assertThat(filter.accept(inputFile("external/some_lib.js"))).isFalse();
     assertThat(logTester.logs(LoggerLevel.DEBUG)).contains("File test_node_modules/node_modules/some_lib.js was excluded by sonar.javascript.exclusions or sonar.typescript.exclusions");
@@ -166,6 +175,46 @@ public class JavaScriptExclusionsFileFilterTest {
     assertThat(filter.accept(inputFile("foo.d.ts"))).isFalse();
     assertThat(filter.accept(inputFile("dir/foo.d.ts"))).isFalse();
     assertThat(logTester.logs(LoggerLevel.DEBUG)).contains("File test_node_modules/dir/foo.d.ts was excluded by sonar.javascript.exclusions or sonar.typescript.exclusions");
+  }
+
+  @Test
+  public void should_exclude_only_on_relative_path() throws Exception {
+    File tmp = temporaryFolder.newFolder();
+    // **/vendor/** is excluded by default, however it should only be excluded under 'basedir', here it's above
+    Path basedirUnderVendor = tmp.toPath().resolve("vendor/basedir");
+    Path file = basedirUnderVendor.resolve("file.js");
+    InputFile inputFile = new TestInputFileBuilder("key", basedirUnderVendor.toFile(), file.toFile())
+      .setContents("alert('hello');")
+      .setCharset(StandardCharsets.UTF_8)
+      .setLanguage("js")
+      .build();
+    JavaScriptExclusionsFileFilter filter = new JavaScriptExclusionsFileFilter(new MapSettings().asConfig());
+    assertThat(filter.accept(inputFile)).isTrue();
+  }
+
+  @Test
+  public void should_exclude_only_jsts_files() throws Exception {
+    JavaScriptExclusionsFileFilter filter = new JavaScriptExclusionsFileFilter(new MapSettings().asConfig());
+    InputFile inputFile = new TestInputFileBuilder("key", "vendor/file.js")
+      .setContents("alert('hello');")
+      .setLanguage("js")
+      .setCharset(StandardCharsets.UTF_8)
+      .build();
+    assertThat(filter.accept(inputFile)).isFalse();
+
+    inputFile = new TestInputFileBuilder("key", "vendor/file.ts")
+      .setContents("alert('hello');")
+      .setLanguage("ts")
+      .setCharset(StandardCharsets.UTF_8)
+      .build();
+    assertThat(filter.accept(inputFile)).isFalse();
+
+    inputFile = new TestInputFileBuilder("key", "vendor/file.ts")
+      .setContents("alert('hello');")
+      .setLanguage("xxx")
+      .setCharset(StandardCharsets.UTF_8)
+      .build();
+    assertThat(filter.accept(inputFile)).isTrue();
   }
 
   /**
