502b3cb86b179393ced03cec1c1ddf4a04fb3c45
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2021-06-22 17:43:34 +0200

    remove impact analysis

--- ws_scan.sh
+++ ws_scan.sh
@@ -4,9 +4,7 @@ set -euox pipefail
 
 readonly URL="https://unified-agent.s3.amazonaws.com/wss-unified-agent.jar"
 readonly UNIFIED_AGENT_JAR="wss-unified-agent.jar"
-readonly X_MODULE_ANALYZER_JAR="xModuleAnalyzer-21.6.1.jar"
 readonly MD5_CHECKSUM="F2EB843816A572904954052756EB66E7"
-readonly SETUP_FILE="setupFile.txt"
 
 get_ws_agent() {
   if [[ ! -f "${UNIFIED_AGENT_JAR}" ]]; then
@@ -35,36 +33,13 @@ get_ws_agent() {
   fi
 }
 
-get_xModuleAnalyzer() {
-  local url="https://unified-agent.s3.amazonaws.com/xModuleAnalyzer/${X_MODULE_ANALYZER_JAR}"
-  curl \
-    --location \
-    --remote-name \
-    --remote-header-name \
-    "${url}"
-
-   if [[ ! -f "${X_MODULE_ANALYZER_JAR}" ]]; then
-    echo "Could not find downloaded Unified Agent" >&2
-    exit 1
-  fi
-
-}
 
 scan() {
-  # remove its/plugin/projects seems exclusion is not taken into account
-  rm -rf its
-  rm -rf ${SETUP_FILE}
-  rm -rf whitesource
-
   export WS_PRODUCTNAME=$(maven_expression "project.name")
   export WS_PROJECTNAME="${WS_PRODUCTNAME} ${PROJECT_VERSION%.*}"
   echo "${WS_PRODUCTNAME} - ${WS_PROJECTNAME}"
-  java -jar ${UNIFIED_AGENT_JAR} -d "${PWD}" -analyzeMultiModule ${SETUP_FILE}
-  cat ${SETUP_FILE}
-
-  java -jar ${X_MODULE_ANALYZER_JAR} -xModulePath ${SETUP_FILE} -fsaJarPath ${UNIFIED_AGENT_JAR} -c wss-unified-agent.config -aggregateModules True -viaDebug True
+  java -jar ${UNIFIED_AGENT_JAR}
 }
 
 get_ws_agent
-get_xModuleAnalyzer
 scan
