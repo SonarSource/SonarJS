75453a38d39577530fb476700a3774cea9540363
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2021-09-06 11:47:13 +0200

    Update comments

--- eslint-bridge/src/utils/utils-regex.ts
+++ eslint-bridge/src/utils/utils-regex.ts
@@ -209,8 +209,8 @@ export class SimplifiedRegexCharacterClass {
    * entry are in the character class and belong to the given tree.<br>
    * For any entry {@code codepoint -> null}, all the codepoints from {@code codepoint} up to (and excluding) the next
    * entry are not part of the character class.<br>
-   * So a codepoint is contained in this class if and only if {@code contents.floorEntry(codePoint).getValue()} is
-   * non-null and the tree returned by {@code getValue} will be the element of the character class which matches that
+   * So a codepoint is contained in this class if and only if {@code contents.le(codePoint).value} is
+   * non-null and the tree returned by {@code value} will be the element of the character class which matches that
    * code point.
    */
   private contents = createTree<number, Node | undefined>();
