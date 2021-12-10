e5e38e6b097ae3214ea5605e4ec5af35fe2fdff2
vilchik.elena <elena.vilchik@sonarsource.com>
2021-12-10 14:55:56 +0100

    Avoid failing if error during program creation

--- sonar-javascript-plugin/src/test/resources/mock-eslint-bridge/startServer.js
+++ sonar-javascript-plugin/src/test/resources/mock-eslint-bridge/startServer.js
@@ -11,32 +11,38 @@ console.log(`additional rules: [${process.argv[7]}]`);
 const requestHandler = (request, response) => {
   let data = "";
   request.on("data", (chunk) => (data += chunk));
-  request.on("end", () => console.log(data));
-  if (request.url === "/status" || request.url === "/new-tsconfig") {
-    response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("OK!");
-  } else if (request.url === "/tsconfig-files") {
-    response.end(
-      "{files: ['abs/path/file1', 'abs/path/file2', 'abs/path/file3']}"
-    );
-  } else if (request.url === "/init-linter") {
-    response.end("OK!");
-  } else if (request.url === "/load-rule-bundles") {
-    response.end("OK!");
-  } else if (request.url === "/close") {
-    response.end();
-    server.close();
-  } else if (request.url === "/create-program") {
-    response.end("{programId: '42', projectReferences: [], files: ['abs/path/file1', 'abs/path/file2', 'abs/path/file3']}");
-  } else if (request.url === "/delete-program") {
-    response.end("OK!");
-  } else {
-    // /analyze-with-program
-    // /analyze-js
-    // /analyze-ts
-    // /analyze-css
-    response.end("{ issues: [] }");
-  }
+  request.on("end", () => {
+    console.log(data);
+
+    if (request.url === "/status" || request.url === "/new-tsconfig") {
+        response.writeHead(200, {"Content-Type": "text/plain"});
+        response.end("OK!");
+      } else if (request.url === "/tsconfig-files") {
+        response.end(
+          "{files: ['abs/path/file1', 'abs/path/file2', 'abs/path/file3']}"
+        );
+      } else if (request.url === "/init-linter") {
+        response.end("OK!");
+      } else if (request.url === "/load-rule-bundles") {
+        response.end("OK!");
+      } else if (request.url === "/close") {
+        response.end();
+        server.close();
+      } else if (request.url === "/create-program" && data.includes('invalid')) {
+        response.end("{ error: 'failed to create program'}");
+      } else if (request.url === "/create-program") {
+        response.end("{programId: '42', projectReferences: [], files: ['abs/path/file1', 'abs/path/file2', 'abs/path/file3']}");
+      } else if (request.url === "/delete-program") {
+        response.end("OK!");
+      } else {
+        // /analyze-with-program
+        // /analyze-js
+        // /analyze-ts
+        // /analyze-css
+        response.end("{ issues: [] }");
+      }
+  });
+
 };
 
 const server = http.createServer(requestHandler);
