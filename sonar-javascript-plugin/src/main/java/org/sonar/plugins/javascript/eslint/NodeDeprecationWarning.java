6b52aa7c2fd514a7c8fefb5dbab362f14c0976df
vilchik.elena <elena.vilchik@sonarsource.com>
2022-02-17 11:51:48 +0100

    Remove unused import

--- sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/eslint/NodeDeprecationWarning.java
+++ sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/eslint/NodeDeprecationWarning.java
@@ -22,7 +22,6 @@ package org.sonar.plugins.javascript.eslint;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.IntStream;
 import org.sonar.api.scanner.ScannerSide;
 import org.sonar.api.utils.log.Logger;
 import org.sonar.api.utils.log.Loggers;
