5c7f483c4d5b61051446140369d295b77bb9b77a
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2022-03-03 19:08:11 +0100

    Support TypeScript 4.5 (#2991)

--- its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/EslintCustomRulesTest.java
+++ its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/EslintCustomRulesTest.java
@@ -94,7 +94,7 @@ class EslintCustomRulesTest {
     BuildResult buildResult = runBuild(orchestrator);
     assertThat(buildResult.getLogsLines(l -> l.matches(".*INFO: Deploying custom rules bundle jar:file:.*/custom-eslint-based-rules-1\\.0\\.0\\.tgz to .*"))).hasSize(1);
     assertThat(buildResult.getLogsLines(l -> l.contains("Custom JavaScript rules are deprecated and API will be removed in future version."))).isEmpty();
-    assertThat(buildResult.getLogsLines(l -> l.contains("TS API in custom rule: TS version 4.4.3"))).hasSize(2);
+    assertThat(buildResult.getLogsLines(l -> l.contains("TS API in custom rule: TS version 4.5.5"))).hasSize(2);
     List<Issue> issues = findIssues("eslint-custom-rules:sqKey", orchestrator);
     assertThat(issues).hasSize(2);
     assertThat(issues).extracting(Issue::getRule, Issue::getComponent, Issue::getLine, Issue::getMessage)
