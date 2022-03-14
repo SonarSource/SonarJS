3e23aa0146e8d8f74aa3373bb365d3ff71deeb20
vilchik.elena <elena.vilchik@sonarsource.com>
2022-03-14 11:04:09 +0100

    Add quick fix for S1534 (no-dupe-keys)

--- eslint-bridge/src/rules/decorators/index.ts
+++ eslint-bridge/src/rules/decorators/index.ts
@@ -1,4 +1,5 @@
 import { decorateAccessorPairs } from './accessor-pairs-decorator';
+import { decorateNoDupeKeys } from './no-dupe-keys-decorator';
 import { decorateNoRedeclare } from './no-redeclare-decorator';
 import { decorateObjectShorthand } from './object-shorthand-decorator';
 import { decoratePreferTemplate } from './prefer-template-decorator';
@@ -8,4 +9,5 @@ export const externalRuleDecorators = [
   { decorate: decoratePreferTemplate, ruleKey: 'prefer-template' },
   { decorate: decorateNoRedeclare, ruleKey: 'no-redeclare' },
   { decorate: decorateObjectShorthand, ruleKey: 'object-shorthand' },
+  { decorate: decorateNoDupeKeys, ruleKey: 'no-dupe-keys' },
 ];
