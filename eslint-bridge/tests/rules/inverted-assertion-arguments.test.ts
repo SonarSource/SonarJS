8d4f87faacf6f8c6c1f3e52dc36e22e093ad0ec8
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2022-03-16 11:43:50 +0100

    Add quick fix for S3415 ('inverted-assertion-arguments')

---
+++ eslint-bridge/tests/rules/inverted-assertion-arguments.test.ts
@@ -0,0 +1,112 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+import { rule } from 'rules/inverted-assertion-arguments';
+import { RuleTester } from 'eslint';
+
+const ruleTester = new RuleTester({ parserOptions: { ecmaVersion: 2018, sourceType: 'module' } });
+
+ruleTester.run('Assertion arguments should be passed in the correct order', rule, {
+  valid: [
+    {
+      code: `
+const assert = require('chai').assert;
+describe('suite', () => {
+  it('test', () => {
+    assert.fail(aNumber, 42);
+  });
+});`,
+    },
+  ],
+  invalid: [
+    {
+      code: `
+const assert = require('chai').assert;
+describe('suite', () => {
+  it('test', () => {
+    assert.fail(42, aNumber);
+  });
+});`,
+      errors: [
+        {
+          suggestions: [
+            {
+              desc: 'Swap arguments',
+              output: `
+const assert = require('chai').assert;
+describe('suite', () => {
+  it('test', () => {
+    assert.fail(aNumber, 42);
+  });
+});`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: `
+const assert = require('chai').expect;
+describe('suite', () => {
+  it('test', () => {
+    expect(42).to.equal(aNumber);
+  });
+});`,
+      errors: [
+        {
+          suggestions: [
+            {
+              output: `
+const assert = require('chai').expect;
+describe('suite', () => {
+  it('test', () => {
+    expect(aNumber).to.equal(42);
+  });
+});`,
+            },
+          ],
+        },
+      ],
+    },
+    {
+      code: `
+const assert = require('chai').should();
+describe('suite', () => {
+  it('test', () => {
+    should.fail(42, aNumber);
+  });
+});`,
+      errors: [
+        {
+          suggestions: [
+            {
+              output: `
+const assert = require('chai').should();
+describe('suite', () => {
+  it('test', () => {
+    should.fail(aNumber, 42);
+  });
+});`,
+            },
+          ],
+        },
+      ],
+    },
+  ],
+});
