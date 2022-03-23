858f350d97b6c8e3be982cad1e5c3527e5da285d
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2022-03-23 12:58:00 +0100

    Enable quick fix for S3981 ('no-collection-size-mischeck')

--- eslint-bridge/src/quickfix.ts
+++ eslint-bridge/src/quickfix.ts
@@ -46,6 +46,7 @@ const quickFixRules = new Set([
   'use-isnan',
 
   // eslint-plugin-sonarjs
+  'no-collection-size-mischeck',
   'no-inverted-boolean-check',
   'prefer-immediate-return',
   'prefer-while',
