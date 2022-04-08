f039b6cd89fcdbfa600c38d10c852b5bfcdfee47
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2022-04-08 08:45:30 +0200

    Fix FP S4662 ('at-rule-no-unknown'): Ignore Sass @forward rule

--- sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/css/rules/AtRuleNoUnknown.java
+++ sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/css/rules/AtRuleNoUnknown.java
@@ -29,7 +29,7 @@ import static org.sonar.plugins.javascript.css.rules.RuleUtils.splitAndTrim;
 @Rule(key = "S4662")
 public class AtRuleNoUnknown implements CssRule {
 
-  private static final String DEFAULT_IGNORED_AT_RULES = "value,at-root,content,debug,each,else,error,for,function,if,include,mixin,return,warn,while,extend,use,/^@.*/";
+  private static final String DEFAULT_IGNORED_AT_RULES = "value,at-root,content,debug,each,else,error,for,function,if,include,mixin,return,warn,while,extend,use,forward,/^@.*/";
 
   @RuleProperty(
     key = "ignoreAtRules",
