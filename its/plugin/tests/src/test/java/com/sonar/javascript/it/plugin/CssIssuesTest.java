6e602f4fe44e418b6b1efdaceab75a4b5a20ce24
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2022-04-11 15:37:22 +0200

    Fix plugin issue expectations
    A consequence of dropping the detection of invalid spacing

--- its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/CssIssuesTest.java
+++ its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/CssIssuesTest.java
@@ -160,13 +160,7 @@ class CssIssuesTest {
       tuple("css:S1116", "css-issues-project:src/file5.htm"),
       tuple("css:S1116", "css-issues-project:src/file6.vue"),
       tuple("css:S5362", "css-issues-project:src/file1.css"),
-      tuple("css:S5362", "css-issues-project:src/file1.css"),
-      tuple("css:S5362", "css-issues-project:src/file1.css"),
       tuple("css:S5362", "css-issues-project:src/file2.less"),
-      tuple("css:S5362", "css-issues-project:src/file2.less"),
-      tuple("css:S5362", "css-issues-project:src/file2.less"),
-      tuple("css:S5362", "css-issues-project:src/file3.scss"),
-      tuple("css:S5362", "css-issues-project:src/file3.scss"),
       tuple("css:S5362", "css-issues-project:src/file3.scss")
     );
   }
