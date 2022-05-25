38f76900b39d66d1907b8765e4bbbc0a2186113f
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-05-25 10:34:53 +0200

    fix extension

--- javascript-checks/src/main/java/org/sonar/javascript/checks/NoOsCommandsFromPathCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/NoOsCommandsFromPathCheck.java
@@ -21,13 +21,13 @@ package org.sonar.javascript.checks;
 
 import org.sonar.check.Rule;
 import org.sonar.plugins.javascript.api.JavaScriptRule;
-import org.sonar.plugins.javascript.api.TestFileCheck;
+import org.sonar.plugins.javascript.api.EslintBasedCheck;
 import org.sonar.plugins.javascript.api.TypeScriptRule;
 
 @TypeScriptRule
 @JavaScriptRule
 @Rule(key = "S4036")
-public class NoOsCommandsFromPathCheck extends TestFileCheck {
+public class NoOsCommandsFromPathCheck implements EslintBasedCheck {
 
   @Override
   public String eslintKey() {
