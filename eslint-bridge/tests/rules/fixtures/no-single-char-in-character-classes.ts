1667c23a8dc86eb80daf0fb8df00e3efcb67d560
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-05-30 17:56:30 +0200

    add more invalid test cases

--- eslint-bridge/tests/rules/fixtures/no-single-char-in-character-classes.ts
+++ eslint-bridge/tests/rules/fixtures/no-single-char-in-character-classes.ts
@@ -8,6 +8,19 @@ let invalid = [
     {
         code: /a[b]d/, // Noncompliant {{Replace this character class by the character itself.}}
         //      ^^^
-    }    
+    },
+    {
+        code: /[0]/, // Noncompliant {{Replace this character class by the character itself.}}
+        //     ^^^
+    },
+    {
+        code: /[[a]]/, // Noncompliant {{Replace this character class by the character itself.}}
+        //      ^^^
+    },
+    {
+        code: /[1-2[3]4-5]/, // Noncompliant {{Replace this character class by the character itself.}}
+        //         ^^^
+    },
 ];
+
   
