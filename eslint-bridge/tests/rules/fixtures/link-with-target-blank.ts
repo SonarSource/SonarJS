5a07ba645e3a217bad53f73a374598347099f1ac
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-06-08 18:27:27 +0200

    fix error msg

--- eslint-bridge/tests/rules/fixtures/link-with-target-blank.ts
+++ eslint-bridge/tests/rules/fixtures/link-with-target-blank.ts
@@ -4,7 +4,7 @@ window.open('https:/example.com/dangerous1', 'windowname', 'noopener');
 
 window.open('https:/example.com/dangerous2', 'windowname', 'resizable,scrollbars,status,noopener');
 
-window.open('https:/example.com/dangerous3'); // Noncompliant {{Replace this character class by the character itself.}}
+window.open('https:/example.com/dangerous3'); // Noncompliant {{Make sure not using "noopener" is safe here.}}
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 window.open('http:/example.com/dangerous4'); // Noncompliant
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
