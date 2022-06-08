eaf055b6871700a8bf447704750d3fe0a7d833c7
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-06-08 17:08:31 +0200

    replace "//" in URL by "/" while comments-based testing framework is fixed.

--- eslint-bridge/tests/rules/fixtures/link-with-target-blank.ts
+++ eslint-bridge/tests/rules/fixtures/link-with-target-blank.ts
@@ -1,12 +1,12 @@
 window.open('test.html');
 
-window.open('https://example.com/dangerous1', 'windowname', 'noopener');
+window.open('https:/example.com/dangerous1', 'windowname', 'noopener');
 
-window.open('https://example.com/dangerous2', 'windowname', 'resizable,scrollbars,status,noopener');
+window.open('https:/example.com/dangerous2', 'windowname', 'resizable,scrollbars,status,noopener');
 
-window.open('https://example.com/dangerous3'); // Noncompliant {{Replace this character class by the character itself.}}
+window.open('https:/example.com/dangerous3'); // Noncompliant {{Replace this character class by the character itself.}}
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-window.open('http://example.com/dangerous4'); // Noncompliant
+window.open('http:/example.com/dangerous4'); // Noncompliant
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-window.open('https://example.com/dangerous5', 'windowname', 'resizable'); // Noncompliant
+window.open('https:/example.com/dangerous5', 'windowname', 'resizable'); // Noncompliant
     // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
