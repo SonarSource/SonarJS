70ddcbb3b408ea84bcc18e25675bb8bf301753e2
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-06-08 18:38:34 +0200

    change error flag range

--- eslint-bridge/tests/rules/fixtures/link-with-target-blank.ts
+++ eslint-bridge/tests/rules/fixtures/link-with-target-blank.ts
@@ -5,8 +5,8 @@ window.open('https://example.com/dangerous1', 'windowname', 'noopener');
 window.open('https://example.com/dangerous2', 'windowname', 'resizable,scrollbars,status,noopener');
 
 window.open('https://example.com/dangerous3'); // Noncompliant {{Make sure not using "noopener" is safe here.}}
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // ^^^^
 window.open('http://example.com/dangerous4'); // Noncompliant
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // ^^^^
 window.open('https://example.com/dangerous5', 'windowname', 'resizable'); // Noncompliant
-    // ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+    // ^^^^
