2e91387efe1a2e8962fdd87ee49fd98cdf2ddb8f
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-06-14 15:02:13 +0200

    cleanup

--- eslint-bridge/tests/testing-framework/issues.ts
+++ eslint-bridge/tests/testing-framework/issues.ts
@@ -17,7 +17,6 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-// import {  } from './parseForEslint';
 import { buildSourceCode } from 'parser';
 import { extractLineIssues } from './comments';
 import { Location, extractLocations, PrimaryLocation, SecondaryLocation } from './locations';
