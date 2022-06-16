1e5c12d1c72c01a09765fee680a529aac8f8d42a
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-06-16 19:58:09 +0200

    reorganize test cases

--- eslint-bridge/tests/rules/comment-based/link-with-target-blank.ts
+++ eslint-bridge/tests/rules/comment-based/link-with-target-blank.ts
@@ -1,10 +1,3 @@
-window.open('test.html');
-window.open(123);
-
-window.open('https://example.com/dangerous1', 'windowname', 'noopener');
-
-window.open('https://example.com/dangerous2', 'windowname', 'resizable,scrollbars,status,noopener');
-
 window.open('https://example.com/dangerous3'); // Noncompliant {{Make sure not using "noopener" is safe here.}}
     // ^^^^
 window.open('http://example.com/dangerous4'); // Noncompliant
@@ -13,6 +6,14 @@ window.open('https://example.com/dangerous5', 'windowname', 'resizable'); // Non
     // ^^^^
 window.open('https://example.com/dangerous6', 'windowname', 123); // Noncompliant
     // ^^^^
+
+window.open('test.html');
+
+window.open(123);
+
+window.open('https://example.com/dangerous1', 'windowname', 'noopener');
+
+window.open('https://example.com/dangerous2', 'windowname', 'resizable,scrollbars,status,noopener');
 
 function open() {
     console.log('hello')
