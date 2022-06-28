e7601f4441050ff4712607391daeda0c7b71e687
vilchik.elena <elena.vilchik@sonarsource.com>
2022-06-28 09:36:27 +0200

    Add 2 more react projects for TypeScript: moose and courselit

--- its/ruling/src/test/java/org/sonar/javascript/it/TypeScriptRulingTest.java
+++ its/ruling/src/test/java/org/sonar/javascript/it/TypeScriptRulingTest.java
@@ -31,12 +31,14 @@ class TypeScriptRulingTest extends JavaScriptRulingTest {
       tsProject("ag-grid", "spec"),
       tsProject("ant-design", "tests"), // todo: many dirs **/__tests__
       tsProject("console", ""), // todo: many dirs **/__tests__
+      tsProject("courselit", ""),
       tsProject("desktop", "app/test"),
       tsProject("eigen", ""), // todo
       tsProject("file-for-rules", ""),
       tsProject("fireface", ""),
       tsProject("ionic2-auth", ""),
       tsProject("Joust", ""), // todo: files **/*.spec.ts
+      tsProject("moose", ""),
       tsProject("postgraphql", ""), // todo: many dirs **/__tests__
       tsProject("prettier-vscode", ""),
       tsProject("rxjs", "spec"),
