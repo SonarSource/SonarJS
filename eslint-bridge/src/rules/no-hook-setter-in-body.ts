28f39d21163e91246c69c0e3e6922155614d9c28
Francois Mora <francois.mora@sonarsource.com>
2022-07-15 18:01:34 +0200

    Fix issues raised in PR.

--- eslint-bridge/src/rules/no-hook-setter-in-body.ts
+++ eslint-bridge/src/rules/no-hook-setter-in-body.ts
@@ -19,7 +19,9 @@
  */
 // https://sonarsource.github.io/rspec/#/rspec/S6442/javascript
 
-import { Rule, Scope as EsLintScope } from 'eslint';
+import { Rule, Scope as ESLintScope } from 'eslint';
+import Variable = ESLintScope.Variable;
+import Scope = ESLintScope.Scope;
 import * as estree from 'estree';
 import {
   getModuleNameOfImportedIdentifier,
@@ -28,10 +30,6 @@ import {
   isIdentifier,
   isMemberExpression,
 } from '../utils';
-import Variable = EsLintScope.Variable;
-import Scope = EsLintScope.Scope;
-
-// Types used in the hook declaration callback signature which reflects the expectations coming from the selector.
 
 type HookDeclarator = estree.VariableDeclarator & {
   id: {
@@ -44,78 +42,54 @@ type SetterCall = estree.CallExpression & {
   callee: estree.Identifier;
 };
 
+const REACT_MODULE = 'react';
+const REACT_ROOT = 'React';
+const REACT_PATTERN = /^[^a-z]/;
+const HOOK_FUNCTION = 'useState';
+
 export const rule: Rule.RuleModule = {
   meta: {
     messages: {
-      noHookSetterInBody: 'Move the setState hook to the render function or body of a component',
+      noHookSetterInBody: 'Move the state setter call to a callback',
     },
   },
   create(context: Rule.RuleContext) {
-    const REACT_MODULE = 'react';
-    const REACT_ROOT = 'React';
-    const REACT_PATTERN = /^[^a-z]/;
-    const HOOK_FUNCTION = 'useState';
-
-    /**
-     * Tells if a name comes from the 'react' module. It uses {@link getModuleNameOfImportedIdentifier()} to get the
-     * module name. In the example below <code>isReactName(node)</code> will return true.
-     * @example
-     * import { useState } from "react";
-     *
-     * function ShowLanguageInvalid() {
-     *   const [language, setLanguage] = useState('fr-FR'); // 'useState' id is a React name.
-     *   // [...]
-     * }
-     */
     function isReactName(identifier: estree.Identifier): boolean {
       const module = getModuleNameOfImportedIdentifier(context, identifier);
       return module?.value === REACT_MODULE;
     }
 
-    /**
-     * Check if a node is a reference to the React <code>useState(value)</code> function. It can be used on:
-     * <ul>
-     *   <li>an Identifier like <code>useState(value)</value>
-     *   <li>a member expression like <code>React.useState(value)</value>
-     * </ul>
-     */
     function isHookCall(node: estree.CallExpression): boolean {
-      let identifier: estree.Identifier | undefined = undefined;
+      let identifier: estree.Identifier | null = null;
       if (isIdentifier(node.callee, HOOK_FUNCTION)) {
         identifier = node.callee;
       } else if (isMemberExpression(node.callee, REACT_ROOT, HOOK_FUNCTION)) {
         identifier = (node.callee as estree.MemberExpression).property as estree.Identifier;
       }
-      return identifier !== undefined && isReactName(identifier) && node.arguments.length === 1;
+      return identifier !== null && isReactName(identifier) && node.arguments.length === 1;
     }
 
-    /**
-     * Returns the current scope if it corresponds to a React component function or undefined otherwise.
-     */
-    function getReactComponentScope(): Scope | undefined {
+    function getReactComponentScope(): Scope | null {
       const scope = context.getScope();
-      const isReact = isFunctionNode(scope.block) && matches(scope.block, REACT_PATTERN, 1);
-      return isReact ? scope : undefined;
+      const isReact = isFunctionNode(scope.block) && matchesReactComponentName(scope.block, 1);
+      return isReact ? scope : null;
     }
 
-    /**
-     * Returns the closest enclosing scope of type 'function' or undefined if there isn't any.
-     */
-    function isInsideFunction(scope: Scope | undefined): boolean {
-      function functionScope(current: Scope | null): Scope | undefined {
+    function isInsideFunctionScope(scope: Scope | null): boolean {
+      function searchUpperFunctionScope(current: Scope | null): Scope | null {
         if (current === null) {
-          return undefined;
+          return null;
         } else if (current.type === 'function') {
           return current;
         } else {
-          return functionScope(current.upper);
+          return searchUpperFunctionScope(current.upper);
         }
       }
 
-      return scope !== undefined && functionScope(context.getScope()) === scope;
+      return scope !== null && searchUpperFunctionScope(context.getScope()) === scope;
     }
 
-    let reactComponentScope: Scope | undefined; // Scope of the React component render function.
+    let reactComponentScope: Scope | null; // Scope of the React component render function.
     const setters: Variable[] = []; // Setter variables returned by the React useState() function.
 
     return {
@@ -126,7 +100,7 @@ export const rule: Rule.RuleModule = {
       ':function:exit'() {
         if (context.getScope() === reactComponentScope) {
           // Clean variables when leaving the React component scope.
-          reactComponentScope = undefined;
+          reactComponentScope = null;
           setters.length = 0;
         }
       },
@@ -136,7 +110,7 @@ export const rule: Rule.RuleModule = {
         ':has(ArrayPattern[elements.length=2][elements.0.type="Identifier"][elements.1.type="Identifier"])'](
         node: estree.VariableDeclarator,
       ) {
-        if (!isInsideFunction(reactComponentScope)) {
+        if (!isInsideFunctionScope(reactComponentScope)) {
           return;
         }
 
@@ -144,7 +118,7 @@ export const rule: Rule.RuleModule = {
 
         if (isHookCall(hookDeclarator.init)) {
           const variable = getVariableFromName(context, hookDeclarator.id.elements[1].name);
-          if (variable !== undefined) {
+          if (variable != null) {
             setters.push(variable);
           }
         }
@@ -152,7 +126,7 @@ export const rule: Rule.RuleModule = {
 
       // Selector matching function calls like: setCount(1)
       'CallExpression[callee.type="Identifier"][arguments.length=1]'(node: estree.CallExpression) {
-        if (!isInsideFunction(reactComponentScope) || setters.length === 0) {
+        if (!isInsideFunctionScope(reactComponentScope) || setters.length === 0) {
           return;
         }
 
@@ -170,39 +144,25 @@ export const rule: Rule.RuleModule = {
   },
 };
 
-/**
- * Check if the node is an eslint Rule.Node that's to say it has a parent property.
- */
-function isRuleNode(node: estree.Node): node is Rule.Node {
-  return (node as Rule.Node).parent !== undefined;
+function hasParent(node: estree.Node): node is Rule.Node {
+  return (node as Rule.Node).parent != null;
 }
 
-/**
- * Check if a node matches a regular expression. It supports:
- * <ul>
- *   <li>Identifier: it uses the name
- *   <li>FunctionDeclaration: it uses the id.
- *   <li>VariableDeclarator: it uses the id.
- *   <li>AssignmentExpression: it uses the left part.
- *   <li>MemberExpression: it uses the property part.
- * </ul>
- * If the node is not supported calls itself recursively on the parent node up to max level.
- */
-function matches(node: estree.Node | null, pattern: RegExp, max = 0): node is estree.Identifier {
+function matchesReactComponentName(node: estree.Node | null, max = 0): boolean {
   if (node == null) {
     return false;
   } else if (isIdentifier(node)) {
-    return pattern.test(node.name);
+    return REACT_PATTERN.test(node.name);
   } else if (node.type === 'FunctionDeclaration') {
-    return matches(node.id, pattern);
+    return matchesReactComponentName(node.id);
   } else if (node.type === 'VariableDeclarator') {
-    return matches(node.id, pattern);
+    return matchesReactComponentName(node.id);
   } else if (node.type === 'AssignmentExpression') {
-    return matches(node.left, pattern);
+    return matchesReactComponentName(node.left);
   } else if (node.type === 'MemberExpression') {
-    return matches(node.property, pattern);
-  } else if (isRuleNode(node) && max > 0) {
-    return matches(node.parent, pattern, max - 1);
+    return matchesReactComponentName(node.property);
+  } else if (hasParent(node) && max > 0) {
+    return matchesReactComponentName(node.parent, max - 1);
   } else {
     return false;
   }
--- eslint-bridge/tests/rules/comment-based/no-hook-setter-in-body.js
+++ eslint-bridge/tests/rules/comment-based/no-hook-setter-in-body.js
@@ -12,8 +12,7 @@ function notAReactComponent() {
 function ShowLanguageInvalid() {
   const [language, setLanguage] = useState("fr-FR");
 
-  // Makes an infinite loop
-  setLanguage(navigator.language); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+  setLanguage(navigator.language); // Noncompliant {{Move the state setter call to a callback}}
 //^^^^^^^^^^^
 
   return (
@@ -27,8 +26,7 @@ function ShowLanguageInvalid() {
 function ShowLanguagePrefixedInvalid() {
     const [language, setLanguage] = React.useState(navigator.language);
 
-    // Makes an infinite loop
-    setLanguage(navigator.language); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+    setLanguage(navigator.language); // Noncompliant
 //  ^^^^^^^^^^^
 
     return (
@@ -39,36 +37,13 @@ function ShowLanguagePrefixedInvalid() {
     );
 }
 
-function ShowLanguageValid() {
-  const [language, setLanguage] = useState(navigator.language);
-
-  return (
-    <section>
-      <h1>Your language is {language}!</h1>
-      <button onClick={() => setLanguage("fr-FR")}>Je préfère le Français</button>
-    </section>
-  );
-}
-
 // Arrow Functions..............
 
 const ShowLanguageInvalidArrow = () => {
   const [language, setLanguage] = useState("fr-FR");
 
-  // Makes an infinite loop
-  setLanguage(navigator.language); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+  setLanguage(navigator.language); // Noncompliant
 //^^^^^^^^^^^
-
-  return (
-    <section>
-      <h1>Your language is {language}!</h1>
-      <button onClick={() => setLanguage("fr-FR")}>Je préfère le Français</button>
-    </section>
-  );
-};
-
-const ShowLanguageValidArrow = () => {
-  const [language, setLanguage] = useState("fr-FR");
 
   return (
     <section>
@@ -83,8 +58,7 @@ const ShowLanguageValidArrow = () => {
 const ShowLanguageInvalidExpression = function() {
   const [language, setLanguage] = useState("fr-FR");
 
-  // Makes an infinite loop
-  setLanguage(navigator.language); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+  setLanguage(navigator.language); // Noncompliant
 //^^^^^^^^^^^
 
   return (
@@ -95,24 +69,12 @@ const ShowLanguageInvalidExpression = function() {
   );
 };
 
-const ShowLanguageValidExpression = function() {
-  const [language, setLanguage] = useState("fr-FR");
-
-  return (
-    <section>
-      <h1>Your language is {language}!</h1>
-      <button onClick={() => setLanguage("fr-FR")}>Je préfère le Français</button>
-    </section>
-  );
-};
-
-// MemberInvalid..............
+// Member..............
 
 exports.MemberInvalid = () => {
     const [language, setLanguage] = useState("fr-FR");
 
-    // Makes an infinite loop
-    setLanguage(navigator.language); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+    setLanguage(navigator.language); // Noncompliant
 //  ^^^^^^^^^^^
 
     return (
@@ -123,20 +85,7 @@ exports.MemberInvalid = () => {
     );
 };
 
-// MemberInvalid..............
-
-exports.MemberValid = () => {
-    const [language, setLanguage] = useState("fr-FR");
-
-    return (
-        <section>
-            <h1>Your language is {language}!</h1>
-            <button onClick={() => setLanguage("fr-FR")}>Je préfère le Français</button>
-        </section>
-    );
-};
-
-// MultipleHookInvalid..............
+// Multiple Hooks..............
 
 function MultipleHookInvalid() {
   const [a, setA] = useState("a");
@@ -146,32 +95,10 @@ function MultipleHookInvalid() {
     MultipleHookInvalid.c = c;
   }
 
-  // Makes an infinite loop
-  setA("A"); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+  setA("A"); // Noncompliant
 //^^^^
-  setB("B"); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+  setB("B"); // Noncompliant
 //^^^^
-  setC("C");
-
-  return (
-    <section>
-      <h1>Multiple Hooks</h1>
-      <var>{a}</var><button onClick={() => setA("A")}>A</button>
-      <var>{b}</var><button onClick={() => setB("B")}>B</button>
-    </section>
-  );
-}
-
-// MultipleHookValid..............
-
-function MultipleHooValid() {
-  const [a, setA] = useState("a");
-  const [b, setB] = useState("b");
-
-  function setC(c) {
-    MultipleHookInvalid.c = c;
-  }
-
   setC("C");
 
   return (
@@ -189,7 +116,7 @@ function NestedInvalid() {
   const [language, setLanguage] = useState(navigator.language);
 
   if (language === 'fr') {
-    setLanguage('en'); // Noncompliant {{Move the setState hook to the render function or body of a component}}
+    setLanguage('en'); // Noncompliant
 //  ^^^^^^^^^^^
   }
 
