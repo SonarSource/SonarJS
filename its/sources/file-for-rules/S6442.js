d48c601532ddf8a244bcce3085fc5951d1854895
Francois Mora <francois.mora@sonarsource.com>
2022-07-15 18:39:50 +0200

    Fix issues raised in PR.

--- its/sources/file-for-rules/S6442.js
+++ its/sources/file-for-rules/S6442.js
@@ -1,15 +1,14 @@
-import { useState } from 'react';
+import { useState } from "react";
 
 function ShowLanguage() {
-  const [language, setLanguage] = useState('fr-FR');
+  const [language, setLanguage] = useState("fr-FR");
 
-  // Makes an infinite loop
   setLanguage(navigator.language);
 
   return (
     <section>
       <h1>Your language is {language}!</h1>
-      <button onClick={() => setLanguage('fr-FR')}>
+      <button onClick={() => setLanguage("fr-FR")}>
         Je préfère le Français
       </button>
     </section>
