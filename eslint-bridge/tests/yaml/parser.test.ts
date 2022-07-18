f7f313d21cf316722aaa98debd76399d2fb59602
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2022-07-18 09:11:25 +0200

    Fix test file import

--- eslint-bridge/tests/yaml/parser.test.ts
+++ eslint-bridge/tests/yaml/parser.test.ts
@@ -19,7 +19,7 @@
  */
 
 import path from 'path';
-import { parseAwsFromYaml } from '../../src/yaml';
+import { parseAwsFromYaml } from 'yaml/parser';
 
 describe('parseAwsFromYaml()', () => {
   it('should parse YAML syntax', () => {
