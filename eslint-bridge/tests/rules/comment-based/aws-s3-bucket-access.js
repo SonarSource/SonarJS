7e6ff6424fade73a23cb633da6a5bfe085e3feab
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-08-05 17:58:56 +0200

    rename minor stuff for clarity

--- eslint-bridge/tests/rules/comment-based/aws-s3-bucket-access.js
+++ eslint-bridge/tests/rules/comment-based/aws-s3-bucket-access.js
@@ -3,22 +3,22 @@ const s3 = require('aws-cdk-lib/aws-s3');
 /// s3Bucket constructor
 
 const missingParams = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'bucket',
 });
 
 /// accessControl param
 
 const invalidParam1 = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'Bucket',
   accessControl: s3.BucketAccessControl.PUBLIC_READ // Noncompliant {{Make sure granting PUBLIC_READ access is safe here.}}
 });
 
 const invalidParam2 = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'Bucket',
   accessControl: s3.BucketAccessControl.PUBLIC_READ_WRITE // Noncompliant {{Make sure granting PUBLIC_READ_WRITE access is safe here.}}
 });
 const invalidParam3 = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'Bucket',
   accessControl: s3.BucketAccessControl.AUTHENTICATED_READ // Noncompliant {{Make sure granting AUTHENTICATED_READ access is safe here.}}
 });
 
@@ -27,7 +27,7 @@ const invalidParam3 = new s3.Bucket(this, 'id', {
 const accessControl = s3.BucketAccessControl.PUBLIC_READ;
 //    ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^> {{Propagated setting.}}
 const invalidParam1Secondary = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'Bucket',
   accessControl, // Noncompliant {{Make sure granting PUBLIC_READ access is safe here.}}
 //^^^^^^^^^^^^^
 });
@@ -35,7 +35,7 @@ const invalidParam1Secondary = new s3.Bucket(this, 'id', {
 /// publicReadAccess param
 
 const otherInvalidParam = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'Bucket',
   publicReadAccess: true // Noncompliant
 });
 
@@ -43,14 +43,14 @@ const otherInvalidParam = new s3.Bucket(this, 'id', {
 
 const publicReadAccess = true; // Secondary Location
 const otherInvalidParamSecondary = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant',
+  bucketName: 'Bucket',
   publicReadAccess, // Noncompliant
 });
 
 // s3Bucket.grantPublicAccess()
 
 const bucketNoncompliant = new s3.Bucket(this, 'id', {
-  bucketName: 'bucketnoncompliant'
+  bucketName: 'Bucket',
 });
 bucketNoncompliant.grantPublicAccess(); // Noncompliant {{Make sure allowing unrestricted access to objects from this bucket is safe here.}}
 //                 ^^^^^^^^^^^^^^^^^
