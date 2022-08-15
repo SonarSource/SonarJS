e542485b9e9b78c468d8aae4bdeaa385bdbb779f
Victor Diez <victor.diez@sonarsource.com>
2022-08-15 15:02:24 +0200

    increase server timeout for routing tests

--- eslint-bridge/tests/routing/routing.test.ts
+++ eslint-bridge/tests/routing/routing.test.ts
@@ -41,7 +41,7 @@ describe('router', () => {
       sonarlint: false,
       bundles: [],
     });
-    server = await start(port, host);
+    server = await start(port, host, 60 * 60 * 1000);
     close = promisify(server.close.bind(server));
   });
 
