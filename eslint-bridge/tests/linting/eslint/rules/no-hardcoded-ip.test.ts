68db68b2757e60d86dcfd49ae97a81f44a770dd5
Victor <106590915+victor-diez-sonarsource@users.noreply.github.com>
2022-08-19 07:41:53 +0200

    Fix S1313: added exception for IP ranges reserved for documentation purposes (#3322)

--- eslint-bridge/tests/linting/eslint/rules/no-hardcoded-ip.test.ts
+++ eslint-bridge/tests/linting/eslint/rules/no-hardcoded-ip.test.ts
@@ -42,7 +42,11 @@ ruleTesterTs.run('Hardcoded ip addresses should be avoided', rule, {
       address = "::1/128";
       address = "::";
       address = "0:0:0:0:0:0:0:0";
-      address = 42`,
+      address = 42,
+      address = "192.0.2.5",
+      address = "198.51.100.5",
+      address = "203.0.113.5",
+      address = "2001:db8:acad:1::ff"`,
     },
   ],
   invalid: [
