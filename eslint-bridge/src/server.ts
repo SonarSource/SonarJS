f9cbc2ef8939b4d6a0505c17cc144f3c7c860195
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-08-23 07:13:19 +0200

    split line to remove code smell

--- eslint-bridge/src/server.ts
+++ eslint-bridge/src/server.ts
@@ -83,7 +83,8 @@ export function start(port = 0, shutdownTimeout = SHUTDOWN_TIMEOUT): Promise<htt
     });
 
     server.on('listening', () => {
-      // since we often use the 0 as the default port, Node assigns it a random port instead, which we get using server.address()
+      // Since we use the 0 as the default port, Node assigns it a random port instead,
+      // which we get using server.address()
       debug(`eslint-bridge server is running at port ${(server.address() as AddressInfo)?.port}`);
       resolve(server);
     });
