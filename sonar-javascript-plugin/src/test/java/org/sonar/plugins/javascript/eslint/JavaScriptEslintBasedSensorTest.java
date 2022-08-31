7484f221bba4c5c172607c5710f6eff3a8cbab38
Francois Mora <francois.mora@sonarsource.com>
2022-08-31 12:06:51 +0200

    Add missing matchers for new sensor context parameter

--- sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/eslint/JavaScriptEslintBasedSensorTest.java
+++ sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/eslint/JavaScriptEslintBasedSensorTest.java
@@ -71,6 +71,7 @@ import org.sonarsource.nodejs.NodeCommandException;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatThrownBy;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.clearInvocations;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.mock;
@@ -136,7 +137,7 @@ class JavaScriptEslintBasedSensorTest {
     DefaultInputFile inputFile = createInputFile(context);
 
     sensor.execute(context);
-    verify(eslintBridgeServerMock, times(1)).initLinter(context, any(), any(), any(), any());
+    verify(eslintBridgeServerMock, times(1)).initLinter(eq(context), any(), any(), any(), any());
     assertThat(context.allIssues()).hasSize(3);
 
     Iterator<Issue> issues = context.allIssues().iterator();
