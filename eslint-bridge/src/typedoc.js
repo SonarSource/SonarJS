99b5f8ee5f1e574f500431593739c53d6ad86c07
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-10-28 09:54:17 +0200

    move typedoc code outside of src

--- eslint-bridge/src/typedoc.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  entryPoints: ["./linting/eslint/rules/helpers/index.ts"],
-  out: "../docs",
-  searchInComments: true,
-  plugin: ["my-plugin"],
-  readme: "../typedoc/README.md"
-}
---
+++ eslint-bridge/typedoc/typedoc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entryPoints: ["../src/linting/eslint/rules/helpers/index.ts"],
+  out: "../docs",
+  searchInComments: true,
+  plugin: ["my-plugin"],
+  readme: "./README.md"
+}
