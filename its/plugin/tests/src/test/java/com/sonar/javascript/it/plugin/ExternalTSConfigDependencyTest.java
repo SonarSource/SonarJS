76ccb7e7f899649d5bc23f7edffd06e511b41a80
Victor Diez <victor.diez@sonarsource.com>
2022-11-08 11:00:27 +0100

    add integration test for external tsconfigs

---
+++ its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/ExternalTSConfigDependencyTest.java
@@ -0,0 +1,62 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2012-2022 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+package com.sonar.javascript.it.plugin;
+
+import com.sonar.orchestrator.Orchestrator;
+import com.sonar.orchestrator.build.SonarScanner;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.sonarqube.ws.Issues.Issue;
+
+import java.io.File;
+
+import static com.sonar.javascript.it.plugin.OrchestratorStarter.getIssues;
+import static com.sonar.javascript.it.plugin.OrchestratorStarter.getSonarScanner;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.tuple;
+
+@ExtendWith(OrchestratorStarter.class)
+class ExternalTSConfigDependencyTest {
+
+  private static final Orchestrator orchestrator = OrchestratorStarter.ORCHESTRATOR;
+
+  private static final String PROJECT = "external-tsconfig-dependency-project";
+  private static final File PROJECT_DIR = TestUtils.projectDir(PROJECT);
+
+  @Test
+  void test() throws Exception {
+    SonarScanner build = getSonarScanner()
+      .setProjectKey(PROJECT)
+      .setSourceEncoding("UTF-8")
+      .setSourceDirs(".")
+      .setProjectDir(PROJECT_DIR)
+      // setting inclusions like this will exclude tsconfig.json files, which is what we want to test
+      .setProperty("sonar.inclusions", "**/*.ts");
+
+    orchestrator.getServer().provisionProject(PROJECT, PROJECT);
+    orchestrator.getServer().associateProjectToQualityProfile(PROJECT, "ts", "eslint-based-rules-profile");
+
+    orchestrator.executeBuild(build);
+
+    assertThat(getIssues(PROJECT)).extracting(Issue::getLine, Issue::getComponent).containsExactlyInAnyOrder(
+      tuple(4, "external-tsconfig-dependency-project:src/bar/main.ts")
+    );
+  }
+}
