100b3c9ea8911fa4f3e1655ae4b1a90ef09e676e
francoismora <francois.mora@sonarsource.com>
2022-11-23 16:42:09 +0100

    Add rule S6478 (react/no-unstable-nested-components): React components should not be nested

---
+++ eslint-bridge/tests/linting/eslint/rules/comment-based/no-unstable-nested-components.jsx
@@ -0,0 +1,41 @@
+function Component() {
+    function UnstableNestedComponent() { // Noncompliant {{Do not define components during render. React will see a new component type on every render and destroy the entire subtree’s DOM nodes and state (https://reactjs.org/docs/reconciliation.html#elements-of-different-types). Instead, move this component definition out of the parent component “Component” and pass data as props.}}
+        return <div />;
+    }
+    return (
+        <div>
+            <UnstableNestedComponent />
+        </div>
+    );
+}
+
+function SomeComponent({ footer: Footer }) {
+    return (
+        <div>
+            <Footer />
+        </div>
+    );
+}
+
+function Component() {
+    return (
+        <div>
+            <SomeComponent footer={ () => <div /> } /> { /* Noncompliant {{Do not define components during render. React will see a new component type on every render and destroy the entire subtree’s DOM nodes and state (https://reactjs.org/docs/reconciliation.html#elements-of-different-types). Instead, move this component definition out of the parent component “Component” and pass data as props. If you want to allow component creation in props, set allowAsProps option to true.}} */ }
+        </div>
+    );
+}
+
+class Component extends React.Component {
+    render() {
+        function UnstableNestedComponent() { { /* Noncompliant {{Do not define components during render. React will see a new component type on every render and destroy the entire subtree’s DOM nodes and state (https://reactjs.org/docs/reconciliation.html#elements-of-different-types). Instead, move this component definition out of the parent component “Component” and pass data as props.}} */ }
+            return <div />;
+        }
+
+        return (
+            <div>
+                <UnstableNestedComponent />
+            </div>
+        );
+    }
+}
+
