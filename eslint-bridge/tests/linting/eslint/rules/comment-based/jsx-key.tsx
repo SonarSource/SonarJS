b5276005fab8b3bd82114f0caf62bbfa6e20e1ae
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-11-28 15:06:23 +0100

    clarify test case

--- eslint-bridge/tests/linting/eslint/rules/comment-based/jsx-key.tsx
+++ eslint-bridge/tests/linting/eslint/rules/comment-based/jsx-key.tsx
@@ -3,7 +3,8 @@ class App extends React.Component {
   const props = {};
   render() {
     [
-      <App {...props}/>,
+      <App key="unique id"/>,
+      <App {...props}/>, // spread operator are an exception
       <App /> // Noncompliant
     ];
   }
