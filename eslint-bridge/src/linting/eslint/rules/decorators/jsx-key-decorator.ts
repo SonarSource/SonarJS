19f0e0ed28cfcbbeb46e4cf5430b4ddb80a972fa
Ilia Kebets <ilia.kebets@sonarsource.com>
2022-11-28 15:58:23 +0100

    fix ruling and import

--- eslint-bridge/src/linting/eslint/rules/decorators/jsx-key-decorator.ts
+++ eslint-bridge/src/linting/eslint/rules/decorators/jsx-key-decorator.ts
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { TSESTree } from '@typescript-eslint/types';
+import { TSESTree } from '@typescript-eslint/experimental-utils';
 import { Rule } from 'eslint';
 import { interceptReportForReact } from './helpers';
 
