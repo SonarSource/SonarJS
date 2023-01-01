30e8264171dc22be3dacb4016bbe80010d39b42b
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-01-01 16:47:36 +0100

    update script so it is isomorphic

--- eslint-bridge/typedoc/searchable-parameters-plugin/setup.js
+++ eslint-bridge/typedoc/searchable-parameters-plugin/setup.js
@@ -6,4 +6,5 @@ const path = require('path');
 const linkPath = path.join(__dirname, '..', '..', 'node_modules', 'searchable-parameters-plugin');
 const targetPath = path.join(__dirname);
 
+if (fs.existsSync(linkPath)) return;
 fs.symlinkSync(targetPath, linkPath);
