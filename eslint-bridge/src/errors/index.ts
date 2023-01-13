ef12dc404ae81cf2b84ddd9b87d7f0e373a0cc0e
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2023-01-13 14:17:42 +0100

    Happy new year!

--- eslint-bridge/src/errors/index.ts
+++ eslint-bridge/src/errors/index.ts
@@ -1,6 +1,6 @@
 /*
  * SonarQube JavaScript Plugin
- * Copyright (C) 2011-2022 SonarSource SA
+ * Copyright (C) 2011-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
@@ -17,5 +17,4 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-
 export * from './error';
--- javascript-checks/src/main/java/org/sonar/javascript/checks/JsxNoBindCheck.java
+++ javascript-checks/src/main/java/org/sonar/javascript/checks/JsxNoBindCheck.java
@@ -1,6 +1,6 @@
 /*
  * SonarQube JavaScript Plugin
- * Copyright (C) 2011-2022 SonarSource SA
+ * Copyright (C) 2011-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
