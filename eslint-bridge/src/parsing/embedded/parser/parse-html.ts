a055ee628328192f82658d3bc35da69e8542b994
Victor Diez <victor.diez@sonarsource.com>
2023-01-26 17:04:33 +0100

    simplified parser

--- eslint-bridge/src/parsing/embedded/parser/parse-html.ts
+++ eslint-bridge/src/parsing/embedded/parser/parse-html.ts
@@ -19,22 +19,17 @@
  */
 import * as htmlparser from 'htmlparser2';
 import { EmbeddedJS } from 'parsing/embedded';
-import { TransformableString } from './html/TransformableString';
 
 // strongly inspired from https://github.com/BenoitZugmeyer/eslint-plugin-html/blob/12047e752d3f0904541e37ad7ffacde6149e2388/src/extract.js#L10
 
-type CdataLocation = {
-  start: number;
-  end: number;
-};
-
 export function parseHTML(code: string) {
   if (!code) return;
+  const lineStarts = computeLineStarts(code);
   const embeddedJSs: EmbeddedJS[] = [];
   const javaScriptTagNames = ['script'];
-  let index = 0;
+  let jsSnippetStartIndex = 0;
+  let jsSnippetEndIndex = 0;
   let inScript = false;
-  let cdatas: CdataLocation[] = [];
 
   const parser = new htmlparser.Parser({
     onopentag(name: string, attrs: { src: string }) {
@@ -49,20 +44,7 @@ export function parseHTML(code: string) {
 
       inScript = true;
 
-      index = parser.endIndex + 1;
-    },
-
-    oncdatastart() {
-      cdatas.push(
-        {
-          start: parser.startIndex,
-          end: parser.startIndex + 9,
-        },
-        {
-          start: parser.endIndex - 2,
-          end: parser.endIndex + 1,
-        },
-      );
+      jsSnippetStartIndex = parser.endIndex + 1;
     },
 
     onclosetag(name: string) {
@@ -72,26 +54,20 @@ export function parseHTML(code: string) {
 
       inScript = false;
 
-      const transformedCode = new TransformableString(code);
-      for (const cdata of cdatas) {
-        transformedCode.replace(cdata.start, cdata.end, '');
-      }
-      transformedCode.replace(0, index, '');
-      transformedCode.replace(parser.startIndex, code.length, '');
+      jsSnippetEndIndex = parser.startIndex;
 
       embeddedJSs.push({
-        code: transformedCode.toString(),
-        line: computeLine(index, transformedCode._lineStarts),
-        column: computeCol(index, transformedCode._lineStarts),
-        offset: index,
-        lineStarts: transformedCode._lineStarts,
+        code: code.slice(jsSnippetStartIndex, jsSnippetEndIndex),
+        line: computeLine(jsSnippetStartIndex, lineStarts),
+        column: computeCol(jsSnippetStartIndex, lineStarts),
+        offset: jsSnippetStartIndex,
+        lineStarts,
         format: 'PLAIN',
         text: code,
         extras: {},
       });
 
-      index = parser.startIndex;
-      cdatas = [];
+      jsSnippetStartIndex = jsSnippetEndIndex;
     },
   });
 
@@ -117,4 +93,23 @@ function computeCol(offset: number, fileLineStarts: number[]) {
     }
   }
   return offset - fileLineStarts[i - 1] + 1;
+}
+
+const lineEndingsRe = /\r\n|\r|\n/g;
+
+/**
+ * Computes the line start offsets for the provided string
+ *
+ * @param str
+ * @returns
+ */
+function computeLineStarts(str: string) {
+  const result = [0];
+  lineEndingsRe.lastIndex = 0;
+  while (true) {
+    const match = lineEndingsRe.exec(str);
+    if (!match) break;
+    result.push(lineEndingsRe.lastIndex);
+  }
+  return result;
 }
