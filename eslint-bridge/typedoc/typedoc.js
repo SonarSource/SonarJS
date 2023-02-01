193080caa09823e614cf641e3dbfda480ad4af43
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-02-01 14:16:08 +0100

    unify quotes style

--- eslint-bridge/typedoc/typedoc.js
+++ eslint-bridge/typedoc/typedoc.js
@@ -1,17 +1,17 @@
 
 // https://typedoc.org/guides/options
 module.exports = {
-  entryPoints: ["../src/linting/eslint/rules/helpers/index.ts"],
-  name: "SonarJS linter helper functions",
-  out: "./site",
+  entryPoints: ['../src/linting/eslint/rules/helpers/index.ts'],
+  name: 'SonarJS linter helper functions',
+  out: './site',
   searchInComments: true,
-  plugin: ["searchable-parameters-plugin"],
-  readme: "./main.md",
-  tsconfig: "../src/tsconfig.json",
-  json: "models/reflections.json",
+  plugin: ['searchable-parameters-plugin'],
+  readme: './main.md',
+  tsconfig: '../src/tsconfig.json',
+  json: 'models/reflections.json',
   pretty: true,
   sidebarLinks: {
-    "ESlint dev guide": "https://eslint.org/docs/latest/developer-guide/working-with-rules",
+    'ESlint dev guide': 'https://eslint.org/docs/latest/developer-guide/working-with-rules',
   },
   skipErrorChecking: true,
 };
