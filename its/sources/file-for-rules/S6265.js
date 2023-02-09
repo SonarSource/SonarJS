29689356571b3e3dc7ef9505c53e3a0c182fd899
Yassin Kammoun <52890329+yassin-kammoun-sonarsource@users.noreply.github.com>
2023-02-09 11:03:32 +0100

    Fix FN S6265 (`aws-s3-bucket-granted-access`): Fix BucketDeployment resolving (#3689)

--- its/sources/file-for-rules/S6265.js
+++ its/sources/file-for-rules/S6265.js
@@ -1,4 +1,5 @@
 const s3 = require('aws-cdk-lib/aws-s3');
+const s3deploy = require('aws-cdk-lib/aws-s3-deployment');
 
 const missingParams = new s3.Bucket(this, 'id', { // Compliant
   bucketName: 'bucket',
@@ -20,7 +21,7 @@ const bucketNoncompliant = new s3.Bucket(this, 'id', {
 });
 bucketNoncompliant.grantPublicAccess(); // Sensitive
 
-new s3.BucketDeployment(this, 'id', {
+new s3deploy.BucketDeployment(this, 'id', {
   bucketName: 'BucketDeployment',
   accessControl: s3.BucketAccessControl.PUBLIC_READ // Sensitive
 });
