99b3d1fc554aa0b7ebfa90b8d94afbbaaf0d14f1
Tibor Blenessy <tibor.blenessy@sonarsource.com>
2023-02-24 15:25:41 +0100

    Move eslint-bridge to root

--- tools/check-distribution-filepath-length.sh
+++ tools/check-distribution-filepath-length.sh
@@ -1,18 +1,18 @@
 #!/usr/bin/env bash
 
-set -e
+set -xe
 
-# set folder to eslint-bridge/
-SCRIPT_FOLDER=$(cd $(dirname "$0"); pwd)
-cd $SCRIPT_FOLDER/..
+# set folder to repo root
+SCRIPT_FOLDER=$(cd "$(dirname "$0")"; pwd)
+cd "$SCRIPT_FOLDER"/..
 
-PACKAGE_FILENAME="target/classes/eslint-bridge-1.0.0.tgz"
+PACKAGE_FILENAME="sonarjs-1.0.0.tgz"
 
 # This value was decided with SonarLint: 120 (+8 chars for "package/" that gets removed)
 MAX_FILEPATH=128
 
 LONGEST_FILEPATH=$(tar tfz $PACKAGE_FILENAME | awk '{print length($0), $0}' | sort -nr | head -n 1)
-LONGEST_LENGTH=$(echo $LONGEST_FILEPATH | grep -o -E "^[0-9]+")
+LONGEST_LENGTH=$(echo "$LONGEST_FILEPATH" | grep -o -E "^[0-9]+")
 
 if [[ $LONGEST_LENGTH -gt $MAX_FILEPATH ]]
 then
