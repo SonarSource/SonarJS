6cf1898a59878c0ebd2393d38d6737e9ec2a72dc
Victor Diez <victor.diez@sonarsource.com>
2023-03-13 11:36:08 +0100

    fix small typo

--- tests/parsing/embedded/html/parser/parse.test.ts
+++ tests/parsing/embedded/html/parser/parse.test.ts
@@ -57,7 +57,7 @@ describe('parseHtml', () => {
     expect(embeddedJSs).toHaveLength(0);
   });
 
-  it('should ignore non js script tags', async () => {
+  it('should ignore non-js script tags', async () => {
     const filePath = path.join(__dirname, 'fixtures', 'non-js.html');
     const fileContent = await readFile(filePath);
     const embeddedJSs = parseHTML(fileContent);
