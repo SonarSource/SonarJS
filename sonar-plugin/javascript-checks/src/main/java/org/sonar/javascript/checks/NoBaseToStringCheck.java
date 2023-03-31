194cec676933d91b8ae9f45a3ebac62f0d9a3695
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-03-31 14:58:24 +0200

    activate for JS as well

--- sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/NoBaseToStringCheck.java
+++ sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/NoBaseToStringCheck.java
@@ -21,8 +21,10 @@ package org.sonar.javascript.checks;
 
 import org.sonar.check.Rule;
 import org.sonar.plugins.javascript.api.EslintBasedCheck;
+import org.sonar.plugins.javascript.api.JavaScriptRule;
 import org.sonar.plugins.javascript.api.TypeScriptRule;
 
+@JavaScriptRule
 @TypeScriptRule
 @Rule(key = "S6551")
 public class NoBaseToStringCheck implements EslintBasedCheck {
