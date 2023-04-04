7f8ceaffdf813768ae9ccf204ee8ab008459f458
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-04-04 11:28:26 +0200

    Docs: update mergeRules() JS doc

--- src/linting/eslint/rules/decorators/helpers/merger.ts
+++ src/linting/eslint/rules/decorators/helpers/merger.ts
@@ -27,10 +27,8 @@ import { Rule } from 'eslint';
  * on a node selector. If the node selector matches, the listener then
  * invokes a callback to proceed further with the node being visited.
  *
- * One needs to pay special attention when merging multiple rules that
- * their respective listeners don't overlap with one another, e.g., two
- * rules listen to `CallExpression` node vists. Unexpected behaviours
- * could happen otherwise.
+ * It supports when multiple rules share the same listeners, e.g., 2 rules
+ * listen to `CallExpression` node visits. They will be run one after the other.
  *
  * @param rules rules to merge
  * @returns the merge of the rules' listeners
