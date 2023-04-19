18af76adbbf3c2b883e44cc368703dfc0b9d4859
Ilia Kebets <104737176+ilia-kebets-sonarsource@users.noreply.github.com>
2023-04-19 18:11:45 +0200

    Add rule S6593 (`no-invalid-void-type`): The type "void" should not be used misleadingly (#3856)

--- sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/CheckList.java
+++ sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/CheckList.java
@@ -252,6 +252,7 @@ public final class CheckList {
       NoIncompleteAssertionsCheck.class,
       NoInferrableTypesCheck.class,
       NoInvalidAwaitCheck.class,
+      NoInvalidVoidTypeCheck.class,
       NoInvertedBooleanCheckCheck.class,
       NoLossOfPrecisionCheck.class,
       NoMagicNumbersCheck.class,
