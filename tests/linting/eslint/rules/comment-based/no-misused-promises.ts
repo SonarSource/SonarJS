d3acb4fa69d122c004185bd5739b75297ce97d58
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-04-21 14:40:14 +0200

    make rules intersection more explicit in test cases

--- tests/linting/eslint/rules/comment-based/no-misused-promises.ts
+++ tests/linting/eslint/rules/comment-based/no-misused-promises.ts
@@ -3,10 +3,17 @@
   await Promise.resolve(num);
 });
 
-new Promise(async (resolve) => { // Noncompliant {{Promise returned in function argument where a void return was expected.}}
+// rule 'no-floating-promises' triggers first
+new Promise(async (resolve) => { // Noncompliant {{Promises must be awaited, end with a call to .catch, or end with a call to .then with a rejection handler.}}
   const a = await Promise.resolve(12);
   resolve(a);
 });
+
+// otherwise rule 'no-misused-promises' gets triggered second
+new Promise(async (resolve) => { // Noncompliant {{Promise returned in function argument where a void return was expected.}}
+  const a = await Promise.resolve(12);
+  resolve(a);
+}).catch(error => {});
 
 (async () => {
   for (const url of ['http://yo.lo', 'http://tro-lo.lo']) {
