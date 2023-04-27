f2ee8736b4312754d504764dfcc6aaae3114c837
Ilia Kebets <104737176+ilia-kebets-sonarsource@users.noreply.github.com>
2023-04-27 09:49:52 +0200

    Revert "Add rule S6593 (`no-invalid-void-type`): The type "void" shou… (#3871)

--- sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/CheckList.java
+++ sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/CheckList.java
@@ -252,7 +252,6 @@ public final class CheckList {
       NoIncompleteAssertionsCheck.class,
       NoInferrableTypesCheck.class,
       NoInvalidAwaitCheck.class,
-      NoInvalidVoidTypeCheck.class,
       NoInvertedBooleanCheckCheck.class,
       NoLossOfPrecisionCheck.class,
       NoMagicNumbersCheck.class,
