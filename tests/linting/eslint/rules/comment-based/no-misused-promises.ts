0187b7b83ac8ff221d1568bba3ec17bde3326481
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-05-05 16:27:12 +0200

    Update S6544 (`no-misused-promises`): don't cover cases from `no-floating-promises`

--- tests/linting/eslint/rules/comment-based/no-misused-promises.ts
+++ tests/linting/eslint/rules/comment-based/no-misused-promises.ts
@@ -3,12 +3,6 @@
   await Promise.resolve(num);
 });
 
-// rule 'no-floating-promises' triggers first
-new Promise(async (resolve) => { // Noncompliant {{Promises must be awaited, end with a call to .catch, or end with a call to .then with a rejection handler.}}
-  const a = await Promise.resolve(12);
-  resolve(a);
-});
-
 // otherwise rule 'no-misused-promises' gets triggered second
 new Promise(async (resolve) => { // Noncompliant {{Promise returned in function argument where a void return was expected.}}
   const a = await Promise.resolve(12);
