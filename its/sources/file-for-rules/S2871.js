a4a66b6cba3210e6365159ebafcebc5c220536a7
Phil Nash <philnash@gmail.com>
2023-05-08 09:58:09 +1000

    Adds rulings for JS for S2871

--- its/sources/file-for-rules/S2871.js
+++ its/sources/file-for-rules/S2871.js
@@ -1,3 +1,3 @@
-var arrayOfNumbers = [80, 3, 9, 34, 23, 5, 1];
+const arrayOfNumbers = [80, 3, 9, 34, 23, 5, 1];
 arrayOfNumbers.sort(); // Noncompliant
 const sorted = arrayOfNumbers.toSorted(); // Noncompliant
