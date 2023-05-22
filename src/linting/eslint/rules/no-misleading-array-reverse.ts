608fbb2bf5f5718a80be31ba82f6d59c1ca9431e
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2023-05-22 16:16:26 +0200

    Update S4043 (`no-misleading-array-reverse`): Report on `splice` and suggest `toSpliced`

--- src/linting/eslint/rules/no-misleading-array-reverse.ts
+++ src/linting/eslint/rules/no-misleading-array-reverse.ts
@@ -32,7 +32,15 @@ import {
   localAncestorsChain,
 } from './helpers';
 
-const arrayMutatingMethods = ['reverse', "'reverse'", '"reverse"', ...sortLike];
+const arrayMutatingMethods = [
+  'splice',
+  "'splice'",
+  '"splice"',
+  'reverse',
+  "'reverse'",
+  '"reverse"',
+  ...sortLike,
+];
 
 export const rule: Rule.RuleModule = {
   meta: {
@@ -62,7 +70,18 @@ export const rule: Rule.RuleModule = {
               isForbiddenOperation(node)
             ) {
               const method = formatMethod(propertyText);
-              const suggestedMethod = method === 'sort' ? 'toSorted' : 'toReversed';
+              let suggestedMethod = '';
+              switch (method) {
+                case 'sort':
+                  suggestedMethod = 'toSorted';
+                  break;
+                case 'reverse':
+                  suggestedMethod = 'toReversed';
+                  break;
+                case 'splice': // splice
+                  suggestedMethod = 'toSpliced';
+                  break;
+              }
               context.report({
                 messageId: 'moveMethod',
                 data: {
