f648d5248e1391fe693b16f01fe1c5b445589e50
Victor <106590915+victor-diez-sonarsource@users.noreply.github.com>
2023-06-06 17:26:04 +0200

    Remove again node_modules on ruling tests (#3965)

--- src/services/program/program.ts
+++ src/services/program/program.ts
@@ -74,8 +74,15 @@ export function getProgramForFile(
   cache = programCache,
   tsconfigs?: ProjectTSConfigs,
 ): ts.Program {
+  /**
+   * SONARJS_LIMIT_DEPS_RESOLUTION is available to limit TS resolution to current baseDir.
+   * However, this does not apply when we rely on typescript-eslint to create watchPrograms.
+   * If we are ever able to provide our own compilerHost to typescript-eslint, we can generalize
+   * for all projects and avoid removing node_modules for ruling tests
+   */
   const topDir =
     process.env['SONARJS_LIMIT_DEPS_RESOLUTION'] === '1' ? toUnixPath(input.baseDir) : undefined;
+
   if (!tsconfigs) {
     tsconfigs = getDefaultTSConfigs(input.baseDir, input.tsConfigs);
   }
