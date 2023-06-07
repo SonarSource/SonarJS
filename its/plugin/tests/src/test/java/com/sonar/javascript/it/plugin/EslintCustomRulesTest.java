61a36c088cdce0602d60220b5bb9625cae4283a2
Alexander Kamushkin <alexander.kamushkin@sonarsource.com>
2023-06-07 17:12:15 +0200

    fix TS version in plugin test

--- its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/EslintCustomRulesTest.java
+++ its/plugin/tests/src/test/java/com/sonar/javascript/it/plugin/EslintCustomRulesTest.java
@@ -126,7 +126,7 @@ class EslintCustomRulesTest {
       )
     )
       .isEmpty();
-    assertThat(buildResult.getLogsLines(l -> l.contains("TS API in custom rule: TS version 5.0.4")))
+    assertThat(buildResult.getLogsLines(l -> l.contains("TS API in custom rule: TS version 5.1.3")))
       .hasSize(2);
     List<Issue> issues = findIssues("eslint-custom-rules:sqKey", orchestrator);
     assertThat(issues).hasSize(2);
