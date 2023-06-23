438e71d25ab59f1bef2d6cce2f9d0b5ba427149b
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-06-23 14:24:14 +0200

    add debug files

--- bundle/hello-path.js
+++ bundle/hello-path.js
@@ -1,5 +1,30 @@
 const { readFileSync } = require('fs');
+const eslint = require('eslint');
 
 const path = process.argv[2];
 const content = readFileSync(path, 'utf-8');
-console.log(content);
+//console.log('read ', content);
+
+const { rules } = require(path);
+const [rule] = rules;
+
+const linter = new eslint.Linter();
+linter.defineRule(rule.ruleId, rule.ruleModule);
+
+// const sourceCode = "foo;";
+
+const sourceCode = `
+var express = require('express');
+var app = express();
+
+app.all('/', function (req, res) {
+    res.redirect(req.query.url);                // Noncompliant (S5146)
+    res.redirect("safe");                       // Compliant
+});
+`;
+
+const ruleId = 'ucfg';
+const config = { [rule.ruleId]: 'error' };
+
+const [message] = linter.verify(sourceCode, { rules: config });
+console.log('got msg', message);
--- bundle/hello.js
+++ bundle/hello.js
@@ -1,1 +1,6 @@
 console.log('hello world!');
+
+const v8 = require('node:v8');
+console.log(v8.getHeapStatistics().total_available_size / 1024 / 1024 + ' MB');
+
+//NODE_OPTIONS='--max-old-space-size=2048' ./dist/hello-size
