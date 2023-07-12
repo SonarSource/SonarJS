8e8056a1b590ec9e1823e8af58a319c3ef0102ba
Alexander Kamushkin <alexander.kamushkin@sonarsource.com>
2023-07-12 18:01:44 +0200

    use decorator to change message, update location

---
+++ src/linting/eslint/rules/decorators/no-lonely-if-decorator.ts
@@ -0,0 +1,46 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://sonarsource.github.io/rspec/#/rspec/S6660/javascript
+
+import { Rule } from 'eslint';
+import { interceptReport } from './helpers';
+
+export function decorateNoLonelyIf(rule: Rule.RuleModule): Rule.RuleModule {
+  return interceptReport(rule, (context, reportDescriptor) => {
+    if ('node' in reportDescriptor && 'messageId' in reportDescriptor) {
+      const { node, messageId, ...rest } = reportDescriptor;
+
+      if (node.type !== 'IfStatement' || messageId !== 'unexpectedLonelyIf' || !('loc' in node)) {
+        return;
+      }
+
+      const { start } = node.loc!;
+
+      context.report({
+        message: "'If' statement should not be the only statement in 'else' block",
+        loc: {
+          start,
+          end: { line: start.line, column: start.column + 2 },
+        },
+        ...rest,
+      });
+    }
+  });
+}
