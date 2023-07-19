353af04c859e125fb73cc3040c98f6e0eaa8c390
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-07-19 18:33:26 +0200

    cleanup hello world

--- bundle/debug/hello.js
+++ bundle/debug/hello.js
@@ -1,8 +1,5 @@
-console.log('hello world!');
-
-console.log('my argv:', process.argv);
 
 const v8 = require('node:v8');
-console.log(v8.getHeapStatistics().total_available_size / 1024 / 1024 + ' MB');
+console.log('I have so much heap RAM:', v8.getHeapStatistics().total_available_size / 1024 / 1024 + ' MB');
 
 //NODE_OPTIONS='--max-old-space-size=2048' ./dist/hello-size
