b0c2acf14668e531a8ae8bb4f71c6506b133b4aa
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-07-19 17:00:10 +0200

    parametrizable NODE_OPTIONS works for pkg 5.6.0 (pre pkg-fetch 3.4.0)

--- bundle/debug/hello.js
+++ bundle/debug/hello.js
@@ -1,5 +1,7 @@
 console.log('hello world!');
 
+console.log('my argv:', process.argv);
+
 const v8 = require('node:v8');
 console.log(v8.getHeapStatistics().total_available_size / 1024 / 1024 + ' MB');
 
