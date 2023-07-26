5cb8b50614fe84d02936004500ed9a740064c9d5
Alexander Kamushkin <alexander.kamushkin@sonarsource.com>
2023-07-26 15:46:48 +0200

    change message, restrict to ===, !==, ==, !=

---
+++ src/linting/eslint/rules/decorators/no-self-compare-decorator.ts
@@ -0,0 +1,44 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+// https://sonarsource.github.io/rspec/#/rspec/S6679/javascript
+
+import { Rule } from 'eslint';
+import { interceptReport } from './helpers';
+
+export function decorateNoSelfCompare(rule: Rule.RuleModule): Rule.RuleModule {
+  return interceptReport(rule, (context, reportDescriptor) => {
+    if ('node' in reportDescriptor && 'messageId' in reportDescriptor) {
+      const { node, messageId, ...rest } = reportDescriptor,
+        operators = new Set(['===', '==', '!==', '!=']);
+
+      if (
+        node.type === 'BinaryExpression' &&
+        operators.has(node.operator) &&
+        node.left.type !== 'Literal'
+      ) {
+        context.report({
+          node,
+          message: "Use 'Number.isNaN()' to check for 'NaN' value",
+          ...rest,
+        });
+      }
+    }
+  });
+}
