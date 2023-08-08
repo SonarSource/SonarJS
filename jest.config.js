f9445ffc5f4d7a3f99a370cdde4f22652b78463f
Alexander Kamushkin <alexander.kamushkin@sonarsource.com>
2023-08-08 19:55:58 +0200

    cleanup tsconfigs

--- jest.config.js
+++ jest.config.js
@@ -1,34 +1,17 @@
 module.exports = {
   collectCoverageFrom: ['packages/*/src/**/*.ts'],
-  globals: {
-    'ts-jest': {
-      tsconfig: 'tests/tsconfig.json',
-    },
-  },
   moduleFileExtensions: ['js', 'ts', 'json'],
-  moduleDirectories: [
-    'node_modules',
-    '<rootDir>/tests/**/fixtures',
-    '<rootDir>packages/*/tests/**/fixtures',
-  ],
+  moduleDirectories: ['node_modules', '<rootDir>packages/*/tests/**/fixtures'],
   moduleNameMapper: {
-    '^@sonar/css$': '<rootDir>/packages/css/src',
-    '^@sonar/html$': '<rootDir>/packages/html/src',
-    '^@sonar/jsts$': '<rootDir>/packages/jsts/src',
-    '^@sonar/yaml$': '<rootDir>/packages/yaml/src',
-    '^@sonar/bridge/(.*)$': '<rootDir>/packages/bridge/src/$1',
-    '^@sonar/html/(.*)$': '<rootDir>/packages/html/src/$1',
-    '^@sonar/jsts/(.*)$': '<rootDir>/packages/jsts/src/$1',
-    '^@sonar/shared/(.*)$': '<rootDir>/packages/shared/src/$1',
-    '^@sonar/yaml/(.*)$': '<rootDir>/packages/yaml/src/$1',
+    '^@sonar/(\\w+)(.*)$': '<rootDir>/packages/$1/src$2',
   },
   modulePathIgnorePatterns: [
     '<rootDir>/packages/jsts/tests/rules/fixtures/no-implicit-dependencies/bom-package-json-project/package.json',
   ],
   testResultsProcessor: 'jest-sonar-reporter',
   transform: {
-    '^.+\\.ts$': 'ts-jest',
+    '^.+\\.ts$': ['ts-jest', { tsconfig: 'packages/tsconfig.test.json' }],
   },
-  testMatch: ['<rootDir>/tests/**/*.test.ts', '<rootDir>/packages/*/tests/**/*.test.ts'],
+  testMatch: ['<rootDir>/packages/*/tests/**/*.test.ts'],
   testTimeout: 20000,
 };
