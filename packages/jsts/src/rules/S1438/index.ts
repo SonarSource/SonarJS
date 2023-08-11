799809618eb592c3b329574106eca000b0847565
Alexander Kamushkin <alexander.kamushkin@sonarsource.com>
2023-08-11 08:35:08 +0200

    fix more plugin rules

--- packages/jsts/src/rules/S1438/index.ts
+++ packages/jsts/src/rules/S1438/index.ts
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { eslintRules } from '../core';
+import { rules } from '@typescript-eslint/eslint-plugin';
 import { decorate } from './decorator';
 
-export const rule = decorate(eslintRules['semi']);
+export const rule = decorate(rules['semi']);
--- packages/jsts/src/rules/S6478/index.ts
+++ packages/jsts/src/rules/S6478/index.ts
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-import { eslintRules } from '../core';
+import { rules } from 'eslint-plugin-react';
 import { decorate } from './decorator';
 
-export const rule = decorate(eslintRules['no-unstable-nested-components']);
+export const rule = decorate(rules['no-unstable-nested-components']);
