922e2b3f29c98768c55e28be4eb2528f5453a1d4
Alexander Kamushkin <132259666+alexander-kamushkin-sonarsource@users.noreply.github.com>
2023-08-18 09:23:56 +0200

    Fix FP S4123 (`no-invalid-await`): do not raise issue for `unknown` type (#4069)

--- packages/jsts/src/rules/S4123/unit.test.ts
+++ packages/jsts/src/rules/S4123/unit.test.ts
@@ -122,6 +122,30 @@ ruleTester.run('await should only be used with promises.', rule, {
       }
       `,
     },
+    {
+      code: `
+      async function foo(x: unknown) {
+        await x;
+      }
+      `,
+    },
+    {
+      code: `
+      export class NoErrorThrownError extends Error {};
+      export class TestUtils {
+          public static getError = async (
+              call: () => PromiseLike<unknown> | unknown
+          ): Promise<TError> => {
+              try {
+                  await call();
+                  throw new NoErrorThrownError();
+              } catch (error) {
+                  return error as TError;
+              }
+          };
+      }
+      `,
+    },
   ],
   invalid: [
     {
