7b88b5b53c24cad6dd3558307598607b1110c5e9
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2023-08-24 13:38:59 +0200

    Add quick fix for S6326: Regular expressions should not contain multiple spaces

--- packages/jsts/src/rules/S6326/rule.ts
+++ packages/jsts/src/rules/S6326/rule.ts
@@ -19,38 +19,54 @@
  */
 // https://sonarsource.github.io/rspec/#/rspec/S6326/javascript
 
-import { Rule } from 'eslint';
+import { AST, Rule } from 'eslint';
 import * as regexpp from '@eslint-community/regexpp';
-import { createRegExpRule } from '../helpers/regex';
+import { createRegExpRule, getRegexpRange } from '../helpers/regex';
 
-export const rule: Rule.RuleModule = createRegExpRule(context => {
-  let rawPattern: string;
+export const rule: Rule.RuleModule = createRegExpRule(
+  context => {
+    let rawPattern: string;
 
-  return {
-    onRegExpLiteralEnter: (node: regexpp.AST.RegExpLiteral) => {
-      rawPattern = node.raw;
-    },
-    onCharacterEnter: (node: regexpp.AST.Character) => {
-      if (node.raw !== ' ' || node.parent.type === 'CharacterClass') {
-        return;
-      }
-
-      const nextChar = rawPattern[node.start + 1];
-      if (nextChar !== ' ') {
-        const spacesBefore = countSpacesBefore(rawPattern, node.start);
-        if (spacesBefore > 0) {
-          const spacesNumber = spacesBefore + 1;
-          context.reportRegExpNode({
-            message: `If multiple spaces are required here, use number quantifier ({${spacesNumber}}).`,
-            regexpNode: node,
-            offset: [-spacesNumber + 1, 0],
-            node: context.node,
-          });
+    return {
+      onRegExpLiteralEnter: (node: regexpp.AST.RegExpLiteral) => {
+        rawPattern = node.raw;
+      },
+      onCharacterEnter: (node: regexpp.AST.Character) => {
+        if (node.raw !== ' ' || node.parent.type === 'CharacterClass') {
+          return;
         }
-      }
+
+        const nextChar = rawPattern[node.start + 1];
+        if (nextChar !== ' ') {
+          const spacesBefore = countSpacesBefore(rawPattern, node.start);
+          if (spacesBefore > 0) {
+            const spacesNumber = spacesBefore + 1;
+            const quantifier = `{${spacesNumber}}`;
+            const [start, end] = getRegexpRange(context.node, node);
+            const range: AST.Range = [start - spacesNumber + 1, end];
+            context.reportRegExpNode({
+              message: `If multiple spaces are required here, use number quantifier (${quantifier}).`,
+              regexpNode: node,
+              offset: [-spacesNumber + 1, 0],
+              node: context.node,
+              suggest: [
+                {
+                  desc: `Use quantifier ${quantifier}`,
+                  fix: fixer => fixer.replaceTextRange(range, ` ${quantifier}`),
+                },
+              ],
+            });
+          }
+        }
+      },
+    };
+  },
+  {
+    meta: {
+      hasSuggestions: true,
     },
-  };
-});
+  },
+);
 
 function countSpacesBefore(pattern: string, index: number) {
   let counter = 0;
