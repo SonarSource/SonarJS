58bb12dac60d7e658f6e31cd125d2eea4bed7b58
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-09-12 14:46:59 +0200

    when downloading nodejs runtime validate their shasums

--- tools/fetch-node/node-distros.mjs
+++ tools/fetch-node/node-distros.mjs
@@ -9,13 +9,16 @@ export default [
   {
     id: 'win-x64',
     url: `https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-win-x64.zip`,
+    sha: '5d2596a00699fadf0ffa8e651f47ff5d719991014b920544d59c80d78569d42f',
   },
   {
     id: 'macos-arm64',
     url: `https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-darwin-arm64.tar.gz`,
+    sha: '9cc794517788aee103dfcffa04d0b90ac33854b0d10eb11a26ba4be38403f731',
   },
   {
     id: 'linux-x64',
     url: `https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}-linux-x64.tar.gz`,
+    sha: 'a8678ae00425acdf692e943e3f1cea11a4c46281e4257b82886423bd4ef6f2b5',
   },
 ];
