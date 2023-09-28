9b9f6b711e14cf527a0ff33d92a564b7e7ce7b6c
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-09-28 13:27:34 +0200

    move function up because bash does not hoist

--- .cirrus/prepare_deploy_analyze.bash
+++ .cirrus/prepare_deploy_analyze.bash
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# taken from https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/regular_mvn_build_deploy_analyze#L41-L49
-
-# Analyze with SNAPSHOT version as long as SQ does not correctly handle
-# purge of release data
-CURRENT_VERSION=$(maven_expression "project.version")
-
-. set_maven_build_version "$BUILD_NUMBER"
-
-export MAVEN_OPTS="-Xmx1536m -Xms128m"
-
-check_version_format "$PROJECT_VERSION"
-
 # taken from https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/includes/version_util#L15-L22
 
 # Verify that the version declared in pom.xml or in gradle.properties
@@ -26,3 +14,17 @@ function check_version_format(){
     echo "WARN: This version $version does not match the standardized format used commonly across the organization: '<MAJOR>.<MINOR>.<PATCH>.<BUILD NUMBER>'."
   fi
 }
+
+# taken from https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/regular_mvn_build_deploy_analyze#L41-L49
+
+# Analyze with SNAPSHOT version as long as SQ does not correctly handle
+# purge of release data
+CURRENT_VERSION=$(maven_expression "project.version")
+
+. set_maven_build_version "$BUILD_NUMBER"
+
+export MAVEN_OPTS="-Xmx1536m -Xms128m"
+
+check_version_format "$PROJECT_VERSION"
+
+
