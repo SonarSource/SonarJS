05ad1c05e1e03baac38f9127b9a9c7f2b1ce25e0
Ilia Kebets <104737176+ilia-kebets-sonarsource@users.noreply.github.com>
2023-09-28 16:52:47 +0200

    Optimize CI: run Sonar analysis separately from build task (#4206)

---
+++ .cirrus/analyze.bash
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# inspired by https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/regular_mvn_build_deploy_analyze#L51-L61
+
+mvn sonar:sonar \
+      -Pcoverage \
+      -Dmaven.test.redirectTestOutputToFile=false \
+      -Dsonar.host.url="$SONAR_HOST_URL" \
+      -Dsonar.token="$SONAR_TOKEN" \
+      -Dsonar.projectVersion="$CURRENT_VERSION" \
+      -Dsonar.analysis.buildNumber="$BUILD_NUMBER" \
+      -Dsonar.analysis.pipeline="$PIPELINE_ID" \
+      -Dsonar.analysis.sha1="$GIT_SHA1"  \
+      -Dsonar.analysis.repository="$GITHUB_REPO" \
+      -B -e -V "$@"
---
+++ .cirrus/prepare_deploy_analyze.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# taken from includes/version_util
+# https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/includes/version_util#L15-L22
+
+# Verify that the version declared in pom.xml or in gradle.properties
+# use the following pattern: x.x.x.x (<major>.<minor>.<patch>.<buildNumber>) and warn if not.
+# Args:
+#  $1 The version string to check
+function check_version_format(){
+  local version=$1
+  local extracted_points="${version//[^.]}"
+  local point_count=${#extracted_points}
+  if [[ "$point_count" != 3 ]]; then
+    echo "WARN: This version $version does not match the standardized format used commonly across the organization: '<MAJOR>.<MINOR>.<PATCH>.<BUILD NUMBER>'."
+  fi
+}
+
+# taken from regular_mvn_build_deploy_analyze
+# https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/regular_mvn_build_deploy_analyze#L41-L49
+
+# Analyze with SNAPSHOT version as long as SQ does not correctly handle
+# purge of release data
+CURRENT_VERSION=$(maven_expression "project.version")
+
+. set_maven_build_version "$BUILD_NUMBER"
+
+export MAVEN_OPTS="-Xmx1536m -Xms128m"
+
+check_version_format "$PROJECT_VERSION"
+
+
