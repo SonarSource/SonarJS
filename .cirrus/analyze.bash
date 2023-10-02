0086675b964cc9f24f4bbf7b62fc1e839274c7fc
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-10-02 09:52:04 +0200

    verify is required to run the jacoco phase

--- .cirrus/analyze.bash
+++ .cirrus/analyze.bash
@@ -2,7 +2,7 @@
 
 # inspired by https://github.com/SonarSource/re-ci-images/blob/b9f43ffec926c0828a279aefc5a94cb5060bfee5/docker/bin/src/regular_mvn_build_deploy_analyze#L51-L61
 
-mvn sonar:sonar \
+mvn verify sonar:sonar \
       -Pcoverage \
       -Dmaven.test.redirectTestOutputToFile=false \
       -Dsonar.host.url="$SONAR_HOST_URL" \
