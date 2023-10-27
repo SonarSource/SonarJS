0af57d3495710b266ad17a6888273b21213ed1f5
yassin-kammoun-sonarsource <yassin.kammoun@sonarsource.com>
2023-10-27 15:46:03 +0200

    Improve S6478 (`no-unstable-nested-components`): Replace message and refine location

--- packages/jsts/src/rules/S6478/cb.fixture.jsx
+++ packages/jsts/src/rules/S6478/cb.fixture.jsx
@@ -1,41 +1,26 @@
-function Component() {
-    function UnstableNestedComponent() { // Noncompliant {{Do not define components during render. React will see a new component type on every render and destroy the entire subtree’s DOM nodes and state. Instead, move this component definition out of the parent component “Component” and pass data as props.}}
+function Component1() {
+    function NestedComponent1() { // Noncompliant {{Move this component definition out of the parent component and pass data as props.}}
+//           ^^^^^^^^^^^^^^^^
         return <div />;
     }
     return (
         <div>
-            <UnstableNestedComponent />
+            <NestedComponent1 />
         </div>
     );
 }
 
-function SomeComponent({ footer: Footer }) {
-    return (
-        <div>
-            <Footer />
-        </div>
-    );
-}
-
-function Component() {
-    return (
-        <div>
-            <SomeComponent footer={ () => <div /> } /> { /* Noncompliant {{Do not define components during render. React will see a new component type on every render and destroy the entire subtree’s DOM nodes and state. Instead, move this component definition out of the parent component “Component” and pass data as props. If you want to allow component creation in props, set allowAsProps option to true.}} */ }
-        </div>
-    );
-}
-
-class Component extends React.Component {
-    render() {
-        function UnstableNestedComponent() { { /* Noncompliant {{Do not define components during render. React will see a new component type on every render and destroy the entire subtree’s DOM nodes and state. Instead, move this component definition out of the parent component “Component” and pass data as props.}} */ }
+function Component2() {
+    class NestedComponent2 extends React.Component { // Noncompliant
+//        ^^^^^^^^^^^^^^^^
+        render() {
             return <div />;
         }
+    };
 
-        return (
-            <div>
-                <UnstableNestedComponent />
-            </div>
-        );
-    }
+    return (
+      <div>
+        <NestedComponent2 />
+      </div>
+    );
 }
-
