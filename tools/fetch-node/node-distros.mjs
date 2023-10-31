31b3cc8ec185771f3b234faee5b44d851d6ab56b
Ilia Kebets <104737176+ilia-kebets-sonarsource@users.noreply.github.com>
2023-10-31 15:35:15 +0100

    Upgrade embedded Node.js runtime to 20.9.0 (#4333)

--- tools/fetch-node/node-distros.mjs
+++ tools/fetch-node/node-distros.mjs
@@ -1,4 +1,4 @@
-export const NODE_VERSION = 'v20.5.1';
+export const NODE_VERSION = 'v20.9.0';
 
 const NODE_ORG_URL = `https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}`;
 const NODE_ARTIFACTORY_URL = `https://repox.jfrog.io/artifactory/nodejs-dist/${NODE_VERSION}/node-${NODE_VERSION}`;
@@ -16,28 +16,28 @@ export const DISTROS = [
     id: 'win-x64',
     url: `${NODE_ORG_URL}-win-x64.zip`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-win-x64.zip`,
-    sha: '5d2596a00699fadf0ffa8e651f47ff5d719991014b920544d59c80d78569d42f',
+    sha: '70d87dad2378c63216ff83d5a754c61d2886fc39d32ce0d2ea6de763a22d3780',
     binPath: 'node.exe',
   },
   {
     id: 'darwin-arm64',
     url: `${NODE_ORG_URL}-darwin-arm64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-darwin-arm64.tar.gz`,
-    sha: '9cc794517788aee103dfcffa04d0b90ac33854b0d10eb11a26ba4be38403f731',
+    sha: '31d2d46ae8d8a3982f54e2ff1e60c2e4a8e80bf78a3e8b46dcaac95ac5d7ce6a',
     binPath: 'bin/node',
   },
   {
     id: 'darwin-x64',
     url: `${NODE_ORG_URL}-darwin-x64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-darwin-x64.tar.gz`,
-    sha: '7a451dd07551a14ce64afdcc5bf8a71df12558edc19a2a9446e37cefe1e572fb',
+    sha: 'fc5b73f2a78c17bbe926cdb1447d652f9f094c79582f1be6471b4b38a2e1ccc8',
     binPath: 'bin/node',
   },
   {
     id: 'linux-x64',
     url: `${NODE_ORG_URL}-linux-x64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-linux-x64.tar.gz`,
-    sha: 'a8678ae00425acdf692e943e3f1cea11a4c46281e4257b82886423bd4ef6f2b5',
+    sha: 'f0919f092fbf74544438907fa083c21e76b2d7a4bc287f0607ada1553ef16f60',
     binPath: 'bin/node',
   },
 ];
