c7c365989c573d4a6189b3956f3456612c48b722
Victor Diez <victor.diez@sonarsource.com>
2023-11-10 17:58:35 +0100

    allow overriding config to empty exclusions

--- sonar-plugin/sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/utils/Exclusions.java
+++ sonar-plugin/sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/utils/Exclusions.java
@@ -9,12 +9,21 @@ import org.sonar.plugins.javascript.JavaScriptPlugin;
 public class Exclusions {
 
   public static String[] getExcludedPaths(Configuration configuration) {
-    var jsTsExclusions = concat(
-      stream(configuration.getStringArray(JavaScriptPlugin.JS_EXCLUSIONS_KEY)),
-      stream(configuration.getStringArray(JavaScriptPlugin.TS_EXCLUSIONS_KEY))
-    )
-      .filter(x -> !x.isBlank())
-      .toArray(String[]::new);
-    return jsTsExclusions.length == 0 ? JavaScriptPlugin.EXCLUSIONS_DEFAULT_VALUE : jsTsExclusions;
+    if (isExclusionOverridden(configuration)) {
+      return concat(
+        stream(configuration.getStringArray(JavaScriptPlugin.JS_EXCLUSIONS_KEY)),
+        stream(configuration.getStringArray(JavaScriptPlugin.TS_EXCLUSIONS_KEY))
+      )
+        .filter(x -> !x.isBlank())
+        .toArray(String[]::new);
+    }
+    return JavaScriptPlugin.EXCLUSIONS_DEFAULT_VALUE;
+  }
+
+  private static boolean isExclusionOverridden(Configuration configuration) {
+    return (
+      configuration.get(JavaScriptPlugin.JS_EXCLUSIONS_KEY).isPresent() ||
+      configuration.get(JavaScriptPlugin.TS_EXCLUSIONS_KEY).isPresent()
+    );
   }
 }
