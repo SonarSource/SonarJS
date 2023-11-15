facc22b738ea31d59a988513ab3c7d7b538bb52c
Victor Diez <victor.diez@sonarsource.com>
2023-11-15 13:39:33 +0100

    improve coverage

--- packages/jsts/src/rules/S1874/unit.test.ts
+++ packages/jsts/src/rules/S1874/unit.test.ts
@@ -150,3 +150,65 @@ ReactDOMServer.renderToNodeStream(element);
     },
   ],
 });
+
+shouldRaiseAllIssues(path.join(fixtures, 'noreact1/file.js'));
+shouldRaiseAllIssues(path.join(fixtures, 'noreact2/file.js'));
+
+function shouldRaiseAllIssues(filename) {
+  ruleTester.run(`No React ${filename}`, rule, {
+    valid: [
+      {
+        code: `
+import React from 'react';
+import ReactDOM from 'react-dom';
+`,
+        filename,
+      },
+    ],
+    invalid: [
+      {
+        code: `
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+React.render(<MyComponent />, root);
+
+React.unmountComponentAtNode(root);
+
+React.findDOMNode(this.refs.foo);
+
+React.renderToString(<MyComponent />);
+
+React.renderToStaticMarkup(<MyComponent />);
+
+React.createClass({ /* Class object */ });
+
+const propTypes = {
+  foo: React.PropTypes.bar,
+};
+
+//Any factories under React.DOM
+React.DOM.div();
+
+class ApiCall extends React.Component {
+// old lifecycles (since React 16.9)
+  componentWillMount() {}
+  componentWillReceiveProps() {}
+  componentWillUpdate() {}
+}
+
+// React 18 deprecations
+ReactDOM.render(<div></div>, container);
+
+ReactDOM.hydrate(<div></div>, container);
+
+ReactDOM.unmountComponentAtNode(container);
+
+ReactDOMServer.renderToNodeStream(element);
+`,
+        filename,
+        errors: 15,
+      },
+    ],
+  });
+}
