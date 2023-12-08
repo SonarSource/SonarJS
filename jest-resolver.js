9b97cac3c07c7158c64eff83035ac122c3c8a068
Tibor Blenessy <saberduck@users.noreply.github.com>
2023-12-08 18:03:25 +0100

    Fix jest resolver

---
+++ jest-resolver.js
@@ -0,0 +1,41 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2023 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+ */
+module.exports = (path, options) => {
+  // Call the defaultResolver, so we leverage its cache, error handling, etc.
+  return options.defaultResolver(path, {
+    ...options,
+    // Use packageFilter to process parsed `package.json` before the resolution (see https://www.npmjs.com/package/resolve#resolveid-opts-cb)
+    packageFilter: pkg => {
+      if (pkg.name === '@typescript-eslint/parser') {
+        /**
+         * `@typescript-eslint/parser` uses more modern "exports" field in its package.json, which is not yet supported by jest default
+         * resolver (see https://github.com/browserify/resolve/issues/222, https://github.com/jestjs/jest/issues/9771).
+         * This is a workaround to make it work.
+         */
+        return {
+          ...pkg,
+          main: pkg.exports['.'].default,
+        };
+      } else {
+        return pkg;
+      }
+    },
+  });
+};
