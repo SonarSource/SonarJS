c9272c7a0a53d0cc6bc3aa4142ea6a7f9943415d
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-12-15 16:18:25 +0100

    fix more stuff

--- packages/ruling/tests/jsTsRuling.test.ts
+++ packages/ruling/tests/jsTsRuling.test.ts
@@ -37,7 +37,7 @@ describe('Ruling', () => {
   beforeAll(() => {
     const sourcesPath = path.join(__dirname, '..', '..', '..', 'its', 'sources');
     const jsTsProjectsPath = path.join(sourcesPath, 'jsts', 'projects');
-    projects = getFolders(jsTsProjectsPath);
+    projects = getFolders(jsTsProjectsPath).filter(project => !project.includes('TypeScript'));
   });
   it(
     'should run the ruling tests',
@@ -46,21 +46,27 @@ describe('Ruling', () => {
     },
     20 * 60 * 1000,
   );
+  it.skip('should fix results', () => {
+    const res = require('./actual/jsts/amplify/results.json');
+    //const litsRes = transformResults('/Users/ilia.kebets/Dev/Sonar/SonarJS/its/sources/jsts/projects/amplify', 'wazaa', res);
+    writeResults(
+      'wazaa',
+      res,
+      '/Users/ilia.kebets/Dev/Sonar/SonarJS/its/sources/jsts/projects/amplify',
+    );
+  });
 });
 
 async function runRuling() {
-  setContext({
-    workDir: path.join(os.tmpdir(), 'sonarjs'),
-    shouldUseTypeScriptParserForJS: true,
-    sonarlint: false,
-    bundles: [],
-  });
-
   for (const project of projects) {
     console.log(`Testing project ${project}`);
     const results = await testProject(project);
-    writeResults(project, results);
+    writeResults(pickLastFolder(project), results, project);
   }
+}
+
+function pickLastFolder(projectPath: string) {
+  return projectPath.split(path.posix.sep).at(-1);
 }
 
 /**
@@ -74,12 +80,66 @@ async function runRuling() {
 function writeResults(
   project: string,
   results: ProjectAnalysisOutput,
+  sourceDir: string,
   isJsTs: boolean = true,
   baseDir: string = path.join(__dirname, 'actual'),
 ) {
   const projectDir = path.join(baseDir, isJsTs ? 'jsts' : 'css', project);
   fs.mkdirSync(projectDir, { recursive: true });
-  fs.writeFileSync(path.join(projectDir, 'results.json'), JSON.stringify(results, null, 2));
+  const litsResults = transformResults(sourceDir, project, results);
+  for (const [ruleId, { js: jsIssues, ts: tsIssues }] of Object.entries(litsResults.issues)) {
+    writeIssues(projectDir, ruleId, jsIssues);
+    writeIssues(projectDir, ruleId, tsIssues, false);
+  }
+
+  function writeIssues(projectDir: string, ruleId: string, issues, isJs: boolean = true) {
+    if (Object.keys(issues).length === 0) return;
+    const issueFilename = path.join(
+      projectDir,
+      `${isJs ? 'javascript' : 'typescript'}-${ruleId}.json`,
+    );
+    fs.writeFileSync(issueFilename, JSON.stringify(issues, null, 2));
+  }
+}
+
+type LitsFormattedResult = {
+  issues: {
+    [ruleId: string]: {
+      [filename: string]: number[];
+    };
+  };
+};
+
+function transformResults(
+  projectsSourceDir: string,
+  project: string,
+  results: ProjectAnalysisOutput,
+) {
+  const litsResult: LitsFormattedResult = {
+    issues: {},
+  };
+  for (const [filename, fileData] of Object.entries(results.files)) {
+    const filenamePathInProject = retrieveFilename(projectsSourceDir.length + 1, filename);
+    processIssues(litsResult, `${project}:${filenamePathInProject}`, fileData.issues);
+  }
+  return litsResult;
+
+  function retrieveFilename(prefixLength: number, filename: string) {
+    return filename.substring(prefixLength);
+  }
+
+  function processIssues(result, projectWithFilename, issues) {
+    for (const issue of issues) {
+      const ruleId = issue.ruleId;
+      if (result.issues[ruleId] === undefined) result.issues[ruleId] = { js: {}, ts: {} };
+      if (result.issues[ruleId][isJs(projectWithFilename)][projectWithFilename] === undefined)
+        result.issues[ruleId][isJs(projectWithFilename)][projectWithFilename] = [];
+      result.issues[ruleId][isJs(projectWithFilename)][projectWithFilename].push(issue.line);
+    }
+  }
+  function isJs(filename: string) {
+    return filename.endsWith('js') ? 'js' : 'ts';
+  }
 }
 
 /**
@@ -135,14 +195,14 @@ function testProject(projectPath: string, exclusions: string = '') {
 function getFiles(acc: JsTsFiles, dir: string, exclusions: Minimatch[], type: FileType = 'MAIN') {
   const files = fs.readdirSync(dir, { recursive: true }) as string[];
   for (const file of files) {
-    if (!isJsTsFile(file)) continue;
+    if (!isJsTsFile(path.join(dir, file))) continue;
     if (!isExcluded(file, exclusions)) {
       acc[path.join(dir, file)] = { fileType: type };
     }
   }
 
   function isJsTsFile(filePath: string) {
-    return filePath.endsWith('.js') || filePath.endsWith('.ts');
+    return fs.statSync(filePath).isFile() && (filePath.endsWith('.js') || filePath.endsWith('.ts'));
   }
 
   function isExcluded(filePath: string, exclusions: Minimatch[]) {
