37570fa01fa1d62f59dbb78aab36ee6b42eb894a
Ilia Kebets <ilia.kebets@sonarsource.com>
2023-12-27 10:59:45 +0100

    fix paths

--- jest.config.js
+++ jest.config.js
@@ -14,7 +14,7 @@ module.exports = {
   testMatch: [
     '<rootDir>/packages/*/tests/**/*.test.ts',
     '<rootDir>/packages/*/src/rules/**/*.test.ts',
-    '<rootDir>/packages/ruling/tests/*.ruling.test.ts',
+    '<rootDir>/packages/ruling/tests/projects/*.ruling.test.ts',
   ],
   testTimeout: 20000,
 };
