a14876cdf72b87396fdbaa11a972f4279e97c547
Ilia Kebets <ilia.kebets@sonarsource.com>
2024-01-03 19:14:26 +0100

    add check for node v20.10

--- tools/prepare-ruling.js
+++ tools/prepare-ruling.js
@@ -1,5 +1,12 @@
 const path = require('path');
 const fs = require('fs');
+const semver = require('semver');
+
+const version = process.versions.node;
+if (semver.lt(version, '20.10.0')) {
+  console.error('Node.js 20.10.0 or higher is required');
+  process.exit(1);
+}
 
 const TARGET = path.join(__dirname, '..', 'its', 'sources');
 const LINK = path.join(__dirname, '..', '..', 'sonarjs-ruling-sources');
