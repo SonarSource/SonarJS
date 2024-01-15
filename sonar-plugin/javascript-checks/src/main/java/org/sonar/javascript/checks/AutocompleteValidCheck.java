1be928aa08d52b11f84aa60b4c520d521064997b
Ilia Kebets <ilia.kebets@sonarsource.com>
2024-01-15 15:54:08 +0100

    Revert "Update license to 2024 (#4489)"
    This reverts commit cf8b0438a8016ef85f3502e90d87b9df4fc5bcf0.

--- sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/AutocompleteValidCheck.java
+++ sonar-plugin/javascript-checks/src/main/java/org/sonar/javascript/checks/AutocompleteValidCheck.java
@@ -1,6 +1,6 @@
-/*
+/**
  * SonarQube JavaScript Plugin
- * Copyright (C) 2011-2024 SonarSource SA
+ * Copyright (C) 2011-2023 SonarSource SA
  * mailto:info AT sonarsource DOT com
  *
  * This program is free software; you can redistribute it and/or
