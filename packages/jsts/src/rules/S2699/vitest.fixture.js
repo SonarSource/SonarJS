201cf01758cd0408871a68ff4925ac99a369c433
Eric Morand <156682586+ericmorand-sonarsource@users.noreply.github.com>
2024-03-26 16:55:16 +0100

    Resolve issue #4459: Fix FP S2699 (`Tests should include assertions`): global `expect()` aren't detected (#4628)

--- packages/jsts/src/rules/S2699/vitest.fixture.js
+++ packages/jsts/src/rules/S2699/vitest.fixture.js
@@ -19,6 +19,10 @@ describe('vitest test cases', () => {
     check();
   });
 
+  it('recognizes global expect as an assertion', () => {
+    expect(5).toEqual(5);
+  });
+
   function check() {
     expect(1).toEqual(2);
   }
