55f0df942db5b820e8384ca26fad2e5d114419aa
Victor <victor.diez@sonarsource.com>
2024-04-18 15:51:38 +0200

    [JS-9] Rename rule S6855 to S4084 (#4667)

--- packages/jsts/src/rules/S4084/decorator.ts
+++ packages/jsts/src/rules/S4084/decorator.ts
@@ -17,7 +17,7 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
-// https://sonarsource.github.io/rspec/#/rspec/S6855/javascript
+// https://sonarsource.github.io/rspec/#/rspec/S4084/javascript
 
 import { Rule } from 'eslint';
 import { Node } from 'estree';
