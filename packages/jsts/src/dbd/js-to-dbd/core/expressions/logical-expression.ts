d822beac5a9c1e9af96bea20fed8c72868791bd3
Michal Zgliczynski <michal.zgliczynski@sonarsource.com>
2024-06-03 09:42:33 +0200

    fixup ?? || && operators

--- packages/jsts/src/dbd/js-to-dbd/core/expressions/logical-expression.ts
+++ packages/jsts/src/dbd/js-to-dbd/core/expressions/logical-expression.ts
@@ -1,11 +1,12 @@
 import type { ExpressionHandler } from '../expression-handler';
 import { TSESTree } from '@typescript-eslint/utils';
 import { handleExpression } from './index';
-import { createConstant } from '../values/constant';
 import { createBranchingInstruction } from '../instructions/branching-instruction';
 import { createConditionalBranchingInstruction } from '../instructions/conditional-branching-instruction';
 import { createPhiInstruction } from '../instructions/phi-instruction';
-import { createReference } from '../values/reference';
+import { createNull, createReference } from '../values/reference';
+import { createCallInstruction } from '../instructions/call-instruction';
+import { createBinaryOperationFunctionDefinition } from '../function-definition';
 
 export const handleLogicalExpression: ExpressionHandler<TSESTree.LogicalExpression> = (
   node,
@@ -13,7 +14,7 @@ export const handleLogicalExpression: ExpressionHandler<TSESTree.LogicalExpressi
   scopeReference,
 ) => {
   const { left, right, operator } = node;
-  const { createScopedBlock, blockManager } = context;
+  const { createScopedBlock, blockManager, scopeManager } = context;
 
   const { value: leftValue } = handleExpression(left, context, scopeReference);
   const startingBlock = blockManager.getCurrentBlock();
@@ -24,88 +25,60 @@ export const handleLogicalExpression: ExpressionHandler<TSESTree.LogicalExpressi
   blockManager.pushBlock(consequentBlock);
   const { value: rightValue } = handleExpression(right, context, scopeReference);
   const blockAfterRightExpression = blockManager.getCurrentBlock();
-
-  const handleLogicalOperators = (operator: '||' | '&&') => {
-    const falsyBlock = createScopedBlock(node.loc);
-    falsyBlock.instructions.push(createBranchingInstruction(finallyBlock, node.loc));
-    blockManager.pushBlock(falsyBlock);
-
-    const truthyBlock = context.createScopedBlock(node.loc);
-    truthyBlock.instructions.push(createBranchingInstruction(finallyBlock, node.loc));
-    blockManager.pushBlock(truthyBlock);
-
-    if (operator === '&&') {
-      blockAfterRightExpression.instructions.push(
-        createConditionalBranchingInstruction(rightValue, truthyBlock, falsyBlock, right.loc),
-      );
-
-      startingBlock.instructions.push(
-        createConditionalBranchingInstruction(leftValue, consequentBlock, falsyBlock, left.loc),
-      );
-    } else {
-      blockAfterRightExpression.instructions.push(
-        createConditionalBranchingInstruction(rightValue, truthyBlock, falsyBlock, right.loc),
-      );
-
-      startingBlock.instructions.push(
-        createConditionalBranchingInstruction(leftValue, truthyBlock, consequentBlock, left.loc),
-      );
-    }
-
-    const returnValue = createReference(context.scopeManager.createValueIdentifier());
-    finallyBlock.instructions.push(
-      createPhiInstruction(
-        returnValue,
-        null,
-        new Map([
-          [truthyBlock, createConstant(context.scopeManager.createValueIdentifier(), true)],
-          [falsyBlock, createConstant(context.scopeManager.createValueIdentifier(), false)],
-        ]),
-        node.loc,
-      ),
-    );
-    blockManager.pushBlock(finallyBlock);
-    return {
-      instructions: [],
-      value: returnValue,
-    };
-  };
-
-  const handleNullCoalesce = () => {
-    blockAfterRightExpression.instructions.push(
-      createBranchingInstruction(finallyBlock, right.loc),
-    );
-
-    startingBlock.instructions.push(
-      createConditionalBranchingInstruction(leftValue, finallyBlock, consequentBlock, left.loc),
-    );
-
-    const returnValue = createReference(context.scopeManager.createValueIdentifier());
-    finallyBlock.instructions.push(
-      createPhiInstruction(
-        returnValue,
-        null,
-        new Map([
-          [startingBlock, leftValue],
-          [blockAfterRightExpression, rightValue],
-        ]),
-        node.loc,
-      ),
-    );
-
-    blockManager.pushBlock(finallyBlock);
-    return {
-      instructions: [],
-      value: returnValue,
-    };
-  };
+  blockAfterRightExpression.instructions.push(createBranchingInstruction(finallyBlock, right.loc));
 
   switch (operator) {
-    case '||':
-    case '&&':
-      return handleLogicalOperators(operator);
+    case '||': {
+      startingBlock.instructions.push(
+        createConditionalBranchingInstruction(leftValue, finallyBlock, consequentBlock, left.loc),
+      );
+      break;
+    }
+    case '&&': {
+      startingBlock.instructions.push(
+        createConditionalBranchingInstruction(leftValue, consequentBlock, finallyBlock, left.loc),
+      );
+      break;
+    }
     case '??': {
-      return handleNullCoalesce();
+      const isNullLeftValue = createReference(scopeManager.createValueIdentifier());
+      startingBlock.instructions.push(
+        createCallInstruction(
+          isNullLeftValue.identifier,
+          null,
+          createBinaryOperationFunctionDefinition('=='),
+          [leftValue, createNull()],
+          left.loc,
+        ),
+      );
+      startingBlock.instructions.push(
+        createConditionalBranchingInstruction(
+          isNullLeftValue,
+          consequentBlock,
+          finallyBlock,
+          left.loc,
+        ),
+      );
+      break;
     }
   }
+
+  const returnValue = createReference(scopeManager.createValueIdentifier());
+  finallyBlock.instructions.push(
+    createPhiInstruction(
+      returnValue,
+      null,
+      new Map([
+        [startingBlock, leftValue],
+        [blockAfterRightExpression, rightValue],
+      ]),
+      node.loc,
+    ),
+  );
+
+  blockManager.pushBlock(finallyBlock);
+  return {
+    instructions: [],
+    value: returnValue,
+  };
 };
