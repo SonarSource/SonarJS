55e2152b7b6056d3326039e5d6b7959cd45b58cc
Quentin Jaquier <quentin.jaquier@sonarsource.com>
2024-06-11 14:37:56 +0200

    fix tests

--- sonar-plugin/api/src/test/java/org/sonar/plugins/javascript/api/estree/ESTreeTest.java
+++ sonar-plugin/api/src/test/java/org/sonar/plugins/javascript/api/estree/ESTreeTest.java
@@ -1,9 +1,9 @@
 package org.sonar.plugins.javascript.api.estree;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 import java.util.Arrays;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 class ESTreeTest {
 
@@ -11,21 +11,21 @@ class ESTreeTest {
   void test() {
 
     Class<?>[] classes = ESTree.class.getDeclaredClasses();
-    assertThat(classes).hasSize(108);
+    assertThat(classes).hasSize(105);
 
     //filter all classes that are interface
     var ifaceCount = Arrays.stream(classes).filter(Class::isInterface).count();
     assertThat(ifaceCount).isEqualTo(24);
 
     var recordCount = Arrays.stream(classes).filter(Class::isRecord).count();
-    assertThat(recordCount).isEqualTo(79);
+    assertThat(recordCount).isEqualTo(76);
   }
 
   @Test
   void test_node_subclasses() {
     Class<?> sealedClass = ESTree.Node.class;
     Class<?>[] permittedSubclasses = sealedClass.getPermittedSubclasses();
-    assertThat(permittedSubclasses).hasSize(24);
+    assertThat(permittedSubclasses).hasSize(23);
   }
 
   @Test
