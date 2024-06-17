a76552ec3eb42cde1046aaee63f90072a1b03300
Michal Zgliczynski <michal.zgliczynski@sonarsource.com>
2024-06-17 15:40:24 +0200

    revert test change

--- packages/ruling/tests/tools/testProject.ts
+++ packages/ruling/tests/tools/testProject.ts
@@ -122,28 +122,7 @@ export async function testProject(rulingInput: RulingInput) {
   const { jsTsFiles, htmlFiles, yamlFiles } = getProjectFiles(rulingInput, projectPath, exclusions);
 
   const payload: ProjectAnalysisInput = {
-    rules: [
-      {
-        key: 'dbd',
-        language: 'ts',
-        configurations: [
-          {
-            irDirectory: 'ir/python',
-          },
-        ],
-        fileTypeTarget: 'MAIN',
-      },
-      {
-        key: 'dbd',
-        language: 'js',
-        configurations: [
-          {
-            irDirectory: 'ir/python',
-          },
-        ],
-        fileTypeTarget: 'MAIN',
-      },
-    ] as RuleConfig[],
+    rules: rules as RuleConfig[],
     baseDir: projectPath,
     files: jsTsFiles,
   };
