43b1c47672c47d00c7f1748a35697f9747206f27
Victor Diez <victor.diez@sonarsource.com>
2024-07-08 16:07:59 +0200

    revert changes on java ruling

--- its/ruling/src/test/java/org/sonar/javascript/it/JsTsRulingTest.java
+++ its/ruling/src/test/java/org/sonar/javascript/it/JsTsRulingTest.java
@@ -88,7 +88,56 @@ class JsTsRulingTest {
 
   public static Stream<Arguments> ruling() {
     return Stream.of(
-      jsTsProject("amplify", "external/**", "test")
+      jsTsProject("amplify", "external/**", "test"),
+      jsTsProject("angular.js", "src/ngLocale/**, i18n/**", "test"),
+      jsTsProject("backbone", "test"),
+      jsTsProject("es5-shim", "tests"),
+      jsTsProject("fireact"),
+      jsTsProject("ace"),
+      jsTsProject("ecmascript6-today"),
+      jsTsProject("expressionist.js"),
+      jsTsProject("Ghost"),
+      jsTsProject("http"),
+      jsTsProject("reddit-mobile"),
+      jsTsProject("redux"),
+      jsTsProject("router"),
+      jsTsProject("snoode"),
+      jsTsProject("sonar-web"),
+      jsTsProject("templating"),
+      jsTsProject("watchtower.js"),
+      jsTsProject("jira-clone"),
+      jsTsProject("jquery", "test"),
+      jsTsProject("jshint", "tests"),
+      jsTsProject("jStorage", "tests"),
+      jsTsProject("knockout", "spec"),
+      jsTsProject("mootools-core", "Specs"),
+      jsTsProject("ocanvas", "build/**", ""),
+      jsTsProject("p5.js", "test"),
+      jsTsProject("paper.js", "gulp/jsdoc/**, packages/**", "test"),
+      jsTsProject("prototype", "test"),
+      jsTsProject("qunit", "test"),
+      jsTsProject("react-cloud-music"),
+      jsTsProject("sizzle", "external/**", "test"),
+      jsTsProject("underscore", "test"),
+      jsTsProject("ag-grid", "spec"),
+      jsTsProject("ant-design", "tests"), // todo: many dirs **/__tests__
+      jsTsProject("console"), // todo: many dirs **/__tests__
+      jsTsProject("courselit", ".yarn/**", ""),
+      jsTsProject("desktop", "app/test"),
+      jsTsProject("eigen"), // todo
+      jsTsProject("fireface"),
+      jsTsProject("ionic2-auth"),
+      jsTsProject("Joust"), // todo: files **/*.spec.ts
+      jsTsProject("moose"),
+      jsTsProject("postgraphql"), // todo: many dirs **/__tests__
+      jsTsProject("prettier-vscode"),
+      jsTsProject("rxjs", "spec"),
+      jsTsProject("searchkit"), // todo
+      jsTsProject("TypeScript", "src/harness/unittests"),
+      jsTsProject("vuetify"),
+      jsTsProject("it-tools"),
+      jsTsProject("yaml", "../sources/yaml", "", ""),
+      jsTsProject("file-for-rules", "../sources/jsts/custom", "", "tests")
     );
   }
 
@@ -208,6 +257,7 @@ class JsTsRulingTest {
 
   @ParameterizedTest
   @MethodSource
+  @Execution(ExecutionMode.CONCURRENT)
   void ruling(String project, String sourceDir, String exclusions, String testDir)
     throws Exception {
     runRulingTest(project, sourceDir, exclusions, testDir);
