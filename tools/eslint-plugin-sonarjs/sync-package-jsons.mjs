0ad5a0f9781152109df0d9a4cac09cb820645bbb
Ilia Kebets <ilia.kebets@sonarsource.com>
2024-07-08 13:06:15 +0200

    implement script

--- tools/eslint-plugin-sonarjs/sync-package-jsons.mjs
+++ tools/eslint-plugin-sonarjs/sync-package-jsons.mjs
@@ -2,3 +2,33 @@
  * 1. copy select dependencies from the root package.json to the packages/eslint-plugin-sonarjs/package.json
  * 2. copy engines property from the root package.json to the packages/eslint-plugin-sonarjs/package.json
  */
+import * as fs from 'node:fs';
+import * as path from 'node:path';
+const __dirname = import.meta.dirname;
+import { default as rootPackageJson } from '../../package.json' assert { type: 'json' };
+import { default as pluginPackageJson } from '../../packages/eslint-plugin-sonarjs/package.json' assert { type: 'json' };
+
+const USED_DEPENDENCIES = new Set([
+  'eslint',
+  'eslint-plugin-import',
+  'eslint-plugin-jsx-a11y',
+  'eslint-plugin-react',
+  'eslint-plugin-react-hooks',
+  '@typescript-eslint/eslint-plugin',
+  '@typescript-eslint/utils',
+  'functional-red-black-tree',
+  'bytes',
+  'scslre',
+]);
+console.log(rootPackageJson);
+Object.keys(rootPackageJson.dependencies).map(dependency => {
+  if (USED_DEPENDENCIES.has(dependency)) {
+    pluginPackageJson.dependencies[dependency] = rootPackageJson.dependencies[dependency];
+  }
+});
+pluginPackageJson.engines = rootPackageJson.engines;
+console.log('writin', pluginPackageJson);
+fs.writeFileSync(
+  path.join(__dirname, '..', '..', 'packages', 'eslint-plugin-sonarjs', 'package.json'),
+  JSON.stringify(pluginPackageJson, null, 2),
+);
