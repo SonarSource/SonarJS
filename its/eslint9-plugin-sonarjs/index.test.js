f95b094f20ce266cd20db43b6c7ce6ad580fc335
Victor Diez <victor.diez@sonarsource.com>
2024-07-23 09:51:44 +0200

    wrapping up

--- its/eslint9-plugin-sonarjs/index.test.js
+++ its/eslint9-plugin-sonarjs/index.test.js
@@ -8,13 +8,22 @@ test('should work with CommonJS config', async t => {
     encoding: 'utf-8',
   });
   const output = result.stdout;
-  console.log(output);
   const errorLines = output.split('\n').filter(line => line.includes('error'));
   assert(errorLines.length > 4);
 });
 
 test('should work with ECMAScript modules config', async t => {
   const result = spawn.sync('npx', ['eslint', '-c', 'eslint.config.mjs', 'file.js'], {
+    cwd: __dirname,
+    encoding: 'utf-8',
+  });
+  const output = result.stdout;
+  const errorLines = output.split('\n').filter(line => line.includes('error'));
+  assert(errorLines.length > 4);
+});
+
+test('should work with TSESLint config', async t => {
+  const result = spawn.sync('npx', ['eslint', '-c', 'tseslint.config.mjs', 'file.js'], {
     cwd: __dirname,
     encoding: 'utf-8',
   });
