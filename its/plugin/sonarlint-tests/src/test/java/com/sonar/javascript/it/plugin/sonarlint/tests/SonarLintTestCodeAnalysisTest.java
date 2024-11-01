12baa4d119ccda6b709f3bccd739ec1c9111037b
Victor <victor.diez@sonarsource.com>
2024-11-01 15:33:40 +0100

    JS-385 Fix SonarLint ITS (#4889)

--- its/plugin/sonarlint-tests/src/test/java/com/sonar/javascript/it/plugin/sonarlint/tests/SonarLintTestCodeAnalysisTest.java
+++ its/plugin/sonarlint-tests/src/test/java/com/sonar/javascript/it/plugin/sonarlint/tests/SonarLintTestCodeAnalysisTest.java
@@ -17,6 +17,9 @@
  * along with this program; if not, write to the Free Software Foundation,
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
+
+package com.sonar.javascript.it.plugin.sonarlint.tests;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.nio.file.Files;
