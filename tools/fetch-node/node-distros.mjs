b00b93531bc54b34e49c19eb6e6e83738f63ec0a
zglicz <michal.zgliczynski@sonarsource.com>
2024-11-20 13:41:05 +0100

    JS-383 Update embedded node to v22.11.0 (#4909)

--- tools/fetch-node/node-distros.mjs
+++ tools/fetch-node/node-distros.mjs
@@ -1,4 +1,4 @@
-export const NODE_VERSION = 'v20.17.0';
+export const NODE_VERSION = 'v22.11.0';
 
 const NODE_ORG_URL = `https://nodejs.org/dist/${NODE_VERSION}/node-${NODE_VERSION}`;
 const NODE_ARTIFACTORY_URL = `https://repox.jfrog.io/artifactory/nodejs-dist/${NODE_VERSION}/node-${NODE_VERSION}`;
@@ -16,35 +16,35 @@ export const DISTROS = [
     id: 'win-x64',
     url: `${NODE_ORG_URL}-win-x64.zip`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-win-x64.zip`,
-    sha: 'e323fff0aba197090faabd29c4c23f334557ff24454324f0c83faa7e399dbb74',
+    sha: '905373a059aecaf7f48c1ce10ffbd5334457ca00f678747f19db5ea7d256c236',
     binPath: 'node.exe',
   },
   {
     id: 'darwin-arm64',
     url: `${NODE_ORG_URL}-darwin-arm64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-darwin-arm64.tar.gz`,
-    sha: '476324108c4361935465631eec47df1c943ba2c87bc050853385b1d1c71f0b1f',
+    sha: '2e89afe6f4e3aa6c7e21c560d8a0453d84807e97850bbb819b998531a22bdfde',
     binPath: 'bin/node',
   },
   {
     id: 'darwin-x64',
     url: `${NODE_ORG_URL}-darwin-x64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-darwin-x64.tar.gz`,
-    sha: 'eefe9447dbb0b5b233d42730989c6c364487de4043145db2f63da94e9623c380',
+    sha: '668d30b9512137b5f5baeef6c1bb4c46efff9a761ba990a034fb6b28b9da2465',
     binPath: 'bin/node',
   },
   {
     id: 'linux-arm64',
     url: `${NODE_ORG_URL}-linux-arm64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-linux-arm64.tar.gz`,
-    sha: '18afbf2781edfcc9918343f4bf74a8c35d74d778b85d40a0c09b232adc0ea82c',
+    sha: '27453f7a0dd6b9e6738f1f6ea6a09b102ec7aa484de1e39d6a1c3608ad47aa6a',
     binPath: 'bin/node',
   },
   {
     id: 'linux-x64',
     url: `${NODE_ORG_URL}-linux-x64.tar.gz`,
     artifactoryUrl: `${NODE_ARTIFACTORY_URL}-linux-x64.tar.gz`,
-    sha: '21e656f6f4e34080ddc5d75fbfe58ce8482fe6e70a76aeae14afdcdc1e23079d',
+    sha: '4f862bab52039835efbe613b532238b6e4dde98d139a34e6923193e073438b13',
     binPath: 'bin/node',
   },
 ];
