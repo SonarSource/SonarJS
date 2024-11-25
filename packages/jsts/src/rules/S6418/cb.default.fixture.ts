48f4c64b722c158cc12f82ea144d3d5d22a7a08b
Ilia Kebets <ilia.kebets@sonarsource.com>
2024-11-25 12:11:36 +0100

    de-pythonize

--- packages/jsts/src/rules/S6418/cb.default.fixture.ts
+++ packages/jsts/src/rules/S6418/cb.default.fixture.ts
@@ -1,44 +1,43 @@
 function func() {
-    const token = "rf6acB24J//1FZLRrKpjmBUYSnUX5CHlt/iD5vVVcgVuAIOB6hzcWjDnv16V6hDLevW0Qs4hKPbP1M4YfuDI16sZna1/VGRLkAbTk6xMPs4epH6A3ZqSyyI-H92y" // Noncompliant
+    const token = 'rf6acB24J//1FZLRrKpjmBUYSnUX5CHlt/iD5vVVcgVuAIOB6hzcWjDnv16V6hDLevW0Qs4hKPbP1M4YfuDI16sZna1/VGRLkAbTk6xMPs4epH6A3ZqSyyI-H92y' // Noncompliant
 //                ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    let api_key = "not enough entropy"
-    api_key = "rf6acB24J//1FZLRrKpjmBUYSnUX5CHlt/iD5vVVcgVuAIOB6hzcWjDnv16V6hDLevW0Qs4hKPbP1M4YfuDI16sZna1/VGRLkAbTk6xMPs4epH6A3ZqSyyI-H92y" // Noncompliant
+    let api_key = 'not enough entropy'
+    api_key = 'rf6acB24J//1FZLRrKpjmBUYSnUX5CHlt/iD5vVVcgVuAIOB6hzcWjDnv16V6hDLevW0Qs4hKPbP1M4YfuDI16sZna1/VGRLkAbTk6xMPs4epH6A3ZqSyyI-H92y' // Noncompliant
 }
-function entropy_too_low() {
-    const token = "rf6acB24J//1FZLRrKpjmBUYSnUX5CHlt/iD5vVaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+function entropyTooLow() {
+    const token = 'rf6acB24J//1FZLRrKpjmBUYSnUX5CHlt/iD5vVaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
   }
 class MyClass {
-    secret = "1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=" // Noncompliant
+    secret = '1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=' // Noncompliant
 }
 
-function in_function_call() {
-  call_with_secret({ secret: "1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=" }) // Noncompliant
+function inFunctionCall() {
+  callWithSecret({ secret: '1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=' }) // Noncompliant
 
-  function call_with_secret({}) {}
+  function callWithSecret({}) {}
 }
-function function_with_secret({ secret = "1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=" }) { // Noncompliant
-//                                       ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+function functionWithSecret({ secret = '1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=' }) { // Noncompliant
+//                                     ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
   }
-function clean_function(some_arg, parameter="a string", another_parameter: 42, ...args) {
-  another_call(42, "a string", parameter, { a_keyword: 42 }, args)
+function cleanFunction(someArg, parameter='a string', anotherParameter: 42, ...args) {
+  another_call(42, 'a string', parameter, { a_keyword: 42 }, args)
 
   function another_call(...foo) {}
 }
 
-const some_dict = {
-    "secret": "1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=", // Noncompliant
-//            ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
-    "not_a_problem": "not_a_secret",
-    42: "forty-two"
+const someObject = {
+  secret: '1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=', // Noncompliant
+//        ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+  not_a_problem: 'not_a_secret',
+  42: 'forty-two'
 }
 
-function multiple_assignment() {
-  let nothing = 1, secret = "1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v.~=", nothing_else = 2; // Noncompliant
-                       //   ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
+function multipleAssignment() {
+  let nothing = 1, secret = '1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v.~=', nothing_else = 2; // Noncompliant
+//                          ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 }
-function assignment_with_type() {
-    const secret: string = "1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=" // Noncompliant
-
-    let some_var: string;
-    const another_var: number = 42
-  }
+function assignmentWithType() {
+  const secret: string = '1IfHMPanImzX8ZxC-Ud6+YhXiLwlXq$f_-3v~.=' // Noncompliant
+  let someVar: string;
+  const anotherVar: number = 42
+}
