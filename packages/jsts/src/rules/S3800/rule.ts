b50ec6c0b6c0944f29378369fa13e6d88253b97d
Tibor Blenessy <saberduck@users.noreply.github.com>
2024-11-28 13:47:47 +0100

    Fix potentional non-linear regex hotspot

--- packages/jsts/src/rules/S3800/rule.ts
+++ packages/jsts/src/rules/S3800/rule.ts
@@ -174,7 +174,7 @@ function prettyPrint(type: ts.Type, checker: ts.TypeChecker): string {
 function isTypedArray(type: ts.Type, checker: ts.TypeChecker) {
   const typeAsString = checker.typeToString(type);
   // Since TS 5.7 typed arrays include the type of the elements in the string, eg. Float32Array<any>
-  return /.*Array(?:<.*>)?$/.test(typeAsString);
+  return /.*Array(?:<[^>]*>)?$/.test(typeAsString);
 }
 
 function isNullLike(type: ts.Type) {
