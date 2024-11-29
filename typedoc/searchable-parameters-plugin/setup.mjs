c316e195ff75594fee1fa0013c4dbb42fcb93b8a
Tibor Blenessy <saberduck@users.noreply.github.com>
2024-11-29 13:50:06 +0100

    JS-465 Fix typedoc: Migrate to ESM (#4965)

--- typedoc/searchable-parameters-plugin/setup.mjs
+++ typedoc/searchable-parameters-plugin/setup.mjs
@@ -16,11 +16,16 @@
  */
 // typedoc looks for plugins in node_modules/, therefore we create a symlink to this folder in node_modules/
 
-const fs = require('fs');
-const path = require('path');
+import fs from 'fs';
+import path from 'path';
+import url from 'url';
+
+// Directory name of the current module
+const __dirname = path.dirname(url.fileURLToPath(import.meta.url));
 
 const linkPath = path.join(__dirname, '..', '..', 'node_modules', 'searchable-parameters-plugin');
 const targetPath = path.join(__dirname);
 
-if (fs.existsSync(linkPath)) return;
-fs.symlinkSync(targetPath, linkPath);
+if (!fs.existsSync(linkPath)) {
+  fs.symlinkSync(targetPath, linkPath);
+}
