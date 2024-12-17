9bb044ee61419126255d1a24e9ad37c5e306f3ad
Victor Diez <victor.diez@sonarsource.com>
2024-12-17 20:46:08 +0100

    wip

--- sonar-plugin/javascript-checks/src/test/java/org/sonar/javascript/checks/S6418Test.java
+++ sonar-plugin/javascript-checks/src/test/java/org/sonar/javascript/checks/S6418Test.java
@@ -29,7 +29,7 @@ class S6418Test {
     // default configuration
     String defaultConfigAsString = new Gson().toJson(check.configurations());
     assertThat(defaultConfigAsString).isEqualTo(
-      "[{\"secretWords\":\"api[_.-]?key,auth,credential,secret,token\",\"randomnessSensibility\":\"5.0\"}]"
+      "[{\"secretWords\":\"api[_.-]?key,auth,credential,secret,token\",\"randomnessSensibility\":5.0}]"
     );
   }
 }
