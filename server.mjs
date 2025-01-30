5f0023afc955cc874cc434788e785a4421d98ca7
Victor Diez <victor.diez@sonarsource.com>
2025-01-30 21:06:28 +0100

    wip

--- server.mjs
+++ server.mjs
@@ -35,10 +35,5 @@ if (isMainThread) {
   }
 
   context.setContext({ workDir, shouldUseTypeScriptParserForJS, sonarlint, debugMemory, bundles });
-  server.start(
-    Number.parseInt(port),
-    host,
-    createWorker(new URL(import.meta.url), getContext()),
-    600000,
-  );
+  server.start(Number.parseInt(port), host, createWorker(new URL(import.meta.url), getContext()));
 }
