58a6b3757743018320bfef2420fc5d83c3ddc110
Michal Zgliczynski <mzglicz@gmail.com>
2025-02-26 10:15:30 +0100

    add S1105/config.ts

---
+++ packages/jsts/src/rules/S1105/config.ts
@@ -0,0 +1,35 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+// https://sonarsource.github.io/rspec/#/rspec/S1105/javascript
+
+import { ESLintConfiguration } from '../helpers/configs.js';
+
+export const fields = [
+  {
+    default: '1tbps',
+    type: 'string',
+    description: 'enforced brace-style: 1tbs, stroustrup or allman.',
+    sqName: 'braceStyle',
+  },
+  [
+    {
+      field: 'allowSingleLine',
+      type: 'boolean',
+      default: true,
+    },
+  ],
+] as const satisfies ESLintConfiguration;
