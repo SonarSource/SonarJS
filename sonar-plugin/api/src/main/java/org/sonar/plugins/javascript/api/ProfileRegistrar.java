1068b83c568cd553b41751c285a370e77d239e2c
Ilia Kebets <ilia.kebets@sonarsource.com>
2025-03-16 19:27:29 +0100

    Declare ProfileRegistrar interface

---
+++ sonar-plugin/api/src/main/java/org/sonar/plugins/javascript/api/ProfileRegistrar.java
@@ -0,0 +1,40 @@
+package org.sonar.plugins.javascript.api;
+
+import java.util.Collection;
+import org.sonar.api.rule.RuleKey;
+import org.sonar.api.server.ServerSide;
+import org.sonarsource.api.sonarlint.SonarLintSide;
+
+/**
+ * This class can be extended to provide additional rule keys in the builtin default quality profile.
+ *
+ * <pre>
+ *   {@code
+ *     public void register(RegistrarContext registrarContext) {
+ *       registrarContext.registerDefaultQualityProfileRules(ruleKeys);
+ *     }
+ *   }
+ * </pre>
+ *
+ *  Note: It's possible to convert checkClass to RuleKey using:
+ * <pre>
+ *   {@code
+ *     RuleKey.of(repositoryKey, RuleAnnotationUtils.getRuleKey(checkClass))
+ *   }
+ * </pre>
+ */
+@SonarLintSide
+@ServerSide
+public interface ProfileRegistrar {
+  /**
+   * This method is called on server side and during an analysis to modify the builtin default quality profile for java.
+   */
+  void register(RegistrarContext registrarContext);
+
+  interface RegistrarContext {
+    /**
+     * Registers additional rules into the "Sonar Way" default quality profile for the language "java".
+     */
+    void registerDefaultQualityProfileRules(Collection<RuleKey> ruleKeys);
+  }
+}
