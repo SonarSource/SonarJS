965a93ec8a8b57d0431c1bcaefffe83ff947f794
Ilia Kebets <ilia.kebets@sonarsource.com>
2025-03-31 15:12:37 +0200

    Use `nyc` for coverage - with @istanbuljs/esm-loader-hook

---
+++ tools/nyc-esm-hook-loader.js
@@ -0,0 +1,22 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+// https://nodejs.org/docs/latest-v22.x/api/module.html#customization-hooks
+import { register } from 'node:module';
+import { pathToFileURL } from 'node:url';
+
+register('@istanbuljs/esm-loader-hook', pathToFileURL('./'));
