1e6c8380d2c4d8544acaa0f9a4c0da0ba853c03f
Victor Diez <victor.diez@sonarsource.com>
2025-04-30 14:52:53 +0200

    JS-153 Capture parsing errors in CSS

--- packages/css/src/analysis/analyzer.ts
+++ packages/css/src/analysis/analyzer.ts
@@ -18,6 +18,8 @@ import { CssAnalysisInput, CssAnalysisOutput } from './analysis.js';
 import { linter } from '../linter/wrapper.js';
 import { createStylelintConfig } from '../linter/config.js';
 import { fillFileContent } from '../../../shared/src/types/analysis.js';
+import { APIError } from '../../../shared/src/errors/error.js';
+import { error } from '../../../shared/src/helpers/logging.js';
 
 /**
  * Analyzes a CSS analysis input
@@ -39,5 +41,8 @@ export async function analyzeCSS(input: CssAnalysisInput): Promise<CssAnalysisOu
     codeFilename: filePath,
     config,
   };
-  return linter.lint(filePath, options);
+  return linter.lint(filePath, options).catch(err => {
+    error(`Linter failed to parse file ${filePath}: ${err}`);
+    throw APIError.linterError(`Linter failed to parse file ${filePath}: ${err}`);
+  });
 }
