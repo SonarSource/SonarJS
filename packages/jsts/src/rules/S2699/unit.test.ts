1dceee10febb3d120ea4a1acf53f2dc2f2128690
Michal Zgliczynski <mzglicz@gmail.com>
2025-05-01 11:06:12 +0200

    add unit.test.ts with DefaultParser

---
+++ packages/jsts/src/rules/S2699/unit.test.ts
@@ -0,0 +1,52 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+import { DefaultParserRuleTester } from '../../../tests/tools/testers/rule-tester.js';
+import { rule } from './index.js';
+import { describe, it } from 'node:test';
+
+describe('S2699', () => {
+  it('S2699', () => {
+    const ruleTester = new DefaultParserRuleTester();
+    ruleTester.run(`Test cases must have assertions`, rule, {
+      valid: [
+        {
+          code: `
+const chai = require('chai');
+const { expect } = chai;
+describe('chai test cases', () => {
+  it('expect', () => {
+    expect(1).to.equal(2);
+  });
+});
+          `,
+        },
+      ],
+      invalid: [
+        {
+          code: `
+const chai = require('chai');
+describe('chai test cases', () => {
+  it('no assertion', () => {
+    const x = 1 + 2;
+  });
+});`,
+          errors: 1,
+        },
+      ],
+    });
+  });
+});
