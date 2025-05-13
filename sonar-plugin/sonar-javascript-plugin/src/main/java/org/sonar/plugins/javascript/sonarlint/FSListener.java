979546affe692e5746cdd6da19ac3f348df0a556
Victor Diez <victor.diez@sonarsource.com>
2025-05-13 10:25:45 +0200

    JS-518 Rewrite JsTsSensor to use analyze-project endpoint

---
+++ sonar-plugin/sonar-javascript-plugin/src/main/java/org/sonar/plugins/javascript/sonarlint/FSListener.java
@@ -0,0 +1,24 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+package org.sonar.plugins.javascript.sonarlint;
+
+import java.util.List;
+import java.util.Map;
+
+public interface FSListener {
+  List<Map.Entry<String, String>> listFSEvents();
+}
