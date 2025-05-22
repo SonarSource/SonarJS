09a35f72f6b7e9359403546be11f5064b8a2e2f6
Renaud Tognelli <renaud.tognelli@sonarsource.com>
2025-05-22 15:51:58 +0200

    Test expected debug output in UT

--- sonar-plugin/sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/analysis/AnalysisConsumersTest.java
+++ sonar-plugin/sonar-javascript-plugin/src/test/java/org/sonar/plugins/javascript/analysis/AnalysisConsumersTest.java
@@ -20,36 +20,65 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.slf4j.event.Level;
+import org.sonar.api.testfixtures.log.LogTesterJUnit5;
 import org.sonar.plugins.javascript.api.JsAnalysisConsumer;
 import org.sonar.plugins.javascript.api.JsFile;
 
 public class AnalysisConsumersTest {
 
+  @org.junit.jupiter.api.extension.RegisterExtension
+  LogTesterJUnit5 logTester = new LogTesterJUnit5();
+
   @Test
-  public void empty_consumer_list_is_disabled() {
+  void empty_consumer_list_is_disabled() {
     AnalysisConsumers consumers = new AnalysisConsumers();
     assertThat(consumers.isEnabled()).isFalse();
   }
 
   @Test
-  public void consumer_list_with_disabled_consumer_is_disabled() {
+  void consumer_list_with_disabled_consumer_is_disabled() {
     AnalysisConsumers consumers = new AnalysisConsumers(List.of(new Consumer(false)));
     assertThat(consumers.isEnabled()).isFalse();
   }
 
   @Test
-  public void consumer_list_with_mix_of_disabled_and_enabled_consumer_is_enabled() {
+  void consumer_list_with_mix_of_disabled_and_enabled_consumer_is_enabled() {
     AnalysisConsumers consumers = new AnalysisConsumers(
       List.of(new Consumer(false), new Consumer(true))
     );
     assertThat(consumers.isEnabled()).isTrue();
   }
 
+  /**
+   * Some e2e tests relies on the AnalysisConsumers debug output.
+   */
+  @Test
+  void produce_list_of_enabled_and_registered_plugins_in_debug_mode() {
+    logTester.setLevel(Level.DEBUG);
+    AnalysisConsumers consumers = new AnalysisConsumers(
+      List.of(
+        new Consumer("myConsumer1", false),
+        new Consumer("myConsumer2", true),
+        new Consumer("myConsumer3", true)
+      )
+    );
+    assertThat(logTester.logs(Level.DEBUG)).contains(
+      "Registered JsAnalysisConsumers [myConsumer2, myConsumer3]"
+    );
+  }
+
   static class Consumer implements JsAnalysisConsumer {
 
     private final boolean enabled;
+    private final String name;
 
     Consumer(boolean enabled) {
+      this("test", enabled);
+    }
+
+    Consumer(String name, boolean enabled) {
+      this.name = name;
       this.enabled = enabled;
     }
 
@@ -62,6 +91,10 @@ public class AnalysisConsumersTest {
     @Override
     public boolean isEnabled() {
       return enabled;
+    }
+
+    public String toString() {
+      return name;
     }
   }
 }
