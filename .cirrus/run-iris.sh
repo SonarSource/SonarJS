50469d261c9402ee69e24a0febf85169871d911d
Victor Diez <victor.diez@sonarsource.com>
2025-07-08 15:55:05 +0200

    Analyze SonarJS on 3 SQ instances

--- .cirrus/run-iris.sh
+++ .cirrus/run-iris.sh
@@ -1,37 +1,19 @@
 #!/bin/bash
 set -euo pipefail
 
-: "${ARTIFACTORY_USERNAME?}" "${ARTIFACTORY_ACCESS_TOKEN?}" "${ARTIFACTORY_URL?}"
-: "${SONAR_SQC_EU_URL?}" "${SONAR_IRIS_SQC_EU_TOKEN?}"
-: "${SONAR_SQC_US_URL?}" "${SONAR_IRIS_SQC_US_TOKEN?}"
-: "${SONAR_NEXT_URL?}" "${SONAR_IRIS_NEXT_TOKEN?}"
+: "${ARTIFACTORY_PRIVATE_USERNAME?}" "${ARTIFACTORY_PRIVATE_ACCESS_TOKEN?}" "${ARTIFACTORY_URL?}"
+: "${SONAR_SOURCE_IRIS_TOKEN?}" "${SONAR_TARGET_IRIS_TOKEN?}" "${SONAR_TARGET_URL?}"
 
-# Run IRIS from SQS to SQC EU
-function run_iris_sqc_eu () {
+function run_iris () {
   java \
     -Diris.source.projectKey="org.sonarsource.javascript:javascript" \
     -Diris.source.organization="sonarsource" \
-    -Diris.source.url="$SONAR_NEXT_URL" \
-    -Diris.source.token="$SONAR_IRIS_NEXT_TOKEN" \
+    -Diris.source.url="https://next.sonarqube.com/sonarqube" \
+    -Diris.source.token="$SONAR_SOURCE_IRIS_TOKEN" \
     -Diris.destination.projectKey="SonarSource_SonarJS" \
     -Diris.destination.organization="sonarsource" \
-    -Diris.destination.url="$SONAR_SQC_EU_URL" \
-    -Diris.destination.token="$SONAR_IRIS_SQC_EU_TOKEN" \
-    -Diris.dryrun=$1 \
-    -jar iris-\[RELEASE\]-jar-with-dependencies.jar
-}
-
-# Run IRIS from SQS to SQC US
-function run_iris_sqc_us () {
-  java \
-    -Diris.source.projectKey="org.sonarsource.javascript:javascript" \
-    -Diris.source.organization="sonarsource" \
-    -Diris.source.url="$SONAR_NEXT_URL" \
-    -Diris.source.token="$SONAR_IRIS_NEXT_TOKEN" \
-    -Diris.destination.projectKey="SonarSource_SonarJS" \
-    -Diris.destination.organization="sonarsource" \
-    -Diris.destination.url="$SONAR_SQC_US_URL" \
-    -Diris.destination.token="$SONAR_IRIS_SQC_US_TOKEN" \
+    -Diris.destination.url="$SONAR_TARGET_URL" \
+    -Diris.destination.token="$SONAR_TARGET_IRIS_TOKEN" \
     -Diris.dryrun=$1 \
     -jar iris-\[RELEASE\]-jar-with-dependencies.jar
 }
@@ -42,7 +24,7 @@ HTTP_CODE=$(\
     --write-out '%{http_code}' \
     --location \
     --remote-name \
-    --user "$ARTIFACTORY_USERNAME:$ARTIFACTORY_ACCESS_TOKEN" \
+    --user "$ARTIFACTORY_PRIVATE_USERNAME:$ARTIFACTORY_PRIVATE_ACCESS_TOKEN" \
     "$ARTIFACTORY_URL/sonarsource-private-releases/com/sonarsource/iris/iris/$VERSION/iris-$VERSION-jar-with-dependencies.jar"\
 )
 
@@ -53,24 +35,13 @@ else
   echo "Downloaded $VERSION"
 fi
 
-echo "===== Execute IRIS SQC EU as dry-run"
-run_iris_sqc_eu "true"
+echo "===== Execute IRIS as dry-run"
+run_iris "true"
 STATUS=$?
 if [ $STATUS -ne 0 ]; then
-  echo "===== Failed to run IRIS SQC EU dry-run"
+  echo "===== Failed to run IRIS dry-run"
   exit 1
 else
-  echo "===== Successful IRIS SQC EU dry-run - executing IRIS for real."
-  run_iris_sqc_eu "false"
-fi
-
-echo "===== Execute IRIS SQC US as dry-run"
-run_iris_sqc_us "true"
-STATUS=$?
-if [ $STATUS -ne 0 ]; then
-  echo "===== Failed to run IRIS SQC US dry-run"
-  exit 1
-else
-  echo "===== Successful IRIS SQC US dry-run - executing IRIS for real."
-  run_iris_sqc_us "false"
+  echo "===== Successful IRIS dry-run - executing IRIS for real."
+  run_iris "false"
 fi
