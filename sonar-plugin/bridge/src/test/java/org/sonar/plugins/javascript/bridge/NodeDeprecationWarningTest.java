ee677634ccfe0494be87ab7c8c452316e41384ee
Victor Diez <victor.diez@sonarsource.com>
2025-07-10 14:47:32 +0200

    fix test

--- sonar-plugin/bridge/src/test/java/org/sonar/plugins/javascript/bridge/NodeDeprecationWarningTest.java
+++ sonar-plugin/bridge/src/test/java/org/sonar/plugins/javascript/bridge/NodeDeprecationWarningTest.java
@@ -48,7 +48,7 @@ class NodeDeprecationWarningTest {
     deprecationWarning.logNodeDeprecation(16);
     assertWarnings(
       "Using Node.js version 16 to execute analysis is not supported. " +
-      "Please upgrade to a newer LTS version of Node.js: [^20.9.0, ^22.9.0]."
+      "Please upgrade to a newer LTS version of Node.js: [^20.12.0, ^22.11.0]."
     );
   }
 
@@ -60,7 +60,7 @@ class NodeDeprecationWarningTest {
     deprecationWarning.logNodeDeprecation(22);
     assertWarnings(
       "Using Node.js version 18 to execute analysis is not supported. " +
-      "Please upgrade to a newer LTS version of Node.js: [^20.9.0, ^22.9.0]."
+      "Please upgrade to a newer LTS version of Node.js: [^20.12.0, ^22.11.0]."
     );
   }
 
