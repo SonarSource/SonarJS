6aaa719f7c6d646e41d7b277368c9d384a744cba
Michal Zgliczynski <mzglicz@gmail.com>
2025-07-16 10:35:20 +0200

    remove arrayContaining

--- packages/jsts/tests/analysis/tsconfigs.test.ts
+++ packages/jsts/tests/analysis/tsconfigs.test.ts
@@ -106,7 +106,7 @@ describe('tsconfigs', () => {
         allowJs: true,
         noImplicitAny: true,
       },
-      files: expect.arrayContaining([`${baseDir}/file.ts`, `${baseDir}/string42.ts`]),
+      files: [`${baseDir}/file.ts`, `${baseDir}/string42.ts`],
     });
     expect(tsConfigStore.getCacheOrigin()).toEqual('fallback');
   });
