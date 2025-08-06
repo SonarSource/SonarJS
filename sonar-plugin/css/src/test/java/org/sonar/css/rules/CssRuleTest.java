d7285cc8cc993a169651cb370a1dfda9cdfd287a
Michal Zgliczynski <mzglicz@gmail.com>
2025-08-06 16:17:56 +0200

    fix unit tests

--- sonar-plugin/css/src/test/java/org/sonar/css/rules/CssRuleTest.java
+++ sonar-plugin/css/src/test/java/org/sonar/css/rules/CssRuleTest.java
@@ -149,7 +149,7 @@ class CssRuleTest {
   void at_rule_unknown_default() {
     String optionsAsJson = GSON.toJson(new AtRuleNoUnknown().stylelintOptions());
     assertThat(optionsAsJson).isEqualTo(
-      "[true,{\"ignoreAtRules\":[\"value\",\"at-root\",\"content\",\"debug\",\"each\",\"else\",\"error\",\"for\",\"function\",\"if\",\"include\",\"mixin\",\"return\",\"warn\",\"while\",\"extend\",\"use\",\"forward\",\"tailwind\",\"apply\",\"layer\",\"container\",\"/^@.*/\"]}]"
+      "[true,{\"ignoreAtRules\":[\"value\",\"at-root\",\"content\",\"debug\",\"each\",\"else\",\"error\",\"for\",\"function\",\"if\",\"include\",\"mixin\",\"return\",\"warn\",\"while\",\"extend\",\"use\",\"forward\",\"tailwind\",\"apply\",\"layer\",\"container\",\"theme\",\"/^@.*/\"]}]"
     );
   }
 
