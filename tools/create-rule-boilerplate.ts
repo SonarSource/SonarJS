1cee6a8715d76ec8576a303c17bf64ce484ed9a6
zglicz <michal.zgliczynski@sonarsource.com>
2025-08-19 10:53:58 +0200

    JS-828 Add @angular-eslint/eslint-plugin (#5597)

---
+++ tools/create-rule-boilerplate.ts
@@ -0,0 +1,160 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource SA
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+
+import { join } from 'node:path/posix';
+import { header, inflateTemplateToFile, RULES_FOLDER, TS_TEMPLATES_FOLDER } from './helpers.js';
+import { mkdir } from 'node:fs/promises';
+import { writeFile } from 'fs/promises';
+import { generateMetaForRule } from './generate-eslint-meta.js';
+import { generateJavaCheckClass } from './generate-java-rule-classes.js';
+import { updateIndexes } from './generate-rule-indexes.js';
+
+const JS_RULE_DATA_FOLDER = join(
+  'sonar-plugin',
+  'javascript-checks',
+  'src',
+  'main',
+  'resources',
+  'org',
+  'sonar',
+  'l10n',
+  'javascript',
+  'rules',
+  'javascript',
+);
+
+const MISSING_RSPEC_JS_RULE_DATA_FOLDER = join('resources', 'rule-data', 'javascript');
+
+export async function createNewRule(
+  sonarKey: string,
+  eslintId: string,
+  implementation: 'original' | 'external' | 'decorated',
+  languages: ('js' | 'ts')[],
+  scope: 'Main' | 'Tests',
+  hasSecondaries?: boolean,
+  externalPackage?: { name: string; prefix: string; rulesImport?: string },
+  missingRspec?: boolean,
+) {
+  const ruleFolder = join(RULES_FOLDER, sonarKey);
+
+  await mkdir(ruleFolder, { recursive: true });
+
+  if (missingRspec) {
+    await writeFile(
+      join(JS_RULE_DATA_FOLDER, `${sonarKey}.json`),
+      JSON.stringify(
+        {
+          title: 'Description',
+          tags: [],
+          type: 'BUG',
+          status: 'ready',
+          scope: scope,
+          compatibleLanguages: languages,
+          quickfix: undefined,
+          defaultQualityProfiles: ['Sonar way'],
+          defaultSeverity: 'Minor',
+          sqKey: sonarKey,
+        },
+        null,
+        2,
+      ),
+    );
+    await writeFile(join(JS_RULE_DATA_FOLDER, `${sonarKey}.html`), '<html></html>');
+  }
+
+  if (implementation !== 'external') {
+    // index.ts
+    await writeFile(join(ruleFolder, `index.ts`), `${header}export { rule } from './rule.js';\n`);
+    // rule.ts
+    await inflateTemplateToFile(
+      join(
+        TS_TEMPLATES_FOLDER,
+        implementation === 'original' ? 'rule.template' : 'rule.decorated.template',
+      ),
+      join(ruleFolder, `rule.ts`),
+      {
+        ___HEADER___: header,
+        ___RULE_KEY___: sonarKey,
+      },
+    );
+
+    // cb.test.ts
+    await inflateTemplateToFile(
+      join(TS_TEMPLATES_FOLDER, 'rule.cbtest.template'),
+      join(ruleFolder, `cb.test.ts`),
+      {
+        ___HEADER___: header,
+        ___RULE_KEY___: sonarKey,
+      },
+    );
+
+    // empty cb.fixture.ts
+    await writeFile(join(ruleFolder, `cb.fixture.ts`), '');
+  } else {
+    // index.ts
+    const importRules =
+      externalPackage?.rulesImport ??
+      `import { rules } from '${externalPackage?.name ?? 'external-plugin'}';`;
+    await writeFile(
+      join(ruleFolder, `index.ts`),
+      `${header}\n${importRules}\nexport const rule = rules['${eslintId}'];\n`,
+    );
+  }
+
+  // meta.ts
+  let extra = '';
+  if (implementation === 'decorated') {
+    extra = `export const externalRules = [\n  { externalPlugin: '${externalPackage?.prefix ?? 'plugin-name'}', externalRule: '${eslintId}' },\n];\n`;
+  } else if (implementation === 'external') {
+    extra = `export const externalPlugin = '${externalPackage?.prefix ?? 'plugin-name'}';\n`;
+  }
+  if (hasSecondaries) {
+    extra += `export const hasSecondaries = true;\n`;
+  }
+
+  await inflateTemplateToFile(
+    join(TS_TEMPLATES_FOLDER, 'meta.template'),
+    join(ruleFolder, `meta.ts`),
+    {
+      ___HEADER___: header,
+      ___IMPLEMENTATION___: implementation,
+      ___ESLINT_ID___: eslintId,
+      ___EXTRA___: extra,
+      ___RULE_KEY___: sonarKey,
+    },
+  );
+
+  // preliminary generated-meta.ts
+  await generateMetaForRule(sonarKey, { compatibleLanguages: languages, scope });
+
+  // generate rule java class
+  await generateJavaCheckClass(sonarKey, { compatibleLanguages: languages, scope });
+
+  console.log(`
+    NEXT STEPS:
+    1. If your rule accepts parameters, export a schema in the meta.ts file. Customize default values in a 'config.ts' file
+    2. After RSPEC for the new rule has been generated, run 'npm run generate-meta'
+    `);
+
+  if (implementation === 'decorated') {
+    console.log(
+      'Please add all rules used in this decorated rule to the "externalRules" array in "meta.ts"',
+    );
+  }
+
+  await updateIndexes();
+}
--- tools/new-rule.mts
+++ tools/new-rule.mts
@@ -14,19 +14,9 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import { mkdir, writeFile } from 'node:fs/promises';
-import { join } from 'node:path/posix';
 import { checkbox, input, select } from '@inquirer/prompts';
-import {
-  header,
-  inflateTemplateToFile,
-  ruleRegex,
-  RULES_FOLDER,
-  TS_TEMPLATES_FOLDER,
-  verifyRuleName,
-} from './helpers.js';
-import { generateMetaForRule } from './generate-eslint-meta.js';
-import { generateJavaCheckClass } from './generate-java-rule-classes.js';
+import { ruleRegex, verifyRuleName } from './helpers.js';
+import { createNewRule } from './create-rule-boilerplate.js';
 
 const sonarKey = await input({ message: 'Enter the Sonar key for the new rule (SXXXX)' });
 const eslintId = await input({ message: 'Enter the ESLint ID for the rule' });
@@ -41,7 +31,7 @@ const scope = (await select({
     },
   ],
 })) satisfies 'Main' | 'Tests';
-const implementation = await select({
+const implementation = (await select({
   message: 'Origin of the rule',
   choices: [
     {
@@ -57,7 +47,7 @@ const implementation = await select({
       value: 'decorated',
     },
   ],
-});
+})) satisfies 'original' | 'external' | 'decorated';
 const languages = (await checkbox({
   message: 'What languages will the rule support?',
   choices: [
@@ -95,85 +85,4 @@ function verifyRspecId(sonarKey: string) {
 verifyRspecId(sonarKey);
 verifyRuleName(eslintId);
 
-const ruleFolder = join(RULES_FOLDER, sonarKey);
-
-await mkdir(ruleFolder, { recursive: true });
-
-if (implementation !== 'external') {
-  // index.ts
-  await writeFile(join(ruleFolder, `index.ts`), `${header}export { rule } from './rule.js';\n`);
-  // rule.ts
-  await inflateTemplateToFile(
-    join(
-      TS_TEMPLATES_FOLDER,
-      implementation === 'original' ? 'rule.template' : 'rule.decorated.template',
-    ),
-    join(ruleFolder, `rule.ts`),
-    {
-      ___HEADER___: header,
-      ___RULE_KEY___: sonarKey,
-    },
-  );
-
-  // cb.test.ts
-  await inflateTemplateToFile(
-    join(TS_TEMPLATES_FOLDER, 'rule.cbtest.template'),
-    join(ruleFolder, `cb.test.ts`),
-    {
-      ___HEADER___: header,
-      ___RULE_KEY___: sonarKey,
-    },
-  );
-
-  // empty cb.fixture.ts
-  await writeFile(join(ruleFolder, `cb.fixture.ts`), '');
-} else {
-  // index.ts
-  await writeFile(
-    join(ruleFolder, `index.ts`),
-    `${header}\nimport { rules } from 'external-plugin';\nexport const rule = rules('rule-name');\n`,
-  );
-}
-
-// meta.ts
-let extra = '';
-if (implementation === 'decorated') {
-  extra = `export const externalRules = [\n  { externalPlugin: 'plugin-name', externalRule: '${eslintId}' },\n];\n`;
-} else if (implementation === 'external') {
-  extra = `export const externalPlugin = 'plugin-name';\n`;
-}
-if (hasSecondaries) {
-  extra += `export const hasSecondaries = true;\n`;
-}
-
-await inflateTemplateToFile(
-  join(TS_TEMPLATES_FOLDER, 'meta.template'),
-  join(ruleFolder, `meta.ts`),
-  {
-    ___HEADER___: header,
-    ___IMPLEMENTATION___: implementation,
-    ___ESLINT_ID___: eslintId,
-    ___EXTRA___: extra,
-    ___RULE_KEY___: sonarKey,
-  },
-);
-
-// preliminary generated-meta.ts
-await generateMetaForRule(sonarKey, { compatibleLanguages: languages, scope });
-
-// generate rule java class
-await generateJavaCheckClass(sonarKey, { compatibleLanguages: languages, scope });
-
-console.log(`
-NEXT STEPS:
-1. If your rule accepts parameters, export a schema in the meta.ts file. Customize default values in a 'config.ts' file
-2. After RSPEC for the new rule has been generated, run 'npm run generate-meta'
-`);
-
-if (implementation === 'decorated') {
-  console.log(
-    'Please add all rules used in this decorated rule to the "externalRules" array in "meta.ts"',
-  );
-}
-
-await import('./generate-rule-indexes.js');
+await createNewRule(sonarKey, eslintId, implementation, languages, scope, hasSecondaries);
