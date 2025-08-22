867f44903e027a948a2c66d77ffa55535521185b
zglicz <michal.zgliczynski@sonarsource.com>
2025-08-22 08:48:26 +0200

    JS-840 Fix handling of built-in modules in fully qualified name generation (#5645)

--- packages/jsts/src/rules/S2699/supertest.fixture.js
+++ packages/jsts/src/rules/S2699/supertest.fixture.js
@@ -18,3 +18,14 @@ describe('supertest', function () { // Compliant
     return supertest(app).get(`/foo/bar`);
   });
 });
+
+// due to this line, the heuristic to get the fully qualified name, has a different list of declarations
+process.env.VARIABLE = 'some-token';
+
+describe("fail", () => {
+  it("should cause issues", () => {
+    const test_input = process.env.OTHER_VARIABLE.substring(0, 6); // this line can use any process.env var.
+    return supertest(app).get(`/foo/bar`).expect('Content-Type', /json/u).expect(200);
+  });
+});
+
