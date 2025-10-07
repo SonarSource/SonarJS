37579b0e944ba389f9379a864282a4f26250fe80
Kevin Silva <kevin.silva@sonarsource.com>
2025-10-07 10:09:35 +0200

    Vibe coded to support deno and node

--- packages/shared/src/helpers/worker.ts
+++ packages/shared/src/helpers/worker.ts
@@ -14,30 +14,73 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import { SHARE_ENV, Worker } from 'node:worker_threads';
 import { debug } from './logging.js';
 
-export async function createWorker(url: string, workerData?: WorkerData) {
-  return new Promise<Worker>((resolve, reject) => {
-    const worker = new Worker(url, {
-      workerData,
-      env: SHARE_ENV,
-    });
+// Detect runtime environment
+const isDeno = typeof (globalThis as any).Deno !== 'undefined';
+const isNode = typeof process !== 'undefined' && process.versions?.node;
 
-    worker.on('online', () => {
-      debug('The worker thread is running');
-      resolve(worker);
-    });
+export async function createWorker(url: string, workerData?: WorkerData): Promise<any> {
+  if (isDeno) {
+    // Use Deno's Web Worker API
+    return new Promise<any>((resolve, reject) => {
+      try {
+        const worker = new Worker(url, {
+          type: 'module',
+        } as any);
 
-    worker.on('exit', code => {
-      debug(`The worker thread exited with code ${code}`);
-    });
+        // Send workerData as the first message
+        if (workerData) {
+          worker.postMessage({ type: 'init', data: workerData });
+        }
 
-    worker.on('error', err => {
-      debug(`The worker thread failed: ${err}`);
-      reject(err);
+        worker.onmessage = event => {
+          if (event.data.type === 'ready') {
+            debug('The worker thread is running');
+            resolve(worker);
+          }
+        };
+
+        worker.onerror = err => {
+          debug(`The worker thread failed: ${err}`);
+          reject(err);
+        };
+
+        worker.onmessageerror = err => {
+          debug(`The worker thread message error: ${err}`);
+          reject(err);
+        };
+      } catch (err) {
+        debug(`Failed to create worker: ${err}`);
+        reject(err);
+      }
     });
-  });
+  } else if (isNode) {
+    // Use Node.js worker threads
+    const { SHARE_ENV, Worker } = await import('node:worker_threads');
+    return new Promise<any>((resolve, reject) => {
+      const worker = new Worker(url, {
+        workerData,
+        env: SHARE_ENV as any,
+      });
+
+      worker.on('online', () => {
+        debug('The worker thread is running');
+        resolve(worker);
+      });
+
+      worker.on('exit', code => {
+        debug(`The worker thread exited with code ${code}`);
+      });
+
+      worker.on('error', err => {
+        debug(`The worker thread failed: ${err}`);
+        reject(err);
+      });
+    });
+  } else {
+    throw new Error('Unsupported runtime environment');
+  }
 }
 
 export type WorkerData = {
