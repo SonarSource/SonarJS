cad04cddb1173fd487dceb5740623fd975e35eba
Victor Diez <victor.diez@sonarsource.com>
2025-10-17 17:07:04 +0200

    Fix S7755 Check for array type

--- packages/jsts/src/rules/S7755/meta.ts
+++ packages/jsts/src/rules/S7755/meta.ts
@@ -15,6 +15,6 @@
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
 // https://sonarsource.github.io/rspec/#/rspec/S7755/javascript
-export const implementation = 'external';
+export const implementation = 'decorated';
 export const eslintId = 'prefer-at';
-export const externalPlugin = 'unicorn';
+export const externalRules = [{ externalPlugin: 'unicorn', externalRule: 'prefer-at' }];
