e3b4b0ce76a99ac0cbc73fabb29a7e3edbdf7cff
Loris Sierra <loris.sierra@sonarsource.com>
2025-11-17 10:48:26 +0100

    tweak oversight

--- its/sources/jsts/custom/S7639.js
+++ its/sources/jsts/custom/S7639.js
@@ -1,4 +1,4 @@
 import { HDNodeWallet } from 'ethers'
 
-const mnemonic = 'Powerful Burning Muppets Betrayed Clerks Meanwhile Superb Spies Denounced Silly Leeks Cautiously'
+const mnemonic = 'Powerful Burning Muppets Betrayed Clerks Meanwhile Superb Spies Denounced Silly Leeks Cautiously' // Noncompliant
 const mnemonicWallet = HDNodeWallet.fromPhrase(mnemonic)
