c50fb71d2474a7e20a2ecb950eaa89dc0c33b9f9
Loris S. <91723853+loris-s-sonarsource@users.noreply.github.com>
2025-11-18 15:27:57 +0100

    Update packages/jsts/src/rules/S7790/rule.ts

--- packages/jsts/src/rules/S7790/rule.ts
+++ packages/jsts/src/rules/S7790/rule.ts
@@ -14,7 +14,7 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-// https://sonarsource.github.io/rspec/#/rspec/S2077/javascript
+// https://sonarsource.github.io/rspec/#/rspec/S7790/javascript
 
 import type { Rule } from 'eslint';
 import type estree from 'estree';
