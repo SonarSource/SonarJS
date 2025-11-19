672f516d3b77df929c1f07772e6147afcee068bb
Loris Sierra <loris.sierra@sonarsource.com>
2025-11-19 17:04:40 +0100

    add its results

--- its/sources/jsts/custom/S2755.js
+++ its/sources/jsts/custom/S2755.js
@@ -5,5 +5,5 @@ import fs from 'node:fs';
 const xml = fs.readFileSync('xxe.xml', 'utf8');
 const opts = { noblanks: true, noent: true, nocdata: true };
 
-libxmljs.parseXmlString(xml, { noblanks: true, noent: true, nocdata: true });
-libxmljs2.parseXmlString(xml, opts);
+libxmljs.parseXmlString(xml, { noblanks: true, noent: true, nocdata: true }); // Detected
+libxmljs2.parseXmlString(xml, opts); // Undetected
