0716089ccfa9bfb7840309a3c00cddbaefe716cd
Loris S. <91723853+loris-s-sonarsource@users.noreply.github.com>
2025-11-19 16:29:48 +0100

    JS-830 Add Rule S6437 (#5947)

---
+++ its/sources/jsts/custom/S6437.js
@@ -0,0 +1,72 @@
+import crypto from 'node:crypto';
+
+const dh = crypto.createDiffieHellman(512);
+dh.setPrivateKey('0123456789abcdef0123456789abcdef');
+
+const ecdh = crypto.createECDH('secp256k1');
+ecdh.setPrivateKey('abcdef0123456789abcdef0123456789');
+
+const sign = crypto.createSign('SHA256');
+sign.write('some data to sign');
+sign.end();
+const signature = sign.sign('privateKeyPEM', 'hex');
+
+const x509 = new crypto.X509Certificate('public-cert.pem (imagine its imported)');
+const value = x509.checkPrivateKey('key');
+
+crypto.createHmac('sha256', 'hardcodedSecret123');
+crypto.createHmac('sha256', process.env.SECRET);
+
+crypto.createSecretKey('mySecretKey123');
+crypto.createSecretKey(Buffer.from(process.env.KEY || '', 'hex'));
+
+const dataBuffer = Buffer.from('data');
+const signatureBuffer = Buffer.from('signature');
+crypto.sign('sha256', dataBuffer, 'privateKeyPEM');
+const privateKeyVar = 'key';
+crypto.sign('sha256', dataBuffer, privateKeyVar);
+
+crypto.privateEncrypt('privateKeyPEM', Buffer.from('data'));
+crypto.privateEncrypt({ key: 'privateKeyPEM', padding: 1 }, Buffer.from('data'));
+const privKey = 'key';
+crypto.privateEncrypt(privKey, Buffer.from('data'));
+
+const encData = Buffer.from('encrypted');
+crypto.privateDecrypt('privateKeyPEM', encData);
+crypto.privateDecrypt(privKey, encData);
+
+import superagent from 'superagent';
+superagent.auth('username:password123');
+const creds = 'user:pass';
+superagent.auth(creds);
+
+import express from 'express';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+app.disable('x-powered-by');
+app.use(cookieParser('mySecretKey123'));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+cookieParser.JSONCookie('{"data":"value"}', 'secret123');
+const jsonSecret = 'secret';
+cookieParser.JSONCookie('{"data":"value"}', jsonSecret);
+
+cookieParser.signedCookie('cookieValue', 'secretKey123');
+cookieParser.signedCookie('cookieValue', process.env.SECRET);
+
+const cookiesObj = {};
+cookieParser.signedCookies(cookiesObj, 'secretKey123');
+const cookieSecret = 'secret';
+cookieParser.signedCookies(cookiesObj, cookieSecret);
+
+import jwt from 'jsonwebtoken';
+jwt.sign({ userId: 123 }, 'mySecretKey123');
+jwt.sign({ userId: 123 }, process.env.JWT_SECRET);
+const jwtSecret = 'secret';
+jwt.sign({ userId: 123 }, jwtSecret);
+
+const tokenStr = 'token';
+jwt.verify(tokenStr, 'mySecretKey123');
+jwt.verify(tokenStr, process.env.JWT_SECRET);
+jwt.verify(tokenStr, jwtSecret);
---
+++ packages/jsts/src/rules/S6437/rule.ts
@@ -0,0 +1,123 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+// https://sonarsource.github.io/rspec/#/rspec/S2077/javascript
+
+import type { Rule } from 'eslint';
+import type estree from 'estree';
+import {
+  generateMeta,
+  getFullyQualifiedName,
+  report,
+  toSecondaryLocation,
+  IssueLocation,
+} from '../helpers/index.js';
+import * as meta from './generated-meta.js';
+
+// Dictionary with fully qualified names of functions and indices of their
+// parameters to analyze for hardcoded credentials.
+const secretSignatures: Record<string, [number]> = {
+  'cookie-parser': [0],
+  'cookie-parser.JSONCookie': [1],
+  'cookie-parser.signedCookies': [1],
+  'cookie-parser.signedCookie': [1],
+  'crypto.X509Certificate.checkPrivateKey': [0],
+  'crypto.createDiffieHellman.setPrivateKey': [0],
+  'crypto.createECDH.setPrivateKey': [0],
+  'crypto.createHmac': [1],
+  'crypto.createSecretKey': [0],
+  'crypto.createSign.sign': [0],
+  'crypto.createVerify': [0],
+  'crypto.privateDecrypt': [0],
+  'crypto.privateEncrypt': [0],
+  'crypto.sign': [2],
+  'jose.SignJWT': [0],
+  'jose.jwtVerify': [1],
+  'jsonwebtoken.sign': [1],
+  'jsonwebtoken.verify': [1],
+  'node-jose.JWK.asKey': [0],
+  'superagent.auth': [0],
+};
+
+export const rule: Rule.RuleModule = {
+  meta: generateMeta(meta, {
+    messages: {},
+  }),
+
+  create(context: Rule.RuleContext) {
+    const hardcodedVariables = new Map<string, estree.Node>();
+
+    function isHardcodedString(expr: estree.Expression): boolean {
+      switch (expr.type) {
+        case 'Literal':
+          return typeof expr.value === 'string';
+        case 'TemplateLiteral':
+          return expr.expressions.length === 0;
+        case 'Identifier':
+          return hardcodedVariables.has(expr.name);
+        default:
+          return false;
+      }
+    }
+
+    function getSecondaryNode(expr: estree.Expression): IssueLocation[] {
+      // If it's an identifier that references a hardcoded string,
+      // report the original declaration
+      if (expr.type === 'Identifier' && hardcodedVariables.has(expr.name)) {
+        const nodeName = hardcodedVariables.get(expr.name);
+        if (nodeName) {
+          return [toSecondaryLocation(nodeName, 'Hardcoded value assigned here')];
+        }
+      }
+      return [];
+    }
+
+    return {
+      Program() {
+        hardcodedVariables.clear();
+      },
+
+      VariableDeclarator(node: estree.VariableDeclarator) {
+        if (node.id.type === 'Identifier' && node.init && isHardcodedString(node.init)) {
+          hardcodedVariables.set(node.id.name, node.init);
+        }
+      },
+
+      CallExpression: (node: estree.Node) => {
+        const callExpression = node as estree.CallExpression;
+        const fqn = getFullyQualifiedName(context, callExpression);
+
+        if (fqn && secretSignatures.hasOwnProperty(fqn) && callExpression.arguments.length > 0) {
+          secretSignatures[fqn].forEach(index => {
+            const arg = callExpression.arguments[index];
+            if (arg && arg.type !== 'SpreadElement' && isHardcodedString(arg)) {
+              const secondaryLocations: IssueLocation[] = getSecondaryNode(arg);
+
+              report(
+                context,
+                {
+                  message: 'Revoke and change this password, as it is compromised.',
+                  loc: callExpression.callee.loc as estree.SourceLocation,
+                },
+                secondaryLocations,
+              );
+            }
+          });
+        }
+      },
+    };
+  },
+};
