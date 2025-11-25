2290fe3f7d698afc4bfc2ef999486cb45ce7dd60
Michal Zgliczynski <michal.zgliczynski@sonarsource.com>
2025-11-25 12:03:58 +0100

    Update ruling

--- packages/jsts/src/rules/S6571/cb.fixture.ts
+++ packages/jsts/src/rules/S6571/cb.fixture.ts
@@ -15,14 +15,14 @@ type B = A | any; // Noncompliant {{'any' overrides all other types in this unio
 type C = number | never; // Noncompliant {{'never' is overridden by other types in this union type.}}
 //                ^^^^^
 
-type D = false & boolean; // Noncompliant {{boolean is overridden by the false in this intersection type.}}
-type E = FALSE & boolean; // Noncompliant {{boolean is overridden by the false in this intersection type.}}
+type D = false & boolean; // Noncompliant {{boolean is overridden by false in this intersection type.}}
+type E = FALSE & boolean; // Noncompliant {{boolean is overridden by false in this intersection type.}}
 type F = number & any; // Noncompliant {{'any' overrides all other types in this intersection type.}}
 type G = number & never; // Noncompliant {{'never' overrides all other types in this intersection type.}}
-type H = 0 & number; // Noncompliant {{number is overridden by the 0 in this intersection type.}}
-type I = ZERO & number; // Noncompliant {{number is overridden by the 0 in this intersection type.}}
-type J = '' & string; // Noncompliant {{string is overridden by the "" in this intersection type.}}
-type K = 0n & bigint; // Noncompliant {{bigint is overridden by the 0n in this intersection type.}}
+type H = 0 & number; // Noncompliant {{number is overridden by 0 in this intersection type.}}
+type I = ZERO & number; // Noncompliant {{number is overridden by 0 in this intersection type.}}
+type J = '' & string; // Noncompliant {{string is overridden by "" in this intersection type.}}
+type K = 0n & bigint; // Noncompliant {{bigint is overridden by 0n in this intersection type.}}
 
 const x: unknown | string = '42'; // Noncompliant {{'unknown' overrides all other types in this union type.}}
 
