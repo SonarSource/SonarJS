0a16d0a2ede1a1c4cd020c8db2c0a1a4f2279978
Michal Zgliczynski <michal.zgliczynski@sonarsource.com>
2025-12-08 15:23:20 +0100

    check vitest expect.not

--- packages/jsts/src/rules/S2699/unit.test.ts
+++ packages/jsts/src/rules/S2699/unit.test.ts
@@ -14,68 +14,43 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import { DefaultParserRuleTester } from '../../../tests/tools/testers/rule-tester.js';
+import { DefaultParserRuleTester, RuleTester } from '../../../tests/tools/testers/rule-tester.js';
 import { rule } from './index.js';
 import { describe, it } from 'node:test';
 
 describe('S2699', () => {
   it('S2699', () => {
-    const ruleTester = new DefaultParserRuleTester();
+    const ruleTester = new RuleTester();
     ruleTester.run(`Test cases must have assertions`, rule, {
       valid: [
         {
           code: `
-const chai = require('chai');
-const { expect } = chai;
-describe('chai test cases', () => {
-  it('expect', () => {
-    expect(1).to.equal(2);
-  });
-  it('should go into function', () => {
-    funcWithExpect();
-  });
-  it('repeated function calls should work', () => {
-    funcWithExpect();
-  });
-});
-
-function funcWithExpect() {
-  expect(1).to.equal(2);
-}
-          `,
-        },
-        {
-          code: `
-const chai = require('chai');
-describe('global expect', () => {
-  it('expect', () => {
-    expect(5).toEqual(4);
-  });
-});
-          `,
-        },
-        {
-          code: `
-describe('no import from test library', () => {
-  it('should not fail', () => {
-    // no-op
+import { expect } from 'vitest';
+describe('vitest test cases', () => {
+  it("And we should NOT get contexts[2]", () => {
+    expect(result).not.toContain(contexts[2]);
   });
 });
 `,
         },
       ],
-      invalid: [
+      invalid: [],
+    });
+    const noTypescriptRuleTester = new DefaultParserRuleTester();
+    noTypescriptRuleTester.run(`Test cases must have assertions`, rule, {
+      valid: [
         {
           code: `
-const chai = require('chai');
-describe('chai test cases', () => {
-  it('no assertion', () => {
-    const x = 1 + 2;
+import { expect } from 'vitest';
+describe('vitest test cases', () => {
+  it("And we should NOT get contexts[2]", () => {
+    expect(result).not.toContain(contexts[2]);
   });
-});`,
-          errors: 1,
+});
+`,
         },
       ],
+      invalid: [],
     });
   });
 });
