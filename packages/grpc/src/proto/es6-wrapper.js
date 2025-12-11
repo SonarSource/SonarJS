db71990e280667be4e6336664793db2e46d369aa
Victor Diez <victor.diez@sonarsource.com>
2025-12-11 12:57:31 +0100

    fix unit tests

--- packages/grpc/src/proto/es6-wrapper.js
+++ packages/grpc/src/proto/es6-wrapper.js
@@ -14,7 +14,7 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import $protobuf from $DEPENDENCY;
+import $protobuf from "protobufjs/minimal.js";
 
 $OUTPUT;
 
