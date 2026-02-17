867dd8417d83e8148793f68af3401362105e1371
Matt Wilcox <nanpx@users.noreply.github.com>
2026-02-17 14:00:19 -0700

    feat: add support for eslint v10

---
+++ its/eslint10-plugin-sonarjs/tseslint.config.mjs
@@ -0,0 +1,30 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) 2011-2025 SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the Sonar Source-Available License Version 1, as published by SonarSource SA.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+// @ts-check
+
+import plugin from 'eslint-plugin-sonarjs';
+import tseslint from 'typescript-eslint';
+import parser from '@typescript-eslint/parser';
+
+console.log(`Loaded ${Object.keys(plugin.configs.recommended.rules ?? {}).length} rules`);
+
+export default tseslint.config(plugin.configs.recommended, {
+  files: ['**/*.ts'],
+  languageOptions: {
+    parser,
+  },
+});
