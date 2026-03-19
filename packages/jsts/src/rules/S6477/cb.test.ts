5a3884909f086e86ad231be2a519eacbba5e8b01
Michal Zgliczynski <michal.zgliczynski@sonarsource.com>
2026-03-19 10:51:47 +0100

    S6477 flatten fixtures and tests

--- packages/jsts/src/rules/S6477/cb.test.ts
+++ packages/jsts/src/rules/S6477/cb.test.ts
@@ -14,12 +14,11 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import { test } from '../../../../tests/tools/testers/comment-based/checker.js';
-import { rule } from '../rule.js';
+import { test } from '../../../tests/tools/testers/comment-based/checker.js';
+import { rule } from './rule.js';
 import { describe } from 'node:test';
-import * as meta from '../generated-meta.js';
+import * as meta from './generated-meta.js';
 
 describe('Rule S6477', () => {
-  process.chdir(import.meta.dirname); // change current working dir to avoid the package.json lookup to up in the tree
   test(meta, rule, import.meta.dirname);
 });
--- packages/jsts/src/rules/S6477/unit.test.ts
+++ packages/jsts/src/rules/S6477/unit.test.ts
@@ -14,14 +14,11 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import { rule } from '../rule.js';
-import { join } from 'node:path';
-import { NoTypeCheckingRuleTester } from '../../../../tests/tools/testers/rule-tester.js';
+import { rule } from './rule.js';
+import { NoTypeCheckingRuleTester } from '../../../tests/tools/testers/rule-tester.js';
 import { describe } from 'node:test';
 
 describe('S6477', () => {
-  const dirname = join(import.meta.dirname, 'fixtures');
-
   const ruleTester = new NoTypeCheckingRuleTester();
   ruleTester.run('S6477 reports missing keys in JSX list items', rule, {
     valid: [],
@@ -40,7 +37,6 @@ describe('S6477', () => {
         );
       }
       `,
-        filename: join(dirname, 'file.jsx'),
         errors: 1,
       },
     ],
