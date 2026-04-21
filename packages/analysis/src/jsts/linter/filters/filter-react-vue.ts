998f0fa16c41e397dc1efc83ccacb2dfe6b0c4ce
Guillem Sardà Parreu <88349941+guillemsarda@users.noreply.github.com>
2026-04-21 16:14:23 +0200

    JS-1621 Fix: remove react rules from vue (#6865)

---
+++ packages/analysis/src/jsts/linter/filters/filter-react-vue.ts
@@ -0,0 +1,33 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+import type { RuleFilter } from './rule-filter.js';
+
+/**
+ * Disables React-specific rules on Vue files.
+ * A rule is considered React-specific when its requiredDependency includes 'react' or 'react-native'.
+ */
+export const filterReactVue: RuleFilter = (_config, meta, ctx) => {
+  if (!meta || ctx.extensionName.toLowerCase() !== '.vue') {
+    return true;
+  }
+  const isReactRequired =
+    'requiredDependency' in meta &&
+    (meta.requiredDependency as string[]).some(
+      dependency => dependency === 'react' || dependency === 'react-native',
+    );
+  return !isReactRequired;
+};
