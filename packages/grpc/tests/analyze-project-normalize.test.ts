eafecf8d8c3602298d930887cd7b5e15f759e7c1
Victor Diez <victor.diez@sonarsource.com>
2026-04-22 11:36:27 +0200

    fix headers

--- packages/grpc/tests/analyze-project-normalize.test.ts
+++ packages/grpc/tests/analyze-project-normalize.test.ts
@@ -1,10 +1,10 @@
 /*
  * SonarQube JavaScript Plugin
- * Copyright (C) SonarSource SÃ rl
+ * Copyright (C) SonarSource Sàrl
  * mailto:info AT sonarsource DOT com
  *
  * You can redistribute and/or modify this program under the terms of
- * the Sonar Source-Available License Version 1, as published by SonarSource SÃ rl.
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,7 +14,6 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-
 import { afterEach, describe, it } from 'node:test';
 import { mkdir, mkdtemp, rm } from 'node:fs/promises';
 import { tmpdir } from 'node:os';
