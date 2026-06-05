ef49ac31d6ce5476a249346db71010cc47a0497d
Francois Mora <francois.mora@sonarsource.com>
2026-06-05 17:39:53 +0200

    test: provide host node path to SonarLint ITs

--- its/plugin/sonarlint-tests/src/test/java/com/sonar/javascript/it/plugin/sonarlint/tests/SonarLintIntegrationTest.java
+++ its/plugin/sonarlint-tests/src/test/java/com/sonar/javascript/it/plugin/sonarlint/tests/SonarLintIntegrationTest.java
@@ -16,6 +16,7 @@
  */
 package com.sonar.javascript.it.plugin.sonarlint.tests;
 
+import static com.sonar.javascript.it.plugin.sonarlint.tests.TestUtils.hostNodeExecutable;
 import static com.sonar.javascript.it.plugin.sonarlint.tests.TestUtils.usingEmbeddedNode;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.awaitility.Awaitility.await;
@@ -350,7 +351,7 @@ class SonarLintIntegrationTest {
       .withInitialFs(CONFIG_SCOPE_ID, baseDir, Arrays.asList(fileDTOs))
       .build();
 
-    backend = harness
+    var backendBuilder = harness
       .newBackend()
       .withStandaloneEmbeddedPluginAndEnabledLanguage(
         new Plugin(
@@ -359,9 +360,14 @@ class SonarLintIntegrationTest {
           "",
           ""
         )
-      )
-      .withUnboundConfigScope(CONFIG_SCOPE_ID)
-      .start(client);
+      );
+
+    if (!usingEmbeddedNode()) {
+      // Avoid slow host Node autodetection on Windows runners during the first analysis.
+      backendBuilder = backendBuilder.withClientNodeJsPath(hostNodeExecutable());
+    }
+
+    backend = backendBuilder.withUnboundConfigScope(CONFIG_SCOPE_ID).start(client);
   }
 
   private void assertResults(Consumer<List<RaisedIssueDto>> assertionLambda) {
