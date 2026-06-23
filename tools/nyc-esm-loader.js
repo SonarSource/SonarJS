8cbe42eb3aaee2b5c7f1a9586d934a228effff2f
Victor Diez <victor.diez@sonarsource.com>
2026-06-23 12:50:23 +0200

    Fix Babel 8 AST serialization and coverage loader

---
+++ tools/nyc-esm-loader.js
@@ -0,0 +1,84 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) SonarSource Sarl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sarl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+import { fileURLToPath } from 'node:url';
+
+import convertSourceMap from 'convert-source-map';
+import loader from '@istanbuljs/load-nyc-config';
+import schema from '@istanbuljs/schema';
+import InstrumenterIstanbul from 'nyc/lib/instrumenters/istanbul.js';
+import TestExclude from 'test-exclude';
+
+function nycEnvironmentConfig() {
+  try {
+    return JSON.parse(process.env.NYC_CONFIG);
+  } catch {
+    return undefined;
+  }
+}
+
+let nycConfig;
+let testExclude;
+let instrumenter;
+
+export async function load(url, context, nextLoad) {
+  if (context.format !== 'module' || loader.isLoading()) {
+    return nextLoad(url, context);
+  }
+
+  if (!nycConfig) {
+    nycConfig = nycEnvironmentConfig() || {
+      ...schema.defaults.nyc,
+      ...(await loader.loadNycConfig({
+        cwd: process.env.NYC_CWD || process.cwd(),
+      })),
+    };
+  }
+
+  if (!testExclude) {
+    testExclude = new TestExclude(nycConfig);
+    instrumenter = InstrumenterIstanbul({
+      compact: nycConfig.compact,
+      preserveComments: nycConfig.preserveComments,
+      produceSourceMap: nycConfig.produceSourceMap,
+      ignoreClassMethods: nycConfig.ignoreClassMethods,
+      esModules: true,
+      parserPlugins: nycConfig.parserPlugins,
+    });
+  }
+
+  const filename = fileURLToPath(url);
+  if (!testExclude.shouldInstrument(filename)) {
+    return nextLoad(url, context);
+  }
+
+  const fromNext = await nextLoad(url, context);
+  let { source } = fromNext;
+  if (typeof source !== 'string') {
+    source = new TextDecoder().decode(source);
+  }
+
+  const sourceMap = convertSourceMap.fromSource(source)?.toObject();
+  const instrumented = instrumenter.instrumentSync(source, filename, {
+    sourceMap,
+    registerMap() {},
+  });
+
+  return {
+    format: fromNext.format,
+    source: instrumented,
+  };
+}
