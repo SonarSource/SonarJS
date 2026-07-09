b2d53bc23953e48f45defae3a18ccdbbb73d3e29
Guillem Sardà Parreu <88349941+guillemsarda@users.noreply.github.com>
2026-07-09 15:00:59 +0200

    JS-1999 Create decorated rule S8982: HTML elements with v-for should have a :key attribute (#7498)

---
+++ packages/analysis/src/jsts/rules/S8982/decorator.ts
@@ -0,0 +1,40 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+// https://sonarsource.github.io/rspec/#/rspec/S8982/javascript
+
+import type { Rule } from 'eslint';
+import { generateMeta } from '../helpers/generate-meta.js';
+import { interceptReport } from '../helpers/decorators/interceptor.js';
+import * as meta from './generated-meta.js';
+
+export function decorate(rule: Rule.RuleModule): Rule.RuleModule {
+  return interceptReport(
+    {
+      ...rule,
+      meta: generateMeta(meta, rule.meta),
+    },
+    (context, reportDescriptor) => {
+      if ('messageId' in reportDescriptor) {
+        const { messageId: _messageId, data: _data, ...rest } = reportDescriptor;
+        context.report({
+          ...rest,
+          message: "Add a ':key' binding to this element.",
+        });
+      }
+    },
+  );
+}
