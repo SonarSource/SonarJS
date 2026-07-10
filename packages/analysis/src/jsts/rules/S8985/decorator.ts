17fa06d9a716c25c492ca92f13470a9c8db28dd9
Nathan Soufflet <nathsou@users.noreply.github.com>
2026-07-10 09:05:07 +0200

    JS-2019: S8985: side effects should not be performed inside "waitFor" callbacks (#7488)

---
+++ packages/analysis/src/jsts/rules/S8985/decorator.ts
@@ -0,0 +1,29 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+// https://sonarsource.github.io/rspec/#/rspec/S8985/javascript
+
+import type { Rule } from 'eslint';
+import { generateMeta } from '../helpers/generate-meta.js';
+import { withStrictImportResolution } from '../helpers/testing-library.js';
+import * as meta from './generated-meta.js';
+
+export function decorate(rule: Rule.RuleModule): Rule.RuleModule {
+  return {
+    ...withStrictImportResolution(rule),
+    meta: generateMeta(meta, rule.meta),
+  };
+}
