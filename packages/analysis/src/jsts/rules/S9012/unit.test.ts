1330625a2362adb4c0e9c608fe6debbaad5800fc
nathsou <nathan.soufflet@sonarsource.com>
2026-07-13 11:20:00 +0200

    JS-2062: Implement S9012 snapshot assertions in async utilities

---
+++ packages/analysis/src/jsts/rules/S9012/unit.test.ts
@@ -0,0 +1,110 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+import { describe, it } from 'node:test';
+import { DefaultParserRuleTester } from '../../../../tests/jsts/tools/testers/rule-tester.js';
+import { rule } from './index.js';
+
+const ruleTester = new DefaultParserRuleTester();
+
+describe('S9012', () => {
+  it('reports snapshots inside Testing Library waitFor callbacks', () => {
+    ruleTester.run(
+      'Snapshots should not be generated inside Testing Library async utilities',
+      rule,
+      {
+        valid: [
+          {
+            code: `
+            import { screen, waitFor } from '@testing-library/react';
+
+            await waitFor(() => expect(screen.getByText('Ready')).toBeInTheDocument());
+            expect(document.body).toMatchSnapshot();
+          `,
+          },
+          {
+            code: `
+            import { waitFor } from './test-utils';
+
+            await waitFor(() => expect(document.body).toMatchSnapshot());
+          `,
+          },
+          {
+            code: `
+            import { vi } from 'vitest';
+
+            await vi.waitFor(() => expect(document.body).toMatchInlineSnapshot());
+          `,
+          },
+        ],
+        invalid: [
+          {
+            code: `
+            import { waitFor } from '@testing-library/react';
+
+            await waitFor(() => expect(document.body).toMatchSnapshot());
+          `,
+            errors: [
+              {
+                message:
+                  'Snapshots should not be generated inside `waitFor`; retries can create unstable snapshot artifacts.',
+              },
+            ],
+          },
+          {
+            code: `
+            import { waitFor } from '@testing-library/react';
+
+            await waitFor(() => expect(document.body).toMatchInlineSnapshot());
+          `,
+            errors: [
+              {
+                message:
+                  'Snapshots should not be generated inside `waitFor`; retries can create unstable snapshot artifacts.',
+              },
+            ],
+          },
+          {
+            code: `
+            import { waitFor as tlWaitFor } from '@testing-library/react';
+
+            await tlWaitFor(() => expect(document.body).toMatchSnapshot());
+          `,
+            errors: [
+              {
+                message:
+                  'Snapshots should not be generated inside `tlWaitFor`; retries can create unstable snapshot artifacts.',
+              },
+            ],
+          },
+          {
+            code: `
+            import { waitForElementToBeRemoved } from '@testing-library/react';
+
+            await waitForElementToBeRemoved(() => expect(document.body).toMatchSnapshot());
+          `,
+            errors: [
+              {
+                message:
+                  'Snapshots should not be generated inside `waitForElementToBeRemoved`; retries can create unstable snapshot artifacts.',
+              },
+            ],
+          },
+        ],
+      },
+    );
+  });
+});
