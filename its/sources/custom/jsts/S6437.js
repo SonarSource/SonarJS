91b4138e1117abb347007c117e9d58677f7d340f
Marcin Stachniuk <marcin.stachniuk@sonarsource.com>
2026-07-17 16:51:18 +0200

    Fix ruling

--- its/sources/custom/jsts/S6437.js
+++ its/sources/custom/jsts/S6437.js
@@ -1,10 +1,10 @@
 import crypto from 'node:crypto';
 
 const dh = crypto.createDiffieHellman(512);
-dh.setPrivateKey('0123456789abcdef0123456789abcdef');
+dh.setPrivateKey('4f2c91a07db3e685f19b4c0da2e7b31c');
 
 const ecdh = crypto.createECDH('secp256k1');
-ecdh.setPrivateKey('abcdef0123456789abcdef0123456789');
+ecdh.setPrivateKey('9a41e6b3f085c72d4e19ab0f637dc258');
 
 const sign = crypto.createSign('SHA256');
 sign.write('some data to sign');
@@ -12,23 +12,23 @@ sign.end();
 const signature = sign.sign('privateKeyPEM', 'hex');
 
 const x509 = new crypto.X509Certificate('public-cert.pem (imagine its imported)');
-const value = x509.checkPrivateKey('key');
+const value = x509.checkPrivateKey('privateKeyRaw01');
 
-crypto.createHmac('sha256', 'hardcodedSecret123');
+crypto.createHmac('sha256', 'hardcodedHmacValue789');
 crypto.createHmac('sha256', process.env.SECRET);
 
-crypto.createSecretKey('mySecretKey123');
+crypto.createSecretKey('myCryptoKey456');
 crypto.createSecretKey(Buffer.from(process.env.KEY || '', 'hex'));
 
 const dataBuffer = Buffer.from('data');
 const signatureBuffer = Buffer.from('signature');
 crypto.sign('sha256', dataBuffer, 'privateKeyPEM');
-const privateKeyVar = 'key';
+const privateKeyVar = 'privateKeyRaw02';
 crypto.sign('sha256', dataBuffer, privateKeyVar);
 
 crypto.privateEncrypt('privateKeyPEM', Buffer.from('data'));
 crypto.privateEncrypt({ key: 'privateKeyPEM', padding: 1 }, Buffer.from('data'));
-const privKey = 'key';
+const privKey = 'privateKeyRaw03';
 crypto.privateEncrypt(privKey, Buffer.from('data'));
 
 const encData = Buffer.from('encrypted');
@@ -36,8 +36,8 @@ crypto.privateDecrypt('privateKeyPEM', encData);
 crypto.privateDecrypt(privKey, encData);
 
 import superagent from 'superagent';
-superagent.auth('username:password123');
-const creds = 'user:pass';
+superagent.auth('myuser:cred0Xy789');
+const creds = 'user:cred09';
 superagent.auth(creds);
 
 import express from 'express';
@@ -46,7 +46,7 @@ import session from 'express-session';
 
 const app = express();
 app.disable('x-powered-by');
-app.use(cookieParser('mySecretKey123'));
+app.use(cookieParser('myCryptoKey456'));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
   secret: 'sample secret',
@@ -54,32 +54,32 @@ app.use(session({
   cookie: { path: '/' }
 }))
 
-cookieParser.JSONCookie('{"data":"value"}', 'secret123');
+cookieParser.JSONCookie('{"data":"value"}', 'cryptoKey987');
 const jsonSecret = 'secret';
 cookieParser.JSONCookie('{"data":"value"}', jsonSecret);
 
-cookieParser.signedCookie('cookieValue', 'secretKey123');
+cookieParser.signedCookie('cookieValue', 'cryptoValue321');
 cookieParser.signedCookie('cookieValue', process.env.SECRET);
 
 const cookiesObj = {};
-cookieParser.signedCookies(cookiesObj, 'secretKey123');
+cookieParser.signedCookies(cookiesObj, 'cryptoValue321');
 const cookieSecret = 'secret';
 cookieParser.signedCookies(cookiesObj, cookieSecret);
 
 import jwt from 'jsonwebtoken';
-jwt.sign({ userId: 123 }, 'mySecretKey123');
+jwt.sign({ userId: 123 }, 'myCryptoKey456');
 jwt.sign({ userId: 123 }, process.env.JWT_SECRET);
 const jwtSecret = 'secret';
 jwt.sign({ userId: 123 }, jwtSecret);
 
 const tokenStr = 'token';
-jwt.verify(tokenStr, 'mySecretKey123');
+jwt.verify(tokenStr, 'myCryptoKey456');
 jwt.verify(tokenStr, process.env.JWT_SECRET);
 jwt.verify(tokenStr, jwtSecret);
 
 import ldap from 'ldapjs';
 const client = ldap.createClient({ url: 'ldap://localhost:389' });
-client.bind('cn=admin,dc=example,dc=org', 'hardcodedPassword123');
+client.bind('cn=admin,dc=example,dc=org', 'hardcodedCred0Ab789');
 client.bind('cn=admin,dc=example,dc=org', process.env.LDAP_PASSWORD);
 const ldapPassword = 'secret';
 client.bind('cn=admin,dc=example,dc=org', ldapPassword);
