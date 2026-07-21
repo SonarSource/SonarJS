9672be3882b8e9912c6901fecf7284633eac9013
nathsou <nathan.soufflet@sonarsource.com>
2026-07-21 10:00:21 +0200

    fix(S8998): format license headers

--- packages/analysis/src/jsts/rules/S8998/meta.ts
+++ packages/analysis/src/jsts/rules/S8998/meta.ts
@@ -8,8 +8,8 @@
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * Sonar Source-Available License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
  *
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
