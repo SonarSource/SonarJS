73964c72da19dd7e7c6adcfe036a31c31c5a98f4
Erwan Le Forestier <erwan.leforestier@sonarsource.com>
2026-07-24 14:38:05 +0200

    JS-1610 Rewire S1481 to typescript-eslint

--- packages/analysis/src/jsts/rules/S1481/rule.ts
+++ packages/analysis/src/jsts/rules/S1481/rule.ts
@@ -16,118 +16,21 @@
  */
 // https://sonarsource.github.io/rspec/#/rspec/S1481/javascript
 
-import type { Rule, Scope } from 'eslint';
-import type estree from 'estree';
+import type { Rule } from 'eslint';
+import { rules as tsEslintRules } from '../external/typescript-eslint/index.js';
 import { generateMeta } from '../helpers/generate-meta.js';
 import * as meta from './generated-meta.js';
 
-export const rule: Rule.RuleModule = {
-  meta: generateMeta(meta, {
-    messages: {
-      unusedFunction: `Remove unused function '{{symbol}}'.`,
-      unusedVariable: `Remove the declaration of the unused '{{symbol}}' variable.`,
-    },
-  }),
-  create(context: Rule.RuleContext) {
-    let toIgnore: estree.Identifier[] = [];
-    let jsxComponentsToIgnore: string[] = [];
-
-    function checkVariable(v: Scope.Variable, toCheck: 'let-const-function' | 'all') {
-      if (v.defs.length === 0) {
-        return;
-      }
-      const type = v.defs[0].type;
-      if (type !== 'Variable' && type !== 'FunctionName') {
-        return;
-      }
-      if (toCheck === 'let-const-function') {
-        const def = v.defs[0];
-        if (def.parent?.type === 'VariableDeclaration' && def.parent.kind === 'var') {
-          return;
-        }
-      }
-
-      const defs = v.defs.map(def => def.name);
-      const unused = v.references.every(ref => defs.includes(ref.identifier));
-
-      if (unused && !toIgnore.includes(defs[0]) && !jsxComponentsToIgnore.includes(v.name)) {
-        const messageAndData =
-          type === 'FunctionName'
-            ? getUnusedFunctionMessageAndData(v.name)
-            : getUnusedVariableMessageAndData(v.name);
-        for (const def of defs) {
-          context.report({
-            node: def,
-            ...messageAndData,
-          });
-        }
-      }
-    }
-
-    function checkScope(
-      scope: Scope.Scope,
-      checkedInParent: 'nothing' | 'let-const-function' | 'all',
-    ) {
-      let toCheck = checkedInParent;
-      if (scope.type === 'function' && !isParentOfModuleScope(scope)) {
-        toCheck = 'all';
-      } else if (checkedInParent === 'nothing' && scope.type === 'block') {
-        toCheck = 'let-const-function';
-      }
-
-      if (toCheck !== 'nothing' && scope.type !== 'function-expression-name') {
-        for (const v of scope.variables) {
-          checkVariable(v, toCheck);
-        }
-      }
-
-      for (const childScope of scope.childScopes) {
-        checkScope(childScope, toCheck);
-      }
-    }
-
-    return {
-      ObjectPattern: (node: estree.Node) => {
-        const elements = (node as estree.ObjectPattern).properties;
-        const hasRest = elements.some(element => (element as any).type === 'RestElement');
-
-        if (!hasRest) {
-          return;
-        }
-
-        for (const element of elements) {
-          if (
-            element.type === 'Property' &&
-            element.shorthand &&
-            element.value.type === 'Identifier'
-          ) {
-            toIgnore.push(element.value);
-          }
-        }
-      },
-
-      JSXIdentifier: (node: estree.Node) => {
-        // using 'any' as standard typings for AST don't provide types for JSX
-        jsxComponentsToIgnore.push((node as any).name);
-      },
-
-      'Program:exit': (node: estree.Node) => {
-        checkScope(context.sourceCode.getScope(node), 'nothing');
-        toIgnore = [];
-        jsxComponentsToIgnore = [];
-      },
-    };
+const baseRule = tsEslintRules['no-unused-vars'];
+const defaultOptions = [
+  {
+    args: 'none',
+    caughtErrors: 'none',
+    ignoreRestSiblings: true,
   },
+];
+
+export const rule: Rule.RuleModule = {
+  meta: generateMeta(meta, { ...baseRule.meta, defaultOptions, fixable: undefined }),
+  create: baseRule.create,
 };
-
-function isParentOfModuleScope(scope: Scope.Scope) {
-  return scope.childScopes.some(s => s.type === 'module');
-}
-
-function getUnusedFunctionMessageAndData(name: string) {
-  return { messageId: 'unusedFunction', data: { symbol: name } };
-}
-
-function getUnusedVariableMessageAndData(name: string) {
-  return { messageId: 'unusedVariable', data: { symbol: name } };
-}
