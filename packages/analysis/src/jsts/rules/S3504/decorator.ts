b632a69bfc85dae08eca36be7dc1616be8303ffc
Erwan Le Forestier <erwan.leforestier@sonarsource.com>
2026-07-24 14:45:49 +0200

    JS-2149 Suppress S3504 in var-heavy files

--- packages/analysis/src/jsts/rules/S3504/decorator.ts
+++ packages/analysis/src/jsts/rules/S3504/decorator.ts
@@ -14,12 +14,21 @@
  * You should have received a copy of the Sonar Source-Available License
  * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
-import type { Rule } from 'eslint';
+import type { Rule, SourceCode } from 'eslint';
 import { generateMeta } from '../helpers/generate-meta.js';
+import { childrenOf } from '../helpers/ancestor.js';
 import { interceptReport } from '../helpers/decorators/interceptor.js';
 import { isVendorFile } from '../helpers/vendor-file-pattern.js';
 import type estree from 'estree';
 import * as meta from './generated-meta.js';
+
+const MIN_VAR_DECLARATIONS = 3;
+const MIN_VAR_RATIO = 0.5;
+
+type VariableDeclarationCounts = {
+  totalDeclarations: number;
+  varDeclarations: number;
+};
 
 export function decorate(rule: Rule.RuleModule): Rule.RuleModule {
   return interceptReport(
@@ -27,7 +36,10 @@ export function decorate(rule: Rule.RuleModule): Rule.RuleModule {
       ...rule,
       meta: generateMeta(meta, rule.meta),
       create(context) {
-        if (isVendorFile(context.physicalFilename)) {
+        if (
+          isVendorFile(context.physicalFilename) ||
+          shouldSuppressVarHeavyFile(context.sourceCode)
+        ) {
           return {};
         }
         return rule.create(context);
@@ -63,4 +75,47 @@ export function decorate(rule: Rule.RuleModule): Rule.RuleModule {
       }
     },
   );
+}
+
+/**
+ * Returns whether S3504 should be suppressed for files that intentionally favor var.
+ * @param sourceCode the file being analyzed
+ * @return true when var is the dominant runtime declaration style and would create noisy reporting
+ */
+function shouldSuppressVarHeavyFile(sourceCode: SourceCode): boolean {
+  const { totalDeclarations, varDeclarations } = countRuntimeVariableDeclarations(sourceCode);
+  return (
+    totalDeclarations > 0 &&
+    varDeclarations >= MIN_VAR_DECLARATIONS &&
+    varDeclarations / totalDeclarations > MIN_VAR_RATIO
+  );
+}
+
+/**
+ * Counts runtime variable declarations while ignoring TypeScript ambient declarations.
+ * @param sourceCode the file being analyzed
+ * @return the total runtime declarations and how many of them use var
+ */
+function countRuntimeVariableDeclarations(sourceCode: SourceCode): VariableDeclarationCounts {
+  const counts: VariableDeclarationCounts = {
+    totalDeclarations: 0,
+    varDeclarations: 0,
+  };
+  const nodesToVisit: estree.Node[] = [sourceCode.ast as estree.Node];
+
+  while (nodesToVisit.length > 0) {
+    const node = nodesToVisit.pop()!;
+    if (node.type === 'VariableDeclaration') {
+      const declaration = node as estree.VariableDeclaration & { declare?: boolean };
+      if (!declaration.declare) {
+        counts.totalDeclarations++;
+        if (declaration.kind === 'var') {
+          counts.varDeclarations++;
+        }
+      }
+    }
+    nodesToVisit.push(...childrenOf(node, sourceCode.visitorKeys));
+  }
+
+  return counts;
 }
