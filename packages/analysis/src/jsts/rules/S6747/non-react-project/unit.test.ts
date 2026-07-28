92a293f08045a71a61e8a39dce2423988eb9ea8c
Francois Mora <francois.mora@sonarsource.com>
2026-07-28 14:51:51 +0200

    JS-2176 support simple JSX prop exceptions in S6747

--- packages/analysis/src/jsts/rules/S6747/non-react-project/unit.test.ts
+++ packages/analysis/src/jsts/rules/S6747/non-react-project/unit.test.ts
@@ -27,11 +27,49 @@ describe('S6947', () => {
     'S6747 reports invalid JSX props even when React is not explicitly imported',
     rule,
     {
-      valid: [],
+      valid: [
+        {
+          code: `import styled from 'styled-components';
+<div css={{ color: "red" }}></div>;`,
+          filename: join(dirname, 'filename.jsx'),
+        },
+        {
+          code: `import { ThemeUIProvider } from 'theme-ui';
+<div sx={{ color: "primary" }}></div>;`,
+          filename: join(dirname, 'filename.jsx'),
+        },
+        {
+          code: `import { jsx } from '@theme-ui/core';
+<div sx={{ color: "primary" }}></div>;`,
+          filename: join(dirname, 'filename.jsx'),
+        },
+      ],
       invalid: [
         {
           code: '<div class="foo"></div>;',
           filename: join(dirname, 'filename.jsx'),
+          errors: 1,
+        },
+        {
+          code: '<div css={{ color: "red" }}></div>;',
+          filename: join(dirname, 'filename.jsx'),
+          errors: 1,
+        },
+        {
+          code: '<div sx={{ color: "primary" }}></div>;',
+          filename: join(dirname, 'filename.jsx'),
+          errors: 1,
+        },
+        {
+          code: `import type { Theme } from 'theme-ui';
+<div sx={{ color: "primary" }}></div>;`,
+          filename: join(dirname, 'filename.tsx'),
+          errors: 1,
+        },
+        {
+          code: `import type styled from 'styled-components';
+<div css={{ color: "red" }}></div>;`,
+          filename: join(dirname, 'filename.tsx'),
           errors: 1,
         },
       ],
