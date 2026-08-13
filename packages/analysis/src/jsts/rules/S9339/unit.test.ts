2a885e9127f54637de409aa309ee08db42517019
nathsou <nathan.soufflet@sonarsource.com>
2026-08-13 12:06:41 +0200

    JS-1014: Implement S9339: Native APIs should be preferred over Axios utility methods
    Flag deprecated Axios all(), spread(), and CancelToken APIs and suggest Promise.all(), array destructuring, and AbortController instead of a fetch migration.

---
+++ packages/analysis/src/jsts/rules/S9339/unit.test.ts
@@ -0,0 +1,250 @@
+/*
+ * SonarQube JavaScript Plugin
+ * Copyright (C) SonarSource Sàrl
+ * mailto:info AT sonarsource DOT com
+ *
+ * You can redistribute and/or modify this program under the terms of
+ * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
+ */
+import { describe, it } from 'node:test';
+import { rule } from './index.js';
+import { DefaultParserRuleTester } from '../../../../tests/jsts/tools/testers/rule-tester.js';
+
+const ALL_MESSAGE =
+  'axios.all() is a deprecated Axios helper; Promise.all() is the native equivalent.';
+const SPREAD_MESSAGE =
+  'axios.spread() is a deprecated Axios helper; array destructuring is the native equivalent.';
+const CANCEL_MESSAGE =
+  'CancelToken is a deprecated Axios API; AbortController is the standard cancellation mechanism.';
+
+describe('S9339', () => {
+  it('reports deprecated Axios helpers and CancelToken', () => {
+    const ruleTester = new DefaultParserRuleTester();
+
+    ruleTester.run('prefer-native-axios-alternative', rule, {
+      valid: [
+        {
+          code: `
+import axios from 'axios';
+axios.get('/a');
+axios.post('/a', data);
+axios.create({ baseURL: '/api' });
+axios.isCancel(error);
+`,
+        },
+        {
+          code: `
+import axios from 'axios';
+axios.create({ cancelToken: source.token });
+`,
+        },
+        {
+          code: `
+const client = { all(values) { return values; } };
+client.all([1, 2]);
+`,
+        },
+        {
+          code: `
+import axios from 'axios';
+axios['all']([axios.get('/a')]);
+axios['spread'](handler);
+axios['CancelToken'].source();
+`,
+        },
+        {
+          code: `
+const config = { cancelToken: source.token };
+doRequest(config);
+`,
+        },
+        {
+          code: `
+import ky from 'ky';
+ky.get('/a');
+`,
+        },
+      ],
+      invalid: [
+        {
+          code: `
+import axios from 'axios';
+axios.all([axios.get('/a'), axios.get('/b')]);
+`,
+          errors: [
+            {
+              message: ALL_MESSAGE,
+              suggestions: [
+                {
+                  desc: 'Replace axios.all() with Promise.all().',
+                  output: `
+import axios from 'axios';
+Promise.all([axios.get('/a'), axios.get('/b')]);
+`,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          code: `
+import axios from 'axios';
+Promise.all([axios.get('/a'), axios.get('/b')])
+  .then(axios.spread((a, b) => {
+    console.log(a.data, b.data);
+  }));
+`,
+          errors: [
+            {
+              message: SPREAD_MESSAGE,
+              suggestions: [
+                {
+                  desc: 'Replace axios.spread() with array destructuring.',
+                  output: `
+import axios from 'axios';
+Promise.all([axios.get('/a'), axios.get('/b')])
+  .then(([a, b]) => {
+    console.log(a.data, b.data);
+  });
+`,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          code: `
+import axios from 'axios';
+const source = axios.CancelToken.source();
+axios.get('/user', { cancelToken: source.token });
+source.cancel();
+`,
+          errors: [
+            { message: CANCEL_MESSAGE },
+            { message: CANCEL_MESSAGE },
+          ],
+        },
+        {
+          code: `
+import { all, spread, CancelToken } from 'axios';
+all([one, two]);
+spread((left, right) => left + right);
+CancelToken.source();
+new CancelToken(cancel => {});
+`,
+          errors: [
+            {
+              message: ALL_MESSAGE,
+              suggestions: [
+                {
+                  desc: 'Replace axios.all() with Promise.all().',
+                  output: `
+import { all, spread, CancelToken } from 'axios';
+Promise.all([one, two]);
+spread((left, right) => left + right);
+CancelToken.source();
+new CancelToken(cancel => {});
+`,
+                },
+              ],
+            },
+            {
+              message: SPREAD_MESSAGE,
+              suggestions: [
+                {
+                  desc: 'Replace axios.spread() with array destructuring.',
+                  output: `
+import { all, spread, CancelToken } from 'axios';
+all([one, two]);
+([left, right]) => left + right;
+CancelToken.source();
+new CancelToken(cancel => {});
+`,
+                },
+              ],
+            },
+            { message: CANCEL_MESSAGE },
+            { message: CANCEL_MESSAGE },
+          ],
+        },
+        {
+          code: `
+const axios = require('axios');
+axios.all([one]);
+`,
+          errors: [
+            {
+              message: ALL_MESSAGE,
+              suggestions: [
+                {
+                  desc: 'Replace axios.all() with Promise.all().',
+                  output: `
+const axios = require('axios');
+Promise.all([one]);
+`,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          code: `
+import * as axios from 'axios';
+axios.all([one]);
+`,
+          errors: [
+            {
+              message: ALL_MESSAGE,
+              suggestions: [
+                {
+                  desc: 'Replace axios.all() with Promise.all().',
+                  output: `
+import * as axios from 'axios';
+Promise.all([one]);
+`,
+                },
+              ],
+            },
+          ],
+        },
+        {
+          code: `
+import axios from 'axios';
+const CancelToken = axios.CancelToken;
+`,
+          errors: [{ message: CANCEL_MESSAGE }],
+        },
+        {
+          code: `
+import axios from 'axios';
+axios({ url: '/user', cancelToken: source.token });
+`,
+          errors: [{ message: CANCEL_MESSAGE }],
+        },
+        {
+          code: `
+import axios from 'axios';
+axios.spread(handler);
+`,
+          errors: [{ message: SPREAD_MESSAGE, suggestions: [] }],
+        },
+        {
+          code: `
+import axios from 'axios';
+const Promise = {};
+axios.all([one]);
+`,
+          errors: [{ message: ALL_MESSAGE, suggestions: [] }],
+        },
+      ],
+    });
+  });
+});
