9d1d3e573e2f65c00a87ab96ec051cd40979619b
Victor Diez <victor.diez@sonarsource.com>
2026-09-07 12:46:08 +0200

    Avoid-broadening-Jasmine-assertion-evidence

--- packages/analysis/src/jsts/rules/helpers/testing/assertion-frameworks.ts
+++ packages/analysis/src/jsts/rules/helpers/testing/assertion-frameworks.ts
@@ -123,15 +123,7 @@ const assertionFrameworks = {
   },
   globalExpect: {
     imports: ['bun:test', '@playwright/test'],
-    dependencies: [
-      'jasmine',
-      'jasmine-core',
-      'jasmine-node',
-      'karma-jasmine',
-      'jest',
-      'cypress',
-      '@playwright/test',
-    ],
+    dependencies: ['jasmine', 'jest', 'cypress', '@playwright/test'],
     isAssertion: (context, node) =>
       node.type === 'CallExpression' && isGlobalExpectAssertion(context, node),
     isTSAssertion: isGlobalTSExpectAssertion,
