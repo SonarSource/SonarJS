abbdf965b86ad5939345cbb142fb733610997e72
Victor Diez <victor.diez@sonarsource.com>
2026-09-24 10:38:03 +0200

    JS-932-Complete-license-headers-for-replay-timings

--- packages/analysis/tests/program-selection-timings.test.ts
+++ packages/analysis/tests/program-selection-timings.test.ts
@@ -5,6 +5,14 @@
  *
  * You can redistribute and/or modify this program under the terms of
  * the Sonar Source-Available License Version 1, as published by SonarSource Sàrl.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the Sonar Source-Available License for more details.
+ *
+ * You should have received a copy of the Sonar Source-Available License
+ * along with this program; if not, see https://sonarsource.com/license/ssal/
  */
 
 import { describe, it, mock } from 'node:test';
